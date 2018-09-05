8ec0b4527117b883a314514d178ca5fd94de30fc
pverhelle <pieter.verhelle@gmail.com>
2018-09-05 11:42:16 +0200

    add dal bundles state

--- libs/dal/src/lib/dal.module.ts
+++ libs/dal/src/lib/dal.module.ts
@@ -2,9 +2,24 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { SDKBrowserModule } from '@diekeure/polpo-api-angular-sdk';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import {
+  initialState as bundlesInitialState,
+  bundlesReducer
+} from './+state/bundles/bundles.reducer';
+import { BundlesEffects } from './+state/bundles/bundles.effects';
 
 @NgModule({
-  imports: [CommonModule, SDKBrowserModule.forRoot(), HttpClientModule]
+  imports: [
+    CommonModule,
+    SDKBrowserModule.forRoot(),
+    HttpClientModule,
+    StoreModule.forFeature('bundles', bundlesReducer, {
+      initialState: bundlesInitialState
+    }),
+    EffectsModule.forFeature([BundlesEffects])
+  ]
 })
 export class DalModule {
   static forRoot(): ModuleWithProviders {
