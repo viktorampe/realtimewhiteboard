82e3eab0e6326f3959a80f399b1989a523ac3b9a
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2018-09-05 10:19:22 +0200

    add position input

--- libs/ui/src/lib/side-sheet/side-sheet.component.ts
+++ libs/ui/src/lib/side-sheet/side-sheet.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+/**
+ * Surface containing supplementary content that is anchored to the left or right edge of the screen.
+ * Side sheets contain content that supplements the screen's primary UI region.
+ * Side sheets display a wide variety of content and layouts through content projection.
+ * Material reference: https://material.io/design/components/sheets-side.html#
+ * @export
+ * @class SideSheetComponent
+ * @implements {OnInit}
+ */
 @Component({
   selector: 'campus-side-sheet',
   templateUrl: './side-sheet.component.html',
   styleUrls: ['./side-sheet.component.scss']
 })
-export class SideSheetComponent implements OnInit {
+export class SideSheetComponent {
+  @Input() position: 'left' | 'right' = 'right';
+  get classes() {
+    const cssClasses = {};
 
-  constructor() { }
-
-  ngOnInit() {
+    cssClasses['container--' + this.position] = true;
+    return cssClasses;
   }
-
+  constructor() {}
 }
--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SideSheetComponent } from './side-sheet/side-sheet.component';
+import {
+  SideSheetComponent,
+} from './side-sheet/side-sheet.component';
 @NgModule({
   imports: [CommonModule],
   declarations: [SideSheetComponent],
-  exports: [SideSheetComponent]
+  exports: [SideSheetComponent ]
 })
 export class UiModule {}
