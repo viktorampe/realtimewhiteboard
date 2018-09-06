ca6a344d0f3b51fb10dfb4b798ac50b569b66eca
pverhelle <pieter.verhelle@gmail.com>
2018-09-06 11:40:06 +0200

    add icon name to class and add tests

--- libs/ui/src/lib/info-panel/action/action.component.spec.ts
+++ libs/ui/src/lib/info-panel/action/action.component.spec.ts
@@ -1,9 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { InfoPanelActionComponent } from './action.component';
 
 describe('InfoPanelActionComponent', () => {
   let component: InfoPanelActionComponent;
   let fixture: ComponentFixture<InfoPanelActionComponent>;
+  let mockData: {
+    actionText: string;
+    icon: string;
+    iconBackgroundColor: 'red' | 'gray';
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -14,10 +20,38 @@ describe('InfoPanelActionComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(InfoPanelActionComponent);
     component = fixture.componentInstance;
+
+    mockData = {
+      actionText: 'desired text',
+      icon: 'given-icon',
+      iconBackgroundColor: 'red'
+    };
+
+    component.actionText = mockData.actionText;
+    component.icon = mockData.icon;
+    component.iconBackgroundColor = mockData.iconBackgroundColor;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should show the actionsText', () => {
+    const displayedText = fixture.debugElement.query(By.css('.text'))
+      .nativeElement.textContent;
+    expect(displayedText).toContain(mockData.actionText);
+  });
+  it('should show a red classed icon element', () => {
+    const icon = fixture.debugElement.query(By.css('.red'));
+    expect(icon).toBeTruthy();
+  });
+  it('should not show a gray classed icon element', () => {
+    const icon = fixture.debugElement.query(By.css('.gray'));
+    expect(icon).toBeFalsy();
+  });
+  it('should show the icon in the class for the icon element', () => {
+    const icon = fixture.debugElement.query(By.css('.icon.given-icon'));
+    expect(icon).toBeTruthy();
   });
 });
