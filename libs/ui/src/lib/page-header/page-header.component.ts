ae2d9ca1df4ec8c92396bbc4efa70eaa2a143ac5
david.boute <david.boute@diekeure.be>
2018-09-07 12:04:45 +0200

    theme barebone toegevoegd

--- libs/ui/src/lib/page-header/page-header.component.ts
+++ libs/ui/src/lib/page-header/page-header.component.ts
@@ -1,9 +1,18 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+/**
+ * Styles the page header.
+ *
+ * @export
+ * @class PageHeaderComponent
+ * @implements {OnInit}
+ *
+ * @param {string} [iconClass] - Css classname for icon
+ */
 @Component({
   selector: 'campus-page-header',
   templateUrl: './page-header.component.html',
-  styleUrls: ['./page-header.component.scss']
+  styleUrls: ['./page-header.component-theme.scss']
 })
 export class PageHeaderComponent implements OnInit {
   @Input() iconClass: string;
