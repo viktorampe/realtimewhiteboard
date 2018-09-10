f7d281c58864be3b391d07e0c63a7243bb5da9c2
david.boute <david.boute@diekeure.be>
2018-09-10 09:49:38 +0200

    Geen component meer voor app-bar-items.
    Content projection via css attribute.

--- libs/ui/src/lib/app-bar/app-bar.component.ts
+++ libs/ui/src/lib/app-bar/app-bar.component.ts
@@ -1,9 +1,19 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+/**
+ * Provides a flexbox container to use as a Top App Bar.
+ * https://material.io/develop/android/components/app-bar-layout/
+ *
+ * Uses css attribute selectors to project content in sections.
+ * Use bar--left, bar-center and bar--right attributes to assign content to a section.
+ *
+ * @export
+ * @class AppBarComponent
+ * @implements {OnInit}
+ */
 @Component({
   selector: 'campus-app-bar',
   templateUrl: './app-bar.component.html',
-  styleUrls: ['./app-bar.component.scss']
+  styleUrls: ['./app-bar.component-theme.scss']
 })
 export class AppBarComponent implements OnInit {
   constructor() {}
--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppBarComponent } from './app-bar/app-bar.component';
-import { AppBarItemComponent } from './app-bar/app-bar-item/app-bar-item.component';
+
 @NgModule({
   imports: [CommonModule],
-  declarations: [AppBarComponent, AppBarItemComponent],
-  exports: [AppBarComponent, AppBarItemComponent]
+  declarations: [AppBarComponent],
+  exports: [AppBarComponent]
 })
 export class UiModule {}
