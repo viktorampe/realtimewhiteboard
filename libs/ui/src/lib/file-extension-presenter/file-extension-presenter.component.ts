db95c23ed4aedeeab935363065cd3f7b60445833
david.boute <david.boute@diekeure.be>
2018-09-12 09:58:01 +0200

    Requested changes + documentation
    Styling as mixin.
    Extra tests.
    Bundles.component cleaned up.
    No longer exported in ui lib.
    Component documented.

--- libs/ui/src/lib/file-extension-presenter/file-extension-presenter.component.ts
+++ libs/ui/src/lib/file-extension-presenter/file-extension-presenter.component.ts
@@ -1,20 +1,25 @@
-import { Component, OnInit, Input } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+/**
+ * Presents the icon of an extension
+ *
+ * @param {string} extensionClass - The Css classname associated with the extension.
+ * @param {boolean} showTitle - Sets the visibility of the title on the component.
+ * @param {boolean} showIcon - Sets the visibility of the icon on the component.
+ *
+ * @export
+ * @class FileExtensionPresenterComponent
+ */
 @Component({
   selector: 'campus-file-extension-presenter',
   templateUrl: './file-extension-presenter.component.html',
   styleUrls: ['./file-extension-presenter.component.scss']
 })
-export class FileExtensionPresenterComponent implements OnInit {
-  @Input() extension: string;
+export class FileExtensionPresenterComponent {
+  @Input() extensionClass = '';
   @Input() showTitle = true;
   @Input() showIcon = true;
 
   get titleContent(): string {
-    return this.showTitle ? this.extension : '';
+    return this.showTitle ? this.extensionClass : '';
   }
-
-  constructor() {}
-
-  ngOnInit() {}
 }
