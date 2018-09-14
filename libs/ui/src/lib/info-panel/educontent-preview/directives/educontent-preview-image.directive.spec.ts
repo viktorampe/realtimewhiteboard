5f103ea174c5654c7f733ba1ced8018438c111b2
Pieter Verhelle <pieter.verhelle@gmail.com>
2018-09-14 16:22:59 +0200

    add content directive

---
+++ libs/ui/src/lib/info-panel/educontent-preview/directives/educontent-preview-image.directive.spec.ts
@@ -0,0 +1,8 @@
+import { CampusContentPreviewImageDirective } from './educontent-preview-image.directive';
+
+describe('CampusContentPreviewImageDirective', () => {
+  it('should create an instance', () => {
+    const directive = new CampusContentPreviewImageDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ libs/ui/src/lib/info-panel/educontent-preview/directives/educontent-preview-image.directive.ts
@@ -0,0 +1,12 @@
+import { Directive } from '@angular/core';
+/**
+ * Body of a side sheet, needed as it's used as a selector.
+ *
+ * @export
+ * @class SideSheetBodyDirective
+ */
+@Directive({
+  selector: '[campusContentPreviewImage], campus-content-preview-image'
+})
+export class CampusContentPreviewImageDirective {
+}
