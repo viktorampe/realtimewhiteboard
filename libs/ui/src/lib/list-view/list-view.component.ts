12b0c0107f4a07c2338175afb4facec7b229d500
david.boute <david.boute@diekeure.be>
2018-09-14 12:58:55 +0200

    eerste analyse

--- libs/ui/src/lib/list-view/list-view.component.ts
+++ libs/ui/src/lib/list-view/list-view.component.ts
@@ -1,4 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+/*
+Component list-view
+ontvangt array van objecten, zet in een lijst -> aparte componenten?
+  -> ook inhoud toevoegen via content projection?
+multiselect mogelijk - selectie in array bijhouden
+moet content ook als grid kunnen weergeven
+  -> Input + ngTemplate?
+events opvangen? waarschijnlijk niet -> verantwoordelijkheid componenten
+
+sources:
+material design :https://material.angular.io/components/list/overview
+github: https://github.com/angular/material2/blob/master/src/lib/list/list.ts
+  -> MatListItem nog eens grondiger bekijken
+
+TODO: navragen hoe material component toevoegen aan code. Import? Copy/paste?
+ */
+
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'campus-list-view',
@@ -6,10 +23,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-view.component.css']
 })
 export class ListViewComponent implements OnInit {
+  @Input() contentArray: object[];
+  // @Input() viewTemplate: 'list' | 'grid';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
