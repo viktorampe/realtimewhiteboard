38bbc934bd507ec9542389570bf58e2dfa68eef7
david.boute <david.boute@diekeure.be>
2018-09-14 12:36:58 +0200

    component generated

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListViewComponent } from './list-view/list-view.component';
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  declarations: [ListViewComponent],
+  exports: [ListViewComponent]
 })
 export class UiModule {}
