7dbd4110588755b889c89d165ddf5a97077ce940
david.boute <david.boute@diekeure.be>
2018-09-14 10:16:05 +0200

    pull request changes

--- libs/ui/src/lib/page-header/page-header.component.ts
+++ libs/ui/src/lib/page-header/page-header.component.ts
@@ -1,25 +1,26 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 /**
  * Styles the page header.
  *
+ * @param {string} [iconClass] - Css classname for icon
+ * @param {string} [title] - the title
+ * @param {string} [subtitle] - the subtitle
+ *
  * @export
  * @class PageHeaderComponent
- * @implements {OnInit}
  *
- * @param {string} [iconClass] - Css classname for icon
  */
 @Component({
   selector: 'campus-page-header',
   templateUrl: './page-header.component.html',
-  styleUrls: ['./page-header.component-theme.scss']
+  styleUrls: [
+    './page-header.component.theme.scss',
+    './page-header.component.scss'
+  ]
 })
-export class PageHeaderComponent implements OnInit {
+export class PageHeaderComponent {
   @Input() iconClass: string;
   @Input() title: string;
   @Input() subtitle: string;
-
-  constructor() {}
-
-  ngOnInit() {}
 }
