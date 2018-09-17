76aa5f81c777632870d61306c0effef1722a77e2
david.boute <david.boute@diekeure.be>
2018-09-17 16:14:55 +0200

    Removed unnecessary import

--- libs/ui/src/lib/file-extension/file-extension.component.spec.ts
+++ libs/ui/src/lib/file-extension/file-extension.component.spec.ts
@@ -1,7 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FileExtensionComponent } from './file-extension.component';
-import { compileComponentFromMetadata } from '@angular/compiler';
 import { By } from '@angular/platform-browser';
 
 describe('FileExtensionComponent', () => {
