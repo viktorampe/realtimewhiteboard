7241071f867b52c695001c7c99025ae0da967a0d
pverhelle <pieter.verhelle@gmail.com>
2018-09-18 09:27:51 +0200

    update to latest campus-file-extension component

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -4,7 +4,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AdaptableListComponent } from './adaptable-list/adaptable-list.component';
 import { AdaptableSelectComponent } from './adaptable-select/adaptable-select.component';
 import { ContentPreviewComponent } from './content-preview/content-preview.component';
-import { FileExtensionPresenterComponent } from './file-extension-presenter/file-extension-presenter.component';
+import { FileExtensionComponent } from './file-extension/file-extension.component';
 import { InputLabelComponent } from './input-label/input-label.component';
 import { LabelAndIconButtonComponent } from './label-and-icon-button/label-and-icon-button.component';
 import { PeriodLabelComponent } from './period-label/period-label.component';
@@ -18,7 +18,7 @@ import { PersonBadgeComponent } from './person-badge/person-badge.component';
     AdaptableListComponent,
     PeriodLabelComponent,
     AdaptableSelectComponent,
-    FileExtensionPresenterComponent,
+    FileExtensionComponent,
     PersonBadgeComponent
   ],
   exports: [
@@ -28,8 +28,8 @@ import { PersonBadgeComponent } from './person-badge/person-badge.component';
     AdaptableListComponent,
     PeriodLabelComponent,
     AdaptableSelectComponent,
-    FileExtensionPresenterComponent,
+    FileExtensionComponent,
     PersonBadgeComponent
   ]
 })
-export class UiModule {}
+export class UiModule { }
