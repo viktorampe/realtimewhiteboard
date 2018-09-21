89c2cb966546a44c7e6d81da5fa37dcde8182af1
david.boute <david.boute@diekeure.be>
2018-09-21 12:56:31 +0200

    generated contentActionButton component

---
+++ libs/ui/src/lib/content-action-button/content-action-button.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'campus-content-action-button',
+  templateUrl: './content-action-button.component.html',
+  styleUrls: ['./content-action-button.component.scss']
+})
+export class ContentActionButtonComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContentThumbnailComponent } from './content-thumbnail/content-thumbnail.component';
+import { ContentActionButtonComponent } from './content-action-button/content-action-button.component';
 @NgModule({
   imports: [CommonModule],
-  declarations: [ContentThumbnailComponent],
-  exports: [ContentThumbnailComponent]
+  declarations: [ContentThumbnailComponent, ContentActionButtonComponent],
+  exports: [ContentThumbnailComponent, ContentActionButtonComponent]
 })
 export class UiModule {}
