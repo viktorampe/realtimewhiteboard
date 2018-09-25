7e87947303cf22fb238f4f7278c466f0c878b3fa
pverhelle <pieter.verhelle@gmail.com>
2018-09-25 10:22:12 +0200

    add educontents panel

---
+++ libs/pages/bundles/src/lib/components/info-panel/info-panel-educontents/info-panel-educontents.component.spec.ts
@@ -0,0 +1,27 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { InfoPanelEducontentsComponent } from './info-panel-educontents.component';
+
+
+describe('InfoPanelEducontentsComponent', () => {
+  let component: InfoPanelEducontentsComponent;
+  let fixture: ComponentFixture<InfoPanelEducontentsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [InfoPanelEducontentsComponent],
+      schemas: [NO_ERRORS_SCHEMA]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InfoPanelEducontentsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ libs/pages/bundles/src/lib/components/info-panel/info-panel-educontents/info-panel-educontents.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'campus-info-panel-educontents',
+  templateUrl: './info-panel-educontents.component.html',
+  styleUrls: ['./info-panel-educontents.component.scss']
+})
+export class InfoPanelEducontentsComponent {
+  @Input() eduContentNames: string[];
+}
