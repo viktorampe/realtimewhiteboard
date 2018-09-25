42c59e64fad22a6e18959d08757e8bf7e7fc39ed
pverhelle <pieter.verhelle@gmail.com>
2018-09-25 10:22:46 +0200

    update module for new components

--- libs/pages/bundles/src/lib/components/bundles.component.ts
+++ libs/pages/bundles/src/lib/components/bundles.component.ts
@@ -6,7 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./bundles.component.scss']
 })
 export class BundlesComponent {
-  period = { start: new Date(2018, 0, 1), end: new Date(2019, 5, 1) };
+  bundle = { name: 'bundle name', description: 'bundle description' };
+  teacher = { displayName: 'Tom mertens' };
+
+  eduContent = {
+    preview: 'https://d1fh3heiqa6frl.cloudfront.net/890f054414a06a81489f35e6b27fc23c9ee12d0ce4e0a867801a569a073438de/7bc034592d6ea1a4ec2f3e99eb63ad56_340-1.png',
+    name: 'educontent name',
+    description: 'educontent description',
+    extention: 'ppt',
+    productType: 'polpo-presentatie',
+    methods: ['opmijkunjerekenen'],
+    status: {
+      options: ['one', 'two'],
+      selectedstatus: 'two'
+    }
+
+  }
+
+  eduContentNames = [{ text: 'one' }, { text: 'two' }, { text: 'three' }];
+
   saveStatus(eventValue: any) {
     console.log(eventValue);
   }
--- libs/pages/bundles/src/lib/pages-bundles.module.ts
+++ libs/pages/bundles/src/lib/pages-bundles.module.ts
@@ -4,6 +4,9 @@ import { PagesSharedModule } from '@campus/pages/shared';
 import { UiModule } from '@campus/ui';
 import { BundlesComponent } from './components/bundles.component';
 import { BundlesViewModel } from './components/bundles.viewmodel';
+import { InfoPanelBundleComponent } from './components/info-panel/info-panel-bundle/info-panel-bundle.component';
+import { InfoPanelEducontentComponent } from './components/info-panel/info-panel-educontent/info-panel-educontent.component';
+import { InfoPanelEducontentsComponent } from './components/info-panel/info-panel-educontents/info-panel-educontents.component';
 import { PagesBundlesRoutingModule } from './pages-bundles-routing.module';
 
 @NgModule({
@@ -13,8 +16,8 @@ import { PagesBundlesRoutingModule } from './pages-bundles-routing.module';
     UiModule,
     PagesSharedModule
   ],
-  declarations: [BundlesComponent],
+  declarations: [BundlesComponent, InfoPanelBundleComponent, InfoPanelEducontentComponent, InfoPanelEducontentsComponent],
 
   providers: [BundlesViewModel]
 })
-export class PagesBundlesModule {}
+export class PagesBundlesModule { }
