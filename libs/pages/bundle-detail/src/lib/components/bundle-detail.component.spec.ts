df28411ddb184e8886f2de06cd8f946fcb14c9fd
david.boute <david.boute@diekeure.be>
2018-10-01 17:13:33 +0200

    analyse

--- libs/pages/bundle-detail/src/lib/components/bundle-detail.component.spec.ts
+++ libs/pages/bundle-detail/src/lib/components/bundle-detail.component.spec.ts
@@ -25,67 +25,35 @@ describe('BundleDetailComponent', () => {
     Analyse bundle detail page (Student version)
   */
 
-  // it('should show a toolbar at the top of the page', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should show a toolbar at the top of the page', () => {});
 
-  // it('should show a pageheader with the bundle icon, name and description ', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should show a pageheader with the bundle icon, name and description ', () => {});
 
-  // it('should show the number of available items', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should show the number of available items', () => {});
 
-  // it('should be able to filter the available items', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should be able to filter the available items', () => {});
 
-  // it('should show a list of all available items in the bundle', () => {
-  // expect(component).toBeTruthy();
-  // });
+  it('should show a list of all available items in the bundle', () => {});
 
-  // it('should be able to toggle the list between list and grid view', () => {
-  // expect(component).toBeTruthy();
-  // });
+  it('should be able to toggle the list between list and grid view', () => {});
 
-  // it('should start the list in grid view on desktop', () => {
-  // expect(component).toBeTruthy();
-  // });
+  it('should start the list in grid view on desktop', () => {});
 
-  // it('should start the list in list view on mobile', () => {
-  // expect(component).toBeTruthy();
-  // });
+  it('should start the list in list view on mobile', () => {});
 
-  // it('should show an infopanel', () => {
-  // expect(component).toBeTruthy();
-  // });
+  it('should show an infopanel', () => {});
 
-  // it('should show the infopanel next to the the list on desktop', () => {
-  // expect(component).toBeTruthy();
-  // });
+  it('should show the infopanel next to the the list on desktop', () => {});
 
-  // it('should show the infopanel as a bottom/side(?) drawer on mobile', () => {
-  // expect(component).toBeTruthy();
-  // });
+  it('should show the infopanel as a bottom/side(?) drawer on mobile', () => {});
 
-  // it('should show the teacher info in the infopanel if no item is selected', () => {
-  // expect(component).toBeTruthy();
-  // });
+  it('should show the teacher info in the infopanel if no item is selected', () => {});
 
-  // it('should show the item info in the infopanel if an item is selected', () => {
-  // expect(component).toBeTruthy();
-  // });
+  it('should show the item info in the infopanel if an item is selected', () => {});
 
-  // it('should be able to set the item status in the infopanel if an item is selected', () => {
-  // expect(component).toBeTruthy();
-  // });
+  it('should be able to set the item status in the infopanel if an item is selected', () => {});
 
-  // it('should show the item descriptions in the infopanel if multiple items are selected', () => {
-  // expect(component).toBeTruthy();
-  // });
+  it('should show the item descriptions in the infopanel if multiple items are selected', () => {});
 
-  // it('should show an error message if a bundle is no longer available', () => {
-  // expect(component).toBeTruthy();
-  // });
+  it('should show an error message if a bundle is no longer available', () => {});
 });
--- libs/pages/bundle-detail/src/lib/components/bundle-detail.viewmodel.spec.ts
+++ libs/pages/bundle-detail/src/lib/components/bundle-detail.viewmodel.spec.ts
@@ -9,3 +9,15 @@ beforeEach(() => {
 test('it should return', () => {
   return;
 });
+
+/*
+    Analyse needed data bundle detail page (Student version)
+  */
+
+it('should get an array of educontents', () => {});
+
+it('should get an array of actions per educontent', () => {});
+
+it('should get the teacherinfo', () => {});
+
+it('should get the teacherinfo', () => {});
