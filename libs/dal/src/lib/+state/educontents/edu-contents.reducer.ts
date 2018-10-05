fd9e57b3406e84f13d28fbc4b2b915e74cff5b35
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2018-10-05 11:17:10 +0200

    add comments

--- libs/dal/src/lib/+state/educontents/edu-contents.reducer.ts
+++ libs/dal/src/lib/+state/educontents/edu-contents.reducer.ts
@@ -5,16 +5,36 @@ import {
   EduContentsActionTypes
 } from './edu-contents.actions';
 
+/**
+ * @ngrx/entity provides a predefined interface for handling
+ * a structured dictionary of records. This interface
+ * includes an array of ids, and a dictionary of the provided
+ * model type by id. This interface is extended to include
+ * any additional interface properties.
+ */
 export interface EduContentsState extends EntityState<EduContentInterface> {
   // additional entities state properties
   loaded: boolean;
   error?: any;
 }
 
+/**
+ * createEntityAdapter creates an object of many helper
+ * functions for single or multiple operations
+ * against the dictionary of records. The configuration
+ * object takes a record id selector function and
+ * a sortComparer option which is set to a compare
+ * function if the records are to be sorted.
+ */
 export const adapter: EntityAdapter<EduContentInterface> = createEntityAdapter<
   EduContentInterface
 >();
 
+/**
+ * getInitialState returns the default initial state
+ * for the generated entity state. Initial state
+ * additional properties can also be defined.
+ */
 export const initialEducontentState: EduContentsState = adapter.getInitialState(
   {
     // additional entity state properties
@@ -28,6 +48,13 @@ export function reducer(
 ): EduContentsState {
   switch (action.type) {
     case EduContentsActionTypes.AddEduContent: {
+      /**
+       * The addOne function provided by the created adapter
+       * adds one record to the entity dictionary
+       * and returns a new state including that records if it doesn't
+       * exist already. If the collection is to be sorted, the adapter will
+       * insert the new record into the sorted array.
+       */
       return adapter.addOne(action.payload.eduContent, state);
     }
 
@@ -36,6 +63,13 @@ export function reducer(
     }
 
     case EduContentsActionTypes.AddEduContents: {
+      /**
+       * The addMany function provided by the created adapter
+       * adds many records to the entity dictionary
+       * and returns a new state including those records. If
+       * the collection is to be sorted, the adapter will
+       * sort each record upon entry into the sorted array.
+       */
       return adapter.addMany(action.payload.eduContents, state);
     }
 
@@ -77,6 +111,14 @@ export function reducer(
   }
 }
 
+/**
+ * Because the data structure is defined within the reducer it is optimal to
+ * locate our selector functions at this level. If store is to be thought of
+ * as a database, and reducers the tables, selectors can be considered the
+ * queries into said database. Remember to keep your selectors small and
+ * focused so they can be combined and composed to fit each particular
+ * use-case.
+ */
 export const {
   selectIds,
   selectEntities,
