d9234c17f8347fd04b7759dc9b3c658f7303938c
pverhelle <pieter.verhelle@gmail.com>
2018-10-08 14:00:01 +0200

    add sectors for the default selections in the scaffold als add selector for selecting specific ids

--- libs/dal/src/lib/+state/educontents/edu-contents.selectors.ts
+++ libs/dal/src/lib/+state/educontents/edu-contents.selectors.ts
@@ -1,5 +1,11 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
-import { selectAll, State } from './edu-contents.reducer';
+import {
+  selectAll,
+  selectEntities,
+  selectIds,
+  selectTotal,
+  State
+} from './edu-contents.reducer';
 
 export const selectEduContentState = createFeatureSelector<State>(
   'eduContents'
@@ -9,12 +15,26 @@ export const getError = createSelector(
   selectEduContentState,
   (state: State) => state.error
 );
-export const selectAllEduContents = createSelector(
-  selectEduContentState,
-  selectAll
-);
 
 export const getLoaded = createSelector(
   selectEduContentState,
   (state: State) => state.loaded
+);
+
+export const getAll = createSelector(selectEduContentState, selectAll);
+
+export const getCount = createSelector(selectEduContentState, selectTotal);
+
+export const getIds = createSelector(selectEduContentState, selectIds);
+
+export const getAllEntities = createSelector(
+  selectEduContentState,
+  selectEntities
+);
+
+export const getByIds = createSelector(
+  selectEduContentState,
+  (state: State, props: { ids: number[] }) => {
+    return props.ids.map(id => state.entities[id]);
+  }
 );
