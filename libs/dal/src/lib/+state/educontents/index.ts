da22b66b13cdddc144b4b3d3303075a79f02c1e5
pverhelle <pieter.verhelle@gmail.com>
2018-10-10 11:08:08 +0200

    remove deprecated state

--- libs/dal/src/lib/+state/educontents/index.ts
+++
@@ -1,5 +0,0 @@
-import * as EduContentActions from './edu-contents.actions';
-import * as EduContent from './edu-contents.reducer';
-import * as EduContentQueries from './edu-contents.selectors';
-
-export { EduContentActions, EduContent, EduContentQueries };
