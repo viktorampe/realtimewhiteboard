3e574ab030d0072d57f638daab3d673defeb9ba8
david.boute <david.boute@diekeure.be>
2018-10-11 15:01:34 +0200

    formatting

--- libs/dal/src/lib/+state/student-content-status/index.ts
+++ libs/dal/src/lib/+state/student-content-status/index.ts
@@ -2,4 +2,8 @@ import * as StudentContentStatusActions from './student-content-status.actions';
 import * as StudentContentStatus from './student-content-status.reducer';
 import * as StudentContentStatusQueries from './student-content-status.selectors';
 
-export { StudentContentStatusActions, StudentContentStatus, StudentContentStatusQueries };
+export {
+  StudentContentStatusActions,
+  StudentContentStatus,
+  StudentContentStatusQueries
+};
