4b6a0c9155424ae47459261d8707ee56174459df
Frederic <frederic.gryspeerdt@gmail.com>
2018-10-11 12:39:46 +0200

    Update user-content.service.interface.ts

--- libs/dal/src/lib/bundle/user-content.service.interface.ts
+++ libs/dal/src/lib/bundle/user-content.service.interface.ts
@@ -2,7 +2,7 @@ import { InjectionToken } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserContentInterface } from '../+models';
 
-export const USER_CONTENT_SERVICE_TOKEN = new InjectionToken('UserContent');
+export const USER_CONTENT_SERVICE_TOKEN = new InjectionToken('UserContentService');
 export interface UserContentServiceInterface {
   getAllForUser(userId: number): Observable<UserContentInterface[]>;
 }
