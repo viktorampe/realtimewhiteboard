7cae175c9d31178665d9f7510f228d945ecd5415
Bert Casier <bert@ernie.be>
2018-10-11 13:34:42 +0200

    fix incorrect stubs

--- libs/dal/src/lib/bundle/user-content.service.interface.ts
+++ libs/dal/src/lib/bundle/user-content.service.interface.ts
@@ -2,7 +2,9 @@ import { InjectionToken } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserContentInterface } from '../+models';
 
-export const USER_CONTENT_SERVICE_TOKEN = new InjectionToken('UserContentService');
+export const USER_CONTENT_SERVICE_TOKEN = new InjectionToken(
+  'UserContentService'
+);
 export interface UserContentServiceInterface {
   getAllForUser(userId: number): Observable<UserContentInterface[]>;
 }
