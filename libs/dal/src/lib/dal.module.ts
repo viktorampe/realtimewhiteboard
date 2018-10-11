fe74b2a5807aa77df0e95f2df9d03fbf7a4e04e3
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2018-10-11 14:24:19 +0200

    pluralize store slices in module

--- libs/dal/src/lib/dal.module.ts
+++ libs/dal/src/lib/dal.module.ts
@@ -1,15 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { ModuleWithProviders, NgModule } from '@angular/core';
-import {
-  BrowserModule as CampusBrowserModule,
-  BROWSER_STORAGE_SERVICE_TOKEN,
-  StorageService
-} from '@campus/browser';
-import {
-  LoopBackConfig,
-  SDKBrowserModule
-} from '@diekeure/polpo-api-angular-sdk';
+import { BrowserModule as CampusBrowserModule, BROWSER_STORAGE_SERVICE_TOKEN, StorageService } from '@campus/browser';
+import { LoopBackConfig, SDKBrowserModule } from '@diekeure/polpo-api-angular-sdk';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { Bundle } from './+state/bundle';
@@ -19,20 +12,10 @@ import { EduContentsEffects } from './+state/edu-content/edu-content.effects';
 import { LearningArea } from './+state/learning-area';
 import { LearningAreasEffects } from './+state/learning-area/learning-area.effects';
 import { UiEffects } from './+state/ui/ui.effects';
-import {
-  initialState as uiInitialState,
-  uiReducer
-} from './+state/ui/ui.reducer';
+import { initialState as uiInitialState, uiReducer } from './+state/ui/ui.reducer';
 import { UserContent } from './+state/user-content';
 import { UserContentsEffects } from './+state/user-content/user-content.effects';
-import {
-  BundleService,
-  BUNDLE_SERVICE_TOKEN,
-  UnlockedContentService,
-  UNLOCKED_CONTENT_SERVICE_TOKEN,
-  UserContentService,
-  USER_CONTENT_SERVICE_TOKEN
-} from './bundle';
+import { BundleService, BUNDLE_SERVICE_TOKEN, UnlockedContentService, UNLOCKED_CONTENT_SERVICE_TOKEN, UserContentService, USER_CONTENT_SERVICE_TOKEN } from './bundle';
 import { EduContentService } from './edu-content/edu-content.service';
 import { EDUCONTENT_SERVICE_TOKEN } from './edu-content/edu-content.service.interface';
 import { LearningAreaService } from './learning-area/learning-area.service';
@@ -52,16 +35,16 @@ interface DalOptions {
     StoreModule.forFeature('ui', uiReducer, {
       initialState: uiInitialState
     }),
-    StoreModule.forFeature('bundle', Bundle.reducer, {
+    StoreModule.forFeature('bundles', Bundle.reducer, {
       initialState: Bundle.initialState
     }),
-    StoreModule.forFeature('learingArea', LearningArea.reducer, {
+    StoreModule.forFeature('learingAreas', LearningArea.reducer, {
       initialState: LearningArea.initialState
     }),
-    StoreModule.forFeature('eduContent', EduContent.reducer, {
+    StoreModule.forFeature('eduContents', EduContent.reducer, {
       initialState: EduContent.initialState
     }),
-    StoreModule.forFeature('userContent', UserContent.reducer, {
+    StoreModule.forFeature('userContents', UserContent.reducer, {
       initialState: UserContent.initialState
     }),
     EffectsModule.forFeature([
