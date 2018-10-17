35e3ad896876407902c701ce0ba0e162d33f4a88
Thomas Ghesquiere <thomas.ghesquiere@gmail.com>
2018-10-17 10:42:48 +0200

    make husky happy

--- libs/pages/bundles/src/lib/components/bundles.component.spec.ts
+++ libs/pages/bundles/src/lib/components/bundles.component.spec.ts
@@ -2,8 +2,9 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { UiModule } from '@campus/ui';
-import { Store } from '@ngrx/store';
+import { Store, StoreModule } from '@ngrx/store';
 import { BundlesComponent } from './bundles.component';
+import { BundlesViewModel } from './bundles.viewmodel';
 
 // TODO fix this test
 describe('BundlesComponent', () => {
@@ -11,18 +12,13 @@ describe('BundlesComponent', () => {
   let fixture: ComponentFixture<BundlesComponent>;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [UiModule],
+      imports: [UiModule, StoreModule.forRoot({})],
       declarations: [BundlesComponent],
       schemas: [NO_ERRORS_SCHEMA],
       providers: [
-        {
-          provide: ActivatedRoute,
-          useValue: []
-        },
-        {
-          provide: Store,
-          useValue: {}
-        }
+        BundlesViewModel,
+        { provide: ActivatedRoute, value: {} },
+        Store
       ]
     }).compileComponents();
   }));
