c57c00161798d5f2da29b980ba402a50f54a5a3f
pverhelle <pieter.verhelle@gmail.com>
2018-10-18 14:14:35 +0200

    add tests for the learningAreaComponent

--- libs/pages/bundles/src/lib/components/learning-areas/learning-areas.component.spec.ts
+++ libs/pages/bundles/src/lib/components/learning-areas/learning-areas.component.spec.ts
@@ -1,13 +1,15 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
+import { LearningAreaInterface } from '@campus/dal';
 import { StateResolver } from '@campus/pages/shared';
+import { ListFormat } from '@campus/ui';
 import { Store, StoreModule } from '@ngrx/store';
+import { hot } from '@nrwl/nx/testing';
 import { BundlesViewModel } from '../bundles.viewmodel';
 import { LearningAreasComponent } from './learning-areas.component';
 
 let bundlesViewModel: MockViewModel;
-
 class MockViewModel extends BundlesViewModel {}
 
 beforeEach(() => {
@@ -24,7 +26,32 @@ test('it should return', () => {
 describe('LearningAreasComponent', () => {
   let component: LearningAreasComponent;
   let fixture: ComponentFixture<LearningAreasComponent>;
-
+  const learningAreas: LearningAreaInterface[] = [
+    {
+      icon: 'polpo-wiskunde',
+      id: 19,
+      color: '#2c354f',
+      name: 'Wiskunde'
+    },
+    {
+      icon: 'polpo-aardrijkskunde',
+      id: 1,
+      color: '#485235',
+      name: 'Aardrijkskunde'
+    },
+    {
+      icon: 'polpo-frans',
+      id: 2,
+      color: '#385343',
+      name: 'Frans'
+    },
+    {
+      icon: 'polpo-godsdienst',
+      id: 13,
+      color: '#325235',
+      name: 'Godsdienst, Didactische & Pedagogische ondersteuning'
+    }
+  ];
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [StoreModule.forRoot({})],
@@ -47,5 +74,34 @@ describe('LearningAreasComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should reset the filterInput$ when calling resetFilterInput', () => {
+    component.resetFilterInput();
+    expect(component.filterInput$).toBeObservable(hot('a', { a: '' }));
+  });
+  it('should change the filterInput$ when calling onChangeFilterInput', () => {
+    component.onChangeFilterInput('the new value');
+    expect(component.filterInput$).toBeObservable(
+      hot('a', { a: 'the new value' })
+    );
+  });
+  it('should call the viewModel changListFormat method when calling clickChangeListFormat', () => {
+    const spy = jest.spyOn(component['bundlesViewModel'], 'changeListFormat');
+    component.clickChangeListFormat('GRID');
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(ListFormat.GRID);
+  });
+  it('should filter the learningAreas with case insensitivity', () => {
+    const learningAreas$ = hot('a--|', { a: learningAreas });
+    const filterInput$ = hot('abc|', {
+      a: undefined,
+      b: 'wISKUN',
+      c: 'nothing nothing nothing'
+    });
+    expect(
+      component.getDisplayedLearningAreas$(learningAreas$, filterInput$)
+    ).toBeObservable(
+      hot('abc|', { a: learningAreas, b: [learningAreas[0]], c: [] })
+    );
   });
 });
