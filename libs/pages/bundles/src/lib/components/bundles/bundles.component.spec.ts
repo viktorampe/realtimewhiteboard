14b6ea4b0c9c356c3930d815b228b11b2ccd16ac
pverhelle <pieter.verhelle@gmail.com>
2018-10-18 12:03:51 +0200

    updated specs for bundles component

--- libs/pages/bundles/src/lib/components/bundles/bundles.component.spec.ts
+++ libs/pages/bundles/src/lib/components/bundles/bundles.component.spec.ts
@@ -1,105 +1,76 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
-import {
-  BundleInterface,
-  EduContentInterface,
-  LearningAreaInterface
-} from '@campus/dal';
+import { BundleInterface, EduContentInterface } from '@campus/dal';
 import { ListFormat } from '@campus/ui';
 import { Store, StoreModule } from '@ngrx/store';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { hot } from '@nrwl/nx/testing';
 import { BundlesViewModel } from '../bundles.viewmodel';
 import { BundlesComponent } from './bundles.component';
 
 let bundlesViewModel: MockViewModel;
+class MockViewModel extends BundlesViewModel {}
 
-class MockViewModel extends BundlesViewModel {
-  bundles$: Observable<BundleInterface[]> = new BehaviorSubject<
-    BundleInterface[]
-  >([
-    this.createBundle('bundle', 19),
-    this.createBundle('bundle 2: the bundleing', 19),
-    this.createBundle('bundle 3: a bundle of sticks', 19),
-    this.createBundle('bundle 4: bundle of joy', 19),
-    this.createBundle('bundle 5: bundle of rights', 19)
-  ]);
+function createEduContent(): EduContentInterface {
+  return {
+    type: '?'
+  };
+}
 
-  learningAreas$: Observable<LearningAreaInterface[]> = new BehaviorSubject<
-    LearningAreaInterface[]
-  >([
-    {
-      icon: 'polpo-wiskunde',
-      id: 19,
-      color: '#2c354f',
-      name: 'Wiskunde'
-    },
-    {
-      icon: 'polpo-aardrijkskunde',
-      id: 1,
-      color: '#485235',
-      name: 'Aardrijkskunde'
-    },
-    {
-      icon: 'polpo-frans',
-      id: 2,
-      color: '#385343',
-      name: 'Frans'
-    },
-    {
-      icon: 'polpo-godsdienst',
-      id: 13,
-      color: '#325235',
-      name: 'Godsdienst, Didactische & Pedagogische ondersteuning'
-    }
-  ]);
+function createBundle(name: string, learningAreaId: number): BundleInterface {
+  const startDate: Date = new Date();
+  const endDate: Date = new Date();
+  endDate.setHours(endDate.getHours() + 2);
 
-  getBundleItemCount(bundle: BundleInterface): number {
-    return 0;
-  }
+  return {
+    id: Math.round(Math.random() * 10000),
+    teacherId: Math.round(Math.random() * 10000),
+    learningAreaId: learningAreaId,
+    name: name,
+    description: 'this description includes' + name,
+    start: startDate,
+    end: endDate,
+    eduContents: [
+      createEduContent(),
+      createEduContent(),
+      createEduContent(),
+      createEduContent(),
+      createEduContent()
+    ]
+  };
+}
 
-  //todo remove when we have actual data
-  createBundle(name: string, learningAreaId: number): BundleInterface {
-    const startDate: Date = new Date();
-    const endDate: Date = new Date();
-    endDate.setHours(endDate.getHours() + 2);
-
-    return {
-      id: Math.round(Math.random() * 10000),
-      teacherId: Math.round(Math.random() * 10000),
-      learningAreaId: learningAreaId,
-      name: name,
-      description: 'this description includes' + name,
-      start: startDate,
-      end: endDate,
-      eduContents: [
-        this.createEduContent(),
-        this.createEduContent(),
-        this.createEduContent(),
-        this.createEduContent(),
-        this.createEduContent()
-      ]
-    };
-  }
-
-  private createEduContent(): EduContentInterface {
-    return {
-      type: '?'
-    };
-  }
+function filterBundlesExpect(
+  component: BundlesComponent,
+  filterInput: string,
+  allBundles: BundleInterface[],
+  filteredBundles: BundleInterface[]
+) {
+  component.filterInput$.next(filterInput);
+  expect(
+    component.getDisplayedBundles(
+      hot('a', { a: allBundles }),
+      component.filterInput$
+    )
+  ).toBeObservable(
+    hot('a', {
+      a: filteredBundles
+    })
+  );
 }
 
 beforeEach(() => {
   bundlesViewModel = new MockViewModel(<ActivatedRoute>{}, <Store<any>>{});
 });
 
-test('it should return', () => {
-  return;
-});
-
 describe('BundlesComponent', () => {
   let component: BundlesComponent;
   let fixture: ComponentFixture<BundlesComponent>;
+
+  const bundle1 = createBundle('text for search one', 19);
+  const bundle2 = createBundle('text for search two', 20);
+  const bundle3 = createBundle('text for search three', 983);
+  const bundle4 = createBundle('text for search four', 378);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -125,52 +96,50 @@ describe('BundlesComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('it should return 5 bundles', () => {
-    component
-      .getDisplayedBundles(bundlesViewModel.bundles$, component.filterInput$)
-      .subscribe((bundles: BundleInterface[]) => {
-        console.log(bundles[0].eduContents[0].publishedEduContentMetadata);
-        expect(bundles.length).toBe(5);
-      });
+  it('it should return all bundles', () => {
+    filterBundlesExpect(
+      component,
+      '',
+      [bundle1, bundle2, bundle3, bundle4],
+      [bundle1, bundle2, bundle3, bundle4]
+    );
   });
 
   it('should filter out all but 1 bundle, case insensitve', () => {
-    component.filterInput$.next('BunDlE oF Joy');
-    component
-      .getDisplayedBundles(bundlesViewModel.bundles$, component.filterInput$)
-      .subscribe((bundles: BundleInterface[]) => {
-        expect(bundles.length).toEqual(1);
-      });
+    filterBundlesExpect(
+      component,
+      'tWo',
+      [bundle1, bundle2, bundle3, bundle4],
+      [bundle2]
+    );
   });
 
   it('should return a no bundles', () => {
-    component.filterInput$.next('lol');
-    component
-      .getDisplayedBundles(bundlesViewModel.bundles$, component.filterInput$)
-      .subscribe((bundles: BundleInterface[]) => {
-        expect(bundles.length).toEqual(0);
-      });
+    filterBundlesExpect(
+      component,
+      'not in the list',
+      [bundle1, bundle2, bundle3, bundle4],
+      []
+    );
   });
 
   it('should reset filter input', () => {
     component.filterInput$.next('lol');
     component.resetFilterInput();
-    component.filterInput$.subscribe(obs => {
-      expect(obs).toBe('');
-    });
+    expect(component.filterInput$).toBeObservable(hot('a', { a: '' }));
   });
 
   it('should change filter input', () => {
-    component.onChangeFilterInput('changedFilterINput');
-    component.filterInput$.subscribe(obs => {
-      expect(obs).toBe('changedFilterINput');
-    });
+    component.onChangeFilterInput('changedFilterInput');
+    expect(component.filterInput$).toBeObservable(
+      hot('a', { a: 'changedFilterInput' })
+    );
   });
 
-  xit('should change listformat', () => {
+  it('should change listformat', () => {
+    const spy = jest.spyOn(component['bundlesViewModel'], 'changeListFormat');
     component.clickChangeListFormat(ListFormat.LINE);
-    component.listFormat$.subscribe(obs => {
-      expect(obs).toBe(ListFormat.LINE);
-    });
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(ListFormat.LINE);
   });
 });
