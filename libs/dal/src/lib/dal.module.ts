9faf558621802de565dc78e2cf9f011f784d07da
pverhelle <pieter.verhelle@gmail.com>
2018-10-18 14:34:50 +0200

    remove  circular dependency in dal module

--- libs/dal/src/lib/dal.module.ts
+++ libs/dal/src/lib/dal.module.ts
@@ -6,7 +6,6 @@ import {
   BROWSER_STORAGE_SERVICE_TOKEN,
   StorageService
 } from '@campus/browser';
-import { AuthService, AuthServiceToken } from '@campus/dal';
 import {
   LoopBackConfig,
   SDKBrowserModule
@@ -62,6 +61,7 @@ import { EduContentService } from './edu-content/edu-content.service';
 import { EDUCONTENT_SERVICE_TOKEN } from './edu-content/edu-content.service.interface';
 import { LearningAreaService } from './learning-area/learning-area.service';
 import { LEARNINGAREA_SERVICE_TOKEN } from './learning-area/learning-area.service.interface';
+import { AuthService, AuthServiceToken } from './persons/auth-service';
 import { StudentContentStatusService } from './student-content-status/student-content-status.service';
 import { STUDENT_CONTENT_STATUS_SERVICE_TOKEN } from './student-content-status/student-content-status.service.interface';
 
