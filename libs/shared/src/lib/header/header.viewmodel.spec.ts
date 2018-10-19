b6fea1f54df1125f646055d8789abe191643cc88
david.boute <david.boute@diekeure.be>
2018-10-19 12:41:22 +0200

    Header ViewModel Tests

--- libs/shared/src/lib/header/header.viewmodel.spec.ts
+++ libs/shared/src/lib/header/header.viewmodel.spec.ts
@@ -3,9 +3,37 @@ import { HeaderViewModel } from './header.viewmodel';
 let headerViewModel: HeaderViewModel;
 
 beforeEach(() => {
-  headerViewModel = new HeaderViewModel();
+  // headerViewModel = new HeaderViewModel();
 });
 
 test('it should return', () => {
+  return;
+});
+
+test('it should provide a stream with the array of breadcrumbs', () => {
+  return;
+});
+
+test('it should (conditionally) provide a stream with the array of recent Alerts', () => {
+  return;
+});
+
+test('it should (conditionally) provide a stream with the array of recent Messages', () => {
+  return;
+});
+
+test('it should provide a stream with the logged in users details', () => {
+  return;
+});
+
+test('it should dispatch an action to poll the server every x seconds', () => {
+  return;
+});
+
+test('it should dispatch an action set an alert as read', () => {
+  return;
+});
+
+test('it should dispatch an action set a message as read', () => {
   return;
 });
