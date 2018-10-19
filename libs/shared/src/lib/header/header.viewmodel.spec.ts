4b1eefbcc8ff22571050564810b5649c2fbcd95a
david.boute <david.boute@diekeure.be>
2018-10-19 11:39:54 +0200

    component scaffolded

---
+++ libs/shared/src/lib/header/header.viewmodel.spec.ts
@@ -0,0 +1,11 @@
+import { HeaderViewModel } from './header.viewmodel';
+
+let headerViewModel: HeaderViewModel;
+
+beforeEach(() => {
+  headerViewModel = new HeaderViewModel();
+});
+
+test('it should return', () => {
+  return;
+});
