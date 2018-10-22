e7a2f7fe659c51f8252a781f25ce6cf052bbc360
david.boute <david.boute@diekeure.be>
2018-10-22 17:08:31 +0200

    cleanup

--- libs/devlib/src/lib/loginpage/loginpage.component.ts
+++ libs/devlib/src/lib/loginpage/loginpage.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AlertQueueInterface, EduContentInterface } from '@campus/dal';
+import { EduContentInterface } from '@campus/dal';
 import { PersonApi } from '@diekeure/polpo-api-angular-sdk';
-import { AlertService } from 'libs/dal/src/lib/alert/alert.service';
 import { Observable } from 'rxjs';
 import { LoginPageViewModel } from './loginpage.viewmodel';
 
@@ -15,34 +14,12 @@ export class LoginpageComponent implements OnInit {
   currentUser: Observable<any>;
   constructor(
     private loginPageviewModel: LoginPageViewModel,
-    private personApi: PersonApi,
-    private alertService: AlertService
+    private personApi: PersonApi
   ) {}
 
   ngOnInit() {}
 
   getCurrentUser() {
     this.currentUser = this.personApi.getCurrent();
-  }
-
-  // tslint:disable-next-line:member-ordering
-  response1$: Observable<AlertQueueInterface[]>;
-  getAllAlerts() {
-    this.response1$ = this.alertService.getAllAlertsForCurrentUser(6);
-  }
-
-  // tslint:disable-next-line:member-ordering
-  response2$: Observable<AlertQueueInterface[]>;
-  getAllAlertsSinceDate() {
-    this.response2$ = this.alertService.getAlertsForCurrentUserByDate(
-      6,
-      new Date(2018, 9 - 1, 4, 14 + 2, 37, 0, 0) // month in 0-based array + GMT+2 -> 2 uur compenseren
-    );
-  }
-
-  // tslint:disable-next-line:member-ordering
-  response3$: Observable<AlertQueueInterface>;
-  setAlertRead() {
-    this.response3$ = this.alertService.setAlertAsRead(6, 25);
   }
 }
