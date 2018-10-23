15f4a5e37c0679db006f9c051d446190abed1d1a
david.boute <david.boute@diekeure.be>
2018-10-23 17:02:43 +0200

    Using Devlib as testing ground

--- libs/devlib/src/lib/loginpage/loginpage.component.ts
+++ libs/devlib/src/lib/loginpage/loginpage.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { EduContentInterface } from '@campus/dal';
+import {
+  AlertActions,
+  AlertQueries,
+  AlertReducer,
+  EduContentInterface
+} from '@campus/dal';
 import { PersonApi } from '@diekeure/polpo-api-angular-sdk';
+import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { LoginPageViewModel } from './loginpage.viewmodel';
 
@@ -14,12 +20,52 @@ export class LoginpageComponent implements OnInit {
   currentUser: Observable<any>;
   constructor(
     private loginPageviewModel: LoginPageViewModel,
-    private personApi: PersonApi
+    private personApi: PersonApi,
+    private store: Store<AlertReducer.State>
   ) {}
 
   ngOnInit() {}
 
   getCurrentUser() {
     this.currentUser = this.personApi.getCurrent();
+  }
+
+  dispatchLoadAction() {
+    this.store.dispatch(new AlertActions.LoadAlerts({ userId: 6 }));
+  }
+
+  dispatchLoadNewAction() {
+    this.store.dispatch(
+      new AlertActions.LoadNewAlerts({
+        userId: 6,
+        timeStamp: new Date(2018, 8, 4)
+      })
+    );
+  }
+
+  dispatchSetAsReadAction() {
+    this.store.dispatch(
+      new AlertActions.SetReadAlerts({
+        personId: 6,
+        alertIds: [25, 4681],
+        read: false
+      })
+    );
+  }
+
+  // tslint:disable-next-line:member-ordering
+  response1$: Observable<any>;
+  getUnreadAlerts() {
+    this.response1$ = this.store.pipe(select(AlertQueries.getUnread));
+  }
+
+  // tslint:disable-next-line:member-ordering
+  response2$: Observable<any>;
+  getRecentAlerts() {
+    this.response2$ = this.store.pipe(
+      select(AlertQueries.getRecentByDate, {
+        timeThreshold: new Date(2018, 9 - 1, 4, 16, 27, 15)
+      })
+    );
   }
 }
