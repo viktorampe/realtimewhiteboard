52785b65107051a0a16e55005bb4839133980c61
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2018-10-24 08:29:19 +0200

    add ui  page for testing

--- libs/devlib/src/lib/devlib.module.ts
+++ libs/devlib/src/lib/devlib.module.ts
@@ -2,22 +2,25 @@ import { CommonModule } from '@angular/common';
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { DalModule } from '@campus/dal';
+import { UiModule } from '@campus/ui';
 import { LoopBackConfig } from '@diekeure/polpo-api-angular-sdk';
 import { DevlibRoutingModule } from './devlib.routing.module';
 import { EduContentComponent } from './edu-content/edu-content.component';
 import { EduContentViewModel } from './edu-content/edu-content.viewmodel';
 import { LoginpageComponent } from './loginpage/loginpage.component';
 import { LoginPageViewModel } from './loginpage/loginpage.viewmodel';
+import { UiPageComponent } from './ui-page/ui-page.component';
 
 @NgModule({
   imports: [
     DalModule.forRoot({ apiBaseUrl: 'http://api.polpo.localhost:3000' }),
     FormsModule,
     CommonModule,
-    DevlibRoutingModule
+    DevlibRoutingModule,
+    UiModule
   ],
   providers: [LoginPageViewModel, EduContentViewModel],
-  declarations: [LoginpageComponent, EduContentComponent]
+  declarations: [LoginpageComponent, EduContentComponent, UiPageComponent]
 })
 export class DevlibModule {
   static forRoot(): ModuleWithProviders {
---
+++ libs/devlib/src/lib/ui-page/ui-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'campus-ui-page',
+  templateUrl: './ui-page.component.html',
+  styleUrls: ['./ui-page.component.scss']
+})
+export class UiPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
