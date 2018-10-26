b268f65954a04cdb8f38e823d4e21d0d12794bb3
Pieter Verhelle <pieter.verhelle@gmail.com>
2018-10-26 11:35:28 +0200

    seperate interfaces

--- libs/shared/src/lib/interfaces/environment.features.interfaces.ts
+++ libs/shared/src/lib/interfaces/environment.features.interfaces.ts
@@ -1,16 +1,19 @@
 import { InjectionToken } from '@angular/core';
 
-export const ENVIRONMENT_FEATURES_TOKEN = new InjectionToken(
-  'environmentFeatures'
+export const ENVIRONMENT_ALERTS_FEATURE_TOKEN = new InjectionToken(
+  'environmentAlertsFeature'
 );
 
-export interface EnvironmentFeaturesInterface {
-  alerts: {
-    enabled: boolean;
-    hasAppBarDropDown: boolean;
-  };
-  messages: {
-    enabled: boolean;
-    hasAppBarDropDown: boolean;
-  };
+export interface EnvironmentAlertsFeatureInterface {
+  enabled: boolean;
+  hasAppBarDropDown: boolean;
+}
+
+export const ENVIRONMENT_MESSAGES_FEATURE_TOKEN = new InjectionToken(
+  'environmentMessagesFeature'
+);
+
+export interface EnvironmentMessagesFeatureInterface {
+  enabled: boolean;
+  hasAppBarDropDown: boolean;
 }
