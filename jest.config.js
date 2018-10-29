8e1a7a43918f2bad626e73fc04c4dd8198b02392
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2018-10-29 12:58:00 +0100

    add some options for debuging

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,7 @@ module.exports = {
   resolver: '@nrwl/builders/plugins/jest/resolver',
   moduleFileExtensions: ['ts', 'js', 'html'],
   collectCoverage: true,
-  coverageReporters: ['html']
+  coverageReporters: ['html'],
+  verbose: true,
+  bail: true
 };
