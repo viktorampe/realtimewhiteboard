5d234c63473c8d8f187010eac448875d4e32dd58
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2018-10-29 11:42:48 +0100

    only run suspicious tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
+  testMatch: [
+    '**/confirmable-select.component.spec.ts',
+    '**/content-preview.component.spec.ts',
+    '**/file-extension.component.spec.ts',
+    '**/notification-dropdown-item.component.spec.ts',
+    '**/list-view.component.spec.ts',
+    '**/shell.component.spec.ts',
+    '**/page-header.component.spec.ts'
+  ],
   transform: {
     '^.+\\.(ts|js|html)$': 'jest-preset-angular/preprocessor.js'
   },
