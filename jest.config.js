ba19572346cee92b1f27f92478f2930eaacc9c8f
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2018-10-29 11:55:40 +0100

    only run ui tests

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,5 @@
 module.exports = {
-  testMatch: [
-    '**/confirmable-select.component.spec.ts',
-    '**/content-preview.component.spec.ts',
-    '**/file-extension.component.spec.ts',
-    '**/notification-dropdown-item.component.spec.ts',
-    '**/list-view.component.spec.ts',
-    '**/shell.component.spec.ts',
-    '**/page-header.component.spec.ts'
-  ],
+  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
   transform: {
     '^.+\\.(ts|js|html)$': 'jest-preset-angular/preprocessor.js'
   },
