a65f66afa2a73d5e82fab3b86a66a3f8b3de7b0b
pverhelle <pieter.verhelle@gmail.com>
2018-10-29 16:10:34 +0100

    add documentation with examples and must dos

--- libs/dal/src/lib/+state/resolver/state.resolver.ts
+++ libs/dal/src/lib/+state/resolver/state.resolver.ts
@@ -4,6 +4,44 @@ import { Action, select, Selector, Store } from '@ngrx/store';
 import { combineLatest, Observable } from 'rxjs';
 import { filter, map, take } from 'rxjs/operators';
 
+/**
+ * 1. the injectable decorator NEEDS to be added to the extending class
+ * 2. custom resolvers extending from this class NEED to call super in their own constructor.
+ * even though dependency injection can be private, passing the store to the super will still work
+ * 3. return an array of actions and an array of boolean selectors in the abstract methods
+ * @example
+ * 1.
+   `@Injectable({
+     providedIn: 'root'
+   })
+   export class BundlesResolver extends StateResolver {...}`
+  
+   2.
+   `constructor(
+     private store: Store<DalState>,
+     @Inject(AUTH_SERVICE_TOKEN) private authService: AuthServiceInterface
+     ) {
+       super(store);
+     }`
+  
+   3.
+   `protected getLoadableActions(): Action[] {
+      return [
+        new BundleActions.LoadBundles({ userId: this.authService.userId })
+      ];
+    }
+
+    protected getResolvedQueries(): Selector<object, boolean>[] {
+      return [
+        UnlockedBoekeStudentQueries.getLoaded
+      ];
+    }`
+ *
+ * @export
+ * @abstract
+ * @class StateResolver
+ * @implements {Resolve<boolean>}
+ */
 export abstract class StateResolver implements Resolve<boolean> {
   constructor(private superStore: Store<DalState>) {}
 
