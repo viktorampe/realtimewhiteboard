74074932ed3288756214b03be2b2154e5337bac7
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2018-10-29 09:38:39 +0100

    only run supicious tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
+  testMatch: [
+    '**/confirmable-select.component.spec.ts',
+    '**/content-preview.component.spec.ts',
+    '**/dropdown-menu.component.spec.ts',
+    '**/filter.component.spec.ts',
+    '**/input-label.component.spec.ts',
+    '**/period-label.component.spec.ts'
+  ],
   transform: {
     '^.+\\.(ts|js|html)$': 'jest-preset-angular/preprocessor.js'
   },
