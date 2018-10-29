f9225326b867b28364f22db88313c8214507e49d
pverhelle <pieter.verhelle@gmail.com>
2018-10-29 15:27:04 +0100

    update routing in the bundles module so the resolver is only added once

--- libs/pages/bundles/src/lib/pages-bundles-routing.module.ts
+++ libs/pages/bundles/src/lib/pages-bundles-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeaderResolver } from '../../../../shared/src/lib/header/header.resolver';
+// import { HeaderResolver } from '../../../../shared/src/lib/header/header.resolver';
 import { BundleDetailComponent } from './components/bundle-detail/bundle-detail.component';
+import { BundlesResolver } from './components/bundles.resolver';
 import { BundlesComponent } from './components/bundles/bundles.component';
 import { LearningAreasComponent } from './components/learning-areas/learning-areas.component';
 
@@ -9,20 +10,20 @@ const routes: Routes = [
   {
     path: '',
     component: LearningAreasComponent,
-    resolve: { isResolved: HeaderResolver }
-  },
-  {
-    path: ':area',
-    resolve: { isResolved: HeaderResolver },
+    resolve: { isResolved: BundlesResolver },
     children: [
       {
-        path: '',
-        component: BundlesComponent
-      },
-      {
-        path: ':bundle',
-        component: BundleDetailComponent,
-        resolve: { isResolved: HeaderResolver }
+        path: ':area',
+        children: [
+          {
+            path: '',
+            component: BundlesComponent
+          },
+          {
+            path: ':bundle',
+            component: BundleDetailComponent
+          }
+        ]
       }
     ]
   }
