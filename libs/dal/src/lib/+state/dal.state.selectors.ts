d0d2651b8f87330ec8a34aeb5045decc531389a2
pverhelle <pieter.verhelle@gmail.com>
2018-10-30 11:17:00 +0100

    create new forFeature moduleWithProvider provider

--- libs/dal/src/lib/+state/dal.state.selectors.ts
+++ libs/dal/src/lib/+state/dal.state.selectors.ts
@@ -1,4 +1,10 @@
-import { createSelector } from '@ngrx/store';
+import { InjectionToken, ModuleWithProviders } from '@angular/core';
+import {
+  Action,
+  ActionReducer,
+  createSelector,
+  StoreModule
+} from '@ngrx/store';
 import { BundleInterface } from '../+models';
 import { BundleQueries, BundleReducer } from './bundle';
 
@@ -16,3 +22,53 @@ export const getLearningAreaBundles = createSelector(
     );
   }
 );
+
+export interface ReducerNamespaceInterface {
+  NAME: string;
+  reducer:
+    | ActionReducer<any, Action>
+    | InjectionToken<ActionReducer<any, Action>>;
+  initialState: any;
+}
+
+export function getModuleWithForFeatureProviders<
+  T extends ReducerNamespaceInterface
+>(reducers: T[]): ModuleWithProviders[] {
+  return reducers.map(reducer => {
+    return StoreModule.forFeature(
+      reducer.NAME,
+      reducer.reducer,
+      reducer.initialState
+    );
+  });
+}
+
+function getReducerNamespaceInterfaces(
+  reducerNamespaces: any[]
+): ReducerNamespaceInterface[] {
+  return reducerNamespaces.map((reducer, index) => {
+    if (!reducer.NAME) throwMissingReducerProperty('NAME', index);
+    if (!reducer.reducer) throwMissingReducerProperty('reducer', index);
+    if (!reducer.initialState)
+      throwMissingReducerProperty('initialState', index);
+    return {
+      NAME: reducer.NAME,
+      reducer: reducer.reducer,
+      initialState: reducer.initialState
+    };
+  });
+}
+
+function throwMissingReducerProperty(property: string, index: number) {
+  throw Error(
+    `The reducer with index ${index} in the provided reducerNameSpaces array is missing property '${property}'`
+  );
+}
+
+export function getStoreModuleForFeatures(
+  reducerNameSpaces: any[]
+): ModuleWithProviders[] {
+  return getModuleWithForFeatureProviders(
+    getReducerNamespaceInterfaces(reducerNameSpaces)
+  );
+}
