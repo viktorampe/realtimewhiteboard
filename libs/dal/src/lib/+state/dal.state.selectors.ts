6dad8e043198c44945b35277e572923269d8c490
pverhelle <pieter.verhelle@gmail.com>
2018-10-30 11:33:33 +0100

    remove builder from selector file

--- libs/dal/src/lib/+state/dal.state.selectors.ts
+++ libs/dal/src/lib/+state/dal.state.selectors.ts
@@ -1,10 +1,4 @@
-import { InjectionToken, ModuleWithProviders } from '@angular/core';
-import {
-  Action,
-  ActionReducer,
-  createSelector,
-  StoreModule
-} from '@ngrx/store';
+import { createSelector } from '@ngrx/store';
 import { BundleInterface } from '../+models';
 import { BundleQueries, BundleReducer } from './bundle';
 
@@ -22,53 +16,3 @@ export const getLearningAreaBundles = createSelector(
     );
   }
 );
-
-export interface ReducerNamespaceInterface {
-  NAME: string;
-  reducer:
-    | ActionReducer<any, Action>
-    | InjectionToken<ActionReducer<any, Action>>;
-  initialState: any;
-}
-
-export function getModuleWithForFeatureProviders<
-  T extends ReducerNamespaceInterface
->(reducers: T[]): ModuleWithProviders[] {
-  return reducers.map(reducer => {
-    return StoreModule.forFeature(
-      reducer.NAME,
-      reducer.reducer,
-      reducer.initialState
-    );
-  });
-}
-
-function getReducerNamespaceInterfaces(
-  reducerNamespaces: any[]
-): ReducerNamespaceInterface[] {
-  return reducerNamespaces.map((reducer, index) => {
-    if (!reducer.NAME) throwMissingReducerProperty('NAME', index);
-    if (!reducer.reducer) throwMissingReducerProperty('reducer', index);
-    if (!reducer.initialState)
-      throwMissingReducerProperty('initialState', index);
-    return {
-      NAME: reducer.NAME,
-      reducer: reducer.reducer,
-      initialState: reducer.initialState
-    };
-  });
-}
-
-function throwMissingReducerProperty(property: string, index: number) {
-  throw Error(
-    `The reducer with index ${index} in the provided reducerNameSpaces array is missing property '${property}'`
-  );
-}
-
-export function getStoreModuleForFeatures(
-  reducerNameSpaces: any[]
-): ModuleWithProviders[] {
-  return getModuleWithForFeatureProviders(
-    getReducerNamespaceInterfaces(reducerNameSpaces)
-  );
-}
