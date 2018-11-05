27e5ba93a064b06f12aa5e6da6bc0e37d8728976
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2018-11-05 17:07:21 +0100

    add unit tests

--- libs/dal/src/lib/content-request/content-request.service.spec.ts
+++ libs/dal/src/lib/content-request/content-request.service.spec.ts
@@ -1,15 +1,41 @@
-import { TestBed, inject } from '@angular/core/testing';
-
+import { inject, TestBed } from '@angular/core/testing';
+import { EduContentApi } from '@diekeure/polpo-api-angular-sdk';
+import { hot } from '@nrwl/nx/testing';
 import { ContentRequestService } from './content-request.service';
+import { ContentRequestServiceInterface } from './content-request.service.interface';
 
+// file.only
 describe('ContentRequestService', () => {
+  let service: ContentRequestServiceInterface;
+  let mockData$: any;
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ContentRequestService]
+      providers: [
+        ContentRequestService,
+        {
+          provide: EduContentApi,
+          useValue: {
+            requestURLRemote: () => mockData$
+          }
+        }
+      ]
     });
+    service = TestBed.get(ContentRequestService);
   });
 
-  it('should be created', inject([ContentRequestService], (service: ContentRequestService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should be created', inject(
+    [ContentRequestService],
+    (contentRequestService: ContentRequestService) => {
+      expect(contentRequestService).toBeTruthy();
+    }
+  ));
+
+  it('should return a temp url', async () => {
+    mockData$ = hot('-a-|', { a: 'tempUrl' });
+    expect(service.requestUrl(1)).toBeObservable(
+      hot('-a-|', {
+        a: 'tempUrl'
+      })
+    );
+  });
 });
