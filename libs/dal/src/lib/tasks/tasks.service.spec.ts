783c16fdc8ac6c1106fc767d2981a8a5e1b4c05f
pverhelle <pieter.verhelle@gmail.com>
2018-11-05 10:50:54 +0100

    add task service interface, injection token and specs

--- libs/dal/src/lib/tasks/tasks.service.spec.ts
+++ libs/dal/src/lib/tasks/tasks.service.spec.ts
@@ -1,15 +1,44 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
+import { PersonApi } from '@diekeure/polpo-api-angular-sdk';
+import { hot } from '@nrwl/nx/testing';
+import { Observable } from 'rxjs';
+import { TaskServiceInterface } from './task.service.interface';
+import { TaskService } from './tasks.service';
 
-import { TasksService } from './tasks.service';
+describe('TaskService', () => {
+  let service: TaskServiceInterface;
+  let mockData$: Observable<object>;
 
-describe('TasksService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [TasksService]
+      providers: [
+        TaskService,
+        {
+          provide: PersonApi,
+          useValue: {
+            getData: () => mockData$
+          }
+        }
+      ]
     });
+    service = TestBed.get(TaskService);
   });
 
-  it('should be created', inject([TasksService], (service: TasksService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should be created and available via DI', inject(
+    [TaskService],
+    (taskService: TaskService) => {
+      expect(taskService).toBeTruthy();
+    }
+  ));
+
+  it('should return tasks', () => {
+    mockData$ = hot('-a-|', {
+      a: { tasks: [{ id: 12331 }] }
+    });
+    expect(service.getAllForUser(1)).toBeObservable(
+      hot('-a-|', {
+        a: [{ id: 12331 }]
+      })
+    );
+  });
 });
