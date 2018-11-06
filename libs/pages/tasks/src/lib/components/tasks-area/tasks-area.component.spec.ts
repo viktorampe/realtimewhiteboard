da1afa46c8d97b32e5a5dd0d6c376ae885fbbbd4
david.boute <david.boute@diekeure.be>
2018-11-06 12:05:02 +0100

    testing

--- libs/pages/tasks/src/lib/components/tasks-area/tasks-area.component.spec.ts
+++ libs/pages/tasks/src/lib/components/tasks-area/tasks-area.component.spec.ts
@@ -1,5 +1,5 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TasksAreaComponent } from './tasks-area.component';
 
 describe('TasksAreaComponent', () => {
@@ -8,7 +8,8 @@ describe('TasksAreaComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [TasksAreaComponent]
+      declarations: [TasksAreaComponent],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
 
