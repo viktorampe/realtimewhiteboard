7367299579d9ed6f1377177ff5b5d9fe8af43497
david.boute <david.boute@diekeure.be>
2018-11-06 09:45:21 +0100

    pagina layout gekopieerd van bundles

--- libs/pages/tasks/src/lib/components/tasks-area/tasks-area.component.ts
+++ libs/pages/tasks/src/lib/components/tasks-area/tasks-area.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { LearningAreaInterface } from '@campus/dal';
+import { ListFormat } from '@campus/ui';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { TasksViewModel } from './../tasks.viewmodel';
 
 @Component({
   selector: 'campus-tasks-area',
@@ -6,7 +11,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tasks-area.component.scss']
 })
 export class TasksAreaComponent implements OnInit {
-  constructor() {}
+  filterInput$ = new BehaviorSubject<string>('');
+  listFormat$: Observable<ListFormat>;
+  learningAreas$: Observable<LearningAreaInterface[]>;
+  displayedLearningAreas$: Observable<LearningAreaInterface[]>;
 
-  ngOnInit() {}
+  constructor(private tasksViewModel: TasksViewModel) {}
+
+  ngOnInit() {
+    // this.listFormat$ = this.tasksViewModel.listFormat$;
+    // this.learningAreas$ = this.tasksViewModel.learningAreas$;
+    // TODO find out why learningarea name is not displayed
+    this.displayedLearningAreas$ = this.getDisplayedLearningAreas$(
+      this.learningAreas$,
+      this.filterInput$
+    );
+  }
+
+  onChangeFilterInput(filterInput: string): void {
+    this.filterInput$.next(filterInput);
+  }
+
+  resetFilterInput(): void {
+    this.filterInput$.next('');
+  }
+
+  clickChangeListFormat(value: string): void {
+    // this.tasksViewModel.changeListFormat(ListFormat[value]);
+  }
+
+  getDisplayedLearningAreas$(
+    learningAreas$: Observable<LearningAreaInterface[]>,
+    filterInput$: Observable<string>
+  ): Observable<LearningAreaInterface[]> {
+    return combineLatest(learningAreas$, filterInput$).pipe(
+      map(([learningAreas, filterInput]: [LearningAreaInterface[], string]) => {
+        if (!filterInput) {
+          return learningAreas;
+        }
+        return learningAreas.filter(learningArea =>
+          learningArea.name.toLowerCase().includes(filterInput.toLowerCase())
+        );
+      })
+    );
+  }
 }
