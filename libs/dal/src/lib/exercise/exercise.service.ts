4ed5a5719fb6700e6fa8ad1dc84d550a56e1c74a
david.boute <david.boute@diekeure.be>
2018-11-09 10:33:22 +0100

    Stubbed ResultService and TempUrlService

--- libs/dal/src/lib/exercise/exercise.service.ts
+++ libs/dal/src/lib/exercise/exercise.service.ts
@@ -2,17 +2,56 @@ import { Injectable } from '@angular/core';
 import { ExerciseInterface, ResultInterface } from '@campus/dal';
 import { combineLatest, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { ResultsService } from '../results/results.service';
 import { ExerciseServiceInterface } from './exercise.service.interface';
+
+// TODO: remove vvv and fix imports
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TempUrlService {
+  public getTempUrl(): Observable<string> {
+    return;
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ResultsService {
+  public getResultForTask(
+    userId: number,
+    taskId: number,
+    educontentId: number
+  ): Observable<ResultInterface> {
+    return;
+  }
+  public getResultForUnlockedContent(
+    userId: number,
+    unlockedContentId: number,
+    educontentId: number
+  ): Observable<ResultInterface> {
+    return;
+  }
+
+  public saveResult(userId: number, cmi: any): Observable<ExerciseInterface> {
+    return;
+  }
+}
+
+export enum ScormCMIMode {
+  CMI_MODE_NORMAL = 'normal',
+  CMI_MODE_BROWSE = 'browse',
+  CMI_MODE_REVIEW = 'review',
+  CMI_MODE_PREVIEW = 'preview'
+}
+
+// end remove ^^^
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExerciseService implements ExerciseServiceInterface {
-  public getAllForUser(userId: number): Observable<ExerciseInterface[]> {
-    return;
-  }
-
   public startExercise(
     userId: number,
     educontentId: number,
@@ -20,6 +59,7 @@ export class ExerciseService implements ExerciseServiceInterface {
     unlockedContentId?: number
   ): Observable<ExerciseInterface> {
     let result$: Observable<ResultInterface>;
+
     if (!(taskId || unlockedContentId) || (taskId && unlockedContentId)) {
       throw new Error('Provide either a taskId or an unlockedContentId');
     }
@@ -55,13 +95,13 @@ export class ExerciseService implements ExerciseServiceInterface {
     const resultId = exercise.result.id;
     const cmi = exercise.result.cmi;
 
-    const result$ = this.resultsService.saveResult(userId, resultId, cmi);
+    const result$ = this.resultsService.saveResult(userId, cmi);
 
     return result$;
   }
 
   constructor(
     private resultsService: ResultsService,
-    private tempUrlService: any
+    private tempUrlService: TempUrlService
   ) {}
 }
