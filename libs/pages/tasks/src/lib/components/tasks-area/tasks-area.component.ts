1b8a9d031889bfeb81abc6438f7037d5515a52ac
david.boute <david.boute@diekeure.be>
2018-11-13 15:06:53 +0100

    Filteren met filter-text-input component

--- libs/pages/tasks/src/lib/components/tasks-area/tasks-area.component.ts
+++ libs/pages/tasks/src/lib/components/tasks-area/tasks-area.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ListFormat } from '@campus/ui';
-import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
+import { FilterServiceInterface, FILTER_SERVICE_TOKEN } from '@campus/shared';
+import { FilterTextInputComponent, ListFormat } from '@campus/ui';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { LearningAreaInterface } from './../../../../../../dal/src/lib/+models/LearningArea.interface';
 import { TasksViewModel } from './../tasks.viewmodel';
 import { LearningAreasWithTaskInstanceInfoInterface } from './../tasks.viewmodel.interfaces';
 
@@ -17,62 +18,90 @@ export class TasksAreaComponent implements OnInit {
   learningAreasWithInfo$: Observable<
     LearningAreasWithTaskInstanceInfoInterface
   >;
-  displayedLearningAreasWithInfo$: Observable<
-    LearningAreasWithTaskInstanceInfoInterface
+  // displayedLearningAreasWithInfo$: Observable<
+  //   LearningAreasWithTaskInstanceInfoInterface
+  // >;
+
+  @ViewChild(FilterTextInputComponent)
+  filterTextInput: FilterTextInputComponent<
+    LearningAreasWithTaskInstanceInfoInterface,
+    {
+      learningArea: LearningAreaInterface;
+      openTasks: number;
+      closedTasks: number;
+    }
   >;
 
-  constructor(private tasksViewModel: TasksViewModel) {}
+  constructor(
+    private tasksViewModel: TasksViewModel,
+    @Inject(FILTER_SERVICE_TOKEN) private filterService: FilterServiceInterface
+  ) {}
 
   ngOnInit() {
     this.listFormat$ = this.tasksViewModel.listFormat$;
     this.learningAreasWithInfo$ = this.tasksViewModel.learningAreasWithTaskInstances$;
-    this.displayedLearningAreasWithInfo$ = this.getDisplayedLearningAreas$(
-      this.learningAreasWithInfo$,
-      this.filterInput$
-    );
+    // this.displayedLearningAreasWithInfo$ = this.getDisplayedLearningAreas$(
+    //   this.learningAreasWithInfo$,
+    //   this.filterInput$
+    // );
+    this.filterTextInput.filterFn = this.filterFn.bind(this);
   }
 
-  onChangeFilterInput(filterInput: string): void {
-    this.filterInput$.next(filterInput);
-  }
+  // onChangeFilterInput(filterInput: string): void {
+  //   this.filterInput$.next(filterInput);
+  // }
 
-  resetFilterInput(): void {
-    this.filterInput$.next('');
-  }
+  // resetFilterInput(): void {
+  //   this.filterInput$.next('');
+  // }
 
   clickChangeListFormat(value: ListFormat): void {
     this.tasksViewModel.changeListFormat(value);
   }
 
-  getDisplayedLearningAreas$(
-    learningAreas$: Observable<LearningAreasWithTaskInstanceInfoInterface>,
-    filterInput$: Observable<string>
-  ): Observable<LearningAreasWithTaskInstanceInfoInterface> {
-    return combineLatest(learningAreas$, filterInput$).pipe(
-      map(
-        ([learningAreas, filterInput]: [
-          LearningAreasWithTaskInstanceInfoInterface,
-          string
-        ]) => {
-          if (!filterInput) {
-            return learningAreas;
-          }
+  // getDisplayedLearningAreas$(
+  //   learningAreas$: Observable<LearningAreasWithTaskInstanceInfoInterface>,
+  //   filterInput$: Observable<string>
+  // ): Observable<LearningAreasWithTaskInstanceInfoInterface> {
+  //   return combineLatest(learningAreas$, filterInput$).pipe(
+  //     map(
+  //       ([learningAreas, filterInput]: [
+  //         LearningAreasWithTaskInstanceInfoInterface,
+  //         string
+  //       ]) => {
+  //         if (!filterInput) {
+  //           return learningAreas;
+  //         }
 
-          const learningAreaArray = learningAreas.learningAreasWithInfo.filter(
-            learningAreaWithInfo =>
-              learningAreaWithInfo.learningArea.name
-                .toLowerCase()
-                .includes(filterInput.toLowerCase())
-          );
+  //         const learningAreaArray = learningAreas.learningAreasWithInfo.filter(
+  //           learningAreaWithInfo =>
+  //             learningAreaWithInfo.learningArea.name
+  //               .toLowerCase()
+  //               .includes(filterInput.toLowerCase())
+  //         );
 
-          const learningAreaWithTaskInstanceInfo = {
-            ...learningAreas,
-            learningAreasWithInfo: learningAreaArray
-          };
+  //         const learningAreaWithTaskInstanceInfo = {
+  //           ...learningAreas,
+  //           learningAreasWithInfo: learningAreaArray
+  //         };
 
-          return learningAreaWithTaskInstanceInfo;
-        }
-      )
-    );
+  //         return learningAreaWithTaskInstanceInfo;
+  //       }
+  //     )
+  //   );
+  // }
+
+  private filterFn(
+    info: LearningAreasWithTaskInstanceInfoInterface,
+    searchText: string
+  ): // interface is defined as part of LearningAreasWithTaskInstanceInfoInterface
+  {
+    learningArea: LearningAreaInterface;
+    openTasks: number;
+    closedTasks: number;
+  }[] {
+    return this.filterService.filter(info.learningAreasWithInfo, {
+      learningArea: { name: searchText }
+    });
   }
 }
