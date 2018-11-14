1d3c930c600b32cf9798118d3a63447fb811404a
pverhelle <pieter.verhelle@gmail.com>
2018-11-14 11:35:03 +0100

    add some steam structure to the component

--- libs/pages/tasks/src/lib/components/task-detail/task-detail.component.ts
+++ libs/pages/tasks/src/lib/components/task-detail/task-detail.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import {
+  LearningAreaInterface,
+  TaskEduContentInterface,
+  TaskInstanceInterface
+} from '@campus/dal';
+import { Observable } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
+import { TasksViewModel } from '../tasks.viewmodel';
 
 @Component({
   selector: 'campus-task-detail',
@@ -6,7 +15,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-detail.component.scss']
 })
 export class TaskDetailComponent implements OnInit {
-  constructor() {}
+  //input streams
+  routerParams$: Params;
 
-  ngOnInit(): void {}
+  //intermediate streams
+  taskEduContents$: Observable<TaskEduContentInterface[]>;
+
+  //output streams
+  learingArea$: Observable<LearningAreaInterface>;
+  taskInstance$: Observable<TaskInstanceInterface>;
+  filteredTaskEduContents$: Observable<TaskEduContentInterface[]>;
+  selectedTaskEduContent$: Observable<TaskEduContentInterface>;
+
+  constructor(
+    private taskViewModel: TasksViewModel,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.loadInputParams();
+    this.loadIntermediateStreams();
+    this.loadOutputStreams();
+  }
+
+  private loadInputParams(): void {
+    this.routerParams$ = this.activatedRoute.params.pipe(shareReplay(1));
+  }
+
+  private loadIntermediateStreams(): void {
+    //TODO add
+  }
+
+  private loadOutputStreams(): void {
+    this.learingArea$ = this.getLearingArea();
+    this.taskInstance$ = this.getTaskInstance();
+    //TODO expand
+    // filteredTaskEduContents$
+    // selectedTaskEduContent$
+  }
+
+  private getLearingArea(): Observable<LearningAreaInterface> {
+    //TODO needs to be
+    return this.taskViewModel.selectedLearningArea$;
+  }
+
+  private getTaskInstance(): Observable<TaskInstanceInterface> {
+    //TODO needs to be
+    return this.taskViewModel.selectedTaskInstance$;
+  }
 }
