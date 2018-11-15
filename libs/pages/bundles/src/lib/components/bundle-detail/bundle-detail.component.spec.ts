9c83c76710ebdf3b00e984463076837ea8b17d6e
Thomas Ghesquiere <thomas.ghesquiere@gmail.com>
2018-11-15 16:11:49 +0100

    re-enable all tests

--- libs/pages/bundles/src/lib/components/bundle-detail/bundle-detail.component.spec.ts
+++ libs/pages/bundles/src/lib/components/bundle-detail/bundle-detail.component.spec.ts
@@ -1,4 +1,3 @@
-//file.only
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
