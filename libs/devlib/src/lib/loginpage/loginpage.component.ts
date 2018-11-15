ae5018ea8584d9b172e1fe5d8b6b9791994fb696
david.boute <david.boute@diekeure.be>
2018-11-15 13:16:17 +0100

    cleanup

--- libs/devlib/src/lib/loginpage/loginpage.component.ts
+++ libs/devlib/src/lib/loginpage/loginpage.component.ts
@@ -1,11 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  AlertReducer,
-  EduContentInterface,
-  ResultInterface,
-  UserActions
-} from '@campus/dal';
-import { ScormCMIMode, ScormResultsService, ScormStatus } from '@campus/shared';
+import { AlertReducer, EduContentInterface, UserActions } from '@campus/dal';
 import { PersonApi } from '@diekeure/polpo-api-angular-sdk';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
@@ -22,8 +16,7 @@ export class LoginpageComponent implements OnInit {
   constructor(
     public loginPageviewModel: LoginPageViewModel,
     private personApi: PersonApi,
-    private store: Store<AlertReducer.State>,
-    private scormResultsService: ScormResultsService
+    private store: Store<AlertReducer.State>
   ) {}
 
   ngOnInit() {}
@@ -35,42 +28,4 @@ export class LoginpageComponent implements OnInit {
   loadCurrentUserinState() {
     this.store.dispatch(new UserActions.LoadUser({ force: true }));
   }
-
-  // tslint:disable-next-line:member-ordering
-  resultTask$: Observable<ResultInterface>;
-  getResultForTask() {
-    const result = this.scormResultsService.getResultForTask(6, 1, 1);
-    this.resultTask$ = result;
-  }
-
-  // tslint:disable-next-line:member-ordering
-  resultUnlockedContent$: Observable<ResultInterface>;
-  getResultForUnlockedContent() {
-    const result = this.scormResultsService.getResultForUnlockedContent(
-      6,
-      1,
-      1
-    );
-    this.resultUnlockedContent$ = result;
-  }
-
-  // tslint:disable-next-line:member-ordering
-  resultSaved$: any;
-  saveResult() {
-    const result = this.scormResultsService.saveResult(6, 5, this.cmi);
-
-    this.resultSaved$ = result;
-  }
-
-  // tslint:disable-next-line:member-ordering
-  cmi = {
-    mode: ScormCMIMode.CMI_MODE_NORMAL,
-    core: {
-      score: { raw: 100 },
-      lesson_location: '',
-      lesson_status: ScormStatus.STATUS_COMPLETED,
-      total_time: '0:1:4.76',
-      session_time: '0000:00:00'
-    }
-  };
 }
