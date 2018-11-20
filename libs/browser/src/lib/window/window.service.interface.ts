bdd0cf9f51174e726cf3ca48d5a38bf91ce89907
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2018-11-20 10:44:52 +0100

    cr changes - part ...

--- libs/browser/src/lib/window/window.service.interface.ts
+++ libs/browser/src/lib/window/window.service.interface.ts
@@ -3,7 +3,7 @@ import { InjectionToken } from '@angular/core';
 export const WINDOW_SERVICE_TOKEN = new InjectionToken('window service');
 
 export interface WindowServiceInterface {
-  openedWindows: { [name: string]: Window };
+  readonly openedWindows: { [name: string]: Window };
   openWindow(name: string, url: string);
   closeWindow(name: string);
 }
