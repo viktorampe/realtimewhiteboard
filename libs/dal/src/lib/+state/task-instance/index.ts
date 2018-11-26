dbcc02cb73e8af6226a7abdc62e0cbaa7844be97
pverhelle <pieter.verhelle@gmail.com>
2018-11-26 15:20:21 +0100

    index file  for the task-instance state

---
+++ libs/dal/src/lib/+state/task-instance/index.ts
@@ -0,0 +1,6 @@
+import * as TaskInstanceActions from './task-instance.actions';
+import { TaskInstanceEffects } from './task-instance.effects';
+import * as TaskInstanceReducer from './task-instance.reducer';
+import * as TaskInstanceQueries from './task-instance.selectors';
+
+export { TaskInstanceActions, TaskInstanceReducer, TaskInstanceQueries, TaskInstanceEffects };
