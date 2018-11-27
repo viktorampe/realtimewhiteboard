4912b37fb72f268317d6128b9a96b3c00f6370df
david.boute <david.boute@diekeure.be>
2018-11-27 14:32:52 +0100

    component opmaken

--- libs/pages/books/src/lib/components/books/books.component.ts
+++ libs/pages/books/src/lib/components/books/books.component.ts
@@ -1,8 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { EduContent } from '@campus/dal';
+import { FilterServiceInterface, FILTER_SERVICE_TOKEN } from '@campus/shared';
+import { FilterTextInputComponent, ListFormat } from '@campus/ui';
+import { Observable } from 'rxjs';
+import { shareReplay, tap } from 'rxjs/operators';
+import { BooksViewModel } from '../books.viewmodel';
 
 @Component({
   selector: 'campus-book',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.scss']
 })
-export class BooksComponent {}
+export class BooksComponent implements OnInit {
+  protected listFormat = ListFormat;
+  listFormat$: Observable<ListFormat> = this.viewModel.listFormat$;
+  books$: Observable<EduContent[]>;
+
+  @ViewChild(FilterTextInputComponent)
+  filterTextInput: FilterTextInputComponent<EduContent[], EduContent>;
+
+  private routeParams$ = this.route.params.pipe(shareReplay(1));
+
+  constructor(
+    private route: ActivatedRoute,
+    private viewModel: BooksViewModel,
+    @Inject(FILTER_SERVICE_TOKEN) private filterService: FilterServiceInterface
+  ) {}
+
+  ngOnInit() {
+    this.books$ = this.viewModel.sharedBooks$.pipe(tap(x => console.log(x)));
+    this.filterTextInput.setFilterableItem(this);
+  }
+
+  setListFormat(format: ListFormat) {
+    // this.viewModel.changeListFormat(format);
+  }
+
+  filterFn(source: EduContent[], searchText: string): EduContent[] {
+    const books = this.filterService.filter(source, { name: searchText });
+    return books;
+  }
+}
