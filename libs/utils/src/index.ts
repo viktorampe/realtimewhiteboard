db6c55e412caa1fcae0701e2ad66df7ec48689a2
Bert Casier <bert@ernie.be>
2018-11-28 10:33:34 +0100

    better folder structure

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -1,6 +1,6 @@
-export * from '../src/lib/filter/filter.service';
+export * from '../src/lib/collections/filter.service';
 export {
   FilterServiceInterface,
   FILTER_SERVICE_TOKEN
-} from '../src/lib/filter/filter.service.interface';
+} from '../src/lib/collections/filter.service.interface';
 export * from './lib/utils.module';
