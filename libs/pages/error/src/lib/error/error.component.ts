5f0850cd86045f059b6f5269c68bce0f87fbcb72
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2018-11-28 10:40:29 +0100

    scaffold error component

---
+++ libs/pages/error/src/lib/error/error.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'campus-error',
+  templateUrl: './error.component.html',
+  styleUrls: ['./error.component.scss']
+})
+export class ErrorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- libs/pages/error/src/lib/pages-error.module.ts
+++ libs/pages/error/src/lib/pages-error.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   imports: [
@@ -9,6 +10,7 @@ import { RouterModule } from '@angular/router';
     RouterModule.forChild([
       /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
     ])
-  ]
+  ],
+  declarations: [ErrorComponent]
 })
 export class PagesErrorModule {}
