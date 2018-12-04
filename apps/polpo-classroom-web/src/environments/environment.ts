264da1deda2702ebbf91b6a88c38c585e581012f
david.boute <david.boute@diekeure.be>
2018-12-04 15:15:33 +0100

    change requests

--- apps/polpo-classroom-web/src/environments/environment.ts
+++ apps/polpo-classroom-web/src/environments/environment.ts
@@ -125,7 +125,7 @@ export const environment = {
     // code: 'assets/icons/code.svg',
     // creditcard: 'assets/icons/creditcard.svg',
     // 'die-keure': 'assets/icons/die-keure.svg',
-    eye: 'assets/icons/eye.svg',
+    'view-content': 'assets/icons/eye.svg',
     // eye2: 'assets/icons/eye2.svg',
     // facebook: 'assets/icons/facebook.svg',
     // favorite: 'assets/icons/favorite.svg',
