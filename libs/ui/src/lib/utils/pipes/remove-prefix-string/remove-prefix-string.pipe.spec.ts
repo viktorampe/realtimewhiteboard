48cbcdcab2701e74e2575d4e195eac5417d63f37
pverhelle <pieter.verhelle@gmail.com>
2018-12-05 08:39:23 +0100

    rename with pipe suffix

--- libs/ui/src/lib/utils/pipes/remove-prefix-string/remove-prefix-string.pipe.spec.ts
+++ libs/ui/src/lib/utils/pipes/remove-prefix-string/remove-prefix-string.pipe.spec.ts
@@ -1,25 +1,25 @@
-import { RemovePrefixString } from './remove-prefix-string.pipe';
+import { RemovePrefixStringPipe } from './remove-prefix-string.pipe';
 
 describe('RemovePrefixString', () => {
   it('create an instance', () => {
-    const pipe = new RemovePrefixString();
+    const pipe = new RemovePrefixStringPipe();
     expect(pipe).toBeTruthy();
   });
 
   it('adds white', () => {
-    const pipe = new RemovePrefixString();
+    const pipe = new RemovePrefixStringPipe();
     const color: string = pipe.transform('no-prefix-present', 'polpo-');
     expect(color).toBe('no-prefix-present');
   });
 
   it('adds black', () => {
-    const pipe = new RemovePrefixString();
+    const pipe = new RemovePrefixStringPipe();
     const color: string = pipe.transform('no-prefix-at-start', 'polpo-');
     expect(color).toBe('no-prefix-at-start');
   });
 
   it('returns with pound', () => {
-    const pipe = new RemovePrefixString();
+    const pipe = new RemovePrefixStringPipe();
     const color: string = pipe.transform('polpo-yes-prefix', 'polpo-');
     expect(color).toBe('yes-prefix');
   });
--- libs/ui/src/lib/utils/pipes/remove-prefix-string/remove-prefix-string.pipe.ts
+++ libs/ui/src/lib/utils/pipes/remove-prefix-string/remove-prefix-string.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'removePrefixString' })
-export class RemovePrefixString implements PipeTransform {
+export class RemovePrefixStringPipe implements PipeTransform {
   transform(value: string, prefix: string): string {
     if (value.indexOf(prefix) !== 0) return value;
     return value.substring(prefix.length);
