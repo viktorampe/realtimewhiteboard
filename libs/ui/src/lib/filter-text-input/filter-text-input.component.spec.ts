3382319fbfb62ed2decaa1ac67d2687a463c3b93
david.boute <david.boute@diekeure.be>
2018-12-06 12:58:34 +0100

    formatting

--- libs/ui/src/lib/filter-text-input/filter-text-input.component.spec.ts
+++ libs/ui/src/lib/filter-text-input/filter-text-input.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconRegistry } from '@angular/material';
 import { By } from '@angular/platform-browser';
+import { MockMatIconRegistry } from '@campus/testing';
 import { UiModule } from '@campus/ui';
 import { FilterTextInputComponent } from './filter-text-input.component';
 
@@ -19,8 +21,8 @@ describe('FilterTextInputComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [FormsModule, ReactiveFormsModule, UiModule]
-      //providers:[{provide: MatIconRegistry, useClass: MockMatIconRegistry}] //TODO after merge
+      imports: [FormsModule, ReactiveFormsModule, UiModule],
+      providers: [{ provide: MatIconRegistry, useClass: MockMatIconRegistry }]
     }).compileComponents();
   }));
 
