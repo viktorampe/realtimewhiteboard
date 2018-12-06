65b1fe25a5aa2b10788fca7eb552237dead67fd0
Bert Casier <bert@ernie.be>
2018-12-06 09:47:44 +0100

    remove linting errors

--- libs/dal/src/lib/+fixtures/TaskInstance.fixture.ts
+++ libs/dal/src/lib/+fixtures/TaskInstance.fixture.ts
@@ -9,16 +9,10 @@ export class TaskInstanceFixture extends TaskInstance {
   constructor(props: Partial<TaskInstance> = {}) {
     super();
     this.start = new Date(new Date().getTime() - 1000);
-    this.start.setMilliseconds(0);
-    this.start.setSeconds(0);
-    this.start.setMinutes(0);
-    this.start.setHours(0);
+    this.start.setHours(0, 0, 0, 0);
 
     this.end = new Date(new Date().getTime() + 3600 * 24 * 7 * 1000);
-    this.end.setMilliseconds(0);
-    this.end.setSeconds(0);
-    this.end.setMinutes(0);
-    this.end.setHours(0);
+    this.end.setHours(0, 0, 0, 0);
 
     Object.assign(this, props);
   }
--- libs/devlib/src/lib/devlib.module.ts
+++ libs/devlib/src/lib/devlib.module.ts
@@ -5,14 +5,11 @@ import { MatIconModule } from '@angular/material';
 import { ScormModule } from '@campus/scorm';
 import { SharedModule } from '@campus/shared';
 import { UiModule } from '@campus/ui';
-import { InfoPanelContentComponent } from '../../../pages/tasks/src/lib/components/info-panel/info-panel-content/info-panel-content.component';
-import { InfoPanelTaskComponent } from '../../../pages/tasks/src/lib/components/info-panel/info-panel-task/info-panel-task.component';
 import { DevlibRoutingModule } from './devlib.routing.module';
 import { EduContentComponent } from './edu-content/edu-content.component';
 import { EduContentViewModel } from './edu-content/edu-content.viewmodel';
 import { LoginpageComponent } from './loginpage/loginpage.component';
 import { LoginPageViewModel } from './loginpage/loginpage.viewmodel';
-import { UiPageComponent } from './ui-page/ui-page.component';
 
 @NgModule({
   imports: [
@@ -25,12 +22,6 @@ import { UiPageComponent } from './ui-page/ui-page.component';
     ScormModule
   ],
   providers: [LoginPageViewModel, EduContentViewModel],
-  declarations: [
-    LoginpageComponent,
-    EduContentComponent,
-    UiPageComponent,
-    InfoPanelTaskComponent,
-    InfoPanelContentComponent
-  ]
+  declarations: [LoginpageComponent, EduContentComponent]
 })
 export class DevlibModule {}
