eb32c5591d6fbcd2f46a70e73c591868dd5d5720
david.boute <david.boute@diekeure.be>
2018-12-10 12:02:52 +0100

    formatting

--- libs/pages/bundles/src/lib/components/bundles.viewmodel.ts
+++ libs/pages/bundles/src/lib/components/bundles.viewmodel.ts
@@ -1,11 +1,35 @@
 import { Inject, Injectable } from '@angular/core';
-import { AlertActions, AuthServiceInterface, AUTH_SERVICE_TOKEN, BundleInterface, BundleQueries, ContentInterface, DalState, EduContent, EduContentQueries, LearningAreaInterface, LearningAreaQueries, PersonFixture, PersonInterface, UiActions, UiQuery, UnlockedBoekeGroupQueries, UnlockedBoekeStudentQueries, UnlockedContentInterface, UnlockedContentQueries, UserContentQueries } from '@campus/dal';
+import {
+  AlertActions,
+  AuthServiceInterface,
+  AUTH_SERVICE_TOKEN,
+  BundleInterface,
+  BundleQueries,
+  ContentInterface,
+  DalState,
+  EduContent,
+  EduContentQueries,
+  LearningAreaInterface,
+  LearningAreaQueries,
+  PersonFixture,
+  PersonInterface,
+  UiActions,
+  UiQuery,
+  UnlockedBoekeGroupQueries,
+  UnlockedBoekeStudentQueries,
+  UnlockedContentInterface,
+  UnlockedContentQueries,
+  UserContentQueries
+} from '@campus/dal';
 import { ListFormat } from '@campus/ui';
 import { Dictionary } from '@ngrx/entity';
 import { select, Store } from '@ngrx/store';
 import { combineLatest, Observable, of } from 'rxjs';
 import { filter, map, shareReplay, switchMap } from 'rxjs/operators';
-import { BundlesWithContentInfoInterface, LearningAreasWithBundlesInfoInterface } from './bundles.viewmodel.interfaces';
+import {
+  BundlesWithContentInfoInterface,
+  LearningAreasWithBundlesInfoInterface
+} from './bundles.viewmodel.interfaces';
 
 export type NestedPartial<T> = { [P in keyof T]?: NestedPartial<T[P]> };
 
@@ -84,7 +108,7 @@ export class BundlesViewModel {
       })
     );
   }
-  
+
   changeListFormat(listFormat: ListFormat): void {
     this.store.dispatch(new UiActions.SetListFormat({ listFormat }));
   }
