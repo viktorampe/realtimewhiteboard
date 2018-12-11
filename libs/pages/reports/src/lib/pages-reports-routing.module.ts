295a46c34dd8d3bfab5184cea9e3b7becde0607d
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2018-12-11 10:09:39 +0100

    rename /results to /reports

--- libs/pages/reports/src/lib/pages-reports-routing.module.ts
+++ libs/pages/reports/src/lib/pages-reports-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ReportsComponent } from './components/reports.component';
-import { ReportsViewModel } from './components/reports.viewmodel';
 
 const routes: Routes = [
   {
     path: '',
-    component: ReportsComponent,
-    resolve: { isResolved: ReportsViewModel }
+    component: ReportsComponent
+    // resolve: { isResolved: ReportsViewModel }
   }
 ];
 
