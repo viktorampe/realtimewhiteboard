2a81c4d28c3743c59be8cea42aa3f0f3d0f5c7b2
pverhelle <pieter.verhelle@gmail.com>
2018-12-11 16:28:25 +0100

    remove unused interface implementations

--- libs/guards/src/lib/can-load/authentication.guard.ts
+++ libs/guards/src/lib/can-load/authentication.guard.ts
@@ -1,10 +1,7 @@
 import { Inject, Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
-  CanActivate,
   CanActivateChild,
-  CanLoad,
-  Route,
   Router,
   RouterStateSnapshot
 } from '@angular/router';
@@ -12,31 +9,11 @@ import { AuthServiceInterface, AUTH_SERVICE_TOKEN } from '@campus/dal';
 import { Observable } from 'rxjs';
 
 @Injectable()
-export class AuthenticationGuard
-  implements CanLoad, CanActivate, CanActivateChild {
+export class AuthenticationGuard implements CanActivateChild {
   constructor(
     @Inject(AUTH_SERVICE_TOKEN) private authService: AuthServiceInterface,
     private router: Router
   ) {}
-
-  canLoad(route: Route): Observable<boolean> | Promise<boolean> | boolean {
-    if (!this.authService.isLoggedIn()) {
-      this.router.navigate(['/login']);
-      return false;
-    }
-    return true;
-  }
-
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<boolean> | Promise<boolean> | boolean {
-    if (!this.authService.isLoggedIn()) {
-      this.router.navigate(['/login']);
-      return false;
-    }
-    return true;
-  }
 
   canActivateChild(
     route: ActivatedRouteSnapshot,
