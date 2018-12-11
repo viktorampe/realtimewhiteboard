45729f470dc7a3f620d8525d9c965b145e2c0eee
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2018-12-11 08:55:01 +0100

    [WIP] source streams

--- libs/pages/reports/src/lib/components/reports.viewmodel.ts
+++ libs/pages/reports/src/lib/components/reports.viewmodel.ts
@@ -1,8 +1,37 @@
 import { Injectable } from '@angular/core';
+import {
+  DalState,
+  LearningAreaInterface,
+  LearningAreaQueries
+} from '@campus/dal';
+import { MemoizedSelectorWithProps, select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { LearningAreasWithResultsInterface } from './reports.viewmodel.interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReportsViewModel {
-  constructor() {}
+  // source streams
+  learningAreas$: Observable<LearningAreaInterface[]>;
+
+  // presentation streams
+  learningAreasWithResults$: Observable<LearningAreasWithResultsInterface>;
+  constructor(private store: Store<DalState>) {
+    this.setSourceStreams();
+    this.setPresentationStreams();
+  }
+
+  private setSourceStreams() {
+    this.learningAreas$ = this.select(LearningAreaQueries.getAll);
+  }
+
+  private setPresentationStreams() {}
+
+  private select<T, Props>(
+    selector: MemoizedSelectorWithProps<DalState, Props, T>,
+    payload?: Props
+  ): Observable<T> {
+    return this.store.pipe(select(selector, payload));
+  }
 }
