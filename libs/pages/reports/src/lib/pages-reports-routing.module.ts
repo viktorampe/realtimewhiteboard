5558eb8c1ca8648bd66a3944d58c55e373436d13
mkellner <melvin.kellner@diekeure.be>
2018-12-17 16:20:00 +0100

    fixed an issue with incorrect import.

--- libs/pages/reports/src/lib/pages-reports-routing.module.ts
+++ libs/pages/reports/src/lib/pages-reports-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OverviewAreaWithResultsComponent } from './components/overview-areas-with-results/overview-area-with-results.component';
 
 const routes: Routes = [
   {
