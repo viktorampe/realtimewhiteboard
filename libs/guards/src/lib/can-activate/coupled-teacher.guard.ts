7bcb7cf1a19855bccfdca45bae4f2f6b5b3959c4
pverhelle <pieter.verhelle@gmail.com>
2018-12-18 08:46:50 +0100

    move some loaders for coupled data to the coupled guard pipe

--- libs/guards/src/lib/can-activate/coupled-teacher.guard.ts
+++ libs/guards/src/lib/can-activate/coupled-teacher.guard.ts
@@ -1,19 +1,9 @@
-import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  Router,
-  RouterStateSnapshot
-} from '@angular/router';
-import {
-  DalState,
-  PersonInterface,
-  RoleInterface,
-  UserQueries
-} from '@campus/dal';
+import { Inject, Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { AuthServiceInterface, AUTH_SERVICE_TOKEN, DalState, LinkedPersonActions, LinkedPersonQueries, PersonActions, PersonInterface, PersonQueries, RoleInterface, UserQueries } from '@campus/dal';
 import { select, Store } from '@ngrx/store';
 import { combineLatest, Observable } from 'rxjs';
-import { map, skipWhile } from 'rxjs/operators';
+import { map, skipWhile, switchMapTo, tap } from 'rxjs/operators';
 
 export enum RolesEnum {
   Teacher = 'teacher',
@@ -24,12 +14,18 @@ export enum RolesEnum {
 export class CoupledTeacherGuard implements CanActivate {
   //input streams
   private currentUser$: Observable<PersonInterface>;
+  private personQueriesLoaded$: Observable<boolean>;
+  private linkedPersonsLoaded$: Observable<boolean>;
   //intermediate streams
   private isTeacher$: Observable<boolean>;
   private isStudent$: Observable<boolean>;
   private hasTeachers$: Observable<boolean>;
 
-  constructor(private store: Store<DalState>, private router: Router) {
+  constructor(
+    private store: Store<DalState>,
+    private router: Router,
+    @Inject(AUTH_SERVICE_TOKEN) private authService: AuthServiceInterface
+  ) {
     this.initialiseInputStreams();
     this.loadIntermediateStream();
   }
@@ -38,17 +34,30 @@ export class CoupledTeacherGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
-    return combineLatest(
-      this.currentUser$,
-      this.isStudent$,
-      this.isTeacher$,
-      this.hasTeachers$
-    ).pipe(
-      skipWhile(
-        ([currentUser, isStudent, isTeacher, hasTeachers]) =>
-          currentUser === null
+    return this.currentUser$.pipe(
+      skipWhile(currentUser => currentUser === null),
+      tap(() => {
+        this.store.dispatch(
+          new LinkedPersonActions.LoadLinkedPersons({
+            userId: this.authService.userId
+          })
+        );
+        this.store.dispatch(
+          new PersonActions.LoadPersons({
+            userId: this.authService.userId
+          })
+        );
+      }),
+      switchMapTo(
+        combineLatest(this.personQueriesLoaded$, this.linkedPersonsLoaded$)
       ),
-      map(([currentUser, isStudent, isTeacher, hasTeachers]) => {
+      skipWhile(([personQueriesLoaded, linkedPersonsLoaded]) => {
+        return !linkedPersonsLoaded || !personQueriesLoaded;
+      }),
+      switchMapTo(
+        combineLatest(this.isStudent$, this.isTeacher$, this.hasTeachers$)
+      ),
+      map(([isStudent, isTeacher, hasTeachers]) => {
         if (isStudent && !isTeacher && hasTeachers) return true;
         this.router.navigate(['/settings']);
         return false;
@@ -58,6 +67,12 @@ export class CoupledTeacherGuard implements CanActivate {
 
   private initialiseInputStreams(): void {
     this.currentUser$ = this.store.pipe(select(UserQueries.getCurrentUser));
+    this.linkedPersonsLoaded$ = this.store.pipe(
+      select(LinkedPersonQueries.getLoaded)
+    );
+    this.personQueriesLoaded$ = this.store.pipe(
+      select(PersonQueries.getLoaded)
+    );
   }
 
   private loadIntermediateStream(): void {
