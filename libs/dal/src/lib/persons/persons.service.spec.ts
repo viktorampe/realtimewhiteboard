159c0f9c6cfe73fc807676520cc31cbd89bf22af
david.boute <david.boute@diekeure.be>
2019-01-03 16:10:27 +0100

    unit tests

--- libs/dal/src/lib/persons/persons.service.spec.ts
+++ libs/dal/src/lib/persons/persons.service.spec.ts
@@ -2,6 +2,7 @@ import { inject, TestBed } from '@angular/core/testing';
 import { PersonApi } from '@diekeure/polpo-api-angular-sdk';
 import { hot } from '@nrwl/nx/testing';
 import { PersonFixture } from '../+fixtures';
+import { PersonInterface } from '../+models';
 import {
   PersonService,
   PersonServiceInterface,
@@ -42,5 +43,45 @@ describe('PersonsService', () => {
         a: [new PersonFixture()]
       })
     );
+  });
+
+  describe('updateProfile', () => {
+    let personApi: PersonApi;
+    let mockData;
+
+    beforeEach(() => {
+      personApi = TestBed.get(PersonApi);
+      mockData = {
+        userId: 34,
+        changedUserData: { firstName: 'foo' } as Partial<PersonInterface>
+      };
+    });
+
+    it('should call the api to update a user', () => {
+      const mockResponse = hot('a', { a: true });
+      personApi.patchAttributes = jest.fn().mockReturnValue(mockResponse);
+
+      const response = service.updateProfile(
+        mockData.userId,
+        mockData.changedUserData
+      );
+
+      expect(response).toBeObservable(mockResponse);
+      expect(personApi.patchAttributes).toHaveBeenCalledTimes(1);
+      expect(personApi.patchAttributes).toHaveBeenCalledWith(
+        mockData.userId,
+        mockData.changedUserData
+      );
+    });
+
+    it('should throw an error when the api does', () => {
+      personApi.patchAttributes = jest
+        .fn()
+        .mockRejectedValue(new Error('this is an error'));
+
+      expect(() =>
+        service.updateProfile(mockData.userId, mockData.changedUserData)
+      ).toThrow();
+    });
   });
 });
