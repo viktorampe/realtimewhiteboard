3302ffd77d1d232dd917ab9b90469bd46530d9c9
david.boute <david.boute@diekeure.be>
2019-01-03 15:17:02 +0100

    add method to service

--- libs/dal/src/lib/persons/persons.service.ts
+++ libs/dal/src/lib/persons/persons.service.ts
@@ -1,13 +1,17 @@
 import { Injectable, InjectionToken } from '@angular/core';
 import { PersonApi } from '@diekeure/polpo-api-angular-sdk';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { PersonInterface } from '../+models';
 
 export const PERSON_SERVICE_TOKEN = new InjectionToken('PersonService');
 
 export interface PersonServiceInterface {
   getAllForUser(userId: number): Observable<PersonInterface[]>;
+  updateProfile(
+    userId: number,
+    updatedProfile: Partial<PersonInterface>
+  ): Observable<boolean>;
 }
 
 @Injectable({
@@ -20,5 +24,15 @@ export class PersonService implements PersonServiceInterface {
     return this.personApi
       .getData(userId, 'persons')
       .pipe(map((res: { persons: PersonInterface[] }) => res.persons));
+  }
+
+  updateProfile(
+    userId: number,
+    profileChanges: Partial<PersonInterface>
+  ): Observable<boolean> {
+    return this.personApi.patchAttributes(userId, profileChanges).pipe(
+      map(_ => true),
+      catchError(error => throwError(error))
+    );
   }
 }
