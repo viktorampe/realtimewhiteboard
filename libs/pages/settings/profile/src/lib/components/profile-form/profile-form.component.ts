4a6315e23b8ff8fe15f1c981b18b6764debf1e6e
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2019-01-04 10:33:04 +0100

    add basic  form functionality

--- libs/pages/settings/profile/src/lib/components/profile-form/profile-form.component.ts
+++ libs/pages/settings/profile/src/lib/components/profile-form/profile-form.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators
+} from '@angular/forms';
+import { PersonFixture, PersonInterface } from '@campus/dal';
 
 @Component({
   selector: 'campus-profile-form',
@@ -6,10 +15,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-form.component.scss']
 })
 export class ProfileFormComponent implements OnInit {
+  profileForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  get password(): FormControl {
+    return this.profileForm.get('password') as FormControl;
   }
 
+  @Input() user: PersonInterface;
+  @Output() saveProfile = new EventEmitter<PersonInterface>();
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.user = new PersonFixture({ username: 'FooBar' }); //TODO: remove
+
+    this.buildForm();
+  }
+
+  buildForm() {
+    this.profileForm = this.fb.group(
+      {
+        lastName: [this.user.name, Validators.required],
+        firstName: [this.user.firstName, Validators.required],
+        username: [this.user.username, Validators.required],
+        email: [this.user.email, [Validators.required, Validators.email]],
+        password: [''],
+        repeatPassword: ['']
+      },
+      { validators: passwordMatchValidator }
+    );
+  }
+
+  onSubmit() {}
+
+  onReset() {}
 }
+
+export const passwordMatchValidator: ValidatorFn = (
+  control: FormGroup
+): ValidationErrors | null => {
+  const password = control.get('password');
+  const repeatPassword = control.get('repeatPassword');
+
+  return password && repeatPassword && password.value !== repeatPassword.value
+    ? { noPasswordMatch: true }
+    : null;
+};
--- libs/pages/settings/profile/src/lib/pages-settings-profile.module.ts
+++ libs/pages/settings/profile/src/lib/pages-settings-profile.module.ts
@@ -1,15 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule, MatInputModule } from '@angular/material';
 import { RouterModule } from '@angular/router';
 import { ProfileFormComponent } from './components/profile-form/profile-form.component';
 
 @NgModule({
   imports: [
     CommonModule,
-
+    ReactiveFormsModule,
     RouterModule.forChild([
-      /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
-    ])
+      { path: '', pathMatch: 'full', component: ProfileFormComponent }
+    ]),
+    MatFormFieldModule,
+    MatInputModule
   ],
   declarations: [ProfileFormComponent]
 })
