c6f66b6735391af12179fd66fdb454b29c7f537a
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2019-01-04 12:46:37 +0100

    add synchronous validators + error messages

--- libs/pages/settings/profile/src/lib/components/profile-form/profile-form.component.ts
+++ libs/pages/settings/profile/src/lib/components/profile-form/profile-form.component.ts
@@ -3,11 +3,34 @@ import {
   FormBuilder,
   FormControl,
   FormGroup,
+  FormGroupDirective,
+  NgForm,
   ValidationErrors,
   ValidatorFn,
   Validators
 } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material';
 import { PersonFixture, PersonInterface } from '@campus/dal';
+
+const passwordMatchValidator: ValidatorFn = (
+  form: FormGroup
+): ValidationErrors | null => {
+  const password = form.get('password');
+  const verifyPassword = form.get('verifyPassword');
+
+  return password && verifyPassword && password.value !== verifyPassword.value
+    ? { noPasswordMatch: true }
+    : null;
+};
+
+class CrossFieldErrorMatcher implements ErrorStateMatcher {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
+    return control.dirty && form.invalid;
+  }
+}
 
 @Component({
   selector: 'campus-profile-form',
@@ -16,11 +39,27 @@ import { PersonFixture, PersonInterface } from '@campus/dal';
 })
 export class ProfileFormComponent implements OnInit {
   profileForm: FormGroup;
+  errorMatcher = new CrossFieldErrorMatcher();
+
+  get lastName(): FormControl {
+    return this.profileForm.get('lastName') as FormControl;
+  }
+  get firstName(): FormControl {
+    return this.profileForm.get('firstName') as FormControl;
+  }
+  get username(): FormControl {
+    return this.profileForm.get('username') as FormControl;
+  }
+  get email(): FormControl {
+    return this.profileForm.get('email') as FormControl;
+  }
 
   get password(): FormControl {
     return this.profileForm.get('password') as FormControl;
   }
-
+  get verifyPassword(): FormControl {
+    return this.profileForm.get('verifyPassword') as FormControl;
+  }
   @Input() user: PersonInterface;
   @Output() saveProfile = new EventEmitter<PersonInterface>();
 
@@ -32,7 +71,7 @@ export class ProfileFormComponent implements OnInit {
     this.buildForm();
   }
 
-  buildForm() {
+  private buildForm() {
     this.profileForm = this.fb.group(
       {
         lastName: [this.user.name, Validators.required],
@@ -40,24 +79,17 @@ export class ProfileFormComponent implements OnInit {
         username: [this.user.username, Validators.required],
         email: [this.user.email, [Validators.required, Validators.email]],
         password: [''],
-        repeatPassword: ['']
+        verifyPassword: ['']
       },
       { validators: passwordMatchValidator }
     );
+  }
+
+  getControl(name: string): FormControl {
+    return this.profileForm.get(name) as FormControl;
   }
 
   onSubmit() {}
 
   onReset() {}
 }
-
-export const passwordMatchValidator: ValidatorFn = (
-  control: FormGroup
-): ValidationErrors | null => {
-  const password = control.get('password');
-  const repeatPassword = control.get('repeatPassword');
-
-  return password && repeatPassword && password.value !== repeatPassword.value
-    ? { noPasswordMatch: true }
-    : null;
-};
