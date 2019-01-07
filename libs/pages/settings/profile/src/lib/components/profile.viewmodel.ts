dd9d5e5009345f8e0a6f3580026b5d9f4bc30df4
david.boute <david.boute@diekeure.be>
2019-01-07 10:19:07 +0100

    update message selector

--- libs/pages/settings/profile/src/lib/components/profile.viewmodel.ts
+++ libs/pages/settings/profile/src/lib/components/profile.viewmodel.ts
@@ -1,16 +1,12 @@
 import { Injectable } from '@angular/core';
 import {
-  DalActions,
   DalState,
   PersonInterface,
   UserActions,
   UserQueries
 } from '@campus/dal';
-import { Actions, ofType } from '@ngrx/effects';
-import { Action, select, Store } from '@ngrx/store';
-import { UNDO_ACTION } from 'ngrx-undo';
-import { merge, Observable } from 'rxjs';
-import { filter, mapTo } from 'rxjs/operators';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -19,7 +15,7 @@ export class ProfileViewModel {
   public currentUser$: Observable<Partial<PersonInterface>>;
   public messages$: Observable<string>;
 
-  constructor(private store: Store<DalState>, private actions$: Actions) {
+  constructor(private store: Store<DalState>) {
     this.setPresentationStreams();
   }
 
@@ -34,30 +30,6 @@ export class ProfileViewModel {
 
   private setPresentationStreams(): void {
     this.currentUser$ = this.store.pipe(select(UserQueries.getCurrentUser));
-    this.messages$ = this.getMessageStream();
-  }
-
-  // TODO: dit zou beter ergens ergens in een selector of zo staan
-  // maar het is nu vrijdagmiddag en ik heb geen goesting meer
-  private getMessageStream(): Observable<string> {
-    const successMessage$ = this.actions$.pipe(
-      ofType(DalActions.DalActionTypes.ActionSuccessful),
-      filter(
-        (action: DalActions.ActionSuccessful) =>
-          action.payload === { successfulAction: 'User updated' }
-      ),
-      mapTo('Je profiel is aangepast.')
-    );
-
-    const failureMessage$ = this.actions$.pipe(
-      ofType(UNDO_ACTION),
-      filter(
-        (undoAction: { payload: Action }) =>
-          undoAction.payload.type === UserActions.UserActionTypes.UpdateUser
-      ),
-      mapTo('Je profiel kon niet aangepast worden.')
-    );
-
-    return merge(successMessage$, failureMessage$);
+    this.messages$ = this.store.pipe(select(UserQueries.getUpdateMessage));
   }
 }
