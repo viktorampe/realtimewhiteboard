71fcc052bcf0377af5e9491bf6fe9e007f10bfe6
Thomas Ghesquiere <thomas.ghesquiere@gmail.com>
2019-01-08 11:22:15 +0100

    CR: extend hasPermission service

--- libs/shared/src/lib/auth/permission.service.spec.ts
+++ libs/shared/src/lib/auth/permission.service.spec.ts
@@ -44,15 +44,37 @@ describe('PermissionService', () => {
     );
   });
 
-  it('should return true if permission is found in array', () => {
+  it('should return true if all permissions are met in the array', () => {
     expect(
-      service.hasPermission(['permission-x', 'permission-a'])
+      service.hasPermission(['permission-b', 'permission-a'])
     ).toBeObservable(hot('a', { a: true }));
   });
 
-  it('should return false if permission is not found in array', () => {
+  it('should return false if not all permissions are met in the array', () => {
     expect(
-      service.hasPermission(['permission-x', 'permission-y'])
+      service.hasPermission(['permission-x', 'permission-a'])
+    ).toBeObservable(hot('a', { a: false }));
+  });
+
+  it('should return true if at least one permission is found in an array of permissions', () => {
+    expect(
+      service.hasPermission([['permission-x', 'permission-a']])
+    ).toBeObservable(hot('a', { a: true }));
+  });
+
+  it('should return true if all conditions are met for combination of string and arrays', () => {
+    expect(
+      service.hasPermission([['permission-x', 'permission-a'], 'permission-b'])
+    ).toBeObservable(hot('a', { a: true }));
+  });
+
+  it('should return false if not all conditions are met for combination of string and arrays', () => {
+    expect(
+      service.hasPermission([['permission-x', 'permission-a'], 'permission-y'])
+    ).toBeObservable(hot('a', { a: false }));
+
+    expect(
+      service.hasPermission([['permission-x', 'permission-y'], 'permission-b'])
     ).toBeObservable(hot('a', { a: false }));
   });
 });
