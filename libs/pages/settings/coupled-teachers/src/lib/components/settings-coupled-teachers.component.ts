9d41061d08a510c299a0baec0cee246f81226e91
mkellner <melvin.kellner@diekeure.be>
2019-01-09 13:09:51 +0100

    setup

---
+++ libs/pages/settings/coupled-teachers/src/lib/components/settings-coupled-teachers.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'campus-settings-coupled-teachers',
+  templateUrl: './settings-coupled-teachers.component.html',
+  styleUrls: ['./settings-coupled-teachers.component.scss']
+})
+export class SettingsCoupledTeachersComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
