8dc9509ea0bf9fa873e23c08640eed8624c8a439
david.boute <david.boute@diekeure.be>
2019-01-09 17:16:32 +0100

    Unit tests

--- libs/dal/src/lib/persons/credentials.service.spec.ts
+++ libs/dal/src/lib/persons/credentials.service.spec.ts
@@ -1,11 +1,41 @@
-import { TestBed } from '@angular/core/testing';
-import { CredentialService } from './credentials.service';
+import { inject, TestBed } from '@angular/core/testing';
+import { PersonApi } from '@diekeure/polpo-api-angular-sdk';
+import { cold } from 'jasmine-marbles';
+import { CredentialFixture } from './../+fixtures/Credential.fixture';
+import {
+  CredentialService,
+  CREDENTIAL_SERVICE_TOKEN
+} from './credentials.service';
 
 describe('CredentialsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: CREDENTIAL_SERVICE_TOKEN, useClass: CredentialService },
+        { provide: PersonApi, useValue: {} }
+      ]
+    })
+  );
 
-  it('should be created', () => {
-    const service: CredentialService = TestBed.get(CredentialService);
-    expect(service).toBeTruthy();
+  it('should be created', inject(
+    [CREDENTIAL_SERVICE_TOKEN],
+    (srv: CredentialService) => {
+      expect(srv).toBeTruthy();
+    }
+  ));
+
+  it('should return credentials', async () => {
+    const mockFixture = new CredentialFixture();
+
+    const credentialService = TestBed.get(CREDENTIAL_SERVICE_TOKEN);
+    const mockresponse$ = cold('-a-|', { a: [mockFixture, mockFixture] });
+
+    credentialService.getAllForUser = jest.fn().mockReturnValue(mockresponse$);
+
+    expect(credentialService.getAllForUser(1)).toBeObservable(
+      cold('-a-|', {
+        a: [mockFixture, mockFixture]
+      })
+    );
   });
 });
