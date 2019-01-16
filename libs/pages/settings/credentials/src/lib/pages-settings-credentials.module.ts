aeac3241771259c0febfe296e0e37d8e02e7d3b9
david.boute <david.boute@diekeure.be>
2019-01-16 14:52:30 +0100

    add resolver to router path

--- libs/pages/settings/credentials/src/lib/pages-settings-credentials.module.ts
+++ libs/pages/settings/credentials/src/lib/pages-settings-credentials.module.ts
@@ -1,13 +1,18 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CredentialsResolver } from './components/credentials.resolver';
 
 @NgModule({
   imports: [
     CommonModule,
-
     RouterModule.forChild([
-      /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
+      {
+        path: '',
+        pathMatch: 'full',
+        component: null, //TODO CredentialsComponent
+        resolve: { isResolved: CredentialsResolver }
+      }
     ])
   ]
 })
