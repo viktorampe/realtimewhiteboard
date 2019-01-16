69bd0170ebbec7c6be594d7b40e9659d5c082898
pverhelle <pieter.verhelle@gmail.com>
2019-01-16 10:27:44 +0100

    update info panel to allow optional incon

--- libs/ui/src/lib/info-panel/info-panel.component.spec.ts
+++ libs/ui/src/lib/info-panel/info-panel.component.spec.ts
@@ -1,3 +1,5 @@
+//file.only
+
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
@@ -30,9 +32,26 @@ describe('InfoPanelComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  it('should show the title', () => {
-    const title = fixture.debugElement.query(By.css('.info-panel__title'))
+  it('should show the title text', () => {
+    const title = fixture.debugElement.query(By.css('.info-panel__title__text'))
       .nativeElement.textContent;
     expect(title).toBe(mockData.titleText);
+  });
+  it('should show the standard Info title text if no title was given', () => {
+    component.titleText = null;
+    fixture.detectChanges();
+    const title = fixture.debugElement.query(By.css('.info-panel__title__text'))
+      .nativeElement.textContent;
+    expect(title).toBe('Info');
+  });
+  it('should not show the title icon if none was given', () => {
+    const icon = fixture.debugElement.query(By.css('.info-panel__title__icon'));
+    expect(icon).toBeFalsy();
+  });
+  it('should show the title icon if one was given', () => {
+    component.icon = 'polpo';
+    fixture.detectChanges();
+    const icon = fixture.debugElement.query(By.css('.info-panel__title__icon'));
+    expect(icon).toBeTruthy();
   });
 });
