3b9964e9690741c8bcf99487b279f22e1b79429f
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2019-01-22 13:44:08 +0100

    basic functionality

--- libs/pages/settings/alerts/src/lib/components/alerts/alerts.component.ts
+++ libs/pages/settings/alerts/src/lib/components/alerts/alerts.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertQueueInterface } from '@campus/dal';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'campus-alerts',
@@ -6,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alerts.component.scss']
 })
 export class AlertsComponent implements OnInit {
+  pageTitle = 'Meldingen';
+  pageIcon = 'notifications';
+
+  alerts$: Observable<AlertQueueInterface[]>;
+
   constructor() {}
 
   ngOnInit() {}
+
+  markAsRead(alertId: number) {}
+
+  deleteAlert(alertId: number) {}
 }
--- libs/pages/settings/alerts/src/lib/pages-settings-alerts.module.ts
+++ libs/pages/settings/alerts/src/lib/pages-settings-alerts.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { UiModule } from '@campus/ui';
 import { AlertsComponent } from './components/alerts/alerts.component';
 
 @NgModule({
   imports: [
     CommonModule,
-
+    UiModule,
     RouterModule.forChild([
       { path: '', pathMatch: 'full', component: AlertsComponent }
     ])
