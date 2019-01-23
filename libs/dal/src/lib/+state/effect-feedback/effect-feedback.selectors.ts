40b9c80aae0cce8144cb207c8e9e2cce2c24c64b
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2019-01-23 14:10:25 +0100

    add selectors

---
+++ libs/dal/src/lib/+state/effect-feedback/effect-feedback.selectors.ts
@@ -0,0 +1,12 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { EffectFeedbackInterface } from './effect-feedback.model';
+import { NAME, selectAll, State } from './effect-feedback.reducer';
+
+export const selectEffectFeedbackState = createFeatureSelector<State>(NAME);
+
+export const getNext = createSelector(
+  selectAll,
+  (effectFeedback: EffectFeedbackInterface[]) => {
+    return effectFeedback[0];
+  }
+);
