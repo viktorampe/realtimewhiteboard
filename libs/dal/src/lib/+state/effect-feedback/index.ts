9699f6a998598d934c373c7d2ee40093fcd5de2d
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2019-01-25 10:32:32 +0100

    export effect feedback model from dal

--- libs/dal/src/lib/+state/effect-feedback/index.ts
+++ libs/dal/src/lib/+state/effect-feedback/index.ts
@@ -1,4 +1,5 @@
 import * as EffectFeedbackActions from './effect-feedback.actions';
 import * as EffectFeedbackReducer from './effect-feedback.reducer';
 import * as EffectFeedbackQueries from './effect-feedback.selectors';
+export * from './effect-feedback.model';
 export { EffectFeedbackActions, EffectFeedbackReducer, EffectFeedbackQueries };
