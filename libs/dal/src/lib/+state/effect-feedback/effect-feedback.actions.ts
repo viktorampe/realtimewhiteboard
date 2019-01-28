89a0e7e4333aab3af36bba9c0d394d6247bb0982
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2019-01-28 16:43:56 +0100

    clean up scaffolded code

--- libs/dal/src/lib/+state/effect-feedback/effect-feedback.actions.ts
+++ libs/dal/src/lib/+state/effect-feedback/effect-feedback.actions.ts
@@ -1,24 +1,9 @@
-import { Update } from '@ngrx/entity';
 import { Action } from '@ngrx/store';
 import { EffectFeedbackInterface } from './effect-feedback.model';
 
 export enum EffectFeedbackActionTypes {
-  LoadEffectFeedbacks = '[EffectFeedback] Load EffectFeedbacks',
   AddEffectFeedback = '[EffectFeedback] Add EffectFeedback',
-  UpsertEffectFeedback = '[EffectFeedback] Upsert EffectFeedback',
-  AddEffectFeedbacks = '[EffectFeedback] Add EffectFeedbacks',
-  UpsertEffectFeedbacks = '[EffectFeedback] Upsert EffectFeedbacks',
-  UpdateEffectFeedback = '[EffectFeedback] Update EffectFeedback',
-  UpdateEffectFeedbacks = '[EffectFeedback] Update EffectFeedbacks',
-  DeleteEffectFeedback = '[EffectFeedback] Delete EffectFeedback',
-  DeleteEffectFeedbacks = '[EffectFeedback] Delete EffectFeedbacks',
-  ClearEffectFeedbacks = '[EffectFeedback] Clear EffectFeedbacks'
-}
-
-export class LoadEffectFeedbacks implements Action {
-  readonly type = EffectFeedbackActionTypes.LoadEffectFeedbacks;
-
-  constructor(public payload: { effectFeedbacks: EffectFeedbackInterface[] }) {}
+  DeleteEffectFeedback = '[EffectFeedback] Delete EffectFeedback'
 }
 
 export class AddEffectFeedback implements Action {
@@ -27,64 +12,10 @@ export class AddEffectFeedback implements Action {
   constructor(public payload: { effectFeedback: EffectFeedbackInterface }) {}
 }
 
-export class UpsertEffectFeedback implements Action {
-  readonly type = EffectFeedbackActionTypes.UpsertEffectFeedback;
-
-  constructor(public payload: { effectFeedback: EffectFeedbackInterface }) {}
-}
-
-export class AddEffectFeedbacks implements Action {
-  readonly type = EffectFeedbackActionTypes.AddEffectFeedbacks;
-
-  constructor(public payload: { effectFeedbacks: EffectFeedbackInterface[] }) {}
-}
-
-export class UpsertEffectFeedbacks implements Action {
-  readonly type = EffectFeedbackActionTypes.UpsertEffectFeedbacks;
-
-  constructor(public payload: { effectFeedbacks: EffectFeedbackInterface[] }) {}
-}
-
-export class UpdateEffectFeedback implements Action {
-  readonly type = EffectFeedbackActionTypes.UpdateEffectFeedback;
-
-  constructor(
-    public payload: { effectFeedback: Update<EffectFeedbackInterface> }
-  ) {}
-}
-
-export class UpdateEffectFeedbacks implements Action {
-  readonly type = EffectFeedbackActionTypes.UpdateEffectFeedbacks;
-
-  constructor(
-    public payload: { effectFeedbacks: Update<EffectFeedbackInterface>[] }
-  ) {}
-}
-
 export class DeleteEffectFeedback implements Action {
   readonly type = EffectFeedbackActionTypes.DeleteEffectFeedback;
 
   constructor(public payload: { id: string }) {}
 }
 
-export class DeleteEffectFeedbacks implements Action {
-  readonly type = EffectFeedbackActionTypes.DeleteEffectFeedbacks;
-
-  constructor(public payload: { ids: string[] }) {}
-}
-
-export class ClearEffectFeedbacks implements Action {
-  readonly type = EffectFeedbackActionTypes.ClearEffectFeedbacks;
-}
-
-export type EffectFeedbackActions =
-  | LoadEffectFeedbacks
-  | AddEffectFeedback
-  | UpsertEffectFeedback
-  | AddEffectFeedbacks
-  | UpsertEffectFeedbacks
-  | UpdateEffectFeedback
-  | UpdateEffectFeedbacks
-  | DeleteEffectFeedback
-  | DeleteEffectFeedbacks
-  | ClearEffectFeedbacks;
+export type EffectFeedbackActions = AddEffectFeedback | DeleteEffectFeedback;
--- libs/dal/src/lib/+state/effect-feedback/effect-feedback.reducer.ts
+++ libs/dal/src/lib/+state/effect-feedback/effect-feedback.reducer.ts
@@ -36,40 +36,8 @@ export function reducer(
       return adapter.addOne(action.payload.effectFeedback, state);
     }
 
-    case EffectFeedbackActionTypes.UpsertEffectFeedback: {
-      return adapter.upsertOne(action.payload.effectFeedback, state);
-    }
-
-    case EffectFeedbackActionTypes.AddEffectFeedbacks: {
-      return adapter.addMany(action.payload.effectFeedbacks, state);
-    }
-
-    case EffectFeedbackActionTypes.UpsertEffectFeedbacks: {
-      return adapter.upsertMany(action.payload.effectFeedbacks, state);
-    }
-
-    case EffectFeedbackActionTypes.UpdateEffectFeedback: {
-      return adapter.updateOne(action.payload.effectFeedback, state);
-    }
-
-    case EffectFeedbackActionTypes.UpdateEffectFeedbacks: {
-      return adapter.updateMany(action.payload.effectFeedbacks, state);
-    }
-
     case EffectFeedbackActionTypes.DeleteEffectFeedback: {
       return adapter.removeOne(action.payload.id, state);
-    }
-
-    case EffectFeedbackActionTypes.DeleteEffectFeedbacks: {
-      return adapter.removeMany(action.payload.ids, state);
-    }
-
-    case EffectFeedbackActionTypes.LoadEffectFeedbacks: {
-      return adapter.addAll(action.payload.effectFeedbacks, state);
-    }
-
-    case EffectFeedbackActionTypes.ClearEffectFeedbacks: {
-      return adapter.removeAll(state);
     }
 
     default: {
