3e6adede22bcb659e0558a9b16fa4c70e745f175
pverhelle <pieter.verhelle@gmail.com>
2019-01-29 09:46:29 +0100

    add optional marker

--- libs/ui/src/lib/info-panel/info-panel.component.ts
+++ libs/ui/src/lib/info-panel/info-panel.component.ts
@@ -8,5 +8,5 @@ import { Component, Input } from '@angular/core';
 export class InfoPanelComponent {
   @Input() titleText: string;
   @Input() titleIcon?: string;
-  @Input() titleRightRouterLink: string | any[];
+  @Input() titleRightRouterLink?: string | any[];
 }
