a00a5d5bc7ba985097243615924edd56f134714e
DavidBoute <david.boute@gmail.com>
2019-01-30 12:01:56 +0100

    register directives

--- libs/utils/src/lib/utils.module.ts
+++ libs/utils/src/lib/utils.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ClickStopPropagationDirective } from './directives/click-stop-propagation.directive';
+import { ClickPreventDefaultDirective } from './directives/click-prevent-default.directive';
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  declarations: [ClickStopPropagationDirective, ClickPreventDefaultDirective],
+  exports: [ClickStopPropagationDirective, ClickPreventDefaultDirective]
 })
 export class UtilsModule {}
