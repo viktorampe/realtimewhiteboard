116d54bcff9700c00494a32a780b3ba1e0b430f7
Thomas Ghesquiere <thomas.ghesquiere@gmail.com>
2019-02-01 11:03:18 +0100

    Update libs/shared/src/lib/directives/campus-externalable-routerlink.directive.ts
    Co-Authored-By: melvin-kellner <melvin.kellner@diekeure.be>

--- libs/shared/src/lib/directives/campus-externalable-routerlink.directive.ts
+++ libs/shared/src/lib/directives/campus-externalable-routerlink.directive.ts
@@ -27,7 +27,7 @@ export class CampusExternalableRouterlinkDirective {
   getExternalLink(linkPath: string): string {
     const rxp = /([a-z][a-z0-9+.-]*):(?:\/\/((?:(?=((?:[a-z0-9-._~!$&'()*+,;=:]|%[0-9A-F]{2})*))(\3)@)?(?=(\[[0-9A-F:.]{2,}\]|(?:[a-z0-9-._~!$&'()*+,;=]|%[0-9A-F]{2})*))\5(?::(?=(\d*))\6)?)(\/(?=((?:[a-z0-9-._~!$&'()*+,;=:@\/]|%[0-9A-F]{2})*))\8)?|(\/?(?!\/)(?=((?:[a-z0-9-._~!$&'()*+,;=:@\/]|%[0-9A-F]{2})*))\10)?)(?:\?(?=((?:[a-z0-9-._~!$&'()*+,;=:@\/?]|%[0-9A-F]{2})*))\11)?(?:#(?=((?:[a-z0-9-._~!$&'()*+,;=:@\/?]|%[0-9A-F]{2})*))\12)?/i;
     const matches = linkPath.match(rxp);
-    if (matches && matches.length > 0) {
+    if (matches) {
       if (matches[0]) {
         const path = matches[0];
         // path should be an external url so we open it in a new tab.
