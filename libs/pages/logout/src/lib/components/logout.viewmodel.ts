7dcc622da260db654366bd28537dd51401b3f575
pverhelle <pieter.verhelle@gmail.com>
2019-02-06 11:50:42 +0100

    use window service to get dom window

--- libs/pages/logout/src/lib/components/logout.viewmodel.ts
+++ libs/pages/logout/src/lib/components/logout.viewmodel.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable } from '@angular/core';
+import { WindowServiceInterface, WINDOW_SERVICE_TOKEN } from '@campus/browser';
 import {
   DalState,
   PersonInterface,
@@ -22,7 +23,8 @@ export class LogoutViewModel {
   constructor(
     private store: Store<DalState>,
     @Inject(ENVIRONMENT_LOGOUT_TOKEN)
-    public environmentLogout: EnvironmentLogoutInterface
+    public environmentLogout: EnvironmentLogoutInterface,
+    @Inject(WINDOW_SERVICE_TOKEN) private windowService: WindowServiceInterface
   ) {}
 
   logout(): void {
@@ -33,7 +35,7 @@ export class LogoutViewModel {
       take(1)
     );
     this.currentNullUser.subscribe(() =>
-      window.location.assign(this.environmentLogout.url)
+      this.windowService.window.location.assign(this.environmentLogout.url)
     );
   }
 }
