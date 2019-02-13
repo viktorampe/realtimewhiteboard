93458b1956739830e9fbd55cf926ed6aed351bbd
pverhelle <pieter.verhelle@gmail.com>
2019-02-13 09:42:02 +0100

    scaffold component for search devving

---
+++ libs/devlib/src/lib/finding-nemo/finding-nemo.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'campus-finding-nemo',
+  templateUrl: './finding-nemo.component.html',
+  styleUrls: ['./finding-nemo.component.scss']
+})
+export class FindingNemoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
