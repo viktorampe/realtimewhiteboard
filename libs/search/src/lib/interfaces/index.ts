21552b7d6b5624910b7e32124a9524351e902c26
mkellner <melvin.kellner@diekeure.be>
2019-02-13 11:10:16 +0100

    renamed file, added to export index

--- libs/search/src/lib/interfaces/index.ts
+++ libs/search/src/lib/interfaces/index.ts
@@ -1,3 +1,8 @@
 import * as KeyWithPropertyType from './keyWithPropertyType.type';
+import * as SearchFilterCriteriaInterface from './searchFilterCriteria.interface';
 import * as SearchStateInterface from './searchState.interface';
-export { KeyWithPropertyType, SearchStateInterface };
+export {
+  KeyWithPropertyType,
+  SearchStateInterface,
+  SearchFilterCriteriaInterface
+};
