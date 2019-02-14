b7ec2aa3813a7bbf745ad8b9b6c874489d322e91
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2019-02-14 13:31:06 +0100

    WIP implement searchFilterCriteria interface

--- libs/search/src/lib/components/breadcrumb-filter/breadcrumb-filter.component.ts
+++ libs/search/src/lib/components/breadcrumb-filter/breadcrumb-filter.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { SearchFilterComponentInterface } from '../../interfaces/search-filter-component-interface';
+import { SearchFilterCriteriaInterface } from '../../interfaces/search-filter-criteria.interface';
 
 @Component({
   selector: 'campus-breadcrumb-filter',
   templateUrl: './breadcrumb-filter.component.html',
   styleUrls: ['./breadcrumb-filter.component.scss']
 })
-export class BreadcrumbFilterComponent implements OnInit {
+export class BreadcrumbFilterComponent
+  implements SearchFilterComponentInterface<unknown, unknown> {
+  @Input() filterCriteria: SearchFilterCriteriaInterface<unknown, unknown>;
+  @Output() filterSelectionChange = new EventEmitter<
+    | SearchFilterCriteriaInterface<unknown, unknown>
+    | SearchFilterCriteriaInterface<unknown, unknown>[]
+  >();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  clickBreadcrumb() {
+    this.filterSelectionChange.emit();
   }
-
 }
