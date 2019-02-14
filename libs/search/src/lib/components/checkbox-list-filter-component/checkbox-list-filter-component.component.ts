bf067825fe0f1b7a363a0614563bf0b698477206
DavidBoute <david.boute@gmail.com>
2019-02-14 11:49:51 +0100

    wip

--- libs/search/src/lib/components/checkbox-list-filter-component/checkbox-list-filter-component.component.ts
+++ libs/search/src/lib/components/checkbox-list-filter-component/checkbox-list-filter-component.component.ts
@@ -1,9 +1,11 @@
 import {
+  AfterContentInit,
   Component,
+  ContentChildren,
   HostBinding,
   Input,
   OnInit,
-  ViewChildren
+  QueryList
 } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
@@ -12,17 +14,22 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   templateUrl: './checkbox-list-filter-component.component.html',
   styleUrls: ['./checkbox-list-filter-component.component.scss']
 })
-export class CheckboxListFilterComponentComponent implements OnInit {
+export class CheckboxListFilterComponentComponent
+  implements OnInit, AfterContentInit {
   @Input() value: string;
   form: FormGroup;
 
-  @ViewChildren() childFilterComponents: any[];
+  // @ViewChildren(CheckboxListFilterComponentComponent)
+  @ContentChildren(CheckboxListFilterComponentComponent)
+  childFilterComponents: QueryList<CheckboxListFilterComponentComponent>;
 
-  constructor(private formBuilder: FormBuilder) {
-    this.form = this.formBuilder.group({});
-  }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {}
+
+  ngAfterContentInit() {
+    this.childFilterComponents.forEach(cfc => console.log(cfc));
+  }
 
   @HostBinding('class.checkbox-filter')
   get isCheckBoxFilterClass() {
