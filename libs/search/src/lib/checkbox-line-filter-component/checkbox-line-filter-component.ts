98db59b6d9256843fc59a5a5cfb6e74691bc4811
mkellner <melvin.kellner@diekeure.be>
2019-02-14 16:03:12 +0100

    implemented correct interfaces and shizzle

--- libs/search/src/lib/checkbox-line-filter-component/checkbox-line-filter-component.ts
+++ libs/search/src/lib/checkbox-line-filter-component/checkbox-line-filter-component.ts
@@ -1,14 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { SearchFilterComponentInterface } from '../interfaces/search-filter-component-interface';
+import { SearchFilterCriteriaInterface } from '../interfaces/search-filter-criteria.interface';
+
+class TestData {
+  data = 'test';
+  selected = true;
+  prediction = 1;
+  visible = true;
+}
 
 @Component({
   selector: 'campus-checkbox-line-filter',
   templateUrl: './checkbox-line-filter-component.html',
   styleUrls: ['./checkbox-line-filter-component.scss']
 })
-export class CheckboxLineFilterComponent implements OnInit {
-  items = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+export class CheckboxLineFilterComponent
+  implements OnInit, SearchFilterComponentInterface<any, any> {
+  @Input() filterCriteria: SearchFilterCriteriaInterface<any, any>;
+  @Output() filterSelectionChange = new EventEmitter<
+    | SearchFilterCriteriaInterface<any, any>
+    | SearchFilterCriteriaInterface<any, any>[]
+  >();
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.filterCriteria = {
+      name: 'selectFilter',
+      label: 'select filter',
+      keyProperty: 'id',
+      displayProperty: 'name',
+      values: [
+        {
+          data: {
+            id: 1,
+            name: 'foo'
+          },
+          selected: false,
+          prediction: 0,
+          visible: true,
+          children: null
+        },
+        {
+          data: {
+            id: 2,
+            name: 'bar'
+          },
+          selected: false,
+          prediction: 0,
+          visible: true,
+          children: null
+        }
+      ]
+    };
+  }
+
+  itemChanged(value: SearchFilterCriteriaInterface<unknown, unknown>) {
+    this.filterSelectionChange.emit(value);
+  }
 }
