13d6e0e64cc847607d00acdd7d25b7936507065d
pverhelle <pieter.verhelle@gmail.com>
2019-02-18 13:34:00 +0100

    add stub to test campus column filter component

--- libs/devlib/src/lib/finding-nemo/finding-nemo.component.ts
+++ libs/devlib/src/lib/finding-nemo/finding-nemo.component.ts
@@ -1,62 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import { CredentialFixture, LearningAreaFixture } from '@campus/dal';
+import { Component } from '@angular/core';
 import { SearchFilterCriteriaInterface } from '@campus/search';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'campus-finding-nemo',
   templateUrl: './finding-nemo.component.html',
   styleUrls: ['./finding-nemo.component.scss']
 })
-export class FindingNemoComponent implements OnInit {
-  selectFilter: SearchFilterCriteriaInterface;
+export class FindingNemoComponent {
+  filterCriteria$ = new BehaviorSubject<SearchFilterCriteriaInterface[]>(null);
 
   constructor() {}
 
-  ngOnInit() {
-    this.selectFilter = {
-      name: 'selectFilter',
-      label: 'select filter',
-      keyProperty: 'id',
-      displayProperty: 'name',
-      values: [
-        {
-          data: new LearningAreaFixture({
-            id: 1,
-            name: 'foo'
-          }),
-          selected: false,
-          prediction: 0,
-          visible: true,
-          child: {
-            name: 'selectFilter',
-            label: 'select filter',
-            keyProperty: 'id',
-            displayProperty: 'provider',
-            values: [
-              {
-                data: new CredentialFixture(),
-                selected: false,
-                prediction: 0,
-                visible: true
-              }
-            ]
+  setMockData() {
+    this.filterCriteria$.next([
+      {
+        name: 'criteria name',
+        label: 'The label of the criteria',
+        keyProperty: 'id',
+        displayProperty: 'name',
+        values: [
+          {
+            data: {
+              id: 1,
+              name: 'foo jaar'
+            },
+            selected: true
+          },
+          {
+            data: {
+              id: 2,
+              name: 'bar jaar'
+            },
+            selected: false
+          },
+          {
+            data: {
+              id: 3,
+              name: 'baz jaar'
+            },
+            selected: false
           }
-        },
-        {
-          data: new LearningAreaFixture({
-            id: 2,
-            name: 'bar'
-          }),
-          selected: false,
-          prediction: 0,
-          visible: true,
-          child: null
-        }
-      ]
-    };
+        ]
+      }
+    ]);
   }
 
-  onFilterSelectionChange(searchFilter: SearchFilterCriteriaInterface) {
-    console.log(searchFilter);
+  catchEvent($event) {
+    console.log($event);
   }
 }
