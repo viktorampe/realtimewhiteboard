fd4b7dad38d6aab98da86bbd048f933bd12ebe1f
pverhelle <pieter.verhelle@gmail.com>
2019-02-18 16:53:54 +0100

    add some dummy data mecanics for testing

--- libs/devlib/src/lib/finding-nemo/finding-nemo.component.ts
+++ libs/devlib/src/lib/finding-nemo/finding-nemo.component.ts
@@ -12,70 +12,45 @@ export class FindingNemoComponent {
 
   constructor() {}
 
+  mockData: SearchFilterCriteriaInterface[] = [
+    {
+      name: 'criteria name',
+      label: 'The label of the criteria',
+      keyProperty: 'id',
+      displayProperty: 'name',
+      values: [
+        {
+          data: {
+            id: 1,
+            name: 'foo jaar'
+          },
+          selected: false
+        },
+        {
+          data: {
+            id: 2,
+            name: 'bar jaar'
+          },
+          selected: false
+        },
+        {
+          data: {
+            id: 3,
+            name: 'baz jaar'
+          },
+          selected: false,
+          prediction: 3
+        }
+      ]
+    }
+  ];
+
   setMockData() {
-    this.filterCriteria$.next([
-      {
-        name: 'criteria name',
-        label: 'The label of the criteria',
-        keyProperty: 'id',
-        displayProperty: 'name',
-        values: [
-          {
-            data: {
-              id: 1,
-              name: 'foo jaar'
-            },
-            selected: false
-          },
-          {
-            data: {
-              id: 2,
-              name: 'bar jaar'
-            },
-            selected: false
-          },
-          {
-            data: {
-              id: 3,
-              name: 'baz jaar'
-            },
-            selected: false,
-            child: {
-              name: 'sub criteria name',
-              label: 'The sub label of the criteria',
-              keyProperty: 'id',
-              displayProperty: 'name',
-              values: [
-                {
-                  data: {
-                    id: 1,
-                    name: 'foo jaar'
-                  },
-                  selected: false
-                },
-                {
-                  data: {
-                    id: 2,
-                    name: 'bar jaar'
-                  },
-                  selected: false
-                },
-                {
-                  data: {
-                    id: 3,
-                    name: 'baz jaar'
-                  },
-                  selected: false
-                }
-              ]
-            }
-          }
-        ]
-      }
-    ]);
+    this.filterCriteria$.next(this.mockData);
   }
 
   catchEvent($event: SearchFilterCriteriaInterface[]) {
-    this.filterCriteria$.next($event);
+    console.log($event);
+    this.filterCriteria$.next([...this.mockData, ...$event]);
   }
 }
