8d3643b785bbe72e903b2d1384cf0f7811a66474
pverhelle <pieter.verhelle@gmail.com>
2019-02-19 12:10:09 +0100

    wrap the list in a mat action list

--- libs/search/src/lib/search.module.ts
+++ libs/search/src/lib/search.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatListModule } from '@angular/material';
 import { ColumnFilterComponent } from './components/column-filter.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MatListModule],
   declarations: [ColumnFilterComponent],
   exports: [ColumnFilterComponent]
 })
