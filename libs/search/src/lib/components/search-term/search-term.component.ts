53652b4a5b7835af61cdd820e725965539c2bc5e
DavidBoute <david.boute@gmail.com>
2019-02-19 12:53:14 +0100

    scaffold

---
+++ libs/search/src/lib/components/search-term/search-term.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'campus-search-term',
+  templateUrl: './search-term.component.html',
+  styleUrls: ['./search-term.component.scss']
+})
+export class SearchTermComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- libs/search/src/lib/search.module.ts
+++ libs/search/src/lib/search.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { SearchTermComponent } from './components/search-term/search-term.component';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  declarations: [SearchTermComponent],
+  exports: [SearchTermComponent]
 })
 export class SearchModule {}
