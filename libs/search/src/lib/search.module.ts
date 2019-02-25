82ab2bdf29481eebdcf6b3a47a2fdb1dcb562708
pverhelle <pieter.verhelle@gmail.com>
2019-02-25 09:34:11 +0100

    add missing import after merge

--- libs/search/src/lib/search.module.ts
+++ libs/search/src/lib/search.module.ts
@@ -6,10 +6,12 @@ import {
   MatBadgeModule,
   MatCheckboxModule,
   MatInputModule,
+  MatListModule,
   MatSelectModule
 } from '@angular/material';
 import { BreadcrumbFilterComponent } from './components/breadcrumb-filter/breadcrumb-filter.component';
 import { CheckboxLineFilterComponent } from './components/checkbox-line-filter/checkbox-line-filter-component';
+import { ColumnFilterComponent } from './components/column-filter/column-filter.component';
 import { SearchTermComponent } from './components/search-term/search-term.component';
 import { SelectFilterComponent } from './components/select-filter-component/select-filter.component';
 
@@ -21,6 +23,7 @@ import { SelectFilterComponent } from './components/select-filter-component/sele
     ReactiveFormsModule,
     MatBadgeModule,
     MatInputModule,
+    MatListModule,
     MatAutocompleteModule,
     FormsModule
   ],
@@ -28,13 +31,15 @@ import { SelectFilterComponent } from './components/select-filter-component/sele
     BreadcrumbFilterComponent,
     SelectFilterComponent,
     SearchTermComponent,
-    CheckboxLineFilterComponent
+    CheckboxLineFilterComponent,
+    ColumnFilterComponent
   ],
   exports: [
     BreadcrumbFilterComponent,
     SelectFilterComponent,
     SearchTermComponent,
-    CheckboxLineFilterComponent
+    CheckboxLineFilterComponent,
+    ColumnFilterComponent
   ]
 })
 export class SearchModule {}
