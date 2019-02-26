bd0f6b83962f6989493eb1bbcdae866bb814bc0f
pverhelle <pieter.verhelle@gmail.com>
2019-02-26 13:22:27 +0100

    add first try at collapsibility

--- libs/ui/src/lib/collapsible-sheet/collapsible-sheet.component.ts
+++ libs/ui/src/lib/collapsible-sheet/collapsible-sheet.component.ts
@@ -1,12 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'campus-collapsible-sheet',
   templateUrl: './collapsible-sheet.component.html',
-  styleUrls: ['./collapsible-sheet.component.scss']
+  styleUrls: ['./collapsible-sheet.component.scss'],
+  animations: [
+    trigger('collapseExpand', [
+      transition('false => true', [
+        animate('200ms ease-in', style({ width: '15%' }))
+      ]),
+      transition('true => false', [
+        animate('200ms ease-in', style({ width: '40%' }))
+      ]),
+      state('true', style({ width: '15%' })),
+      state('false', style({ width: '40%' }))
+    ]),
+    trigger('rotate', [
+      transition('false => true', [
+        animate('200ms ease-in-out', style({ transform: 'rotate(180deg)' }))
+      ]),
+      transition('true => false', [
+        animate('200ms ease-in', style({ transform: 'rotate(-180deg)' }))
+      ]),
+      state('true', style({ transform: 'rotate(180deg)' })),
+      state('false', style({ transform: 'rotate(0deg)' }))
+    ])
+  ]
 })
-export class CollapsibleSheetComponent implements OnInit {
-  constructor() {}
+export class CollapsibleSheetComponent {
+  collapsed = true;
 
-  ngOnInit(): void {}
+  toggleCollapsed(): void {
+    this.collapsed = !this.collapsed;
+  }
 }
