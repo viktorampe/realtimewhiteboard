aacc5fd4fae3fe775dd1b4af36335a6f4b1d8c92
DavidBoute <david.boute@gmail.com>
2019-02-28 15:44:10 +0100

    mocks

--- libs/search/src/lib/components/search.viewmodel.mock.ts
+++ libs/search/src/lib/components/search.viewmodel.mock.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ViewModelInterface } from '@campus/testing';
-import { Observable } from 'rxjs';
+import { Subject } from 'rxjs';
 import {
   SearchFilterCriteriaInterface,
   SearchFilterInterface,
@@ -9,6 +9,11 @@ import {
   SearchStateInterface,
   SortModeInterface
 } from '../interfaces';
+import {
+  SearchFilterCriteriaFixture,
+  SearchFilterCriteriaValuesFixture
+} from './../+fixtures/search-filter-criteria.fixture';
+import { CheckboxLineFilterComponent } from './checkbox-line-filter/checkbox-line-filter-component';
 import { SearchViewModel } from './search.viewmodel';
 
 @Injectable({
@@ -16,8 +21,13 @@ import { SearchViewModel } from './search.viewmodel';
 })
 export class MockSearchViewModel
   implements ViewModelInterface<SearchViewModel> {
-  public searchState$: Observable<SearchStateInterface>;
-  public searchFilters$: Observable<SearchFilterInterface>;
+  public searchState$ = new Subject<SearchStateInterface>();
+  public searchFilters$ = new Subject<SearchFilterInterface>();
+
+  constructor() {
+    this.searchState$.next(this.getMockSearchState());
+    this.searchFilters$.next(this.getMockSearchFilter());
+  }
 
   public reset(state: SearchStateInterface, mode: SearchModeInterface): void {}
   public changeSort(sortMode: SortModeInterface): void {}
@@ -25,4 +35,25 @@ export class MockSearchViewModel
   public changeFilters(criteria: SearchFilterCriteriaInterface): void {}
   public changeSearchTerm(searchTerm: string): void {}
   public updateResult(result: SearchResultInterface<any>): void {} //TODO: remove <any> when other scaffold branch is merged
+
+  private getMockSearchState(): SearchStateInterface {
+    const mockSeachState = {
+      searchTerm: 'nemo',
+      filterCriteriaSelections: new Map<string, string[]>()
+    };
+
+    return mockSeachState;
+  }
+
+  private getMockSearchFilter(): SearchFilterInterface {
+    const mockSearchFilter = {
+      criteria: new SearchFilterCriteriaFixture({}, [
+        new SearchFilterCriteriaValuesFixture()
+      ]),
+      component: new CheckboxLineFilterComponent(),
+      domHost: 'host'
+    };
+
+    return mockSearchFilter;
+  }
 }
