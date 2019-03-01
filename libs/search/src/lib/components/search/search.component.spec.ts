7482059c259e2a3c3bbbf3e1ebb2d8f6e17dbbe6
DavidBoute <david.boute@gmail.com>
2019-03-01 12:17:46 +0100

    write test

--- libs/search/src/lib/components/search/search.component.spec.ts
+++ libs/search/src/lib/components/search/search.component.spec.ts
@@ -1,24 +1,46 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Subject } from 'rxjs';
+import { SearchStateInterface } from '../../interfaces';
+import { SearchViewModel } from '../search.viewmodel';
+import { MockSearchViewModel } from '../search.viewmodel.mock';
 import { SearchComponent } from './search.component';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
+  let searchViewmodel: SearchViewModel;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [SearchComponent]
+      declarations: [SearchComponent],
+      providers: [{ provide: SearchViewModel, useClass: MockSearchViewModel }]
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
+
+    searchViewmodel = TestBed.get(SearchViewModel);
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('searchState', () => {
+    it('should emit the viewmodel searchState$ value', () => {
+      component.searchState.emit = jest.fn();
+
+      const mockSearchState = null; //TODO delete this when mock is merged
+      (searchViewmodel.searchState$ as Subject<SearchStateInterface>).next(
+        mockSearchState
+      );
+
+      expect(component.searchState.emit).toHaveBeenCalled();
+      expect(component.searchState.emit).toHaveBeenCalledWith(mockSearchState);
+    });
   });
 });
