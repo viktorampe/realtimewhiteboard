41636989c5853651488664d58b09f06d85aa0b0f
Thomas Ghesquiere <thomas.ghesquiere@gmail.com>
2019-03-01 10:53:29 +0100

    collapsible sheet CR

--- libs/ui/src/lib/collapsible-sheet/collapsible-sheet.component.ts
+++ libs/ui/src/lib/collapsible-sheet/collapsible-sheet.component.ts
@@ -78,35 +78,30 @@ export class CollapsibleSheetComponent {
   }
 
   private breakPointSize(): string {
-    return this.breakpointObserver.isMatched('(max-width: 599.99px)')
-      ? 'xsmall'
-      : this.breakpointObserver.isMatched(
-          '(min-width: 600px) and (max-width: 959.99px)'
-        )
-      ? 'small'
-      : this.breakpointObserver.isMatched(
-          '(min-width: 960px) and (max-width: 1279.99px)'
-        )
-      ? 'medium'
-      : 'large';
+    const breakpointsMap = {
+      xsmall: '(max-width: 599.99px)',
+      small: '(min-width: 600px) and (max-width: 959.99px)',
+      medium: '(min-width: 960px) and (max-width: 1279.99px)'
+    };
+
+    if (this.breakpointObserver.isMatched(breakpointsMap.xsmall)) {
+      console.log('xsmall');
+
+      return 'xsmall';
+    }
+    if (this.breakpointObserver.isMatched(breakpointsMap.small)) {
+      console.log('small');
+      return 'small';
+    }
+    if (this.breakpointObserver.isMatched(breakpointsMap.medium)) {
+      console.log('medium');
+      return 'medium';
+    }
+    console.log('large');
+    return 'large';
   }
 
   toggleCollapsed(): void {
     this.collapsed = !this.collapsed;
   }
 }
-
-// $ui-breakpoint-xsmall: '(max-width: 599.99px)';
-// $ui-breakpoint-small: '(min-width: 600px) and (max-width: 959.99px)';
-// $ui-breakpoint-medium: '(min-width: 960px) and (max-width: 1279.99px)';
-// $ui-breakpoint-large: '(min-width: 1280px) and (max-width: 1919.99px)';
-// $ui-breakpoint-xlarge: '(min-width: 1920px)';
-// $ui-breakpoint-handset: '(max-width: 599.99px) and (orientation: portrait), (max-width: 959.99px) and (orientation: landscape)';
-// $ui-breakpoint-tablet: '(min-width: 600px) and (max-width: 839.99px) and (orientation: portrait), (min-width: 960px) and (max-width: 1279.99px) and (orientation: landscape)';
-// $ui-breakpoint-web: '(min-width: 840px) and (orientation: portrait), (min-width: 1280px) and (orientation: landscape)';
-// $ui-breakpoint-handsetportrait: '(max-width: 599.99px) and (orientation: portrait)';
-// $ui-breakpoint-tabletportrait: '(min-width: 600px) and (max-width: 839.99px) and (orientation: portrait)';
-// $ui-breakpoint-webportrait: '(min-width: 840px) and (orientation: portrait)';
-// $ui-breakpoint-handsetlandscape: '(max-width: 959.99px) and (orientation: landscape)';
-// $ui-breakpoint-tabletlandscape: '(min-width: 960px) and (max-width: 1279.99px) and (orientation: landscape)';
-// $ui-breakpoint-weblandscape: '(min-width: 1280px) and (orientation: landscape)';
