c59b4d3cdceb61ab3811b562e9c302896f49b96b
DavidBoute <david.boute@gmail.com>
2019-03-01 11:59:20 +0100

    set up subscription

--- libs/search/src/lib/components/search/search.component.ts
+++ libs/search/src/lib/components/search/search.component.ts
@@ -1,28 +1,53 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
-import { EventEmitter } from 'events';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output
+} from '@angular/core';
+import { Subscription } from 'rxjs';
 import { SearchModeInterface } from './../../interfaces/search-mode-interface';
 import { SearchResultInterface } from './../../interfaces/search-result-interface';
 import { SearchStateInterface } from './../../interfaces/search-state.interface';
+import { SearchViewModel } from './../search.viewmodel';
 
 @Component({
   selector: 'campus-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, OnDestroy {
+  private subscriptions = new Subscription();
+
   @Input() public searchMode: SearchModeInterface;
   @Input() public autoComplete: string[];
   @Input() public initialState: SearchStateInterface;
   @Input() public searchResults: SearchResultInterface;
 
-  @Output() public searchState = new EventEmitter();
-  constructor() {}
+  @Output() public searchState = new EventEmitter<SearchStateInterface>();
 
-  ngOnInit() {}
+  constructor(private searchViewmodel: SearchViewModel) {}
+
+  ngOnInit() {
+    this.setupSubscriptions();
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
 
   public reset(): void {}
   public onSort(): void {}
   public onFilterSelectionChange(): void {}
   public onSearchTermChange(): void {}
   public onScroll(): void {}
+
+  private setupSubscriptions(): void {
+    this.subscriptions.add(
+      this.searchViewmodel.searchState$.subscribe(searchState =>
+        this.searchState.emit(searchState)
+      )
+    );
+  }
 }
