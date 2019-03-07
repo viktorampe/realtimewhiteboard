21eb15d136413c07d09d21670c08a9a9d1cf9e50
pverhelle <pieter.verhelle@gmail.com>
2019-03-07 11:40:10 +0100

    update missing multiplier from scaffold in component

--- libs/pages/edu-contents/src/lib/components/edu-contents/edu-contents.component.ts
+++ libs/pages/edu-contents/src/lib/components/edu-contents/edu-contents.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'campus-edu-content',
-  templateUrl: './edu-content.component.html',
-  styleUrls: ['./edu-content.component.scss']
+  selector: 'campus-edu-contents',
+  templateUrl: './edu-contents.component.html',
+  styleUrls: ['./edu-contents.component.scss']
 })
 export class EduContentsComponent {}
