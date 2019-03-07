e7ddb5024e9b083eade5c03dc33b4130cc9edd23
pverhelle <pieter.verhelle@gmail.com>
2019-03-07 12:57:43 +0100

    add resolver to main route of edu-contents module

--- libs/pages/edu-contents/src/lib/pages-edu-contents-routing.module.ts
+++ libs/pages/edu-contents/src/lib/pages-edu-contents-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EduContentsResolver } from './components/edu-contents.resolver';
 import { EduContentLearningAreaOverviewComponent } from './components/edu-contents/edu-contents.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: EduContentLearningAreaOverviewComponent
+    component: EduContentLearningAreaOverviewComponent,
+    resolve: { isResolved: EduContentsResolver },
+    runGuardsAndResolvers: 'always'
   }
 ];
 
