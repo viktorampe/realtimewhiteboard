d7d41c53c69778dea7a10152b9cefa326497b791
pverhelle <pieter.verhelle@gmail.com>
2019-03-07 13:17:55 +0100

    component template link fixes

--- libs/pages/edu-contents/src/lib/components/edu-contents-learning-area-overview/edu-contents-learning-area-overview.component.ts
+++ libs/pages/edu-contents/src/lib/components/edu-contents-learning-area-overview/edu-contents-learning-area-overview.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'campus-edu-contents',
-  templateUrl: './edu-contents.component.html',
-  styleUrls: ['./edu-contents.component.scss']
+  templateUrl: './edu-contents-learning-area-overview.component.html',
+  styleUrls: ['./edu-contents-learning-area-overview.component.scss']
 })
 export class EduContentLearningAreaOverviewComponent {}
