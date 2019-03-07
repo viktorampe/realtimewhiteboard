44e2ab7d815ff0bc8c638e6e20a2622580257052
pverhelle <pieter.verhelle@gmail.com>
2019-03-07 11:22:12 +0100

    move root component to folder and update ref in module

--- libs/pages/edu-contents/src/lib/pages-edu-contents.module.ts
+++ libs/pages/edu-contents/src/lib/pages-edu-contents.module.ts
@@ -2,9 +2,9 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { PagesSharedModule } from '@campus/pages/shared';
 import { UiModule } from '@campus/ui';
-import { EduContentsComponent } from './components/edu-contents.component';
 import { EduContentsViewModel } from './components/edu-contents.viewmodel';
 import { PagesEduContentsRoutingModule } from './pages-edu-contents-routing.module';
+import { EduContentsComponent } from './components/edu-contents/edu-contents.component';
 
 @NgModule({
   imports: [
