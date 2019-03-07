ad4865d9835d52306702b1a712b4a9d808e7be21
pverhelle <pieter.verhelle@gmail.com>
2019-03-07 15:42:57 +0100

    scaffold new component

---
+++ libs/ui/src/lib/drop-area/drop-area.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'campus-drop-area',
+  templateUrl: './drop-area.component.html',
+  styleUrls: ['./drop-area.component.scss']
+})
+export class DropAreaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
