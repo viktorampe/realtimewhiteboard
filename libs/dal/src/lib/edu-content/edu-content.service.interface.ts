ba20901cc47c1e53d34a81ba6956013b1d5fb356
pverhelle <pieter.verhelle@gmail.com>
2019-03-12 13:22:24 +0100

    update token interface

--- libs/dal/src/lib/edu-content/edu-content.service.interface.ts
+++ libs/dal/src/lib/edu-content/edu-content.service.interface.ts
@@ -1,10 +1,13 @@
 import { InjectionToken } from '@angular/core';
+import { SearchResultInterface, SearchStateInterface } from '@campus/search';
 import { Observable } from 'rxjs';
-import { EduContentInterface } from '../+models/EduContent.interface';
+import { EduContentInterface } from '../+models';
 
 export const EDU_CONTENT_SERVICE_TOKEN = new InjectionToken(
   'EduContentService'
 );
 export interface EduContentServiceInterface {
   getAllForUser(userId: number): Observable<EduContentInterface[]>;
+  search(state: SearchStateInterface): Observable<SearchResultInterface>;
+  autoComplete(state: SearchStateInterface): Observable<String[]>;
 }
