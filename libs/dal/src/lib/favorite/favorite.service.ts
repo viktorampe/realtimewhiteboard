e9c0bb1f0baa1b3adc0e8feb090e7a8157ebe4e0
DavidBoute <david.boute@gmail.com>
2019-03-13 10:33:00 +0100

    make interface, injection token make service implement interface

--- libs/dal/src/lib/favorite/favorite.service.ts
+++ libs/dal/src/lib/favorite/favorite.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FavoriteInterface } from '../+models';
+import { FavoriteServiceInterface } from './favorite.service.interface';
 
 @Injectable({
   providedIn: 'root'
 })
-export class FavoriteService {
-
-  constructor() { }
+export class FavoriteService implements FavoriteServiceInterface {
+  getAllForUser(userId: number): Observable<FavoriteInterface[]> {
+    return;
+  }
+  addFavorite(
+    userId: number,
+    favorite: FavoriteInterface
+  ): Observable<FavoriteInterface> {
+    return;
+  }
+  removeFavorite(userId: number, favoriteId: number) {
+    return;
+  }
 }
