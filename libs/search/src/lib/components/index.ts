a2c54cfd638ea557a9eda5f9187334b2abb058a0
pverhelle <pieter.verhelle@gmail.com>
2019-03-19 10:37:16 +0100

    barrel the component

--- libs/search/src/lib/components/index.ts
+++ libs/search/src/lib/components/index.ts
@@ -1,3 +1,4 @@
 export * from './checkbox-line-filter/checkbox-line-filter-component';
 export * from './checkbox-list-filter/checkbox-list-filter.component';
+export * from './column-filter/column-filter.component';
 export * from './results-list/result.component.base';
