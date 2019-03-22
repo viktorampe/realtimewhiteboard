cc848982590b10bda24dc5cbc23ce44d3a730eec
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2019-03-22 13:22:39 +0100

    Favorite areas component done
    Still needs tests

--- libs/pages/edu-contents/src/lib/components/favorite-areas/favorite-areas.component.ts
+++ libs/pages/edu-contents/src/lib/components/favorite-areas/favorite-areas.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TileSecondaryActionInterface } from '@campus/ui';
+import { LearningAreaInterface } from '@diekeure/polpo-api-angular-sdk';
 
 @Component({
   selector: 'campus-favorite-areas',
   templateUrl: './favorite-areas.component.html',
   styleUrls: ['./favorite-areas.component.scss']
 })
-export class FavoriteAreasComponent {}
+export class FavoriteAreasComponent {
+  @Input() favoriteLearningAreas: LearningAreaInterface[];
+  @Output() removeAreaFromFavorites = new EventEmitter<LearningAreaInterface>();
+
+  createSecondaryAction(
+    learningArea: LearningAreaInterface
+  ): TileSecondaryActionInterface {
+    return {
+      label: 'Verwijderen uit favorieten',
+      icon: 'verwijder',
+      onClick: () => {
+        this.removeAreaFromFavorites.emit(learningArea);
+      }
+    };
+  }
+}
