4daf21be0f4b67a511b2b042821e9511583ae202
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2019-03-22 11:19:11 +0100

    Favorite areas scaffolding

---
+++ libs/pages/edu-contents/src/lib/components/favorite-areas/favorite-areas.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'campus-favorite-areas',
+  templateUrl: './favorite-areas.component.html',
+  styleUrls: ['./favorite-areas.component.scss']
+})
+export class FavoriteAreasComponent {}
