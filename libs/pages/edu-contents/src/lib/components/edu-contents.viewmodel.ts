e0e2262417e126041a9e628fc56dd0430b6269f7
Thomas Ghesquiere <thomas.ghesquiere@gmail.com>
2019-03-25 14:21:55 +0100

    SearchPageViewModel: basic streams

--- libs/pages/edu-contents/src/lib/components/edu-contents.viewmodel.ts
+++ libs/pages/edu-contents/src/lib/components/edu-contents.viewmodel.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
-import { FavoriteInterface, LearningAreaInterface } from '@campus/dal';
+import {
+  DalState,
+  FavoriteInterface,
+  FavoriteQueries,
+  LearningAreaInterface,
+  LearningAreaQueries
+} from '@campus/dal';
 import { SearchModeInterface, SearchStateInterface } from '@campus/search';
 import { EduContentSearchResultInterface } from '@campus/shared';
+import { select, Store } from '@ngrx/store';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +19,21 @@ export class EduContentsViewModel {
   public learningAreas$: Observable<LearningAreaInterface[]>;
   public favoriteLearningAreas$: Observable<LearningAreaInterface[]>;
   public searchResults$: Observable<EduContentSearchResultInterface[]>;
+  public eduContentFavorites$: Observable<FavoriteInterface[]>;
 
   private searchState$: BehaviorSubject<SearchStateInterface>;
-  private eduContentFavorites$: Observable<FavoriteInterface[]>;
+
+  constructor(private store: Store<DalState>) {
+    this.initialize();
+  }
+
+  private initialize() {
+    this.learningAreas$ = this.store.pipe(select(LearningAreaQueries.getAll));
+    this.favoriteLearningAreas$ = this.getFavoriteLearningAreas();
+    this.eduContentFavorites$ = this.store.pipe(
+      select(FavoriteQueries.getByType, { type: 'educontent' })
+    );
+  }
 
   /*
    * let the page component pass through the updated state from the search component
@@ -61,4 +81,31 @@ export class EduContentsViewModel {
    * set the streams for favorites, learningAreas via store selectors
    */
   private setupStreams(): void {}
+
+  private getFavoriteLearningAreas(): Observable<LearningAreaInterface[]> {
+    return this.store.pipe(
+      select(FavoriteQueries.getByType, { type: 'area' }),
+      map(
+        (favorites): { [key: number]: FavoriteInterface } => {
+          const learningAreaIds: { [key: number]: FavoriteInterface } = {};
+          favorites.forEach(favorite => {
+            learningAreaIds[favorite.learningAreaId] = favorite;
+          });
+          return learningAreaIds;
+        }
+      ),
+      switchMap(
+        (learningAreaIds): Observable<LearningAreaInterface[]> => {
+          return this.learningAreas$.pipe(
+            map(
+              (learningAreas): LearningAreaInterface[] =>
+                learningAreas.filter(
+                  learningArea => !!learningAreaIds[learningArea.id]
+                )
+            )
+          );
+        }
+      )
+    );
+  }
 }
