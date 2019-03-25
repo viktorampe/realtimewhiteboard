8b8716d98ffe1b8aec1f497ae7af9d277f6867cb
pverhelle <pieter.verhelle@gmail.com>
2019-03-25 13:13:54 +0100

    make messages more readable

--- .vscode/wallaby/wallaby-setup.js
+++ .vscode/wallaby/wallaby-setup.js
@@ -1,9 +1,9 @@
 var options = require('./wallaby-options');
 var fs = require('fs');
-console.log('Starting Wallaby config file generation');
+console.log('\x1b[36m%s\x1b[0m', 'Starting Wallaby config file generation');
 var selectedOption = process.argv[2];
 var selectedFile = process.argv[3];
-console.log(`Project options - ${selectedOption} - was chosen`);
+console.log(`Project option - ${selectedOption} - was chosen`);
 if (selectedFile) console.log(`File - ${selectedFile}.spec.ts - was chosen`);
 var usedOptions = options[selectedOption];
 if (selectedFile)
@@ -12,15 +12,16 @@ if (selectedFile)
     `libs/**/${selectedFile}.spec.ts`
   ];
 if (!usedOptions)
-  throw Error(
-    `Provided option - ${selectedOption} - could not be found in - wallaby-options.json -`
+  console.log(
+    '\x1b[31m%s\x1b[0m',
+    `Provided option - ${selectedOption} - could not be found in - wallaby-options.json -.\nPlease verify that the wallabyProject options exist in the - wallaby-options.json - file`
   );
 else
   fs.writeFile(
     '.vscode/wallaby/wallaby-used.json',
     JSON.stringify(usedOptions),
     function(err) {
-      if (err) throw err;
-      console.log('Wallaby config file generated');
+      if (err) console.log('\x1b[31m%s\x1b[0m', err.message);
+      console.log('\x1b[32m%s\x1b[0m', 'Wallaby config file generated');
     }
   );
