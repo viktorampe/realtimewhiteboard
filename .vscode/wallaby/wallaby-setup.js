173ec07a2c4ae1441a261b07c70433f9f56a32c6
pverhelle <pieter.verhelle@gmail.com>
2019-03-25 11:44:59 +0100

    improve error when options don't add up

--- .vscode/wallaby/wallaby-setup.js
+++ .vscode/wallaby/wallaby-setup.js
@@ -1,12 +1,11 @@
 var options = require('./wallaby-options');
 var fs = require('fs');
-
 console.log('Starting Wallaby config file generation');
 var selectedOption = process.argv[process.argv.length - 1];
 console.log(`Project options - ${selectedOption} - was chosen`);
 var usedOptions = options[selectedOption];
 if (!usedOptions)
-  console.log(
+  throw Error(
     `Provided option - ${selectedOption} - could not be found in - wallaby-options.json -`
   );
 else
@@ -14,7 +13,7 @@ else
     '.vscode/wallaby/wallaby-used.json',
     JSON.stringify(usedOptions),
     function(err) {
-      if (err) console.log(err);
+      if (err) throw err;
       console.log('Wallaby config file generated');
     }
   );
