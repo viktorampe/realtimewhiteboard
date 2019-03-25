80589f371b6505ba021da582a18ade249a1c060e
pverhelle <pieter.verhelle@gmail.com>
2019-03-25 12:58:48 +0100

    add option to test one file

--- .vscode/wallaby/wallaby-setup.js
+++ .vscode/wallaby/wallaby-setup.js
@@ -1,9 +1,16 @@
 var options = require('./wallaby-options');
 var fs = require('fs');
 console.log('Starting Wallaby config file generation');
-var selectedOption = process.argv[process.argv.length - 1];
+var selectedOption = process.argv[2];
+var selectedFile = process.argv[3];
 console.log(`Project options - ${selectedOption} - was chosen`);
+if (selectedFile) console.log(`File - ${selectedFile}.spec.ts - was chosen`);
 var usedOptions = options[selectedOption];
+if (selectedFile)
+  usedOptions.tests = [
+    `apps/**/${selectedFile}.spec.ts`,
+    `libs/**/${selectedFile}.spec.ts`
+  ];
 if (!usedOptions)
   throw Error(
     `Provided option - ${selectedOption} - could not be found in - wallaby-options.json -`
