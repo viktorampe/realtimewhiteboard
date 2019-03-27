c84cd3e93b143c54f9ad1dbddeddfce8e6eef078
Thomas Ghesquiere <thomas.ghesquiere@gmail.com>
2019-03-27 17:27:48 +0100

    fix #766: Search By Term: implement clear filter

--- libs/pages/edu-contents/src/lib/components/edu-contents-search-by-term/edu-contents-search-by-term.component.ts
+++ libs/pages/edu-contents/src/lib/components/edu-contents-search-by-term/edu-contents-search-by-term.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  SearchComponent,
+  SearchModeInterface,
+  SearchStateInterface
+} from '@campus/search';
+import { Observable } from 'rxjs';
+import { EduContentsViewModel } from '../edu-contents.viewmodel';
 
 @Component({
   selector: 'campus-edu-contents-search-by-term',
   templateUrl: './edu-contents-search-by-term.component.html',
   styleUrls: ['./edu-contents-search-by-term.component.scss']
 })
-export class EduContentSearchByTermComponent {}
+export class EduContentSearchByTermComponent implements OnInit {
+  public searchMode: SearchModeInterface;
+  public searchState$: Observable<SearchStateInterface>;
+
+  @ViewChild(SearchComponent) public searchComponent: SearchComponent;
+
+  constructor(private eduContentsViewModel: EduContentsViewModel) {}
+
+  ngOnInit(): void {
+    this.searchMode = this.eduContentsViewModel.getSearchMode('search');
+    this.searchState$ = this.eduContentsViewModel.getInitialSearchState();
+  }
+
+  clearSearchFilters() {
+    if (this.searchComponent) {
+      this.searchComponent.reset();
+    }
+  }
+}
