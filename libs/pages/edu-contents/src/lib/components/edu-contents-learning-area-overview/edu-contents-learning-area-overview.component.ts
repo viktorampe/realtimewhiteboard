6ec4ba61568af9d5c49e62bd3f8bf14f6d65edd4
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2019-03-27 13:46:34 +0100

    Added VM + tests

--- libs/pages/edu-contents/src/lib/components/edu-contents-learning-area-overview/edu-contents-learning-area-overview.component.ts
+++ libs/pages/edu-contents/src/lib/components/edu-contents-learning-area-overview/edu-contents-learning-area-overview.component.ts
@@ -1,8 +1,46 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EnvironmentSearchModesInterface } from '@campus/shared';
+import { Observable, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { EduContentsViewModel } from '../edu-contents.viewmodel';
 
 @Component({
   selector: 'campus-edu-contents',
   templateUrl: './edu-contents-learning-area-overview.component.html',
   styleUrls: ['./edu-contents-learning-area-overview.component.scss']
 })
-export class EduContentLearningAreaOverviewComponent {}
+export class EduContentLearningAreaOverviewComponent {
+  public searchModes: EnvironmentSearchModesInterface;
+  public searchTerm$: Subject<string>;
+  public autoCompleteValues$: Observable<string[]>;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private eduContentsViewModel: EduContentsViewModel
+  ) {}
+
+  public ngOnInit(): void {
+    this.searchModes = this.eduContentsViewModel.searchModes;
+    this.searchTerm$ = new Subject();
+    this.autoCompleteValues$ = this.searchTerm$.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      switchMap(searchTerm =>
+        this.eduContentsViewModel.requestAutoComplete(searchTerm)
+      )
+    );
+  }
+
+  public openSearchByTerm(searchTerm: string) {
+    this.router.navigate(['term'], {
+      relativeTo: this.route,
+      queryParams: { searchTerm }
+    });
+  }
+
+  public searchTermChanged(searchTerm: string) {
+    this.searchTerm$.next(searchTerm);
+  }
+}
