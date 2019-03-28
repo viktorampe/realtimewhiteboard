bdabf8d80d20a39c1a302b67b128e1d7d3bb59a6
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2019-03-28 10:16:37 +0100

    clean up

--- libs/pages/edu-contents/src/lib/components/edu-contents-learning-area-overview/edu-contents-learning-area-overview.component.ts
+++ libs/pages/edu-contents/src/lib/components/edu-contents-learning-area-overview/edu-contents-learning-area-overview.component.ts
@@ -9,27 +9,15 @@ import { EduContentsViewModelMock } from '../edu-contents.viewmodel.mock';
   styleUrls: ['./edu-contents-learning-area-overview.component.scss']
 })
 export class EduContentLearningAreaOverviewComponent {
-  dragStarted = false;
   dropZoneIsHovered = false;
   constructor(public viewModel: EduContentsViewModelMock) {}
 
-  changeDragStatus(started: boolean) {
-    this.dragStarted = started;
-  }
-
   onFavoriteDrop(event: CdkDragDrop<LearningAreaInterface[]>) {
     const learningAreaToAddToFavorite = event.item.data;
-    console.log(learningAreaToAddToFavorite);
     this.viewModel.toggleFavoriteArea(learningAreaToAddToFavorite);
   }
 
   setHoverState(state: boolean) {
-    console.log(state);
-
     this.dropZoneIsHovered = state;
-  }
-
-  onDragEnter(event: DragEvent) {
-    console.log('drag enter', event);
   }
 }
