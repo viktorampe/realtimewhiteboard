7e242917b961f019cf5dc4f61187e80e3329a638
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2019-03-28 12:42:43 +0100

    make flex wrap

--- libs/pages/edu-contents/src/lib/components/edu-contents.viewmodel.mock.ts
+++ libs/pages/edu-contents/src/lib/components/edu-contents.viewmodel.mock.ts
@@ -41,6 +41,51 @@ export class EduContentsViewModelMock
       id: 3,
       name: 'bazLearningArea',
       icon: 'polpo-biologie'
+    }),
+    new LearningAreaFixture({
+      id: 4,
+      name: 'fooLearningArea',
+      icon: 'polpo-aardrijkskunde'
+    }),
+    new LearningAreaFixture({
+      id: 5,
+      name: 'barLearningArea',
+      icon: 'polpo-engels'
+    }),
+    new LearningAreaFixture({
+      id: 6,
+      name: 'bazLearningArea',
+      icon: 'polpo-biologie'
+    }),
+    new LearningAreaFixture({
+      id: 7,
+      name: 'fooLearningArea',
+      icon: 'polpo-aardrijkskunde'
+    }),
+    new LearningAreaFixture({
+      id: 8,
+      name: 'barLearningArea',
+      icon: 'polpo-engels'
+    }),
+    new LearningAreaFixture({
+      id: 9,
+      name: 'bazLearningArea',
+      icon: 'polpo-biologie'
+    }),
+    new LearningAreaFixture({
+      id: 10,
+      name: 'fooLearningArea',
+      icon: 'polpo-aardrijkskunde'
+    }),
+    new LearningAreaFixture({
+      id: 11,
+      name: 'barLearningArea',
+      icon: 'polpo-engels'
+    }),
+    new LearningAreaFixture({
+      id: 12,
+      name: 'bazLearningArea',
+      icon: 'polpo-biologie'
     })
   ];
 
