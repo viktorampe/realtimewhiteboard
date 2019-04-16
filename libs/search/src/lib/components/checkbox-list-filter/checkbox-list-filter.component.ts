75e28dfa20c09629b1e67850dfc5bc534c51d802
pverhelle <pieter.verhelle@gmail.com>
2019-04-16 15:19:54 +0200

    set the optionsFilter in the CheckboxListFilterComponent so it can be passed to it's children

--- libs/search/src/lib/components/checkbox-list-filter/checkbox-list-filter.component.ts
+++ libs/search/src/lib/components/checkbox-list-filter/checkbox-list-filter.component.ts
@@ -10,10 +10,15 @@ import { SearchFilterCriteriaInterface } from './../../interfaces/search-filter-
 export class CheckboxListFilterComponent
   implements SearchFilterComponentInterface {
   public showMoreChildren = false;
+  public maxVisibleItems = 0; // 0 == no limit
 
-  @Input() maxVisibleItems = 0; // 0 == no limit
   @Input() filterCriteria: SearchFilterCriteriaInterface;
-
+  @Input()
+  public set filterOptions(value: any) {
+    if (value && value.maxVisibleItems) {
+      this.maxVisibleItems = value.maxVisibleItems;
+    }
+  }
   @Output()
   filterSelectionChange = new EventEmitter<SearchFilterCriteriaInterface[]>();
 
