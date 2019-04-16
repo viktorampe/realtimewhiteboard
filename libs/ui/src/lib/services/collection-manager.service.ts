56891e8240d585e45604a4c7ef140836d6df267a
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2019-04-16 15:42:43 +0200

    [WIP]

--- libs/ui/src/lib/services/collection-manager.service.ts
+++ libs/ui/src/lib/services/collection-manager.service.ts
@@ -1,9 +1,37 @@
-import { Injectable } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+// mock component
+@Component({
+  selector: 'campus-manage-collection',
+  template: ``
+})
+export class ManageCollectionComponent {
+  constructor() {}
+}
+
+class ItemToggledInCollectionEvent {}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CollectionManagerService {
+  constructor() {}
 
-  constructor() { }
+  manageCollections(
+    item: ManageCollectionItemInterface,
+    linkableItems: ManageCollectionItemInterface,
+    linkedItemIds: number[],
+    recentItemIds: number[]
+  ): Observable<ItemToggledInCollectionEvent> {
+    // open dialog
+    // use the ManageCollectionComponent
+    // inject the right data
+    // listen to component and bubble itemToggledInCollectionEvent
+    // clean up subscription on dialog close
+    // complete observable on dialog close
+
+    // return observable
+    return of(new ItemToggledInCollectionEvent());
+  }
 }
