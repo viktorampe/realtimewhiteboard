490dd3436ddade55e4af3a71ad6f4e73d0f2b5b8
DavidBoute <david.boute@gmail.com>
2019-05-07 13:22:25 +0200

    scaffold

---
+++ libs/shared/src/lib/components/quick-link/quick-link.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'campus-quick-link',
+  templateUrl: './quick-link.component.html',
+  styleUrls: ['./quick-link.component.scss']
+})
+export class QuickLinkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
