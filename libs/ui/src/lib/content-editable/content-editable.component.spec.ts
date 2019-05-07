8b1c35b11868f33a6716f3fc26bac2c53f175ec3
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2019-05-07 15:19:04 +0200

    Finished tests

--- libs/ui/src/lib/content-editable/content-editable.component.spec.ts
+++ libs/ui/src/lib/content-editable/content-editable.component.spec.ts
@@ -48,14 +48,22 @@ describe('ContentEditableComponent', () => {
   });
 
   it('should not display the inputs when active is false', () => {
-    let input = fixture.debugElement.query(
+    const input = fixture.debugElement.query(
       By.css('.ui-content-editable__form-field')
     );
 
     expect(input.styles['display']).toBe('none');
   });
 
-  describe('editing enabled (active = true)', () => {
+  it('should not display the buttons when active is false', () => {
+    const buttons = fixture.debugElement.query(
+      By.css('.ui-content-editable__actions')
+    );
+
+    expect(buttons).toBeFalsy();
+  });
+
+  describe('begin editing (= setting active to true)', () => {
     it('should display the form field containing the inputs', () => {
       component.active = true;
       fixture.detectChanges();
@@ -67,18 +75,193 @@ describe('ContentEditableComponent', () => {
       expect(formField.styles['display']).toBeNull();
     });
 
-    it('should focus the input ', fakeAsync(() => {
-      const inputElement = fixture.debugElement.query(
+    function validateElementFocused(element) {
+      const text = 'hello';
+      component.text = text;
+      enableEditing();
+
+      expect(element.value).toBe(text);
+      expect(element.selectionStart).toBe(0);
+      expect(element.selectionEnd).toBe(text.length);
+    }
+
+    function validateElementTextSelected(element) {
+      const text = 'hello';
+      component.text = text;
+      enableEditing();
+
+      expect(element.value).toBe(text);
+      expect(element.selectionStart).toBe(0);
+      expect(element.selectionEnd).toBe(text.length);
+    }
+
+    describe('input (no multiline)', () => {
+      let inputEl;
+
+      beforeEach(() => {
+        inputEl = fixture.debugElement.query(
+          By.css('.ui-content-editable__form-field input')
+        ).nativeElement;
+      });
+
+      it('should focus the input ', fakeAsync(() => {
+        validateElementFocused(inputEl);
+      }));
+
+      it('should select all text in the input on focus ', fakeAsync(() => {
+        validateElementTextSelected(inputEl);
+      }));
+    });
+
+    describe('textarea (multiline on)', () => {
+      let textareaEl;
+
+      beforeEach(() => {
+        component.multiline = true;
+        fixture.detectChanges();
+
+        textareaEl = fixture.debugElement.query(
+          By.css('.ui-content-editable__form-field textarea')
+        ).nativeElement;
+      });
+
+      it('should focus the textarea ', fakeAsync(() => {
+        validateElementFocused(textareaEl);
+      }));
+
+      it('should select all text in the textarea on focus ', fakeAsync(() => {
+        validateElementTextSelected(textareaEl);
+      }));
+    });
+  });
+
+  describe('editing', () => {
+    const defaultText = 'hello';
+    const newText = 'world';
+    let inputEl, cancelEl;
+
+    beforeEach(fakeAsync(() => {
+      component.text = defaultText;
+      enableEditing();
+
+      inputEl = fixture.debugElement.query(
         By.css('.ui-content-editable__form-field input')
       ).nativeElement;
 
-      spyOn(inputElement, 'focus');
+      //We don't make a variable for the confirm button
+      //because it's only sometimes there, when there's an input value
 
-      component.active = true;
-      fixture.detectChanges();
-      tick();
-
-      expect(inputElement.focus).toHaveBeenCalled();
+      cancelEl = fixture.debugElement.query(
+        By.css('.ui-content-editable__actions__cancel')
+      ).nativeElement;
     }));
+
+    it('should save changes on pressing enter', () => {
+      enterText(newText);
+      pressEnter();
+
+      expect(component.text).toBe(newText);
+    });
+
+    it('should save changes when clicking confirm', () => {
+      enterText(newText);
+      clickConfirm();
+
+      expect(component.text).toBe(newText);
+    });
+
+    it('should NOT save changes when clicking cancel', () => {
+      enterText(newText);
+      clickCancel();
+
+      expect(component.text).toBe(defaultText);
+    });
+
+    it('should NOT save changes when pressing enter with empty value', () => {
+      enterText('');
+      pressEnter();
+
+      expect(component.text).toBe(defaultText);
+    });
+
+    it('should not show confirm button if new text is empty', () => {
+      enterText('');
+      fixture.detectChanges();
+
+      expect(getConfirmButton()).toBeFalsy();
+    });
+
+    it('should emit event when changes are confirmed', () => {
+      component.textChanged.emit = jest.fn();
+
+      enterText(newText);
+      pressEnter();
+
+      expect(component.textChanged.emit).toHaveBeenCalledWith(newText);
+    });
+
+    it('should NOT emit event when confirming with empty value', () => {
+      component.textChanged.emit = jest.fn();
+
+      enterText('');
+      pressEnter();
+
+      expect(component.textChanged.emit).not.toHaveBeenCalled();
+    });
+
+    describe('multiline', () => {
+      let textareaEl;
+
+      beforeEach(() => {
+        component.multiline = true;
+        fixture.detectChanges();
+
+        textareaEl = fixture.debugElement.query(
+          By.css('.ui-content-editable__form-field textarea')
+        ).nativeElement;
+      });
+
+      it('should NOT call saveChanges when pressing enter in multiline', () => {
+        spyOn(component, 'saveChanges');
+
+        textareaEl.value = newText;
+        textareaEl.dispatchEvent(new Event('input'));
+        textareaEl.dispatchEvent(
+          new KeyboardEvent('keydown', { key: 'Enter' })
+        );
+
+        expect(component.saveChanges).not.toHaveBeenCalled();
+      });
+    });
+
+    function enterText(text: string) {
+      inputEl.value = text;
+      inputEl.dispatchEvent(new Event('input'));
+    }
+
+    function pressEnter() {
+      inputEl.dispatchEvent(new KeyboardEvent('keydown', { key: 'Enter' }));
+    }
+
+    //Used to also check for its existence, that's why this is separate
+    function getConfirmButton() {
+      return fixture.debugElement.query(
+        By.css('.ui-content-editable__actions__confirm')
+      );
+    }
+
+    function clickConfirm() {
+      getConfirmButton().nativeElement.click();
+    }
+
+    function clickCancel() {
+      cancelEl.click();
+    }
   });
+
+  function enableEditing() {
+    component.active = true;
+    fixture.detectChanges();
+    tick();
+  }
 });
