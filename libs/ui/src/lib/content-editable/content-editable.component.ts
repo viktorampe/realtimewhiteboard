7869e49027c1c05c880f0d75674a97ed62bb3932
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2019-05-07 09:24:23 +0200

    ContentEditable scaffolding

---
+++ libs/ui/src/lib/content-editable/content-editable.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'campus-content-editable',
+  templateUrl: './content-editable.component.html',
+  styleUrls: ['./content-editable.component.scss']
+})
+export class ContentEditableComponent {}
