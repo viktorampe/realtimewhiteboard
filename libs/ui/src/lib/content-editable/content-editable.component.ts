0c39cc7b08ee48f74dcc33052a713224ec286110
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2019-05-07 09:58:57 +0200

    Basic edit functionality

--- libs/ui/src/lib/content-editable/content-editable.component.ts
+++ libs/ui/src/lib/content-editable/content-editable.component.ts
@@ -1,8 +1,48 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ViewChild
+} from '@angular/core';
 
 @Component({
   selector: 'campus-content-editable',
   templateUrl: './content-editable.component.html',
   styleUrls: ['./content-editable.component.scss']
 })
-export class ContentEditableComponent {}
+export class ContentEditableComponent implements OnInit, OnChanges {
+  @Input() text: string;
+  @Input() active: boolean;
+
+  //Submit on <enter> is true if multiline is off
+  @Input() multiline = false;
+
+  @Output() textChanged: EventEmitter<string> = new EventEmitter();
+
+  @ViewChild('inputField')
+  inputField: ElementRef;
+
+  ngOnInit() {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.active) {
+      this.focusInputField();
+    }
+  }
+
+  focusInputField() {
+    setTimeout(() => {
+      this.inputField.nativeElement.focus();
+    });
+  }
+
+  toggleActive() {
+    this.active = !this.active;
+    this.focusInputField();
+  }
+}
