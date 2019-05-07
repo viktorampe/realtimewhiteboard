ba82461d80484a06861664a8341382e03e6f5c51
DavidBoute <david.boute@gmail.com>
2019-05-07 12:54:11 +0200

    scaffold

---
+++ libs/shared/src/lib/components/quick-link/quick-link.viewmodel.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {
+  EffectFeedbackInterface,
+  FavoriteInterface,
+  HistoryInterface
+} from '@campus/dal';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuickLinkViewModel {
+  public quickLinks$: Observable<HistoryInterface[] | FavoriteInterface[]>;
+  public feedback$: Observable<EffectFeedbackInterface>;
+  public update(id: number, name: string, mode: QuickLinkTypeEnum): void {}
+  public delete(id: number, mode: QuickLinkTypeEnum): void {}
+}
+
+enum QuickLinkTypeEnum {} // todo: replace with actual enum
