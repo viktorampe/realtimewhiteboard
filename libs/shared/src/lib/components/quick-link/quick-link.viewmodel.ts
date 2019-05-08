4978ad123bf58d28e84611c90a888cd99a50ce68
DavidBoute <david.boute@gmail.com>
2019-05-08 11:48:06 +0200

    getting started

--- libs/shared/src/lib/components/quick-link/quick-link.viewmodel.ts
+++ libs/shared/src/lib/components/quick-link/quick-link.viewmodel.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
 import {
+  DalState,
+  EduContent,
+  EduContentQueries,
   EffectFeedbackInterface,
   FavoriteInterface,
-  HistoryInterface
+  FavoriteQueries,
+  HistoryInterface,
+  LearningAreaQueries
 } from '@campus/dal';
-import { Observable } from 'rxjs';
+import { Dictionary } from '@ngrx/entity';
+import { select, Store } from '@ngrx/store';
+import { combineLatest, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { QuickLinkTypeEnum } from './quick-link-type.enum';
 
 @Injectable()
@@ -13,4 +21,24 @@ export class QuickLinkViewModel {
   public feedback$: Observable<EffectFeedbackInterface>;
   public update(id: number, name: string, mode: QuickLinkTypeEnum): void {}
   public delete(id: number, mode: QuickLinkTypeEnum): void {}
+
+  constructor(private store: Store<DalState>) {}
+
+  private getFavoriteQuickLink$(): Observable<FavoriteInterface[]> {
+    return combineLatest(
+      this.store.pipe(select(LearningAreaQueries.getAllEntities)),
+      this.store.pipe(select(FavoriteQueries.getAll)),
+      this.store.pipe(
+        select(EduContentQueries.getAllEntities),
+        map(object => object as Dictionary<EduContent>)
+      )
+    ).pipe(
+      map(([learningAreaDict, favorites, eduContentDict]) => {
+        favorites.forEach(fav => {
+          fav.learningArea = learningAreaDict[fav.learningAreaId];
+        });
+        return favorites;
+      })
+    );
+  }
 }
