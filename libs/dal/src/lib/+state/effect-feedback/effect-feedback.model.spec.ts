3ca0372a3df372ba72b8ad58b865e9691a6435bd
pverhelle <pieter.verhelle@gmail.com>
2019-05-14 15:57:13 +0200

    update tests

--- libs/dal/src/lib/+state/effect-feedback/effect-feedback.model.spec.ts
+++ libs/dal/src/lib/+state/effect-feedback/effect-feedback.model.spec.ts
@@ -1,58 +1,127 @@
 import {
   EffectFeedback,
-  FeedbackTriggeringAction
+  FeedbackTriggeringAction,
+  FeedbackTriggeringPayload
 } from './effect-feedback.model';
 
 describe('EffectFeedback model', () => {
   describe('generateErrorFeedback', () => {
-    it('should display feedback if useCustomErrorHandler = false', () => {
-      const action = ({
-        payload: { customFeedbackHandlers: { useCustomErrorHandler: false } }
-      } as unknown) as FeedbackTriggeringAction;
+    it('should display error feedback depending on action.payload.customFeedbackHandlers.useCustomErrorHandler data', () => {
+      const assertionData: {
+        action: { payload: FeedbackTriggeringPayload };
+        expectedDisplay: boolean;
+      }[] = [
+        {
+          action: null,
+          expectedDisplay: true
+        },
+        {
+          action: { payload: null },
+          expectedDisplay: true
+        },
+        {
+          action: { payload: {} },
+          expectedDisplay: true
+        },
+        {
+          action: { payload: { customFeedbackHandlers: null } },
+          expectedDisplay: true
+        },
+        {
+          action: {
+            payload: {
+              customFeedbackHandlers: { useCustomErrorHandler: null }
+            }
+          },
+          expectedDisplay: true
+        },
+        {
+          action: {
+            payload: {
+              customFeedbackHandlers: { useCustomErrorHandler: false }
+            }
+          },
+          expectedDisplay: true
+        },
+        {
+          action: {
+            payload: {
+              customFeedbackHandlers: { useCustomErrorHandler: true }
+            }
+          },
+          expectedDisplay: false
+        }
+      ];
+      assertionData.forEach(data => {
+        const action = (data.action as unknown) as FeedbackTriggeringAction;
 
-      const result = EffectFeedback.generateErrorFeedback(
-        'foo',
-        action,
-        'foo message'
-      );
-      expect(result.display).toEqual(true);
+        const result = EffectFeedback.generateErrorFeedback(
+          'foo',
+          action,
+          'foo message'
+        );
+        expect(result.display).toEqual(data.expectedDisplay);
+      });
     });
 
-    it('should display feedback if the action does not have a useCustomErrorHandler flag', () => {
-      const action = ({
-        payload: {}
-      } as unknown) as FeedbackTriggeringAction;
+    it('should display success feedback depending on action.payload.customFeedbackHandlers.useCustomSuccessHandler data', () => {
+      const assertionData: {
+        action: { payload: FeedbackTriggeringPayload };
+        expectedDisplay: boolean;
+      }[] = [
+        {
+          action: null,
+          expectedDisplay: true
+        },
+        {
+          action: { payload: null },
+          expectedDisplay: true
+        },
+        {
+          action: { payload: {} },
+          expectedDisplay: true
+        },
+        {
+          action: {
+            payload: { customFeedbackHandlers: null }
+          },
+          expectedDisplay: true
+        },
+        {
+          action: {
+            payload: {
+              customFeedbackHandlers: { useCustomSuccessHandler: null }
+            }
+          },
+          expectedDisplay: true
+        },
+        {
+          action: {
+            payload: {
+              customFeedbackHandlers: { useCustomSuccessHandler: false }
+            }
+          },
+          expectedDisplay: true
+        },
+        {
+          action: {
+            payload: {
+              customFeedbackHandlers: { useCustomSuccessHandler: true }
+            }
+          },
+          expectedDisplay: false
+        }
+      ];
+      assertionData.forEach(data => {
+        const action = (data.action as unknown) as FeedbackTriggeringAction;
 
-      const result = EffectFeedback.generateErrorFeedback(
-        'foo',
-        action,
-        'foo message'
-      );
-      expect(result.display).toEqual(true);
-    });
-
-    it('should display feedback if the action does not have a payload', () => {
-      const action = ({} as unknown) as FeedbackTriggeringAction;
-
-      const result = EffectFeedback.generateErrorFeedback(
-        'foo',
-        action,
-        'foo message'
-      );
-      expect(result.display).toBe(true);
-    });
-
-    it('should not display feedback if useCustomErrorHandler = true', () => {
-      const action = ({
-        payload: { customFeedbackHandlers: { useCustomErrorHandler: true } }
-      } as unknown) as FeedbackTriggeringAction;
-
-      const result = EffectFeedback.generateErrorFeedback(
-        'foo',
-        action,
-        'foo message'
-      );
-      expect(result.display).toEqual(false);
+        const result = EffectFeedback.generateSuccessFeedback(
+          'foo',
+          action,
+          'foo message'
+        );
+        expect(result.display).toEqual(data.expectedDisplay);
+      });
     });
   });
 });
