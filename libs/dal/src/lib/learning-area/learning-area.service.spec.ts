ed91a281c8e5ddc51b1b34d6d1fc263f152e1c0a
DavidBoute <david.boute@gmail.com>
2019-05-16 11:33:15 +0200

    add prefix to icon

--- libs/dal/src/lib/learning-area/learning-area.service.spec.ts
+++ libs/dal/src/lib/learning-area/learning-area.service.spec.ts
@@ -1,17 +1,26 @@
 import { inject, TestBed } from '@angular/core/testing';
 import { LearningAreaApi } from '@diekeure/polpo-api-angular-sdk';
+import { cold } from 'jasmine-marbles';
+import { of } from 'rxjs';
+import { LearningAreaInterface } from '../+models';
+import { LearningAreaFixture } from './../+fixtures/LearningArea.fixture';
 import { LearningAreaService } from './learning-area.service';
-
-export class MockLearningAreaApi {}
+import { LearningAreaServiceInterface } from './learning-area.service.interface';
 
 describe('LearningAreaService', () => {
+  let learningAreaApi: LearningAreaApi;
+  let learningAreaService: LearningAreaServiceInterface;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
         LearningAreaService,
-        { provide: LearningAreaApi, useClass: MockLearningAreaApi }
+        { provide: LearningAreaApi, useValue: { find: () => {} } }
       ]
     });
+
+    learningAreaApi = TestBed.get(LearningAreaApi);
+    learningAreaService = TestBed.get(LearningAreaService);
   });
 
   it('should be created', inject(
@@ -20,4 +29,22 @@ describe('LearningAreaService', () => {
       expect(service).toBeTruthy();
     }
   ));
+
+  it('should add a prefix to the icon, if needed', () => {
+    const mockLearningArea: LearningAreaInterface[] = [
+      new LearningAreaFixture({ id: 1, icon: 'foo' }),
+      new LearningAreaFixture({ id: 2, icon: 'learning-area:bar' })
+    ];
+
+    learningAreaApi.find = jest.fn().mockReturnValue(of(mockLearningArea));
+
+    expect(learningAreaService.getAll()).toBeObservable(
+      cold('(a|)', {
+        a: [
+          new LearningAreaFixture({ id: 1, icon: 'learning-area:foo' }),
+          new LearningAreaFixture({ id: 2, icon: 'learning-area:bar' })
+        ]
+      })
+    );
+  });
 });
