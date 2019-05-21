8633ecbb1049af6f276f69460eacf21ca6aa512f
DavidBoute <david.boute@gmail.com>
2019-05-21 10:54:24 +0200

    start upsert effect

--- libs/dal/src/lib/+state/history/history.effects.spec.ts
+++ libs/dal/src/lib/+state/history/history.effects.spec.ts
@@ -4,19 +4,28 @@ import { provideMockActions } from '@ngrx/effects/testing';
 import { Action, StoreModule } from '@ngrx/store';
 import { DataPersistence, NxModule } from '@nrwl/nx';
 import { hot } from '@nrwl/nx/testing';
-import { Observable, of } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { HistoryReducer } from '.';
+import { FavoriteFixture } from '../../+fixtures';
+import { HistoryInterface } from '../../+models';
 import {
   HistoryLoaded,
   HistoryLoadError,
-  LoadHistory
+  LoadHistory,
+  StartUpsertHistory,
+  UpsertHistory
 } from './history.actions';
-import { HistoryEffects, HISTORY_SERVICE_TOKEN } from './history.effects';
+import {
+  HistoryEffects,
+  HistoryServiceInterface,
+  HISTORY_SERVICE_TOKEN
+} from './history.effects';
 
 describe('HistoryEffects', () => {
   let actions: Observable<any>;
   let effects: HistoryEffects;
   let usedState: any;
+  let historyService: HistoryServiceInterface;
 
   const expectInAndOut = (
     effect: Observable<any>,
@@ -69,7 +78,8 @@ describe('HistoryEffects', () => {
         {
           provide: HISTORY_SERVICE_TOKEN,
           useValue: {
-            getAllForUser: () => {}
+            getAllForUser: () => {},
+            upsertHistory: () => {}
           }
         },
         HistoryEffects,
@@ -79,6 +89,7 @@ describe('HistoryEffects', () => {
     });
 
     effects = TestBed.get(HistoryEffects);
+    historyService = TestBed.get(HISTORY_SERVICE_TOKEN);
   });
 
   describe('loadHistory$', () => {
@@ -161,6 +172,47 @@ describe('HistoryEffects', () => {
       it('should return a error action if force is true', () => {
         expectInAndOut(effects.loadHistory$, forcedLoadAction, loadErrorAction);
       });
+    });
+  });
+
+  describe('startUpsertHistory$', () => {
+    let mockHistory: HistoryInterface;
+    let mockStartUpsertAction: StartUpsertHistory;
+    let upsertAction$: BehaviorSubject<StartUpsertHistory>;
+
+    beforeEach(() => {
+      // TODO replace with HistoryFixture when merged
+      mockHistory = new FavoriteFixture();
+      mockStartUpsertAction = new StartUpsertHistory({ history: mockHistory });
+      upsertAction$ = new BehaviorSubject(null);
+      actions = upsertAction$;
+    });
+
+    it('should call the upsertHistory function on the service', () => {
+      jest.spyOn(historyService, 'upsertHistory');
+      upsertAction$.next(mockStartUpsertAction);
+
+      // service won't be called unless there is a subscriber
+      effects.startUpsertHistory$.subscribe();
+
+      expect(historyService.upsertHistory).toHaveBeenCalled();
+      expect(historyService.upsertHistory).toHaveBeenCalledTimes(1);
+      expect(historyService.upsertHistory).toHaveBeenCalledWith(mockHistory);
+    });
+
+    it('should dispatch an UpsertHistory action', () => {
+      const upsertedHistory = { ...mockHistory, id: 42 };
+      jest
+        .spyOn(historyService, 'upsertHistory')
+        .mockReturnValue(of(upsertedHistory));
+
+      const expected = new UpsertHistory({ history: upsertedHistory });
+
+      expectInAndOut(
+        effects.startUpsertHistory$,
+        mockStartUpsertAction,
+        expected
+      );
     });
   });
 });
