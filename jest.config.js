7437833d1a78dacff61e65086393af37be5b10f6
Bert Casier <bert@ernie.be>
2019-05-22 20:21:39 +0200

    publish code coverage

--- jest.config.js
+++ jest.config.js
@@ -5,9 +5,9 @@ module.exports = {
   },
   resolver: '@nrwl/builders/plugins/jest/resolver',
   moduleFileExtensions: ['ts', 'js', 'html'],
-  collectCoverage: false,
-  coverageReporters: ['html'],
-  reporters: process.env.JUNIT
+  collectCoverage: process.env.CI,
+  coverageReporters: ['cobertura'],
+  reporters: process.env.CI
     ? ['default', ['jest-junit', { outputDirectory: './junit', outputName: new Date().getTime() + '.xml' }]]
     : ['default'],
   bail: true
