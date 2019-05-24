50a47fdf11f5a6864b5bca75cc0dd243d1dd0eca
Pieter Verhelle <pieter.verhelle@gmail.com>
2019-05-24 12:16:59 +0200

    add example logout

--- apps/polpo-classroom-web-e2e/src/integration/app.spec.ts
+++ apps/polpo-classroom-web-e2e/src/integration/app.spec.ts
@@ -1,8 +1,8 @@
 /// <reference types="cypress" />
 
-import { login } from '../support/app.po';
+import { login, logoutByUI } from '../support/app.po';
 
-describe('temp', () => {
+describe('api login example', () => {
   beforeEach(() => {
     login('teacher1', 'testje');
     cy.visit('dev');
@@ -10,5 +10,15 @@ describe('temp', () => {
 
   it('should display welcome message', () => {
     cy.get('h1').contains('Welcome to the dev side');
+  });
+});
+
+describe('exported const as command example', () => {
+  beforeEach(() => {
+    login('teacher1', 'testje');
+    cy.visit('');
+  });
+  it('should logout if the logout using the ui command', () => {
+    logoutByUI();
   });
 });
