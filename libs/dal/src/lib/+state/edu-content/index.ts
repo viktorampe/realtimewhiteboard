05b7b7cc0f69a05490179ba2e4b003621803713f
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2019-05-24 09:17:36 +0200

    remove export asAeduContent

--- libs/dal/src/lib/+state/edu-content/index.ts
+++ libs/dal/src/lib/+state/edu-content/index.ts
@@ -2,12 +2,10 @@ import * as EduContentActions from './edu-content.actions';
 import { EduContentsEffects } from './edu-content.effects';
 import * as EduContentReducer from './edu-content.reducer';
 import * as EduContentQueries from './edu-content.selectors';
-import { asEduContent } from './edu-content.selectors';
 
 export {
   EduContentActions,
   EduContentReducer,
   EduContentQueries,
-  EduContentsEffects,
-  asEduContent
+  EduContentsEffects
 };
