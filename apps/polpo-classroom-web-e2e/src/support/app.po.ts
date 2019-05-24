893d43c1792b41e2859acfc298749162b8bcc66a
Pieter Verhelle <pieter.verhelle@gmail.com>
2019-05-24 12:01:01 +0200

    add login command

--- apps/polpo-classroom-web-e2e/src/support/app.po.ts
+++ apps/polpo-classroom-web-e2e/src/support/app.po.ts
@@ -1,2 +1,23 @@
 /// <reference types="cypress" />
-export const getGreeting = () => cy.get('h1');
+
+const apiUrl = Cypress.env('apiUrl');
+
+export const login = (username: string, password: string) =>
+  cy
+    .request({
+      method: 'POST',
+      url: `${apiUrl}api/People/login?include=user`,
+      body: {
+        username: username,
+        password: password
+      }
+    })
+    .then(resp => {
+      // set the cookies that the loopback sdk needs
+      cy.setCookie('$LoopBackSDK$created', resp.body.created);
+      cy.setCookie('$LoopBackSDK$id', resp.body.id);
+      cy.setCookie('$LoopBackSDK$rememberMe', 'true');
+      cy.setCookie('$LoopBackSDK$ttl', resp.body.ttl + '');
+      cy.setCookie('$LoopBackSDK$user', JSON.stringify(resp.body.user));
+      cy.setCookie('$LoopBackSDK$userId', resp.body.userId + '');
+    });
