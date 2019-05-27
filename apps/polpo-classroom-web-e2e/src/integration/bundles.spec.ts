574eb303caeb1ceb4e651b7dc497f65fc8b97adc
pverhelle <pieter.verhelle@gmail.com>
2019-05-27 16:55:40 +0200

    add preliminary tests

---
+++ apps/polpo-classroom-web-e2e/src/integration/bundles.spec.ts
@@ -0,0 +1,104 @@
+/// <reference types="cypress" />
+
+import { dataCy, login, performStudentSetup } from '../support/commands';
+import { StudentOpenBundleContentInterface } from '../support/interfaces';
+
+describe('Bundles', () => {
+  const bundlesPath = 'bundles';
+  let setup: StudentOpenBundleContentInterface;
+  before(() => {
+    performStudentSetup().then(res => {
+      setup = res.body;
+      console.log(JSON.stringify(res.body));
+    });
+  });
+  beforeEach(() => {
+    login(
+      setup.studentOpenBundleContent.login.username,
+      setup.studentOpenBundleContent.login.password
+    );
+  });
+  describe('leaningarea page', () => {
+    beforeEach(() => {
+      cy.visit(`${bundlesPath}`);
+    });
+    it('should show learningAreas', () => {
+      dataCy('learningArea-count').contains('1 van 1 weergegeven');
+      dataCy('learningArea-details')
+        .contains('1 bundel')
+        .contains('1 boek');
+      dataCy('learningArea')
+        .should(
+          'have.attr',
+          'ng-reflect-router-link',
+          `${setup.studentOpenBundleContent.learningArea.id}`
+        )
+        .contains(setup.studentOpenBundleContent.learningArea.name)
+        .click()
+        .location('pathname')
+        .should(
+          'be',
+          `${bundlesPath}/${setup.studentOpenBundleContent.learningArea.id}`
+        );
+    });
+  });
+  describe('bundles page', () => {
+    beforeEach(() => {
+      cy.visit(
+        `${bundlesPath}/${setup.studentOpenBundleContent.learningArea.id}`
+      );
+    });
+    it('should show the bundle', () => {
+      dataCy('bundle-count').contains('1 van 1 weergegeven');
+      dataCy('bundle-details').contains('3 items');
+      dataCy('bundle')
+        .contains(`${setup.studentOpenBundleContent.bundle.name}`)
+        .click()
+        .location('pathname')
+        .should(
+          'be',
+          `${bundlesPath}/${
+            setup.studentOpenBundleContent.bundle.learningAreaId
+          }/${setup.studentOpenBundleContent.bundle.id}`
+        );
+    });
+    it('should show the boeke', () => {
+      dataCy('boeke-count').contains(
+        `Je hebt 1 boek voor ${
+          setup.studentOpenBundleContent.learningArea.name
+        }`
+      );
+      dataCy('boeke').contains('boek-e');
+      //TODO -- add check for window opening when clicked
+    });
+  });
+  describe('bundle details page', () => {
+    beforeEach(() => {
+      cy.visit(
+        `${bundlesPath}/${setup.studentOpenBundleContent.learningArea.id}/${
+          setup.studentOpenBundleContent.bundle.id
+        }`
+      );
+    });
+    it('should show the bundle details', () => {
+      dataCy('bundle-header').contains(
+        `${setup.studentOpenBundleContent.bundle.name}`
+      );
+      dataCy('bundle-count').contains('3 van 3 weergegeven');
+      dataCy('contents')
+        .children() //first child is the ngfor container
+        .children() //container children are the actual content thumbnail elements
+        .should('have.length', 3)
+        .as('contents');
+      cy.get('@contents')
+        .first()
+        .contains('oefening');
+      cy.get('@contents')
+        .eq(1)
+        .contains('mp4');
+      cy.get('@contents')
+        .last()
+        .contains('link');
+    });
+  });
+});
