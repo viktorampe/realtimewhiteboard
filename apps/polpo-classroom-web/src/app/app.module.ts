bdaff39b3d7f054a06007979aab1fd1cb4cda0f7
pverhelle <pieter.verhelle@gmail.com>
2019-07-01 15:42:10 +0200

    remove non-used app reducer

--- apps/polpo-classroom-web/src/app/app.module.ts
+++ apps/polpo-classroom-web/src/app/app.module.ts
@@ -21,10 +21,6 @@ import { NxModule } from '@nrwl/nx';
 import { storeFreeze } from 'ngrx-store-freeze';
 import { configureBufferSize, handleUndo } from 'ngrx-undo';
 import { environment } from '../environments/environment';
-import {
-  appReducer,
-  initialState as appInitialState
-} from './+state/app.reducer';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EduContentSearchResultComponent } from './components/searchresults/edu-content-search-result.component';
@@ -68,9 +64,8 @@ configureBufferSize(150);
     DalModule.forRoot({ apiBaseUrl: environment.api.APIBase }),
     GuardsModule,
     StoreModule.forRoot(
-      { app: appReducer, router: routerReducer },
+      { app: undefined, router: routerReducer },
       {
-        initialState: appInitialState,
         metaReducers: !environment.production
           ? [storeFreeze, handleUndo]
           : [handleUndo]
