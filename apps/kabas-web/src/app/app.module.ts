8c1945f916456f2942cba81241a3e5f89c4412e7
pverhelle <pieter.verhelle@gmail.com>
2019-07-02 10:54:07 +0200

    add neede modules for basic routing to the app module

--- apps/kabas-web/src/app/app.module.ts
+++ apps/kabas-web/src/app/app.module.ts
@@ -1,18 +1,51 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { MatIconModule } from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { routerReducer } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { NxModule } from '@nrwl/nx';
-import { RouterModule } from '@angular/router';
+import { storeFreeze } from 'ngrx-store-freeze';
+import { configureBufferSize, handleUndo } from 'ngrx-undo';
+import { SharedModule } from '../../../../libs/shared/src';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+configureBufferSize(150);
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatIconModule,
+    SharedModule.forRoot(
+      environment.features.alerts,
+      environment.features.messages,
+      environment.features.errorManagement,
+      environment.iconMapping,
+      environment.website,
+      environment.logout,
+      environment.login,
+      environment.termPrivacy,
+      environment.api,
+      environment.sso,
+      environment.searchModes,
+      environment.testing
+    ),
     NxModule.forRoot(),
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([]),
+    StoreModule.forRoot(
+      { app: undefined, router: routerReducer },
+      {
+        metaReducers: !environment.production
+          ? [storeFreeze, handleUndo]
+          : [handleUndo]
+      }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
