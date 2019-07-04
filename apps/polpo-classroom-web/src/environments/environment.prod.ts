db9f80dad08fb4a77c3f9ff710298ab2ed41de00
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2019-07-04 13:42:53 +0200

    Update production environment settings

--- apps/polpo-classroom-web/src/environments/environment.prod.ts
+++ apps/polpo-classroom-web/src/environments/environment.prod.ts
@@ -1,24 +1,178 @@
+import { Type } from '@angular/core';
+import { SearchFilterFactory } from '@campus/search';
+import { EduContentSearchResultComponent } from '../app/components/searchresults/edu-content-search-result.component';
+import { GlobalSearchTermFilterFactory } from '../app/factories/global-search-term-filter/global-search-term-filter.factory';
+import { SearchTermFilterFactory } from '../app/factories/search-term-filter/search-term-filter.factory';
+import { icons } from './icons';
+
 export const environment = {
   production: true,
   // promo website settings
   website: {
-    url: 'https://www.polpo.be'
+    url: 'https://www.polpo.be',
+    title: 'POLPO',
+    favicon: 'assets/icons/favicon.ico'
+  },
+  login: {
+    url: 'https://www.polpo.be/identificatie/start'
   },
   logout: {
     url: 'https://www.polpo.be/identificatie/start'
+  },
+  termPrivacy: {
+    url: 'https://www.polpo.be/identificatie/start'
+  },
+  iconMapping: icons,
+  api: {
+    APIBase: 'https://api.polpo.be'
   },
   features: {
     alerts: {
       enabled: true,
       hasAppBarDropDown: true,
-      appBarPollingInterval: 30000
+      appBarPollingInterval: 3000
     },
     messages: {
-      enabled: true,
-      hasAppBarDropDown: true
+      enabled: false,
+      hasAppBarDropDown: false
     },
     errorManagement: {
-      managedStatusCodes: [500, 401, 404]
+      managedStatusCodes: [500, 401, 404, 0],
+      allowedErrors: [
+        {
+          status: 404,
+          statusText: 'Not Found',
+          urlRegex: 'http.*assets\\/icons.*.svg'
+        },
+        {
+          status: 404,
+          messageRegex: 'no_teacher_found_for_given_key'
+        }
+      ]
     }
+  },
+  sso: {
+    facebook: {
+      enabled: true,
+      linkUrl: 'https://api.polpo.be/link/facebook/callback',
+      description: 'Facebook',
+      logoIcon: 'facebook',
+      className: 'button-facebook'
+    },
+    google: {
+      enabled: true,
+      linkUrl: 'https://api.polpo.be/link/google/callback',
+      description: 'Google',
+      logoIcon: 'google',
+      className: 'button-google'
+    },
+    smartschool: {
+      enabled: true,
+      linkUrl: 'https://api.polpo.be/link/smartschool/callback',
+      description: 'Smartschool',
+      logoIcon: 'smartschool:orange',
+      className: 'button-smartschool',
+      maxNumberAllowed: 10
+    }
+  },
+  searchModes: {
+    toc: {
+      name: 'toc',
+      label: 'Zoeken op <b>inhoudstafel</b>',
+      dynamicFilters: false,
+      //TODO: All '{} as Type' must be replaced with actual components
+      searchFilterFactory: {} as Type<SearchFilterFactory>,
+      results: {
+        component: EduContentSearchResultComponent,
+        sortModes: [
+          {
+            description: 'alfabetisch',
+            name: 'title.raw',
+            icon: 'sort-alpha-down'
+          }
+        ],
+        pageSize: 20
+      }
+    },
+    plan: {
+      name: 'plan',
+      label: 'Zoeken op <b>leerplan</b>',
+      dynamicFilters: false,
+      searchFilterFactory: {} as Type<SearchFilterFactory>,
+      results: {
+        component: EduContentSearchResultComponent,
+        sortModes: [
+          {
+            description: 'alfabetisch',
+            name: 'title.raw',
+            icon: 'sort-alpha-down'
+          }
+        ],
+        pageSize: 20
+      }
+    },
+    term: {
+      name: 'term',
+      label: '<b>Standaard</b> zoeken',
+      dynamicFilters: false,
+      searchTerm: {
+        domHost: 'hostTop'
+      },
+      searchFilterFactory: SearchTermFilterFactory,
+      results: {
+        component: EduContentSearchResultComponent,
+        sortModes: [
+          {
+            description: 'relevantie',
+            name: '_score',
+            icon: 'sort-numeric-down'
+          },
+          {
+            description: 'alfabetisch',
+            name: 'title.raw',
+            icon: 'sort-alpha-down'
+          },
+          {
+            description: 'laatst gewijzigd',
+            name: 'published',
+            icon: 'calendar-plus'
+          }
+        ],
+        pageSize: 20
+      }
+    },
+    globalterm: {
+      name: 'globalterm',
+      label: '<b>Standaard</b> zoeken',
+      dynamicFilters: true,
+      searchTerm: {
+        domHost: 'hostTop'
+      },
+      searchFilterFactory: GlobalSearchTermFilterFactory,
+      results: {
+        component: EduContentSearchResultComponent,
+        sortModes: [
+          {
+            description: 'relevantie',
+            name: '_score',
+            icon: 'sort-numeric-down'
+          },
+          {
+            description: 'alfabetisch',
+            name: 'title.raw',
+            icon: 'sort-alpha-down'
+          },
+          {
+            description: 'laatst gewijzigd',
+            name: 'published',
+            icon: 'calendar-plus'
+          }
+        ],
+        pageSize: 20
+      }
+    }
+  },
+  testing: {
+    removeDataCyAttributes: true
   }
 };
