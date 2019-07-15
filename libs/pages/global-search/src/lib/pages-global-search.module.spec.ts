a17e86d54de251f689988967888b2b77ab5517e2
pverhelle <pieter.verhelle@gmail.com>
2019-07-15 09:28:23 +0200

    use ng bullet for this test

--- libs/pages/global-search/src/lib/pages-global-search.module.spec.ts
+++ libs/pages/global-search/src/lib/pages-global-search.module.spec.ts
@@ -1,12 +1,13 @@
-import { async, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { configureTestSuite } from 'ng-bullet';
 import { PagesGlobalSearchModule } from './pages-global-search.module';
 
 describe('PagesGlobalSearchModule', () => {
-  beforeEach(async(() => {
+  configureTestSuite(() => {
     TestBed.configureTestingModule({
       imports: [PagesGlobalSearchModule]
     }).compileComponents();
-  }));
+  });
 
   it('should create', () => {
     expect(PagesGlobalSearchModule).toBeDefined();
