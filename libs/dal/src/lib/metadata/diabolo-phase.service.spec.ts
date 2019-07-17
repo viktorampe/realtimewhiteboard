d7aae023b78bcbe1796382c23c37d6a4957430b7
DavidBoute <david.boute@gmail.com>
2019-07-17 11:33:02 +0200

    unit test

--- libs/dal/src/lib/metadata/diabolo-phase.service.spec.ts
+++ libs/dal/src/lib/metadata/diabolo-phase.service.spec.ts
@@ -1,12 +1,45 @@
 import { TestBed } from '@angular/core/testing';
-
+import { DiaboloPhaseApi } from '@diekeure/polpo-api-angular-sdk';
+import { hot } from 'jasmine-marbles';
+import { configureTestSuite } from 'ng-bullet';
+import { DiaboloPhaseFixture } from './../+fixtures/DiaboloPhase.fixture';
 import { DiaboloPhaseService } from './diabolo-phase.service';
+import { DiaboloPhaseServiceInterface } from './diabolo-phase.service.interface';
 
 describe('DiaboloPhaseService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let mockData$: any;
+  let diaboloPhaseService: DiaboloPhaseServiceInterface;
+
+  configureTestSuite(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: DiaboloPhaseApi, useValue: { find: () => mockData$ } }
+      ]
+    });
+
+    diaboloPhaseService = TestBed.get(DiaboloPhaseService);
+  });
 
   it('should be created', () => {
-    const service: DiaboloPhaseService = TestBed.get(DiaboloPhaseService);
-    expect(service).toBeTruthy();
+    expect(diaboloPhaseService).toBeTruthy();
+  });
+
+  describe('getAll', () => {
+    it('should return diaboloPhases', async () => {
+      mockData$ = hot('-a-|', {
+        a: [
+          new DiaboloPhaseFixture({ id: 1 }),
+          new DiaboloPhaseFixture({ id: 2 })
+        ]
+      });
+      expect(diaboloPhaseService.getAll()).toBeObservable(
+        hot('-a-|', {
+          a: [
+            new DiaboloPhaseFixture({ id: 1 }),
+            new DiaboloPhaseFixture({ id: 2 })
+          ]
+        })
+      );
+    });
   });
 });
