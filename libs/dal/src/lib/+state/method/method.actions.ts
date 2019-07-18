3706982e937c9c28bb12555f0c521dad899126fd
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2019-07-18 14:53:10 +0200

    Frederic CR

--- libs/dal/src/lib/+state/method/method.actions.ts
+++ libs/dal/src/lib/+state/method/method.actions.ts
@@ -11,7 +11,9 @@ export enum MethodsActionTypes {
 export class LoadMethods implements Action {
   readonly type = MethodsActionTypes.LoadMethods;
 
-  constructor(public payload: { force?: boolean; userId: number } = {}) {}
+  constructor(
+    public payload: { force?: boolean; userId: number } = { userId: null }
+  ) {}
 }
 
 export class MethodsLoaded implements Action {
