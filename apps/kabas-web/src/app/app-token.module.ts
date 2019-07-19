8d0289ac553412e29c63aaad04c281b9eb7fc343
Pieter Verhelle <pieter.verhelle@gmail.com>
2019-07-19 14:37:55 +0200

    fix formatting error due to merge

--- apps/kabas-web/src/app/app-token.module.ts
+++ apps/kabas-web/src/app/app-token.module.ts
@@ -1,5 +1,14 @@
 import { NgModule } from '@angular/core';
-import { AuthService, AUTH_SERVICE_TOKEN, DiaboloPhaseService, DIABOLO_PHASE_SERVICE_TOKEN, PersonService, PERSON_SERVICE_TOKEN, TocService, TOC_SERVICE_TOKEN } from '@campus/dal';
+import {
+  AuthService,
+  AUTH_SERVICE_TOKEN,
+  DiaboloPhaseService,
+  DIABOLO_PHASE_SERVICE_TOKEN,
+  PersonService,
+  PERSON_SERVICE_TOKEN,
+  TocService,
+  TOC_SERVICE_TOKEN
+} from '@campus/dal';
 
 @NgModule({
   providers: [
@@ -7,7 +16,7 @@ import { AuthService, AUTH_SERVICE_TOKEN, DiaboloPhaseService, DIABOLO_PHASE_SER
 
     // dal services
     { provide: AUTH_SERVICE_TOKEN, useClass: AuthService },
-    { provide: TOC_SERVICE_TOKEN, useClass: TocService }
+    { provide: TOC_SERVICE_TOKEN, useClass: TocService },
     { provide: PERSON_SERVICE_TOKEN, useClass: PersonService },
     { provide: DIABOLO_PHASE_SERVICE_TOKEN, useClass: DiaboloPhaseService }
   ]
