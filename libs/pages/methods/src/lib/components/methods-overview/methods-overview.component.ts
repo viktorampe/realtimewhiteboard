8ff223694c993f2bef728309154f4f9889ca0a42
DavidBoute <david.boute@gmail.com>
2019-07-22 11:53:09 +0200

    setup component prototype on page

--- libs/pages/methods/src/lib/components/methods-overview/methods-overview.component.ts
+++ libs/pages/methods/src/lib/components/methods-overview/methods-overview.component.ts
@@ -6,6 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./methods-overview.component.scss']
 })
 export class MethodsOverviewComponent implements OnInit {
+  public data = {
+    logoUrl: 'beautemps.svg',
+    name: 'testnaam',
+    years: [
+      {
+        id: 1,
+        name: 'L1',
+        bookId: 2
+      },
+      {
+        id: 2,
+        name: 'L2',
+        bookId: 3
+      }
+    ]
+  };
   constructor() {}
 
   ngOnInit() {}
--- libs/pages/methods/src/lib/pages-methods.module.ts
+++ libs/pages/methods/src/lib/pages-methods.module.ts
@@ -1,20 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from '@campus/shared';
+import { UiModule } from '@campus/ui';
 import { MethodChapterLessonComponent } from './components/method-chapter-lesson/method-chapter-lesson.component';
 import { MethodChapterComponent } from './components/method-chapter/method-chapter.component';
+import { MethodYearTileComponent } from './components/method-year-tile/method-year-tile.component';
 import { MethodComponent } from './components/method/method.component';
 import { MethodsOverviewComponent } from './components/methods-overview/methods-overview.component';
 import { PagesMethodsRoutingModule } from './pages-pages-routing.module';
-import { MethodYearTileComponent } from './components/method-year-tile/method-year-tile.component';
 
 @NgModule({
-  imports: [CommonModule, PagesMethodsRoutingModule],
+  imports: [CommonModule, UiModule, SharedModule, PagesMethodsRoutingModule],
   declarations: [
     MethodsOverviewComponent,
     MethodComponent,
     MethodChapterComponent,
     MethodChapterLessonComponent,
     MethodYearTileComponent
-  ]
+  ],
+  exports: []
 })
 export class PagesMethodsModule {}
