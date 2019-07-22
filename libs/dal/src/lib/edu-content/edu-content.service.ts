5a59630e4339ccc9f0dff1575a64d33c1a447662
pverhelle <pieter.verhelle@gmail.com>
2019-07-22 11:51:50 +0200

    make correction in call name for sdk

--- libs/dal/src/lib/edu-content/edu-content.service.ts
+++ libs/dal/src/lib/edu-content/edu-content.service.ts
@@ -25,10 +25,10 @@ export class EduContentService implements EduContentServiceInterface {
       );
   }
 
-  getGeneralEduContentsForBookId(
+  getGeneralEduContentForBookId(
     bookId: number
   ): Observable<EduContentInterface[]> {
-    return this.eduContentApi.getGeneralEduContentsForBookId(bookId);
+    return this.eduContentApi.getGeneralEduContentForBookId(bookId);
   }
 
   search(state: SearchStateInterface): Observable<SearchResultInterface> {
