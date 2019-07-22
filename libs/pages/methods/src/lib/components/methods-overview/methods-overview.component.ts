b2f2eec3911442c618c20955b9a6c35f265a17d1
DavidBoute <david.boute@gmail.com>
2019-07-22 14:59:08 +0200

    basic page layout

--- libs/pages/methods/src/lib/components/methods-overview/methods-overview.component.ts
+++ libs/pages/methods/src/lib/components/methods-overview/methods-overview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'campus-methods-overview',
@@ -6,32 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./methods-overview.component.scss']
 })
 export class MethodsOverviewComponent implements OnInit {
-  public data = {
-    logoUrl: 'beautemps.svg',
-    name: 'testnaam',
-    years: [
-      {
-        id: 1,
-        name: 'L1',
-        bookId: 2
-      },
-      {
-        id: 2,
-        name: 'L2',
-        bookId: 3
-      },
-      {
-        id: 3,
-        name: 'L3',
-        bookId: 4
-      },
-      {
-        id: 4,
-        name: 'L4',
-        bookId: 5
-      }
-    ]
-  };
+  public books$ = of([
+    {
+      logoUrl: 'beautemps.svg',
+      name: 'testnaam',
+      years: [
+        {
+          id: 1,
+          name: 'L1',
+          bookId: 2
+        },
+        {
+          id: 2,
+          name: 'L2',
+          bookId: 3
+        },
+        {
+          id: 3,
+          name: 'L3',
+          bookId: 4
+        },
+        {
+          id: 4,
+          name: 'L4',
+          bookId: 5
+        }
+      ]
+    }
+  ]);
   constructor() {}
 
   ngOnInit() {}
