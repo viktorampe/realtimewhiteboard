33ff5762a44006a265ec7a8e07086cb5c7796570
DavidBoute <david.boute@gmail.com>
2019-07-23 10:54:27 +0200

    fix tests

--- libs/pages/methods/src/lib/components/method-year-tile/method-year-tile.component.spec.ts
+++ libs/pages/methods/src/lib/components/method-year-tile/method-year-tile.component.spec.ts
@@ -1,16 +1,19 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { UiModule } from '@campus/ui';
+import { configureTestSuite } from 'ng-bullet';
 import { MethodYearTileComponent } from './method-year-tile.component';
 
 describe('MethodYearTileComponent', () => {
   let component: MethodYearTileComponent;
   let fixture: ComponentFixture<MethodYearTileComponent>;
 
-  beforeEach(async(() => {
+  configureTestSuite(() => {
     TestBed.configureTestingModule({
+      imports: [UiModule, RouterTestingModule],
       declarations: [MethodYearTileComponent]
-    }).compileComponents();
-  }));
+    });
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MethodYearTileComponent);
