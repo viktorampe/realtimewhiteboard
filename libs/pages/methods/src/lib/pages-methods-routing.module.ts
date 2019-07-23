ea11ca74b3156736e660cead2a6011d827b7af2d
pverhelle <pieter.verhelle@gmail.com>
2019-07-23 11:53:33 +0200

    change toc in route back to book since we use the bookId

--- libs/pages/methods/src/lib/pages-methods-routing.module.ts
+++ libs/pages/methods/src/lib/pages-methods-routing.module.ts
@@ -20,7 +20,7 @@ const routes: Routes = [
         component: MethodsOverviewComponent
       },
       {
-        path: ':toc',
+        path: ':book',
         resolve: { isResolved: MethodsResolver },
         runGuardsAndResolvers: 'always',
         children: [
