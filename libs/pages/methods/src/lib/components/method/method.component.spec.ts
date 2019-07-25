9297573a10d5d83fa5d0ad2ddf768135b3f583ce
Thomas Ghesquiere <thomas.ghesquiere@gmail.com>
2019-07-25 15:29:47 +0200

    fix test imports

--- libs/pages/methods/src/lib/components/method/method.component.spec.ts
+++ libs/pages/methods/src/lib/components/method/method.component.spec.ts
@@ -1,4 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  MatCardModule,
+  MatIconModule,
+  MatListModule,
+  MatTabsModule
+} from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { UiModule } from '@campus/ui';
 import { configureTestSuite } from 'ng-bullet';
 import { MethodViewModel } from '../method.viewmodel';
 import { MockMethodViewModel } from '../method.viewmodel.mock';
@@ -11,6 +20,15 @@ describe('MethodComponent', () => {
 
   configureTestSuite(() => {
     TestBed.configureTestingModule({
+      imports: [
+        MatCardModule,
+        MatIconModule,
+        MatListModule,
+        MatTabsModule,
+        NoopAnimationsModule,
+        RouterTestingModule,
+        UiModule
+      ],
       declarations: [MethodComponent],
       providers: [{ provide: MethodViewModel, useClass: MockMethodViewModel }]
     }).compileComponents();
