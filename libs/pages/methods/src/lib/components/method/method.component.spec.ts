6c8d8235616747ff3f32cff7f2ed5587b278f02a
Thomas Ghesquiere <thomas.ghesquiere@gmail.com>
2019-07-25 17:50:38 +0200

    methodComponent: tests + mocks

--- libs/pages/methods/src/lib/components/method/method.component.spec.ts
+++ libs/pages/methods/src/lib/components/method/method.component.spec.ts
@@ -1,14 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import {
+  MatCard,
   MatCardModule,
   MatIconModule,
+  MatListItem,
   MatListModule,
   MatTabsModule
 } from '@angular/material';
+import { By } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ActivatedRoute, Params } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { UiModule } from '@campus/ui';
 import { configureTestSuite } from 'ng-bullet';
+import { BehaviorSubject } from 'rxjs';
 import { MethodViewModel } from '../method.viewmodel';
 import { MockMethodViewModel } from '../method.viewmodel.mock';
 import { MethodComponent } from './method.component';
@@ -17,8 +22,10 @@ describe('MethodComponent', () => {
   let component: MethodComponent;
   let fixture: ComponentFixture<MethodComponent>;
   let methodViewModel: MethodViewModel;
+  let params: BehaviorSubject<Params>;
 
   configureTestSuite(() => {
+    params = new BehaviorSubject<Params>({ book: 1 });
     TestBed.configureTestingModule({
       imports: [
         MatCardModule,
@@ -30,7 +37,10 @@ describe('MethodComponent', () => {
         UiModule
       ],
       declarations: [MethodComponent],
-      providers: [{ provide: MethodViewModel, useClass: MockMethodViewModel }]
+      providers: [
+        { provide: ActivatedRoute, useValue: { params } },
+        { provide: MethodViewModel, useClass: MockMethodViewModel }
+      ]
     }).compileComponents();
 
     methodViewModel = TestBed.get(MethodViewModel);
@@ -44,5 +54,27 @@ describe('MethodComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show the chapter links', () => {
+    const chapters = fixture.debugElement.queryAll(By.directive(MatListItem));
+    expect(chapters.length).toBe(4);
+  });
+
+  it('should show the method info card', () => {
+    const methodCard = fixture.debugElement.query(By.directive(MatCard));
+    expect(methodCard).toBeDefined();
+  });
+
+  it('should show the general files', () => {
+    const generalFiles = fixture.debugElement.queryAll(
+      By.css('.method-method__container__file')
+    );
+    expect(generalFiles.length).toBe(3);
+
+    const productTypeHeaders = fixture.debugElement.queryAll(
+      By.css('div[main] h3[mat-subheader]')
+    );
+    expect(productTypeHeaders.length).toBe(2);
   });
 });
