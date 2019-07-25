7e568610af69cf25545029a6c6aa3072dd9987bd
pverhelle <pieter.verhelle@gmail.com>
2019-07-25 13:11:32 +0200

    move helper functions to seperate file so they can be used in multiple tests

--- libs/dal/src/lib/+state/edu-content-book/edu-content-book.selectors.spec.ts
+++ libs/dal/src/lib/+state/edu-content-book/edu-content-book.selectors.spec.ts
@@ -1,59 +1,6 @@
 import { EduContentBookQueries } from '.';
-import { EduContentBookInterface } from '../../+models';
+import { createEduContentBook, createState } from './edu-content-book.helpers';
 import { State } from './edu-content-book.reducer';
-
-export function createState(
-  eduContentBooks: EduContentBookInterface[],
-  loaded: boolean = false,
-  error?: any
-): State {
-  return {
-    ids: eduContentBooks
-      ? eduContentBooks.map(eduContentBook => eduContentBook.id)
-      : [],
-    entities: eduContentBooks
-      ? eduContentBooks.reduce(
-          (entityMap, eduContentBook) => ({
-            ...entityMap,
-            [eduContentBook.id]: eduContentBook
-          }),
-          {}
-        )
-      : {},
-    loaded: loaded,
-    error: error
-  };
-}
-
-interface CreateEduContentBookOptions {
-  methodId?: number;
-  years?: {
-    id: number;
-    name: string;
-  }[];
-  diabolo?: boolean;
-}
-
-export function createEduContentBook(
-  id: number,
-  options?: CreateEduContentBookOptions
-): EduContentBookInterface | any {
-  const data: EduContentBookInterface | any = {
-    id: id
-  };
-  if (options) {
-    if (options.methodId) {
-      data.methodId = options.methodId;
-      data.method = {
-        name: `method ${options.methodId}`,
-        logoUrl: `logo for method ${options.methodId}`
-      };
-    }
-    if (options.years) data.years = options.years;
-    if (options.diabolo) data.diabolo = options.diabolo;
-  }
-  return data;
-}
 
 const eduContentBooksArray = [
   createEduContentBook(4),
