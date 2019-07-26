6d6f78d85c40ed0059e96e5fb5ad738f3994432b
pverhelle <pieter.verhelle@gmail.com>
2019-07-26 10:36:20 +0200

    add load actions and loaded queries to guard

--- libs/guards/src/lib/can-activate/allowed-method.guard.ts
+++ libs/guards/src/lib/can-activate/allowed-method.guard.ts
@@ -1,23 +1,48 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
   RouterStateSnapshot
 } from '@angular/router';
-import { DalState, EduContentBookQueries, MethodQueries } from '@campus/dal';
+import {
+  AuthServiceInterface,
+  AUTH_SERVICE_TOKEN,
+  DalState,
+  EduContentBookActions,
+  EduContentBookQueries,
+  MethodActions,
+  MethodQueries
+} from '@campus/dal';
 import { select, Store } from '@ngrx/store';
 import { Observable, of } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { filter, switchMap, tap } from 'rxjs/operators';
 
 @Injectable()
 export class AllowedMethodGuard implements CanActivate {
-  constructor(private store: Store<DalState>) {}
+  constructor(
+    private store: Store<DalState>,
+    @Inject(AUTH_SERVICE_TOKEN) private authService: AuthServiceInterface
+  ) {}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
+    this.store.dispatch(
+      new MethodActions.LoadAllowedMethods({ userId: this.authService.userId })
+    );
     return this.store.pipe(
-      select(EduContentBookQueries.getById, { id: +route.params.book }),
+      select(MethodQueries.getAllowedMethodsLoaded),
+      filter(allowedMethodsLoaded => allowedMethodsLoaded),
+      tap(() =>
+        this.store.dispatch(new EduContentBookActions.LoadEduContentBooks())
+      ),
+      switchMap(() => this.store.pipe(select(EduContentBookQueries.getLoaded))),
+      filter(eduContentBooksLoaded => eduContentBooksLoaded),
+      switchMap(() =>
+        this.store.pipe(
+          select(EduContentBookQueries.getById, { id: +route.params.book })
+        )
+      ),
       switchMap(book => {
         if (!book) return of(false);
         return this.store.pipe(
