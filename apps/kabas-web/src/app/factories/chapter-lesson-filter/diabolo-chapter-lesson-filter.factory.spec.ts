3f1935423e226b888a170da2d90b964780c7cc6a
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2019-07-29 09:20:27 +0200

    Add diabolo chapter lesson factory tests

---
+++ apps/kabas-web/src/app/factories/chapter-lesson-filter/diabolo-chapter-lesson-filter.factory.spec.ts
@@ -0,0 +1,154 @@
+import { TestBed } from '@angular/core/testing';
+import {
+  DalState,
+  DiaboloPhaseActions,
+  DiaboloPhaseReducer,
+  EduContentProductTypeActions,
+  EduContentProductTypeFixture,
+  EduContentProductTypeReducer,
+  getStoreModuleForFeatures
+} from '@campus/dal';
+import {
+  SearchFilterInterface,
+  SearchStateInterface,
+  SelectFilterComponent
+} from '@campus/search';
+import { Store, StoreModule } from '@ngrx/store';
+import { cold } from '@nrwl/nx/testing';
+import { DiaboloPhaseFixture } from 'libs/dal/src/lib/+fixtures/DiaboloPhase.fixture';
+import { configureTestSuite } from 'ng-bullet';
+import { DiaboloChapterLessonFilterFactory } from './diabolo-chapter-lesson-filter.factory';
+
+describe('DiaboloChapterLessonFilterFactory', () => {
+  let store: Store<DalState>;
+  let factory: DiaboloChapterLessonFilterFactory;
+
+  const mockEduContentProductTypes = [
+    new EduContentProductTypeFixture({ id: 12, parent: 0 }),
+    new EduContentProductTypeFixture({ id: 13, parent: 0 }),
+    new EduContentProductTypeFixture({ id: 14, parent: 13 })
+  ];
+
+  const mockDiaboloPhases = [
+    new DiaboloPhaseFixture({ id: 1, phase: 1 }),
+    new DiaboloPhaseFixture({ id: 2, phase: 2 }),
+    new DiaboloPhaseFixture({ id: 3, phase: 3 })
+  ];
+
+  configureTestSuite(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        StoreModule.forRoot({}),
+        ...getStoreModuleForFeatures([
+          EduContentProductTypeReducer,
+          DiaboloPhaseReducer
+        ])
+      ],
+      providers: [DiaboloChapterLessonFilterFactory, Store]
+    });
+  });
+
+  beforeEach(() => {
+    store = TestBed.get(Store);
+    factory = TestBed.get(DiaboloChapterLessonFilterFactory);
+
+    loadInStore();
+  });
+
+  it('should be created', () => {
+    expect(factory).toBeTruthy();
+  });
+
+  describe('getFilters', () => {
+    const mockSearchState: SearchStateInterface = {
+      searchTerm: '',
+      filterCriteriaSelections: new Map<string, (number | string)[]>()
+    };
+
+    it('should return the right filters', () => {
+      const result = factory.getFilters(mockSearchState);
+
+      expect(result).toBeObservable(
+        cold('a', {
+          a: [
+            getExpectedEduContentProductTypeFilter(),
+            getExpectedDiaboloPhaseFilter()
+          ]
+        })
+      );
+    });
+  });
+
+  function getExpectedFilter(
+    name,
+    label,
+    keyProperty,
+    displayProperty,
+    values,
+    component,
+    maxVisibleItems?
+  ): SearchFilterInterface {
+    const searchFilter = {
+      criteria: {
+        name: name,
+        label: label,
+        keyProperty: keyProperty,
+        displayProperty: displayProperty,
+        values: values.map(val => ({
+          data: val,
+          visible: true,
+          child: (val as any).children
+            ? getExpectedFilter(
+                name,
+                label,
+                keyProperty,
+                displayProperty,
+                (val as any).children,
+                component
+              ).criteria
+            : undefined
+        }))
+      },
+      component: component,
+      domHost: 'hostTop'
+    } as SearchFilterInterface;
+    if (maxVisibleItems) searchFilter.options = { maxVisibleItems };
+    return searchFilter;
+  }
+
+  function getExpectedEduContentProductTypeFilter() {
+    return getExpectedFilter(
+      'eduContentProductType',
+      'Type',
+      'id',
+      'name',
+      mockEduContentProductTypes,
+      SelectFilterComponent
+    );
+  }
+
+  function getExpectedDiaboloPhaseFilter() {
+    return getExpectedFilter(
+      'diaboloPhase',
+      'Diabolo-fase',
+      'id',
+      'name',
+      mockDiaboloPhases,
+      SelectFilterComponent
+    );
+  }
+
+  function loadInStore() {
+    store.dispatch(
+      new EduContentProductTypeActions.EduContentProductTypesLoaded({
+        eduContentProductTypes: mockEduContentProductTypes
+      })
+    );
+
+    store.dispatch(
+      new DiaboloPhaseActions.DiaboloPhasesLoaded({
+        diaboloPhases: mockDiaboloPhases
+      })
+    );
+  }
+});
