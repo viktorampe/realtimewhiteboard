fcf0fd7186571375fb1b2adf6a131507691bb590
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2019-07-29 12:53:19 +0200

    Add remaining tests

--- apps/kabas-web/src/app/components/searchresults/edu-content-search-result.component.spec.ts
+++ apps/kabas-web/src/app/components/searchresults/edu-content-search-result.component.spec.ts
@@ -4,9 +4,15 @@ import {
   MatIconRegistry,
   MatTooltipModule
 } from '@angular/material';
+import { By } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { EduContentFixture } from '@campus/dal';
 import {
+  DiaboloPhaseFixture,
+  EduContentFixture,
+  EduContentMetadataFixture
+} from '@campus/dal';
+import {
+  ContentActionInterface,
   ContentActionsServiceInterface,
   CONTENT_ACTIONS_SERVICE_TOKEN,
   EduContentSearchResultInterface
@@ -23,6 +29,34 @@ describe('EduContentSearchResultComponent', () => {
   let contentActionsServiceInterface: ContentActionsServiceInterface;
   const mockIsFavorite = new BehaviorSubject(false);
   let dateMock: MockDate;
+
+  const mockEduContent = new EduContentFixture({
+    id: 1,
+    publishedEduContentMetadata: new EduContentMetadataFixture({
+      title: 'De breuken',
+      description: 'mock description',
+      fileExt: 'pdf',
+      fileLabel: 'pdfLabel',
+      diaboloPhase: new DiaboloPhaseFixture({
+        icon: 'diabolo-intro'
+      })
+    })
+  });
+
+  const mockActions: ContentActionInterface[] = [
+    {
+      label: 'Downloaden',
+      icon: 'download',
+      tooltip: 'Download dit bestand',
+      handler: jest.fn()
+    },
+    {
+      label: 'Stream',
+      icon: 'stream',
+      tooltip: 'Stream deze video',
+      handler: jest.fn()
+    }
+  ];
 
   beforeAll(() => {
     dateMock = new MockDate();
@@ -45,7 +79,7 @@ describe('EduContentSearchResultComponent', () => {
         {
           provide: CONTENT_ACTIONS_SERVICE_TOKEN,
           useValue: {
-            getActionsForEduContent: () => {}
+            getActionsForEduContent: () => mockActions
           }
         }
       ]
@@ -59,7 +93,7 @@ describe('EduContentSearchResultComponent', () => {
     component = fixture.componentInstance;
 
     component.data = {
-      eduContent: new EduContentFixture({ id: 1 })
+      eduContent: mockEduContent
     } as EduContentSearchResultInterface;
 
     fixture.detectChanges();
@@ -67,5 +101,83 @@ describe('EduContentSearchResultComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('template', () => {
+    it('should show the file extension of the eduContent', () => {
+      const extensionDE = fixture.debugElement.query(
+        By.css('.app-educontentsearchresult__extension')
+      );
+
+      expect(extensionDE.componentInstance.extensionClass).toBe(
+        mockEduContent.fileExtension
+      );
+
+      expect(extensionDE.componentInstance.title).toBe(
+        mockEduContent.fileTypeLabel
+      );
+    });
+
+    it('should show the title and description of the eduContent', () => {
+      const titleDE = fixture.debugElement.query(
+        By.css('.app-educontentsearchresult__content__header__title')
+      );
+
+      const descriptionDE = fixture.debugElement.query(
+        By.css('.app-educontentsearchresult__content__body__description')
+      );
+
+      expect(titleDE.nativeElement.textContent).toBe(mockEduContent.name);
+
+      expect(descriptionDE.nativeElement.textContent).toBe(
+        mockEduContent.description
+      );
+    });
+
+    it('should show the title and description of the eduContent', () => {
+      const titleDE = fixture.debugElement.query(
+        By.css('.app-educontentsearchresult__content__header__title')
+      );
+
+      const descriptionDE = fixture.debugElement.query(
+        By.css('.app-educontentsearchresult__content__body__description')
+      );
+
+      expect(titleDE.nativeElement.textContent).toBe(mockEduContent.name);
+
+      expect(descriptionDE.nativeElement.textContent).toBe(
+        mockEduContent.description
+      );
+    });
+
+    it('should show the diabolo phase icon of the eduContent', () => {
+      const diaboloIconDE = fixture.debugElement.query(
+        By.css('.app-educontentsearchresult__content__header__diabolo-phase')
+      );
+
+      expect(diaboloIconDE.componentInstance.svgIcon).toBe(
+        mockEduContent.diaboloPhase.icon
+      );
+    });
+
+    it('should show the possible actions for the eduContent', () => {
+      const actionDEs = fixture.debugElement.queryAll(
+        By.css('.app-educontentsearchresult__content__header__action')
+      );
+
+      expect(actionDEs.length).toBe(mockActions.length);
+      actionDEs.forEach((actionDE, index) => {
+        const mockAction = mockActions[index];
+        const clickAction = jest.spyOn(component, 'onActionClick');
+
+        expect(actionDE.nativeElement.textContent).toBe(mockAction.label);
+        expect(actionDE.componentInstance.iconClass).toBe(mockAction.icon);
+
+        actionDE.nativeElement.click();
+        expect(clickAction).toHaveBeenCalled();
+
+        clickAction.mockReset();
+      });
+    });
   });
 });
