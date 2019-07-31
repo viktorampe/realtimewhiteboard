23e6ff4649f13928b5fdb1dc1624c761695d389a
DavidBoute <david.boute@gmail.com>
2019-07-31 09:25:26 +0200

    theme

--- libs/pages/global-search/src/lib/pages-global-search.module.ts
+++ libs/pages/global-search/src/lib/pages-global-search.module.ts
@@ -1,12 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SearchModule } from '@campus/search';
+import { SharedModule } from '@campus/shared';
+import { UiModule } from '@campus/ui';
 import { GlobalSearchComponent } from './components/global-search/global-search.component';
 
 @NgModule({
   imports: [
     CommonModule,
-
+    UiModule,
+    SharedModule,
+    SearchModule,
     RouterModule.forChild([
       { path: '', pathMatch: 'full', component: GlobalSearchComponent }
     ])
