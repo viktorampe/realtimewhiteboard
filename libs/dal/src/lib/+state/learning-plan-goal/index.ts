ffd27536c3d10a1b9098ba99ff721d18e8ce3cc5
DavidBoute <david.boute@gmail.com>
2019-08-05 11:27:53 +0200

    formatting

--- libs/dal/src/lib/+state/learning-plan-goal/index.ts
+++ libs/dal/src/lib/+state/learning-plan-goal/index.ts
@@ -3,4 +3,9 @@ import { LearningPlanGoalEffects } from './learning-plan-goal.effects';
 import * as LearningPlanGoalReducer from './learning-plan-goal.reducer';
 import * as LearningPlanGoalQueries from './learning-plan-goal.selectors';
 
-export { LearningPlanGoalActions, LearningPlanGoalReducer, LearningPlanGoalQueries, LearningPlanGoalEffects };
+export {
+  LearningPlanGoalActions,
+  LearningPlanGoalReducer,
+  LearningPlanGoalQueries,
+  LearningPlanGoalEffects
+};
