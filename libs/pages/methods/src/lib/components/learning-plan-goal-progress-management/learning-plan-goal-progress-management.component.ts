2fb222dbae4d0474db63fefa76ab1c162e20c558
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2019-08-21 14:31:35 +0200

    provide vm on component level + add mock vm

--- libs/pages/methods/src/lib/components/learning-plan-goal-progress-management/learning-plan-goal-progress-management.component.ts
+++ libs/pages/methods/src/lib/components/learning-plan-goal-progress-management/learning-plan-goal-progress-management.component.ts
@@ -2,11 +2,13 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material';
 import { ClassGroupInterface, LearningPlanGoalInterface } from '@campus/dal';
 import { LearningPlanGoalProgressManagementInterface } from './learning-plan-goal-progress-management-dialog.interface';
+import { LearningPlanGoalProgressManagementViewModel } from './learning-plan-goal-progress-management.viewmodel';
 
 @Component({
   selector: 'campus-learning-plan-goal-progress-management',
   templateUrl: './learning-plan-goal-progress-management.component.html',
-  styleUrls: ['./learning-plan-goal-progress-management.component.scss']
+  styleUrls: ['./learning-plan-goal-progress-management.component.scss'],
+  providers: [LearningPlanGoalProgressManagementViewModel]
 })
 export class LearningPlanGoalProgressManagementComponent implements OnInit {
   protected learningPlanGoal: LearningPlanGoalInterface;
@@ -14,7 +16,8 @@ export class LearningPlanGoalProgressManagementComponent implements OnInit {
 
   constructor(
     @Inject(MAT_DIALOG_DATA)
-    public data: LearningPlanGoalProgressManagementInterface
+    public data: LearningPlanGoalProgressManagementInterface,
+    private learningPlanGoalProgressManagerVM: LearningPlanGoalProgressManagementViewModel
   ) {}
 
   ngOnInit() {
