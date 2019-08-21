e5bd0a6f612dbcdaa9342b35a28056c112b8693b
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2019-08-21 13:17:53 +0200

    update test

--- libs/pages/methods/src/lib/components/learning-plan-goal-progress-management/learning-plan-goal-progress-management.component.spec.ts
+++ libs/pages/methods/src/lib/components/learning-plan-goal-progress-management/learning-plan-goal-progress-management.component.spec.ts
@@ -1,21 +1,29 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ClassGroupFixture, LearningPlanGoalFixture } from '@campus/dal';
+import { configureTestSuite } from 'ng-bullet';
 import { LearningPlanGoalProgressManagementComponent } from './learning-plan-goal-progress-management.component';
 
 describe('LearningPlanGoalProgressManagementComponent', () => {
   let component: LearningPlanGoalProgressManagementComponent;
   let fixture: ComponentFixture<LearningPlanGoalProgressManagementComponent>;
+  let mockClassGroup = new ClassGroupFixture();
+  let mockLearningPlanGoal = new LearningPlanGoalFixture();
 
-  beforeEach(async(() => {
+  configureTestSuite(() => {
     TestBed.configureTestingModule({
       declarations: [LearningPlanGoalProgressManagementComponent]
     }).compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(
       LearningPlanGoalProgressManagementComponent
     );
+
     component = fixture.componentInstance;
+    component.classGroup = mockClassGroup;
+    component.learningPlanGoal = mockLearningPlanGoal;
+
     fixture.detectChanges();
   });
 
