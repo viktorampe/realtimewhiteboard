947b503788e77f5e32d7f77474153d655df3768f
pverhelle <pieter.verhelle@gmail.com>
2019-08-26 13:59:48 +0200

    set up autocomplete input and filter mecanism

--- libs/shared/src/lib/components/learning-plan-goal-progress-management/learning-plan-goal-progress-management.component.ts
+++ libs/shared/src/lib/components/learning-plan-goal-progress-management/learning-plan-goal-progress-management.component.ts
@@ -1,11 +1,14 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material';
 import {
   ClassGroupInterface,
   EduContentBookInterface,
-  LearningPlanGoalInterface
+  LearningPlanGoalInterface,
+  UserLessonInterface
 } from '@campus/dal';
-import { Observable } from 'rxjs';
+import { combineLatest, Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 import { LearningPlanGoalProgressManagementInterface } from './learning-plan-goal-progress-management-dialog.interface';
 import { LearningPlanGoalProgressManagementViewModel } from './learning-plan-goal-progress-management.viewmodel';
 
@@ -16,6 +19,9 @@ import { LearningPlanGoalProgressManagementViewModel } from './learning-plan-goa
   providers: [LearningPlanGoalProgressManagementViewModel]
 })
 export class LearningPlanGoalProgressManagementComponent implements OnInit {
+  protected inputFromControl: FormControl;
+  protected userLessons$: Observable<UserLessonInterface[]>;
+  protected filteredUserLessons$: Observable<UserLessonInterface[]>;
   protected learningPlanGoal: LearningPlanGoalInterface;
   protected classGroup: ClassGroupInterface;
   protected book: EduContentBookInterface;
@@ -30,12 +36,46 @@ export class LearningPlanGoalProgressManagementComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+    this.inputFromControl = new FormControl();
     this.learningPlanGoal = this.data.learningPlanGoal;
     this.classGroup = this.data.classGroup;
     this.book = this.data.book;
     this.methodLessonsForBook$ = this.learningPlanGoalProgressManagerVM.getMethodLessonsForBook(
       this.book.id,
       this.learningPlanGoal.id
+    );
+    this.userLessons$ = this.learningPlanGoalProgressManagerVM.userLessons$;
+    this.filteredUserLessons$ = this.getFilteredUserLessons();
+  }
+
+  displayUserLesson(userLesson: UserLessonInterface): string {
+    return userLesson ? userLesson.description : undefined;
+  }
+
+  getFilteredUserLessons(): Observable<UserLessonInterface[]> {
+    return combineLatest([
+      this.inputFromControl.valueChanges.pipe(startWith('')),
+      this.userLessons$
+    ]).pipe(
+      map(
+        ([value, userLessons]: [
+          string | UserLessonInterface,
+          UserLessonInterface[]
+        ]) => this.inputFilter(value, userLessons)
+      )
+    );
+  }
+
+  private inputFilter(
+    value: string | UserLessonInterface,
+    userLessons: UserLessonInterface[]
+  ): UserLessonInterface[] {
+    const filterValue =
+      typeof value === 'string'
+        ? value.toLowerCase()
+        : value.description.toLowerCase();
+    return userLessons.filter(userLesson =>
+      userLesson.description.toLowerCase().includes(filterValue)
     );
   }
 }
