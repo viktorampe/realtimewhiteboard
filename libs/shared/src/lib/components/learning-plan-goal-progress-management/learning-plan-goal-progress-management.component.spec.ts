a005db1259a4e58372d97a0a21b7bfdbc8aacc41
DavidBoute <david.boute@gmail.com>
2019-08-26 14:08:00 +0200

    tests for component

--- libs/shared/src/lib/components/learning-plan-goal-progress-management/learning-plan-goal-progress-management.component.spec.ts
+++ libs/shared/src/lib/components/learning-plan-goal-progress-management/learning-plan-goal-progress-management.component.spec.ts
@@ -1,6 +1,14 @@
+// file.only
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatDialogModule, MAT_DIALOG_DATA } from '@angular/material';
+import {
+  MatDialogModule,
+  MatDialogRef,
+  MAT_DIALOG_DATA
+} from '@angular/material';
+import { HAMMER_LOADER } from '@angular/platform-browser';
 import { ClassGroupFixture, LearningPlanGoalFixture } from '@campus/dal';
+import { UiModule } from '@campus/ui';
 import { configureTestSuite } from 'ng-bullet';
 import { LearningPlanGoalProgressManagementInterface } from './learning-plan-goal-progress-management-dialog.interface';
 import { LearningPlanGoalProgressManagementComponent } from './learning-plan-goal-progress-management.component';
@@ -15,11 +23,20 @@ describe('LearningPlanGoalProgressManagementComponent', () => {
     learningPlanGoal: new LearningPlanGoalFixture()
   };
 
+  let lpgpManagementViewModel: LearningPlanGoalProgressManagementViewModel;
+
   configureTestSuite(() => {
     TestBed.configureTestingModule({
       declarations: [LearningPlanGoalProgressManagementComponent],
-      imports: [MatDialogModule],
-      providers: [{ provide: MAT_DIALOG_DATA, useValue: mockInjectedData }]
+      imports: [MatDialogModule, UiModule],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: mockInjectedData },
+        { provide: MatDialogRef, useValue: { close: () => {} } },
+        {
+          provide: HAMMER_LOADER,
+          useValue: () => new Promise(() => {})
+        }
+      ]
     })
       .overrideComponent(LearningPlanGoalProgressManagementComponent, {
         set: {
@@ -41,10 +58,83 @@ describe('LearningPlanGoalProgressManagementComponent', () => {
 
     component = fixture.componentInstance;
 
+    // provided on component level
+    lpgpManagementViewModel = component['learningPlanGoalProgressManagerVM'];
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('data and streams', () => {
+    describe('data', () => {
+      it('should inject the data', () => {
+        expect(component.learningPlanGoal).toBe(
+          mockInjectedData.learningPlanGoal
+        );
+        expect(component.classGroup).toBe(mockInjectedData.classGroup);
+      });
+    });
+  });
+
+  describe('event handlers', () => {
+    it('closeDialog should close the dialog', () => {
+      const dialogRef = TestBed.get(MatDialogRef);
+      dialogRef.close = jest.fn();
+
+      component.closeDialog();
+
+      expect(dialogRef.close).toHaveBeenCalled();
+    });
+
+    it('saveForUserLesson should call the correct method on the viewmodel and close the dialog', () => {
+      lpgpManagementViewModel.createLearningPlanGoalProgressForUserLesson = jest.fn();
+      component.closeDialog = jest.fn();
+
+      const userLesson = { description: 'foo' };
+
+      component.saveForUserLesson(userLesson);
+
+      expect(
+        lpgpManagementViewModel.createLearningPlanGoalProgressForUserLesson
+      ).toHaveBeenCalled();
+      expect(
+        lpgpManagementViewModel.createLearningPlanGoalProgressForUserLesson
+      ).toHaveBeenCalledTimes(1);
+      expect(
+        lpgpManagementViewModel.createLearningPlanGoalProgressForUserLesson
+      ).toHaveBeenCalledWith(
+        mockInjectedData.learningPlanGoal,
+        mockInjectedData.classGroup,
+        userLesson
+      );
+      expect(component.closeDialog).toHaveBeenCalled();
+    });
+
+    it('saveForEduContentTOCselection should call the correct method on the viewmodel and close the dialog', () => {
+      lpgpManagementViewModel.createLearningPlanGoalProgressForEduContentTOCs = jest.fn();
+      component.closeDialog = jest.fn();
+
+      const eduContentTOCids = [1, 2, 3];
+
+      component.saveForEduContentTOCselection(eduContentTOCids);
+
+      expect(
+        lpgpManagementViewModel.createLearningPlanGoalProgressForEduContentTOCs
+      ).toHaveBeenCalled();
+      expect(
+        lpgpManagementViewModel.createLearningPlanGoalProgressForEduContentTOCs
+      ).toHaveBeenCalledTimes(1);
+      expect(
+        lpgpManagementViewModel.createLearningPlanGoalProgressForEduContentTOCs
+      ).toHaveBeenCalledWith(
+        mockInjectedData.learningPlanGoal,
+        mockInjectedData.classGroup,
+        eduContentTOCids
+      );
+      expect(component.closeDialog).toHaveBeenCalled();
+    });
   });
 });
