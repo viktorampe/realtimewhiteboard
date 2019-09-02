3df0199f59ef3c4a803a0f9cb82a632b026f1c99
DavidBoute <david.boute@gmail.com>
2019-09-02 14:29:11 +0200

    add interface + fixture

---
+++ libs/dal/src/lib/+fixtures/UnlockedFreePractice.fixture.ts
@@ -0,0 +1,15 @@
+import { UnlockedFreePracticeInterface } from '../+models';
+
+export class UnlockedFreePracticeFixture
+  implements UnlockedFreePracticeInterface {
+  // defaults
+  id = 1;
+  eduContentBookId = 1;
+  eduContentTOCId = 1;
+  classGroupId = 1;
+
+  constructor(props: Partial<UnlockedFreePracticeInterface> = {}) {
+    // overwrite defaults
+    Object.assign(this, props);
+  }
+}
