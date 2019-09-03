e9ccef00f76cc7483dc03ec1ddf1d28bdb66db95
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2019-09-03 15:13:53 +0200

    Make viewmodel streams + tests

--- libs/pages/practice/src/lib/components/practice.viewmodel.spec.ts
+++ libs/pages/practice/src/lib/components/practice.viewmodel.spec.ts
@@ -4,9 +4,16 @@ import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import {
   AUTH_SERVICE_TOKEN,
+  ClassGroupFixture,
+  ClassGroupQueries,
   CustomSerializer,
   DalState,
-  getStoreModuleForFeatures
+  EduContentBookFixture,
+  EduContentBookQueries,
+  EduContentTOCFixture,
+  EduContentTocQueries,
+  getStoreModuleForFeatures,
+  MethodQueries
 } from '@campus/dal';
 import {
   NavigationActionTiming,
@@ -18,8 +25,9 @@ import {
   StoreRouterConnectingModule
 } from '@ngrx/router-store';
 import { Store, StoreModule } from '@ngrx/store';
+import { hot } from '@nrwl/nx/testing';
 import { configureTestSuite } from 'ng-bullet';
-import { PracticeViewModel } from './practice.viewmodel';
+import { CurrentPracticeParams, PracticeViewModel } from './practice.viewmodel';
 
 describe('PracticeViewModel', () => {
   let practiceViewModel: PracticeViewModel;
@@ -58,6 +66,140 @@ describe('PracticeViewModel', () => {
   describe('creation', () => {
     it('should be defined', () => {
       expect(practiceViewModel).toBeDefined();
+    });
+  });
+
+  /*
+  public currentPracticeParams$: Observable<CurrentPracticeParams>;
+  public bookTitle$: Observable<string>;
+  public bookChapters$: Observable<EduContentTOCInterface[]>;
+  public filteredClassGroups$: Observable<ClassGroupInterface[]>;
+
+  */
+
+  describe('presentation streams', () => {
+    describe('currentPracticeParams$', () => {
+      it('should contain the current bookId from the route', () => {
+        navigateWithParams({ book: 1 });
+
+        expect(practiceViewModel.currentPracticeParams$).toBeObservable(
+          hot('a', {
+            a: {
+              book: 1
+            } as CurrentPracticeParams
+          })
+        );
+      });
+    });
+
+    describe('bookTitle$', () => {
+      const mockBookTitle = 'Kompas 1';
+      const storeState = jasmine.anything();
+
+      beforeEach(() => {
+        jest
+          .spyOn(MethodQueries, 'getMethodWithYearByBookId')
+          .mockReturnValue(mockBookTitle);
+      });
+
+      it('should be null if not in a book', () => {
+        navigateWithParams({});
+
+        expect(practiceViewModel.bookTitle$).toBeObservable(
+          hot('a', {
+            a: null
+          })
+        );
+      });
+
+      it('should return the book title if in a book', () => {
+        navigateWithParams({ book: 1 });
+
+        expect(practiceViewModel.bookTitle$).toBeObservable(
+          hot('a', {
+            a: mockBookTitle
+          })
+        );
+
+        expect(MethodQueries.getMethodWithYearByBookId).toHaveBeenCalledWith(
+          storeState,
+          { id: 1 }
+        );
+      });
+    });
+
+    describe('bookChapters$', () => {
+      const bookChapters = [
+        new EduContentTOCFixture({ id: 1 }),
+        new EduContentTOCFixture({ id: 2 }),
+        new EduContentTOCFixture({ id: 3 })
+      ];
+      const storeState = jasmine.anything();
+
+      beforeEach(() => {
+        jest
+          .spyOn(EduContentTocQueries, 'getChaptersForBook')
+          .mockReturnValue(bookChapters);
+      });
+
+      it('should be an empty array if not in a book', () => {
+        navigateWithParams({});
+
+        expect(practiceViewModel.bookChapters$).toBeObservable(
+          hot('a', {
+            a: []
+          })
+        );
+      });
+
+      it('should be the chapters of the book if in a book', () => {
+        navigateWithParams({ book: 1 });
+
+        expect(practiceViewModel.bookChapters$).toBeObservable(
+          hot('a', {
+            a: bookChapters
+          })
+        );
+
+        expect(EduContentTocQueries.getChaptersForBook).toHaveBeenCalledWith(
+          storeState,
+          { bookId: 1 }
+        );
+      });
+    });
+
+    describe('filteredClassGroups$', () => {
+      const methodId = 5;
+      const mockBook = new EduContentBookFixture({ id: 1, methodId });
+      const mockClassGroups = [
+        new ClassGroupFixture({ id: 1 }),
+        new ClassGroupFixture({ id: 2 })
+      ];
+      const storeState = jasmine.anything();
+
+      beforeEach(() => {
+        //Make currentBook$ return the mockBook
+        jest.spyOn(EduContentBookQueries, 'getById').mockReturnValue(mockBook);
+
+        jest
+          .spyOn(ClassGroupQueries, 'getByMethodId')
+          .mockReturnValue(mockClassGroups);
+      });
+
+      it('should return the classGroups matching the current book method', () => {
+        navigateWithParams({ book: 1 });
+
+        expect(practiceViewModel.filteredClassGroups$).toBeObservable(
+          hot('a', {
+            a: mockClassGroups
+          })
+        );
+
+        expect(ClassGroupQueries.getByMethodId).toHaveBeenCalledWith(
+          storeState,
+          { id: methodId }
+        );
+      });
     });
   });
 
--- libs/pages/practice/src/lib/components/practice.viewmodel.ts
+++ libs/pages/practice/src/lib/components/practice.viewmodel.ts
@@ -1,8 +1,147 @@
 import { Injectable } from '@angular/core';
+import {
+  ClassGroupInterface,
+  ClassGroupQueries,
+  DalState,
+  EduContentBookInterface,
+  EduContentBookQueries,
+  EduContentTOCInterface,
+  EduContentTocQueries,
+  getRouterState,
+  MethodQueries,
+  RouterStateUrl
+} from '@campus/dal';
+import { RouterReducerState } from '@ngrx/router-store';
+import { select, Store } from '@ngrx/store';
+import { merge, Observable } from 'rxjs';
+import {
+  distinctUntilChanged,
+  filter,
+  map,
+  mapTo,
+  shareReplay,
+  switchMap
+} from 'rxjs/operators';
+
+export interface CurrentPracticeParams {
+  book?: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PracticeViewModel {
-  constructor() {}
+  //Presentation streams
+  public currentPracticeParams$: Observable<CurrentPracticeParams>;
+  public bookTitle$: Observable<string>;
+  public bookChapters$: Observable<EduContentTOCInterface[]>;
+  public filteredClassGroups$: Observable<ClassGroupInterface[]>;
+
+  //Source streams
+  private routerState$: Observable<RouterReducerState<RouterStateUrl>>;
+  private currentBook$: Observable<EduContentBookInterface>;
+
+  constructor(private store: Store<DalState>) {
+    this.initialize();
+  }
+
+  private initialize() {
+    this.setSourceStreams();
+    this.setPresentationStreams();
+  }
+
+  private setSourceStreams() {
+    this.routerState$ = this.store.pipe(select(getRouterState));
+    this.currentPracticeParams$ = this.getCurrentPracticeParamsStream();
+
+    this.currentBook$ = this.getCurrentBookStream();
+  }
+
+  private setPresentationStreams() {
+    this.bookTitle$ = this.getBookTitleStream();
+    this.bookChapters$ = this.getBookChaptersStream();
+    this.filteredClassGroups$ = this.getFilteredClassGroupsStream();
+  }
+
+  private getCurrentPracticeParamsStream(): Observable<CurrentPracticeParams> {
+    return this.routerState$.pipe(
+      filter(routerState => !!routerState),
+      map((routerState: RouterReducerState<RouterStateUrl>) => ({
+        book: +routerState.state.params.book || undefined
+      })),
+      distinctUntilChanged((a, b) => a.book === b.book),
+      shareReplay(1)
+    );
+  }
+
+  private getCurrentBookStream(): Observable<EduContentBookInterface> {
+    const currentBookWhenEmpty$ = this.currentPracticeParams$.pipe(
+      filter(params => !params.book),
+      mapTo(null)
+    );
+
+    const currentBookWhenExists$ = this.currentPracticeParams$.pipe(
+      filter(params => !!params.book),
+      switchMap(params => {
+        return this.store.pipe(
+          select(EduContentBookQueries.getById, {
+            id: params.book
+          })
+        );
+      })
+    );
+
+    return merge(currentBookWhenEmpty$, currentBookWhenExists$);
+  }
+
+  private getBookTitleStream(): Observable<string> {
+    const bookTitleWhenNoBookId$ = this.currentPracticeParams$.pipe(
+      filter(params => !params.book),
+      mapTo(null)
+    );
+
+    const bookTitleWhenBookId$ = this.currentPracticeParams$.pipe(
+      filter(params => !!params.book),
+      switchMap(params => {
+        return this.store.pipe(
+          select(MethodQueries.getMethodWithYearByBookId, {
+            id: params.book
+          })
+        );
+      })
+    );
+
+    return merge(bookTitleWhenNoBookId$, bookTitleWhenBookId$);
+  }
+
+  private getBookChaptersStream(): Observable<EduContentTOCInterface[]> {
+    const chaptersStreamWhenNoBookId$ = this.currentPracticeParams$.pipe(
+      filter(params => !params.book),
+      mapTo([])
+    );
+
+    const chaptersStreamWhenBookId$ = this.currentPracticeParams$.pipe(
+      filter(params => !!params.book),
+      switchMap(params => {
+        return this.store.pipe(
+          select(EduContentTocQueries.getChaptersForBook, {
+            bookId: params.book
+          })
+        );
+      })
+    );
+
+    return merge(chaptersStreamWhenNoBookId$, chaptersStreamWhenBookId$);
+  }
+
+  private getFilteredClassGroupsStream(): Observable<ClassGroupInterface[]> {
+    return this.currentBook$.pipe(
+      filter(currentBook => !!currentBook),
+      switchMap(currentBook => {
+        return this.store.pipe(
+          select(ClassGroupQueries.getByMethodId, { id: currentBook.methodId })
+        );
+      })
+    );
+  }
 }
