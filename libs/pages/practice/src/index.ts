1dc72e87fafe187a1784dd192e67d28695292768
DavidBoute <david.boute@gmail.com>
2019-09-03 09:29:57 +0200

    formatting

--- libs/pages/practice/src/index.ts
+++ libs/pages/practice/src/index.ts
@@ -1,2 +1,1 @@
 export * from './lib/pages-practice.module';
-
--- libs/pages/practice/src/lib/components/practice.viewmodel.spec.ts
+++ libs/pages/practice/src/lib/components/practice.viewmodel.spec.ts
@@ -2,11 +2,37 @@ import { NgZone } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AUTH_SERVICE_TOKEN, CustomSerializer, DalState, getStoreModuleForFeatures } from '@campus/dal';
-import { NavigationActionTiming, RouterNavigationAction, RouterNavigationPayload, routerReducer, RouterStateSerializer, ROUTER_NAVIGATION, StoreRouterConnectingModule } from '@ngrx/router-store';
+import {
+  AUTH_SERVICE_TOKEN,
+  CustomSerializer,
+  DalState,
+  getStoreModuleForFeatures
+} from '@campus/dal';
+import {
+  NavigationActionTiming,
+  RouterNavigationAction,
+  RouterNavigationPayload,
+  routerReducer,
+  RouterStateSerializer,
+  ROUTER_NAVIGATION,
+  StoreRouterConnectingModule
+} from '@ngrx/router-store';
 import { Store, StoreModule } from '@ngrx/store';
 import { configureTestSuite } from 'ng-bullet';
 import { PracticeViewModel } from './practice.viewmodel';
+
+describe('TrainingViewModel', () => {
+  let practiceViewModel: PracticeViewModel;
+  let store: Store<DalState>;
+  let router: Router;
+  let zone: NgZone;
+
+  const userId = 1;
+
+  configureTestSuite(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        StoreModule.forRoot({ router: routerReducer }),
         ...getStoreModuleForFeatures([]),
         RouterTestingModule.withRoutes([]),
         StoreRouterConnectingModule.forRoot({
