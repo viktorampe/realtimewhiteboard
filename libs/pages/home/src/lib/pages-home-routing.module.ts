54ccfa6039c24793c522f591590483a83ff08b93
DavidBoute <david.boute@gmail.com>
2019-09-04 17:22:19 +0200

    add resolver, remove child routes

--- libs/pages/home/src/lib/pages-home-routing.module.ts
+++ libs/pages/home/src/lib/pages-home-routing.module.ts
@@ -1,19 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { HomeResolver } from './components/resolvers/pages-home.resolver';
 
 const routes: Routes = [
   {
     path: '',
-    // resolve: { isResolved: MethodResolver },
+    resolve: { isResolved: HomeResolver },
     runGuardsAndResolvers: 'always',
-    children: [
-      {
-        path: '',
-        runGuardsAndResolvers: 'always',
-        component: HomeComponent
-      }
-    ]
+    component: HomeComponent
   }
 ];
 
