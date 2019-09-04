b6976368cac2f8954675f750cf15aeeb0e658023
DavidBoute <david.boute@gmail.com>
2019-09-04 17:12:59 +0200

    add module to project, app, task, typescript, ...

--- libs/pages/home/src/lib/pages-home.module.ts
+++ libs/pages/home/src/lib/pages-home.module.ts
@@ -3,19 +3,10 @@ import { NgModule } from '@angular/core';
 import { PagesSharedModule } from '@campus/pages/shared';
 import { UiModule } from '@campus/ui';
 import { HomeComponent } from './components/home.component';
-import { HomeViewModel } from './components/home.viewmodel';
 import { PagesHomeRoutingModule } from './pages-home-routing.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    PagesHomeRoutingModule,
-    UiModule,
-    PagesSharedModule
-  ],
-  declarations: [
-    HomeComponent,
-  ],
-  providers: [HomeViewModel]
+  imports: [CommonModule, PagesHomeRoutingModule, UiModule, PagesSharedModule],
+  declarations: [HomeComponent]
 })
 export class PagesHomeModule {}
