e5a4579e908f083539455ec778ef763b97d6782f
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2019-09-05 12:49:20 +0200

    basic component setup

--- libs/pages/practice/src/lib/components/practice-method-detail/practice-method-detail.component.ts
+++ libs/pages/practice/src/lib/components/practice-method-detail/practice-method-detail.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ClassGroupInterface, EduContentTOCInterface } from '@campus/dal';
+import {
+  MultiCheckBoxTableChangeEventInterface,
+  MultiCheckBoxTableItemColumnInterface,
+  MultiCheckBoxTableItemInterface,
+  MultiCheckBoxTableRowHeaderColumnInterface
+} from '@campus/ui';
+import { Observable } from 'rxjs';
+import { PracticeViewModel } from '../practice.viewmodel';
 
 @Component({
   selector: 'campus-practice-method-detail',
@@ -6,10 +15,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./practice-method-detail.component.scss']
 })
 export class PracticeMethodDetailComponent implements OnInit {
+  public breadCrumbTitles$: Observable<string>; // used to show book method + year
 
-  constructor() { }
+  public eduContentTOCsTableHeaders: MultiCheckBoxTableRowHeaderColumnInterface<
+    EduContentTOCInterface
+  >[];
+  public classGroupColumns$: Observable<
+    MultiCheckBoxTableItemColumnInterface<ClassGroupInterface>[]
+  >;
+  public eduContentTOCsWithSelectionForClassGroups$: Observable<
+    MultiCheckBoxTableItemInterface<EduContentTOCInterface>[]
+  >;
 
-  ngOnInit() {
+  constructor(private viewModel: PracticeViewModel) {}
+
+  ngOnInit() {}
+
+  clickFreePracticeForBook() {}
+
+  clickFreePracticeForChapter(
+    event: MultiCheckBoxTableChangeEventInterface<
+      EduContentTOCInterface,
+      ClassGroupInterface,
+      undefined
+    >
+  ) {
+    if (event.previousCheckboxState) {
+      // if the checkbox becomes unchecked
+      this.viewModel.deleteUnLockedFreePracticeForEduContentTOCClassGroup(
+        event.item, // eduContentTOC
+        event.column // classgroup
+      );
+    } else {
+      this.viewModel.addUnLockedFreePracticeForEduContentTOCClassGroup(
+        event.item, // eduContentTOC
+        event.column // classGroup
+      );
+    }
   }
-
 }
