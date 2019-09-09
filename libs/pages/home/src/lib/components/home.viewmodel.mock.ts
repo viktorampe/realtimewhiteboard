583ca064619abf53a4143b334eeaea60a9711cd5
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2019-09-09 17:10:25 +0200

    Update MockHomeViewModel

--- libs/pages/home/src/lib/components/home.viewmodel.mock.ts
+++ libs/pages/home/src/lib/components/home.viewmodel.mock.ts
@@ -1,8 +1,43 @@
 import { Injectable } from '@angular/core';
+import {
+  EduContent,
+  EduContentFixture,
+  FavoriteFixture,
+  FavoriteTypesEnum
+} from '@campus/dal';
 import { ViewModelInterface } from '@campus/testing';
+import { BehaviorSubject } from 'rxjs';
 import { HomeViewModel } from './home.viewmodel';
+import { FavoriteWithEduContent } from './home.viewmodel.selectors';
 
 @Injectable({
   providedIn: 'root'
 })
-export class MockHomeViewModel implements ViewModelInterface<HomeViewModel> {}
+export class MockHomeViewModel implements ViewModelInterface<HomeViewModel> {
+  public favoritesWithEduContent$ = new BehaviorSubject<
+    FavoriteWithEduContent[]
+  >([
+    {
+      favorite: new FavoriteFixture({
+        id: 1,
+        name: 'Katapult 1',
+        type: FavoriteTypesEnum.BOEKE,
+        eduContentId: 1
+      }),
+      bookId: 3,
+      eduContent: new EduContentFixture({ id: 1 }, { eduContentBookId: 3 })
+    },
+    {
+      favorite: new FavoriteFixture({
+        id: 2,
+        name: 'Mol en Beer 1',
+        type: FavoriteTypesEnum.BOEKE,
+        eduContentId: 2
+      }),
+      bookId: 4,
+      eduContent: new EduContentFixture({ id: 2 }, { eduContentBookId: 4 })
+    }
+  ]);
+
+  openBoeke(eduContent: EduContent): void {}
+}
