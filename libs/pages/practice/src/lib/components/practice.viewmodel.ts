dffaa84faa2829a06647aedee53abb1a8b986597
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2019-09-09 14:17:52 +0200

    Add userId to delete in practice VM

--- libs/pages/practice/src/lib/components/practice.viewmodel.ts
+++ libs/pages/practice/src/lib/components/practice.viewmodel.ts
@@ -264,6 +264,7 @@ export class PracticeViewModel {
     } else {
       this.store.dispatch(
         new UnlockedFreePracticeActions.DeleteUnlockedFreePractices({
+          userId: this.authService.userId,
           ids: unlockedFreePractices.map(ufp => ufp.id)
         })
       );
