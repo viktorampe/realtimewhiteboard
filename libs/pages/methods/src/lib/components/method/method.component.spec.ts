1c3690027995e8919a8790ce68341ba464096c28
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2019-09-13 10:42:23 +0200

    Fix missing book$ stream

--- libs/pages/methods/src/lib/components/method/method.component.spec.ts
+++ libs/pages/methods/src/lib/components/method/method.component.spec.ts
@@ -17,6 +17,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import {
   ClassGroupFixture,
   ClassGroupInterface,
+  EduContentBookFixture,
   EduContentFixture
 } from '@campus/dal';
 import {
@@ -92,6 +93,31 @@ describe('MethodComponent', () => {
   it('should show the method info card', () => {
     const methodCard = fixture.debugElement.query(By.directive(MatCard));
     expect(methodCard).toBeDefined();
+  });
+
+  describe('diabolo info', () => {
+    it('should show diabolo info if the current book is diabolo', () => {
+      const diaboloDE = fixture.debugElement.query(
+        By.css('.method-method__container__boeke__diabolo')
+      );
+      const method = methodViewModel.currentMethod$.value;
+
+      expect(diaboloDE).toBeTruthy();
+      expect(diaboloDE.nativeElement.textContent.trim()).toBe(
+        `${method.name} is een diabolo methode. Lees meer over diabolo`
+      );
+    });
+
+    it('should not show diabolo info if the current book is diabolo', () => {
+      methodViewModel.currentBook$.next(new EduContentBookFixture());
+      fixture.detectChanges();
+
+      const diaboloDE = fixture.debugElement.query(
+        By.css('.method-method__container__boeke__diabolo')
+      );
+
+      expect(diaboloDE).toBeFalsy();
+    });
   });
 
   it('should show the general files', () => {
