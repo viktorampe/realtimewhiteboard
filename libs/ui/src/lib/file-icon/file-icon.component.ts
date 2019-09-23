e72ecba51cfb1c63211b04b2a7bc3f2bc23dcf74
Thomas Ghesquiere <thomas.ghesquiere@gmail.com>
2019-09-23 16:48:56 +0200

    file icon component scaffold

---
+++ libs/ui/src/lib/file-icon/file-icon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'campus-file-icon',
+  templateUrl: './file-icon.component.html',
+  styleUrls: ['./file-icon.component.scss']
+})
+export class FileIconComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
