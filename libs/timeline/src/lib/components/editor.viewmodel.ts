2430c473dba835343d907f221bd21410527340f9
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2019-10-03 16:41:46 +0200

    add raw setup

--- libs/timeline/src/lib/components/editor.viewmodel.ts
+++ libs/timeline/src/lib/components/editor.viewmodel.ts
@@ -1,6 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TimelineConfig } from '../interfaces/timeline';
+import { EditorHttpServiceInterface } from '../services/editor-http.service.interface';
 
 @Injectable({
   providedIn: 'root'
 })
-export class EditorViewModel {}
+export class EditorViewModel {
+  constructor(private editorHttpService: EditorHttpServiceInterface) {}
+
+  getTimeline(eduContentMetadataId: number): Observable<TimelineConfig> {
+    return this.editorHttpService.getJson(eduContentMetaDataId);
+  }
+
+  updateTimeline(eduContentMetadataId: number, data: TimelineConfig): void {
+    // returned true / false
+    this.editorHttpService.setJson(eduContentMetadataId, data);
+  }
+
+  previewTimeline(eduContentMetadataId: number): Observable<string> {
+    return this.editorHttpService.openPreview();
+  }
+
+  uploadFile(eduContentMetadataId: number, file: string): void {
+    // true or false
+    this.editorHttpService.uploadFile(eduContentMetaDataId, file);
+  }
+}
