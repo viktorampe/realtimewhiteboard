1ec978ba1da0c928ade26fd5983e25de20b8fdd1
DavidBoute <david.boute@gmail.com>
2019-10-03 17:10:44 +0200

    setup service prototype

--- libs/timeline/src/lib/services/editor-http.service.ts
+++ libs/timeline/src/lib/services/editor-http.service.ts
@@ -1,4 +1,10 @@
-import { Injectable, InjectionToken } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable, InjectionToken } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { TimelineConfig } from '../interfaces/timeline';
+import { ENVIRONMENT_API_TOKEN } from './../../../../shared/src/lib/interfaces/environment.injectiontokens';
+import { EnvironmentApiInterface } from './../../../../shared/src/lib/interfaces/environment.interfaces';
 import { EditorHttpServiceInterface } from './editor-http.service.interface';
 
 export const EDITOR_HTTP_SERVICE_TOKEN = new InjectionToken(
@@ -9,11 +15,49 @@ export const EDITOR_HTTP_SERVICE_TOKEN = new InjectionToken(
   providedIn: 'root'
 })
 export class EditorHttpService implements EditorHttpServiceInterface {
-  getJson() {}
+  constructor(
+    private http: HttpClient,
+    @Inject(ENVIRONMENT_API_TOKEN)
+    private environmentApi: EnvironmentApiInterface
+  ) {}
 
-  setJson() {}
+  public getJson(eduContentMetadataId: number): Observable<TimelineConfig> {
+    const response$ = this.http.get(
+      this.environmentApi + '/api/eduContentMetaData/' + eduContentMetadataId
+      // +
+      // '/timeline'
+    );
 
-  openPreview() {}
+    const timelineConfig$: Observable<TimelineConfig> = response$.pipe(
+      map(response => this.convertGetJsonReponseToTimelineConfig(response))
+    );
 
-  uploadFile() {}
+    timelineConfig$.subscribe(response => console.log(response));
+
+    return timelineConfig$;
+  }
+
+  public setJson(
+    eduContentMetadataId: number,
+    timeLineConfig: TimelineConfig
+  ): Observable<boolean> {
+    return;
+  }
+
+  public openPreview(): Observable<string> {
+    return;
+  }
+
+  public uploadFile(file: string): Observable<boolean> {
+    return;
+  }
+
+  private convertGetJsonReponseToTimelineConfig(response) {
+    console.log(
+      'log: EditorHttpService -> convertGetJsonReponseToTimelineConfig -> response',
+      response
+    );
+
+    return {} as TimelineConfig;
+  }
 }
