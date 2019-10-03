facd5fc163e074eea7899a266b0effa401ddee04
DavidBoute <david.boute@gmail.com>
2019-10-03 17:07:26 +0200

    provide environment

--- apps/timeline-editor/src/app/app.module.ts
+++ apps/timeline-editor/src/app/app.module.ts
@@ -1,19 +1,23 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Injector, NgModule } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { TimelineModule } from '@campus/timeline';
+import { environment } from '../environments/environment';
+import { ENVIRONMENT_API_TOKEN } from './../../../../libs/shared/src/lib/interfaces/environment.injectiontokens';
 import { TimelineEditorComponent } from './timeline-editor/timeline-editor.component';
 
 @NgModule({
   declarations: [TimelineEditorComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
     TimelineModule
   ],
   entryComponents: [TimelineEditorComponent],
-  providers: []
+  providers: [{ provide: ENVIRONMENT_API_TOKEN, useValue: environment.api }]
 })
 export class AppModule {
   constructor(injector: Injector) {
