0eaf0b7d7488b0854d8deb7d2167353ce3baa05c
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2019-10-04 10:33:03 +0200

    rename file

--- libs/timeline/src/lib/+fixtures/timeline-era.fixture.ts
+++ libs/timeline/src/lib/+fixtures/timeline-era.fixture.ts
@@ -1,6 +1,6 @@
-import { TimelineDateFixture } from './timeline-data.fixture';
-import { TimelineTextFixture } from './timeline-text.fixture';
 import { TimelineEra } from '../interfaces/timeline';
+import { TimelineDateFixture } from './timeline-date.fixture';
+import { TimelineTextFixture } from './timeline-text.fixture';
 export class TimelineEraFixture implements TimelineEra {
   // defaults
   start_date = new TimelineDateFixture();
