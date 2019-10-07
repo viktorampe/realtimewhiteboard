ff455a0f15e545b62934c48c4cb37c7844adb6aa
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2019-10-07 10:23:19 +0200

    basic  form setup

--- libs/timeline/src/lib/components/slide-detail/slide-detail.component.ts
+++ libs/timeline/src/lib/components/slide-detail/slide-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { TimelineSlide } from '../../interfaces/timeline';
 
 @Component({
   selector: 'campus-slide-detail',
@@ -6,7 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slide-detail.component.scss']
 })
 export class SlideDetailComponent implements OnInit {
-  constructor() {}
+  @Input() slide: TimelineSlide;
 
-  ngOnInit() {}
+  slideForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.buildForm();
+  }
+
+  buildForm(): void {
+    this.slideForm = this.fb.group({
+      start_date: [this.slide.start_date],
+      end_date: [this.slide.end_date],
+      group: [this.slide.group],
+      text: [this.slide.text],
+      background: [this.slide.background],
+      media: [this.slide.media],
+      display_data: [this.slide.display_date]
+    });
+  }
+
+  getControl(name: string): FormControl {
+    return this.slideForm.get(name) as FormControl;
+  }
+
+  onSubmit() {}
 }
--- libs/timeline/src/lib/timeline.module.ts
+++ libs/timeline/src/lib/timeline.module.ts
@@ -1,12 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { EditorTimelineComponent } from './components/editor-timeline/editor-timeline.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { SlideDetailComponent } from './components/slide-detail/slide-detail.component';
 import { SlideListComponent } from './components/slide-list/slide-list.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule
+  ],
   declarations: [
     EditorTimelineComponent,
     SlideListComponent,
