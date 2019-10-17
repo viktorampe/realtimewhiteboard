9af402715a033608d9b1d508c17071aa95dc7a64
ernie58 <bert@ernie.be>
2019-10-17 22:10:30 +0200

    fix padding

--- libs/timeline/src/lib/components/settings/settings.component.ts
+++ libs/timeline/src/lib/components/settings/settings.component.ts
@@ -5,8 +5,7 @@ import {
   Input,
   OnDestroy,
   OnInit,
-  Output,
-  ViewEncapsulation
+  Output
 } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MAT_TOOLTIP_DEFAULT_OPTIONS } from '@angular/material';
@@ -19,8 +18,7 @@ import { TimelineSettingsInterface } from '../../interfaces/timeline';
   styleUrls: ['./settings.component.scss'],
   providers: [
     { provide: MAT_TOOLTIP_DEFAULT_OPTIONS, useValue: { position: 'after' } }
-  ],
-  encapsulation: ViewEncapsulation.None
+  ]
 })
 export class SettingsComponent implements OnInit, OnDestroy {
   public settingsForm: FormGroup;
