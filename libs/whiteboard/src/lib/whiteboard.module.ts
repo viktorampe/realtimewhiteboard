48d1b552cb0624627850e85d031d6df075e891c0
Lukas Nys <lukas.nys2@student.vives.be>
2019-10-18 10:17:55 +0200

    Updated module to export whiteboard-component

--- libs/whiteboard/src/lib/whiteboard.module.ts
+++ libs/whiteboard/src/lib/whiteboard.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { WhiteboardComponent } from './whiteboard/whiteboard.component';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  declarations: [WhiteboardComponent],
+  exports : [WhiteboardComponent]
 })
 export class WhiteboardModule {}
