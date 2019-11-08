611ed7f7622543634975d85e95592fd8ef58c1a6
viktorampe <ampe.viktor@gmail.com>
2019-11-08 09:36:34 +0100

    added ngModel to input

--- libs/whiteboard/src/lib/card/card.component.ts
+++ libs/whiteboard/src/lib/card/card.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+  cardContent: String = '';
+
   constructor() {}
 
   ngOnInit() {}
