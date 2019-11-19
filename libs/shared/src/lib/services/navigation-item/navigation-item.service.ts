266891505f298353c363d974ac41a09eaca40304
Thomas Ghesquiere <thomas.ghesquiere@gmail.com>
2019-11-19 10:51:12 +0100

    add hideWhenRequiredPermissions to navigation-item service

--- libs/shared/src/lib/services/navigation-item/navigation-item.service.ts
+++ libs/shared/src/lib/services/navigation-item/navigation-item.service.ts
@@ -20,19 +20,30 @@ export class NavigationItemService implements NavigationItemServiceInterface {
     private appNavigationTree: AppNavTreeInterface,
     @Inject(PERMISSION_SERVICE_TOKEN)
     private permissionService: PermissionServiceInterface
-  ) {}
+  ) { }
 
   getNavItemsForTree(
     tree: AppNavTreeKeys,
     userPermissions: string[]
   ): NavItem[] {
     return this.appNavigationTree[tree].filter(navItem => {
-      // if the nav item doesn't have a requiredPermissions key --> return true
       return (
-        !navItem.requiredPermissions ||
-        this.permissionService.hasPermission(
-          navItem.requiredPermissions,
-          userPermissions
+        // hide nav item when a userPermissions is found in hideWhenRequiredPermissions
+        !(
+          navItem.hideWhenRequiredPermissions &&
+          this.permissionService.hasPermission(
+            navItem.hideWhenRequiredPermissions,
+            userPermissions
+          )
+        )
+        &&
+        (
+          // if the nav item doesn't have a requiredPermissions key --> return true
+          !navItem.requiredPermissions ||
+          this.permissionService.hasPermission(
+            navItem.requiredPermissions,
+            userPermissions
+          )
         )
       );
     });
