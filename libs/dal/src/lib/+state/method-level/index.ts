b09490805f134c199cfce8bed72ec6d230fe4ef4
DavidBoute <david.boute@gmail.com>
2019-11-20 10:48:06 +0100

    formatting

--- libs/dal/src/lib/+state/method-level/index.ts
+++ libs/dal/src/lib/+state/method-level/index.ts
@@ -3,4 +3,9 @@ import { MethodLevelEffects } from './method-level.effects';
 import * as MethodLevelReducer from './method-level.reducer';
 import * as MethodLevelQueries from './method-level.selectors';
 
-export { MethodLevelActions, MethodLevelReducer, MethodLevelQueries, MethodLevelEffects };
+export {
+  MethodLevelActions,
+  MethodLevelReducer,
+  MethodLevelQueries,
+  MethodLevelEffects
+};
--- libs/dal/src/lib/+state/method-level/method-level.selectors.ts
+++ libs/dal/src/lib/+state/method-level/method-level.selectors.ts
@@ -20,11 +20,20 @@ export const getLoaded = createSelector(
   (state: State) => state.loaded
 );
 
-export const getAll = createSelector(selectMethodLevelState, selectAll);
+export const getAll = createSelector(
+  selectMethodLevelState,
+  selectAll
+);
 
-export const getCount = createSelector(selectMethodLevelState, selectTotal);
+export const getCount = createSelector(
+  selectMethodLevelState,
+  selectTotal
+);
 
-export const getIds = createSelector(selectMethodLevelState, selectIds);
+export const getIds = createSelector(
+  selectMethodLevelState,
+  selectIds
+);
 
 export const getAllEntities = createSelector(
   selectMethodLevelState,
@@ -44,7 +53,6 @@ export const getByIds = createSelector(
     return props.ids.map(id => state.entities[id]);
   }
 );
-
 
 /**
  * returns array of objects in the order of the given ids
