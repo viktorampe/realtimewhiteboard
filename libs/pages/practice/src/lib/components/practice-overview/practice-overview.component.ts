b93fa721b75a09b8a75b917ade0aed04f8d0b166
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2019-11-20 08:58:35 +0100

    remove empty onInit

--- libs/pages/practice/src/lib/components/practice-overview/practice-overview.component.ts
+++ libs/pages/practice/src/lib/components/practice-overview/practice-overview.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'campus-practice-overview',
   templateUrl: './practice-overview.component.html',
   styleUrls: ['./practice-overview.component.scss']
 })
-export class PracticeOverviewComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class PracticeOverviewComponent {
+  constructor() {}
 }
