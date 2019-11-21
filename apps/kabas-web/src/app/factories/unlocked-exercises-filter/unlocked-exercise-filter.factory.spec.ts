688a7927aa3042092de4ddcd2043cfaef085f295
DavidBoute <david.boute@gmail.com>
2019-11-21 14:28:15 +0100

    create unlocked exercise filter factory

---
+++ apps/kabas-web/src/app/factories/unlocked-exercises-filter/unlocked-exercise-filter.factory.spec.ts
@@ -0,0 +1,132 @@
+import { TestBed } from '@angular/core/testing';
+import {
+  DalState,
+  getStoreModuleForFeatures,
+  MethodLevelActions,
+  MethodLevelFixture,
+  MethodLevelReducer
+} from '@campus/dal';
+import {
+  ButtonToggleFilterComponent,
+  SearchFilterInterface,
+  SearchStateInterface
+} from '@campus/search';
+import { Store, StoreModule } from '@ngrx/store';
+import { cold } from '@nrwl/nx/testing';
+import { configureTestSuite } from 'ng-bullet';
+import { UnlockedExerciseFilterFactory } from './unlocked-exercise-filter.factory';
+
+describe('ChapterLessonFilterFactory', () => {
+  let store: Store<DalState>;
+  let factory: UnlockedExerciseFilterFactory;
+
+  const mockMethodLevels = [
+    new MethodLevelFixture({ id: 11, methodId: 1, levelId: 1, label: 'basis' }),
+    new MethodLevelFixture({ id: 12, methodId: 1, levelId: 2, label: 'extra' }),
+    new MethodLevelFixture({ id: 21, methodId: 2, levelId: 1, label: 'groen' }),
+    new MethodLevelFixture({ id: 22, methodId: 2, levelId: 2, label: 'blauw' })
+  ];
+
+  configureTestSuite(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        StoreModule.forRoot({}),
+        ...getStoreModuleForFeatures([MethodLevelReducer])
+      ],
+      providers: [UnlockedExerciseFilterFactory, Store]
+    });
+  });
+
+  beforeEach(() => {
+    store = TestBed.get(Store);
+    factory = TestBed.get(UnlockedExerciseFilterFactory);
+
+    loadInStore();
+  });
+
+  it('should be created', () => {
+    expect(factory).toBeTruthy();
+  });
+
+  describe('getFilters', () => {
+    const mockSearchState: SearchStateInterface = {
+      searchTerm: '',
+      filterCriteriaSelections: new Map<string, (number | string)[]>([
+        ['method', [2]]
+      ])
+    };
+
+    it('should return the right filters', () => {
+      const result = factory.getFilters(mockSearchState);
+
+      expect(result).toBeObservable(
+        cold('a', {
+          a: [getExpectedMethodLevelFilter()]
+        })
+      );
+    });
+  });
+
+  describe('getPredictionFilterNames', () => {
+    it('should return the correct filter names', () => {
+      const filternames = factory.getPredictionFilterNames();
+      expect(filternames).toEqual(['methodLevel']);
+    });
+  });
+
+  function getExpectedFilter(
+    name,
+    label,
+    keyProperty,
+    displayProperty,
+    values,
+    component,
+    options?
+  ): SearchFilterInterface {
+    const searchFilter = {
+      criteria: {
+        name: name,
+        label: label,
+        keyProperty: keyProperty,
+        displayProperty: displayProperty,
+        values: values.map(val => ({
+          data: val,
+          visible: true,
+          child: (val as any).children
+            ? getExpectedFilter(
+                name,
+                label,
+                keyProperty,
+                displayProperty,
+                (val as any).children,
+                component
+              ).criteria
+            : undefined
+        }))
+      },
+      component: component,
+      domHost: 'hostTop'
+    } as SearchFilterInterface;
+    if (options) searchFilter.options = options;
+    return searchFilter;
+  }
+
+  function getExpectedMethodLevelFilter() {
+    return getExpectedFilter(
+      'methodLevel',
+      'Type',
+      'id',
+      'label',
+      mockMethodLevels, // TODO: change to [mockMethodLevels[2], mockMethodLevels[3]]
+      ButtonToggleFilterComponent
+    );
+  }
+
+  function loadInStore() {
+    store.dispatch(
+      new MethodLevelActions.MethodLevelsLoaded({
+        methodLevels: mockMethodLevels
+      })
+    );
+  }
+});
---
+++ apps/kabas-web/src/app/factories/unlocked-exercises-filter/unlocked-exercise-filter.factory.ts
@@ -0,0 +1,148 @@
+import { Injectable, InjectionToken } from '@angular/core';
+import { DalState, MethodLevelQueries } from '@campus/dal';
+import {
+  ButtonToggleFilterComponent,
+  SearchFilterCriteriaInterface,
+  SearchFilterFactory,
+  SearchFilterInterface,
+  SearchStateInterface
+} from '@campus/search';
+import {
+  MemoizedSelector,
+  MemoizedSelectorWithProps,
+  select,
+  Store
+} from '@ngrx/store';
+import { combineLatest, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { FilterQueryInterface } from '../filter-query.interface';
+
+export const CHAPTER_LESSON_FILTER_FACTORY_TOKEN = new InjectionToken(
+  'ChapterLessonFilterFactory'
+);
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UnlockedExerciseFilterFactory implements SearchFilterFactory {
+  private keyProperty = 'id';
+  private displayProperty = 'label';
+  private component = ButtonToggleFilterComponent;
+  private domHost = 'hostTop';
+
+  protected filterSortOrder = ['methodLevel'];
+
+  protected filterQueries: {
+    [key: string]: FilterQueryInterface;
+  } = {
+    methodLevel: {
+      // TODO: getAllForMethodId -> need to create this one -> dict
+      query: MethodLevelQueries.getAll,
+      name: 'methodLevel',
+      label: 'Type',
+      methodDependent: true
+    }
+  };
+
+  constructor(public store: Store<DalState>) {}
+
+  public getFilters(
+    searchState: SearchStateInterface
+  ): Observable<SearchFilterInterface[]> {
+    return combineLatest(this.createFilters(searchState)).pipe(
+      map(searchFilters =>
+        searchFilters
+          .filter(f => {
+            return f.criteria.values.length > 0;
+          })
+          .sort((a, b) => this.filterSorter(a, b, this.filterSortOrder))
+      )
+    );
+  }
+
+  public getPredictionFilterNames(): string[] {
+    return Object.values(this.filterQueries).map(value => value.name);
+  }
+
+  protected createFilters(
+    searchState: SearchStateInterface
+  ): Observable<SearchFilterInterface>[] {
+    const filters: Observable<SearchFilterInterface>[] = [];
+    const methodLevelFilter = this.buildFilter('methodLevel', searchState);
+    filters.push(methodLevelFilter);
+
+    return filters;
+  }
+
+  private getFilter<T>(
+    entities: T[],
+    filterQuery: FilterQueryInterface,
+    searchState: SearchStateInterface
+  ): SearchFilterInterface {
+    const searchFilter = {
+      criteria: {
+        name: filterQuery.name,
+        label: filterQuery.label,
+        keyProperty: this.keyProperty,
+        displayProperty: filterQuery.displayProperty || this.displayProperty,
+        values: entities.map(entity => ({
+          data: entity,
+          visible: true,
+          child: (entity as any).children
+            ? this.getFilter((entity as any).children, filterQuery, searchState)
+                .criteria
+            : undefined
+        }))
+      },
+      component: filterQuery.component || this.component,
+      domHost: filterQuery.domHost || this.domHost
+    } as SearchFilterInterface;
+    if (filterQuery.options) searchFilter.options = filterQuery.options;
+    return searchFilter;
+  }
+
+  private filterSorter(
+    a: SearchFilterInterface,
+    b: SearchFilterInterface,
+    order: string[]
+  ): number {
+    let aIndex = order.indexOf(
+      (a.criteria as SearchFilterCriteriaInterface).name
+    );
+    aIndex = aIndex === -1 ? order.length : aIndex; // not found -> add at end
+
+    let bIndex = order.indexOf(
+      (b.criteria as SearchFilterCriteriaInterface).name
+    );
+    bIndex = bIndex === -1 ? order.length : bIndex; // not found -> add at end
+
+    return aIndex - bIndex;
+  }
+
+  protected buildFilter(
+    name: string,
+    searchState: SearchStateInterface
+  ): Observable<SearchFilterInterface> {
+    const filterQuery = this.filterQueries[name];
+    if (filterQuery.methodDependent) {
+      return this.store.pipe(
+        select(
+          filterQuery.query as MemoizedSelectorWithProps<Object, any, any>,
+          { methodIds: searchState.filterCriteriaSelections.get('method') }
+        ),
+        // TODO -> waiting for selector
+        // map((dict: { [id: string]: MethodLevelInterface }) =>
+        //   this.getFilter(Object.values(dict), filterQuery, searchState)
+        // )
+        map(entities => this.getFilter(entities, filterQuery, searchState))
+      );
+    } else {
+      return this.store.pipe(
+        select(filterQuery.query as MemoizedSelector<Object, any[]>),
+        map(entities => {
+          return this.getFilter(entities, filterQuery, searchState);
+        })
+      );
+    }
+  }
+}
