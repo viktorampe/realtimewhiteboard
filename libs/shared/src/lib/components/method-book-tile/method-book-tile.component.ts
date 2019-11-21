8f65abba93d1b774e862700a9e45011f132c240d
Yannis dejonghe <yannis.dejonghe@hotmail.com>
2019-11-21 15:26:49 +0100

    CR

--- libs/shared/src/lib/components/method-book-tile/method-book-tile.component.ts
+++ libs/shared/src/lib/components/method-book-tile/method-book-tile.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'campus-method-book-tile',
@@ -11,6 +11,8 @@ export class MethodBookTileComponent implements OnInit {
   @Input() logoUrl: string;
   @Input() bookId: number;
 
+  @HostBinding('class.shared-method-book-tile')
+  pagesMethodsMethodBookTileClass = true;
   constructor() {}
 
   ngOnInit() {}
