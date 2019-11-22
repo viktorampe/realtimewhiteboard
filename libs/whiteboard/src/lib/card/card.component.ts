592650b37c86bd0a126a049ee36ac0a4b4f8d4a7
Lukas Nys <lukas.nys2@student.vives.be>
2019-11-22 10:04:19 +0100

    Changed styling to use padding instead of width

--- libs/whiteboard/src/lib/card/card.component.ts
+++ libs/whiteboard/src/lib/card/card.component.ts
@@ -1,12 +1,4 @@
-import {
-  Component,
-  ElementRef,
-  EventEmitter,
-  Input,
-  OnInit,
-  Output,
-  ViewChild
-} from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import Card from '../../interfaces/Card';
 
 @Component({
