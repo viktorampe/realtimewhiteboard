75a91f8a0fec97fe0d97b0ddf0714c7570917766
DavidBoute <david.boute@gmail.com>
2019-11-25 15:54:00 +0100

    more CR

--- libs/dal/src/lib/toc/toc.service.ts
+++ libs/dal/src/lib/toc/toc.service.ts
@@ -61,6 +61,6 @@ export class TocService implements TocServiceInterface {
   getEduContentTocEduContentForBookId(
     bookId: number
   ): Observable<EduContentTOCEduContentInterface[]> {
-    return this.eduContentBookApi['getUnlockedExercisesRemote'](bookId);
+    return this.eduContentBookApi.getUnlockedExercisesRemote(bookId);
   }
 }
