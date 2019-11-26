ed5074417ac960a3486ab4b88bf27973ea3e4829
karlvaniseghem <karl.vaniseghem@icloud.com>
2019-11-26 14:05:22 +0100

    added tests

--- libs/pages/practice/src/lib/components/practice.viewmodel.mock.ts
+++ libs/pages/practice/src/lib/components/practice.viewmodel.mock.ts
@@ -1,12 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import {
   ClassGroupFixture,
   ClassGroupInterface,
+  EduContent,
+  EduContentFixture,
+  EduContentInterface,
+  EduContentProductTypeFixture,
   EduContentTOCFixture,
   EduContentTOCInterface,
   MethodYearsInterface,
+  SearchResultInterface,
+  SearchStateInterface,
   UnlockedFreePracticeInterface
 } from '@campus/dal';
+import { SearchModeInterface } from '@campus/search';
+import {
+  EnvironmentSearchModesInterface,
+  ENVIRONMENT_SEARCHMODES_TOKEN
+} from '@campus/shared';
 import { ViewModelInterface } from '@campus/testing';
 import {
   MultiCheckBoxTableItemColumnInterface,
@@ -14,7 +25,8 @@ import {
   MultiCheckBoxTableRowHeaderColumnInterface
 } from '@campus/ui';
 import { Dictionary } from '@ngrx/entity';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { CurrentPracticeParams, PracticeViewModel } from './practice.viewmodel';
 import {
   ChapterWithStatusInterface,
@@ -26,6 +38,12 @@ import {
 })
 export class MockPracticeViewModel
   implements ViewModelInterface<PracticeViewModel> {
+  public searchResults$: Observable<SearchResultInterface>;
+  public searchState$ = new BehaviorSubject<SearchStateInterface>({
+    searchTerm: '',
+    filterCriteriaSelections: new Map<string, (string | number)[]>()
+  });
+
   public methodYears$ = new BehaviorSubject<MethodYearsInterface[]>(
     this.getAllowedBooks$()
   );
@@ -95,9 +113,120 @@ export class MockPracticeViewModel
     ChapterWithStatusInterface[]
   >(this.getChaptersWithStatus(20));
 
-  constructor() {}
+  constructor(
+    @Inject(ENVIRONMENT_SEARCHMODES_TOKEN)
+    private searchModes: EnvironmentSearchModesInterface
+  ) {
+    this.setupSearchResults();
+  }
+
+  public getSearchMode(mode: string): Observable<SearchModeInterface> {
+    return of(this.searchModes[mode]);
+  }
+
+  public getInitialSearchState(): Observable<SearchStateInterface> {
+    return this.searchState$;
+  }
 
   public toggleUnlockedFreePractice(): void {}
+
+  public updateState(state: SearchStateInterface): void {}
+
+  openEduContentAsExercise(eduContent: any): void {}
+  openEduContentAsSolution(eduContent: EduContent): void {}
+  openEduContentAsStream(eduContent: EduContent): void {}
+  openEduContentAsDownload(eduContent: EduContent): void {}
+  openBoeke(eduContent: EduContent): void {}
+
+  /*
+   *
+   */
+  private setupSearchResults(): void {
+    this.searchResults$ = this.getMockResults().pipe(
+      map(searchResult => {
+        return {
+          ...searchResult,
+          results: searchResult.results.map(
+            (searchResultItem: EduContentInterface) => {
+              const eduContent = Object.assign<EduContent, EduContentInterface>(
+                new EduContent(),
+                searchResultItem
+              );
+
+              return {
+                eduContent: eduContent
+                // add additional props for the resultItemComponent here
+              };
+            }
+          )
+        };
+      })
+    );
+  }
+
+  // tslint:disable-next-line: member-ordering
+  private loadedMockResults = false;
+  private getMockResults(): Observable<SearchResultInterface> {
+    if (this.loadedMockResults) {
+      return of({
+        count: 3,
+        results: [],
+        filterCriteriaPredictions: new Map()
+      });
+    }
+    this.loadedMockResults = true;
+
+    const mockResults: EduContent[] = [
+      new EduContentFixture(
+        {},
+        {
+          title: 'Aanliggende hoeken',
+          description:
+            'In dit leerobject maken leerlingen 3 tikoefeningen op aanliggende hoeken.',
+          fileExt: 'ludo.zip'
+        }
+      ),
+      new EduContentFixture(
+        {},
+        {
+          thumbSmall:
+            'https://avatars3.githubusercontent.com/u/31932368?s=460&v=4'
+        }
+      ),
+      new EduContentFixture(
+        {},
+        {
+          eduContentProductType: new EduContentProductTypeFixture({
+            pedagogic: true
+          })
+        }
+      )
+    ];
+
+    for (let i = 0; i < 25; i++) {
+      mockResults.push(
+        new EduContentFixture(
+          {},
+          {
+            title: 'Aanliggende hoeken',
+            description:
+              'In dit leerobject maken leerlingen 3 tikoefeningen op aanliggende hoeken.',
+            fileExt: 'ludo.zip'
+          }
+        )
+      );
+    }
+
+    return of({
+      count: mockResults.length,
+      results: mockResults,
+      filterCriteriaPredictions: new Map([
+        ['methods', new Map([[34, 3], [2, 50]])],
+        ['eduContentTOC', new Map()],
+        ['methodLevel', new Map()]
+      ])
+    });
+  }
 
   private getAllowedBooks$(): MethodYearsInterface[] {
     return [
