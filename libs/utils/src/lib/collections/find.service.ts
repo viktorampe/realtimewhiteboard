fdb83fb0b0e50871cc00cf113f57460c921d5d66
DavidBoute <david.boute@gmail.com>
2019-12-04 14:38:07 +0100

    create utility function

---
+++ libs/utils/src/lib/collections/find.service.ts
@@ -0,0 +1,15 @@
+export function findOneInArray<T>(entities: T[], props: Partial<T>) {
+  return entities.find(entity => {
+    return Object.keys(props).every(
+      prop => !props[prop] || entity[prop] === props[prop]
+    );
+  });
+}
+
+export function findManyInArray<T>(entities: T[], props: Partial<T>) {
+  return Object.values(entities).filter(entity => {
+    return Object.keys(props).every(
+      prop => !props[prop] || entity[prop] === props[prop]
+    );
+  });
+}
