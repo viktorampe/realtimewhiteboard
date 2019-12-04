ff5f5a868ba13d693c0f5afd16e29901cd37981c
Yanndejo <yannis.dejonghe@hotmail.com>
2019-12-04 14:55:19 +0100

    scaffold state for task-group

---
+++ libs/dal/src/lib/+state/task-group/index.ts
@@ -0,0 +1,6 @@
+import * as TaskGroupActions from './task-group.actions';
+import { TaskGroupEffects } from './task-group.effects';
+import * as TaskGroupReducer from './task-group.reducer';
+import * as TaskGroupQueries from './task-group.selectors';
+
+export { TaskGroupActions, TaskGroupReducer, TaskGroupQueries, TaskGroupEffects };
---
+++ libs/dal/src/lib/+state/task-group/task-group.selectors.ts
@@ -0,0 +1,59 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import {
+  NAME,
+  selectAll,
+  selectEntities,
+  selectIds,
+  selectTotal,
+  State
+} from './task-group.reducer';
+
+export const selectTaskGroupState = createFeatureSelector<State>(NAME);
+
+export const getError = createSelector(
+  selectTaskGroupState,
+  (state: State) => state.error
+);
+
+export const getLoaded = createSelector(
+  selectTaskGroupState,
+  (state: State) => state.loaded
+);
+
+export const getAll = createSelector(selectTaskGroupState, selectAll);
+
+export const getCount = createSelector(selectTaskGroupState, selectTotal);
+
+export const getIds = createSelector(selectTaskGroupState, selectIds);
+
+export const getAllEntities = createSelector(
+  selectTaskGroupState,
+  selectEntities
+);
+
+/**
+ * returns array of objects in the order of the given ids
+ * @example
+ * taskGroup$: TaskGroupInterface[] = this.store.pipe(
+    select(TaskGroupQueries.getByIds, { ids: [2, 1, 3] })
+  );
+ */
+export const getByIds = createSelector(
+  selectTaskGroupState,
+  (state: State, props: { ids: number[] }) => {
+    return props.ids.map(id => state.entities[id]);
+  }
+);
+
+
+/**
+ * returns array of objects in the order of the given ids
+ * @example
+ * taskGroup$: TaskGroupInterface = this.store.pipe(
+    select(TaskGroupQueries.getById, { id: 3 })
+  );
+ */
+export const getById = createSelector(
+  selectTaskGroupState,
+  (state: State, props: { id: number }) => state.entities[props.id]
+);
