9c307a640c005480ffbdf59a54de67044e7dc8cb
DavidBoute <david.boute@gmail.com>
2019-12-05 09:32:58 +0100

    content + tests

--- libs/dal/src/lib/group/group.service.ts
+++ libs/dal/src/lib/group/group.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { PersonApi } from '@diekeure/polpo-api-angular-sdk';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { GroupInterface } from '../+models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroupService {
+  constructor(private personApi: PersonApi) {}
 
-  constructor() { }
+  public getAllForUser(userId: number): Observable<GroupInterface[]> {
+    return this.personApi
+      .getData(userId, 'groups') //TODO differentiate between owned groups and assigned groups?
+      .pipe(map((res: { groups: GroupInterface[] }) => res.groups));
+  }
 }
