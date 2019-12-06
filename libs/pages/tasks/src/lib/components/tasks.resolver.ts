39333647b856349acc87feb135ec94df675a2b2b
DavidBoute <david.boute@gmail.com>
2019-12-06 13:02:30 +0100

    CR David

--- libs/pages/tasks/src/lib/components/tasks.resolver.ts
+++ libs/pages/tasks/src/lib/components/tasks.resolver.ts
@@ -2,32 +2,20 @@ import { Inject, Injectable } from '@angular/core';
 import {
   AuthServiceInterface,
   AUTH_SERVICE_TOKEN,
-  ClassGroupActions,
-  ClassGroupQueries,
   DalState,
   EduContentActions,
   EduContentQueries,
-  GroupActions,
-  GroupQueries,
   LearningAreaActions,
   LearningAreaQueries,
-  LinkedPersonActions,
-  LinkedPersonQueries,
   MethodActions,
   MethodQueries,
   StateResolver,
   TaskActions,
-  TaskClassGroupActions,
-  TaskClassGroupQueries,
   TaskEduContentActions,
   TaskEduContentQueries,
-  TaskGroupActions,
-  TaskGroupQueries,
   TaskInstanceActions,
   TaskInstanceQueries,
-  TaskQueries,
-  TaskStudentActions,
-  TaskStudentQueries
+  TaskQueries
 } from '@campus/dal';
 import { Action, Selector, Store } from '@ngrx/store';
 
@@ -56,27 +44,6 @@ export class TasksResolver extends StateResolver {
       }),
       new TaskInstanceActions.LoadTaskInstances({
         userId: this.authService.userId
-      }),
-      new TaskGroupActions.LoadTaskGroups({
-        userId: this.authService.userId
-      }),
-      new TaskStudentActions.LoadTaskStudents({
-        userId: this.authService.userId
-      }),
-      new TaskClassGroupActions.LoadTaskClassGroups({
-        userId: this.authService.userId
-      }),
-      new GroupActions.LoadGroups({
-        userId: this.authService.userId
-      }),
-      new ClassGroupActions.LoadClassGroups({
-        userId: this.authService.userId
-      }),
-      new LinkedPersonActions.LoadLinkedPersons({
-        userId: this.authService.userId
-      }),
-      new ClassGroupActions.LoadClassGroups({
-        userId: this.authService.userId
       })
     ];
   }
@@ -88,14 +55,7 @@ export class TasksResolver extends StateResolver {
       EduContentQueries.getLoaded,
       TaskQueries.getLoaded,
       TaskInstanceQueries.getLoaded,
-      TaskEduContentQueries.getLoaded,
-      TaskGroupQueries.getLoaded,
-      TaskStudentQueries.getLoaded,
-      ClassGroupQueries.getLoaded,
-      LinkedPersonQueries.getLoaded,
-      ClassGroupQueries.getLoaded,
-      GroupQueries.getLoaded,
-      TaskClassGroupQueries.getLoaded
+      TaskEduContentQueries.getLoaded
     ];
   }
 }
