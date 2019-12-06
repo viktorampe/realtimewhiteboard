b1b037ff562806111a453d624edc210b9ad9aa05
Karl Van Iseghem <karl.vaniseghem@icloud.com>
2019-12-06 13:37:35 +0100

    Apply suggestions from code review
    Co-Authored-By: DavidBoute <31932368+DavidBoute@users.noreply.github.com>

--- libs/pages/kabas-tasks/src/lib/components/kabas-tasks.viewmodel.mock.ts
+++ libs/pages/kabas-tasks/src/lib/components/kabas-tasks.viewmodel.mock.ts
@@ -18,9 +18,9 @@ export class MockKabasTasksViewModel
 
   constructor() {
     const tasks = this.setupTaskWithAssignments();
-    this.tasksWithAssignments$ = of(tasks);
+    this.tasksWithAssignments$ = new BehaviorSubject<TaskWithAssigneesInterface>(tasks);
 
-    this.paperTasksWithAssignments$ = of(
+    this.paperTasksWithAssignments$ = new BehaviorSubject<TaskWithAssigneesInterface>(
       tasks.map(task => {
         return {
           ...task,
