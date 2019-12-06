fb620750a6f31a31c5b05bf4432439c00761b2da
karlvaniseghem <karl.vaniseghem@icloud.com>
2019-12-06 12:50:41 +0100

    fixed missing guardsmodule

--- libs/pages/kabas-tasks/src/lib/pages-kabas-tasks-routing.module.ts
+++ libs/pages/kabas-tasks/src/lib/pages-kabas-tasks-routing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PermissionGuard } from '@campus/guards';
 import { ManageKabasTasksOverviewComponent } from './components/manage-kabas-tasks-overview/manage-kabas-tasks-overview.component';
 
 const routes: Routes = [
   {
     path: 'manage',
-    component: ManageKabasTasksOverviewComponent
-    // runGuardsAndResolvers: 'always',
-    // data: {
-    //   requiredPermissions: 'manageTasks'
-    // },
-    // canActivate: [PermissionGuard]
+    component: ManageKabasTasksOverviewComponent,
+    runGuardsAndResolvers: 'always',
+    data: {
+      requiredPermissions: 'manageTasks'
+    },
+    canActivate: [PermissionGuard]
   }
 ];
 
