2213c5b4ad8ac09956218064f855d60111363f00
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2019-12-10 17:30:57 +0100

    basic multifilter setup

--- libs/pages/kabas-tasks/src/lib/components/manage-kabas-tasks-overview/manage-kabas-tasks-overview.component.ts
+++ libs/pages/kabas-tasks/src/lib/components/manage-kabas-tasks-overview/manage-kabas-tasks-overview.component.ts
@@ -1,13 +1,33 @@
-import { Component, Inject, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  Inject,
+  OnInit,
+  ViewChild
+} from '@angular/core';
 import { MatSelectionList } from '@angular/material';
 import { ActivatedRoute, Router } from '@angular/router';
+import {
+  SearchFilterCriteriaFixture,
+  SearchFilterCriteriaInterface,
+  SearchFilterCriteriaValuesFixture,
+  SelectFilterComponent
+} from '@campus/search';
 import { FilterTextInputComponent } from '@campus/ui';
 import { FilterServiceInterface, FILTER_SERVICE_TOKEN } from '@campus/utils';
-import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { combineLatest, Observable } from 'rxjs';
+import { map, shareReplay, startWith, tap } from 'rxjs/operators';
 import { KabasTasksViewModel } from '../kabas-tasks.viewmodel';
 import { MockKabasTasksViewModel } from '../kabas-tasks.viewmodel.mock';
 import { TaskWithAssigneesInterface } from '../kabas-tasks.viewmodel.selectors';
+
+interface FilterState {
+  searchText?: string;
+  learningArea?: number[];
+  dateInterval?: { start: Date; end: Date };
+  assignees?: number[];
+}
 
 @Component({
   selector: 'campus-manage-kabas-tasks-overview',
@@ -18,10 +38,19 @@ import { TaskWithAssigneesInterface } from '../kabas-tasks.viewmodel.selectors';
     { provide: KabasTasksViewModel, useClass: MockKabasTasksViewModel }
   ]
 })
-export class ManageKabasTasksOverviewComponent implements OnInit {
+export class ManageKabasTasksOverviewComponent
+  implements OnInit, AfterViewInit {
+  public mockFilterCriteria = new SearchFilterCriteriaFixture({}, [
+    new SearchFilterCriteriaValuesFixture()
+  ]);
+
   public tasksWithAssignments$: Observable<TaskWithAssigneesInterface[]>;
   public paperTasksWithAssignments$: Observable<TaskWithAssigneesInterface[]>;
   public currentTab$: Observable<number>;
+  public filteredTasks$: Observable<TaskWithAssigneesInterface[]>;
+
+  private learningAreaFilter: SelectFilterComponent;
+  private learningAreaFilterSelection$: Observable<number[]>;
 
   @ViewChild('digitalTasksTextFilter')
   digitalTasksFilterTextInput: FilterTextInputComponent<
@@ -36,20 +65,35 @@ export class ManageKabasTasksOverviewComponent implements OnInit {
 
   @ViewChild('paper') paperTaskList: MatSelectionList;
   @ViewChild('digital') digitalTaskList: MatSelectionList;
+  @ViewChild('learningAreaSelectFilter')
+  set learningAreaSelectFilter(
+    learningAreaSelectFilter: SelectFilterComponent
+  ) {
+    this.learningAreaFilter = learningAreaSelectFilter;
+    this.learningAreaFilterSelection$ = this.getLearningAreaSelections();
+    if (!this.filteredTasks$) this.filteredTasks$ = this.getFilteredTasks();
+  }
 
   constructor(
     private viewModel: KabasTasksViewModel,
     private router: Router,
     private route: ActivatedRoute,
-    @Inject(FILTER_SERVICE_TOKEN) private filterService: FilterServiceInterface
+    @Inject(FILTER_SERVICE_TOKEN) private filterService: FilterServiceInterface,
+    private cd: ChangeDetectorRef
   ) {}
 
   ngOnInit() {
     this.currentTab$ = this.getCurrentTab();
     this.tasksWithAssignments$ = this.viewModel.tasksWithAssignments$;
     this.paperTasksWithAssignments$ = this.viewModel.paperTasksWithAssignments$;
+
     this.digitalTasksFilterTextInput.setFilterableItem(this);
     this.paperTasksFilterTextInput.setFilterableItem(this);
+  }
+
+  ngAfterViewInit(): void {
+    this.learningAreaFilter.filterCriteria = this.mockFilterCriteria;
+    this.cd.detectChanges();
   }
 
   clickAddDigitalTask() {
@@ -67,6 +111,72 @@ export class ManageKabasTasksOverviewComponent implements OnInit {
 
   // TODO:
   // - merge all filter results in one stream 'filteredTasks$'
+  // - filter based on:
+  //  - text
+  //  - learningArea
+  //  - date
+  //  - assignee
+  //  - stopped/started/not yet started
+
+  private getLearningAreaSelections() {
+    return this.learningAreaFilter.filterSelectionChange.pipe(
+      map((filterCriteria: SearchFilterCriteriaInterface[]) => {
+        // array is emitted, but there is only one value
+        const criterium = filterCriteria[0];
+        // extract selected options from filter
+        const selectedOptions = criterium.values
+          .filter(value => value.selected)
+          .map(selectedValue => selectedValue.data.id);
+        return selectedOptions;
+      })
+    );
+  }
+
+  private getFilteredTasks(): Observable<TaskWithAssigneesInterface[]> {
+    return combineLatest([
+      this.currentTab$,
+      this.learningAreaFilterSelection$.pipe(startWith([])),
+      this.digitalTasksFilterTextInput.result$,
+      this.paperTasksFilterTextInput.result$
+    ]).pipe(
+      map(
+        ([
+          currentTabIndex,
+          selectedLearningAreas,
+          textFilteredDigitalTasks,
+          textFilteredPaperTasks
+        ]) => {
+          const newFilterState: FilterState = {
+            learningArea: selectedLearningAreas
+          };
+
+          return this.filterTasks(
+            newFilterState,
+            currentTabIndex === 0
+              ? textFilteredDigitalTasks
+              : textFilteredPaperTasks
+          );
+        }
+      ),
+      shareReplay(1)
+    );
+  }
+
+  private filterTasks(
+    filterState: FilterState,
+    tasks: TaskWithAssigneesInterface[]
+  ): TaskWithAssigneesInterface[] {
+    if (tasks.length === 0) return [];
+    let filteredTasks = [...tasks];
+    if (filterState.learningArea && filterState.learningArea.length) {
+      // filter on learning areas
+      filteredTasks = tasks.filter(task => {
+        return filterState.learningArea.includes(task.learningAreaId);
+      });
+    }
+
+    return filteredTasks;
+  }
 
   private getCurrentTab(): Observable<number> {
     return this.route.queryParams.pipe(
--- libs/pages/kabas-tasks/src/lib/pages-kabas-tasks.module.ts
+++ libs/pages/kabas-tasks/src/lib/pages-kabas-tasks.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { GuardsModule } from '@campus/guards';
 import { PagesSharedModule } from '@campus/pages/shared';
+import { SearchModule } from '@campus/search';
 import { SharedModule } from '@campus/shared';
 import { UiModule } from '@campus/ui';
 import { ManageKabasTasksOverviewComponent } from './components/manage-kabas-tasks-overview/manage-kabas-tasks-overview.component';
@@ -14,6 +15,7 @@ import { PagesKabasTasksRoutingModule } from './pages-kabas-tasks-routing.module
     UiModule,
     PagesSharedModule,
     SharedModule,
+    SearchModule,
     GuardsModule
   ],
   declarations: [ManageKabasTasksOverviewComponent],
