64422a4f523490cb21faae2838dd6fac1564de9d
DavidBoute <david.boute@gmail.com>
2019-12-12 11:49:11 +0100

    page structure

--- libs/pages/login/src/lib/pages-login.module.ts
+++ libs/pages/login/src/lib/pages-login.module.ts
@@ -1,5 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule
+} from '@angular/material';
 import { PagesSharedModule } from '@campus/pages/shared';
 import { UiModule } from '@campus/ui';
 import { LoginComponent } from './components/login.component';
@@ -11,11 +17,13 @@ import { PagesLoginRoutingModule } from './pages-login-routing.module';
     CommonModule,
     PagesLoginRoutingModule,
     UiModule,
-    PagesSharedModule
+    PagesSharedModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
   ],
-  declarations: [
-    LoginComponent,
-  ],
+  declarations: [LoginComponent],
   providers: [LoginViewModel]
 })
 export class PagesLoginModule {}
