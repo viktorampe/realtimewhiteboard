c7c3ff0edc6e783e3ac61026958a3f00f7ae3123
DavidBoute <david.boute@gmail.com>
2019-12-13 12:38:42 +0100

    unit tests

--- libs/pages/login/src/lib/components/login.component.spec.ts
+++ libs/pages/login/src/lib/components/login.component.spec.ts
@@ -5,9 +5,12 @@ import {
   MatIconModule,
   MatInputModule
 } from '@angular/material';
+import { By } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { UiModule } from '@campus/ui';
+import { PersonFixture, PersonInterface } from '@campus/dal';
+import { ButtonComponent, UiModule } from '@campus/ui';
 import { configureTestSuite } from 'ng-bullet';
+import { BehaviorSubject } from 'rxjs';
 import { LoginComponent } from './login.component';
 import { LoginViewModel } from './login.viewmodel';
 import { MockLoginViewModel } from './login.viewmodel.mock';
@@ -15,6 +18,7 @@ import { MockLoginViewModel } from './login.viewmodel.mock';
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let loginViewModel: LoginViewModel;
 
   configureTestSuite(() => {
     TestBed.configureTestingModule({
@@ -34,10 +38,90 @@ describe('LoginComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    loginViewModel = TestBed.get(LoginViewModel);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('template', () => {
+    let currentUser$: BehaviorSubject<PersonInterface>;
+
+    beforeEach(() => {
+      currentUser$ = loginViewModel.currentUser$ as BehaviorSubject<
+        PersonInterface
+      >;
+    });
+
+    describe('logged in', () => {
+      const person = new PersonFixture();
+
+      beforeEach(() => {
+        currentUser$.next(person);
+        fixture.detectChanges();
+      });
+
+      it('should show the current user name', () => {
+        const formTitleDE = fixture.debugElement.query(
+          By.css('.pages-login__form__title')
+        );
+
+        expect(formTitleDE.nativeElement.textContent.trim()).toBe(
+          'Je bent ingelogd als ' + person.displayName
+        );
+      });
+
+      it('should show a log out button', () => {
+        const buttonDE = fixture.debugElement.query(
+          By.directive(ButtonComponent)
+        );
+
+        expect(buttonDE.nativeElement.textContent.trim()).toBe('Log uit');
+      });
+    });
+
+    describe('not logged in', () => {
+      beforeEach(() => {
+        currentUser$.next(undefined);
+        fixture.detectChanges();
+      });
+
+      it('should show the login form', () => {
+        const formTitleDE = fixture.debugElement.query(
+          By.css('.pages-login__form__title')
+        );
+
+        expect(formTitleDE.nativeElement.textContent.trim()).toBe(
+          'Vul uw gebruikersgegevens in'
+        );
+      });
+
+      it('should show a log in button', () => {
+        const buttonDE = fixture.debugElement.query(
+          By.directive(ButtonComponent)
+        );
+
+        expect(buttonDE.nativeElement.textContent.trim()).toBe('Log in');
+      });
+
+      it('should show the login presets', () => {
+        const loginPresets = loginViewModel.loginPresets;
+
+        const buttonDEs = fixture.debugElement.queryAll(
+          By.directive(ButtonComponent)
+        );
+
+        // first button is log in button
+        const loginPresetButtonDEs = buttonDEs.slice(1);
+
+        loginPresets.forEach((preset, index) => {
+          expect(
+            loginPresetButtonDEs[index].nativeElement.textContent.trim()
+          ).toBe(preset.label);
+        });
+      });
+    });
   });
 });
