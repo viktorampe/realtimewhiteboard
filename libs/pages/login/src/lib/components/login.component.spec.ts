b636412ac57411718841520bae02bfc12383deb6
karlvaniseghem <karl.vaniseghem@icloud.com>
2019-12-13 13:24:11 +0100

    added tests

--- libs/pages/login/src/lib/components/login.component.spec.ts
+++ libs/pages/login/src/lib/components/login.component.spec.ts
@@ -5,6 +5,7 @@ import {
   MatIconModule,
   MatInputModule
 } from '@angular/material';
+import { By } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { UiModule } from '@campus/ui';
 import { configureTestSuite } from 'ng-bullet';
@@ -14,6 +15,7 @@ import { MockLoginViewModel } from './login.viewmodel.mock';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
+  let viewmodel: MockLoginViewModel;
   let fixture: ComponentFixture<LoginComponent>;
 
   configureTestSuite(() => {
@@ -33,11 +35,33 @@ describe('LoginComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
+    viewmodel = TestBed.get(LoginViewModel);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call clearError on keydown', () => {
+    jest.spyOn(component, 'clearError');
+    viewmodel.currentUser$.next(null);
+    fixture.detectChanges();
+
+    const event = new KeyboardEvent('keydown', {
+      bubbles: true,
+      cancelable: true,
+      shiftKey: false
+    });
+
+    const inputFields = fixture.debugElement.queryAll(By.css('input'));
+
+    inputFields.forEach(field => {
+      const inputElement = field.nativeElement;
+      inputElement.dispatchEvent(event);
+    });
+
+    expect(component.clearError).toHaveBeenCalledTimes(2);
   });
 });
