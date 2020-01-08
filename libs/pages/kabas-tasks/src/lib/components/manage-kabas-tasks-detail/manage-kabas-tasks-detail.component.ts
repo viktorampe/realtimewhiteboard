c55984158b9ea7f2501fff6a87844b12cacc2fd0
karlvaniseghem <karl.vaniseghem@icloud.com>
2020-01-08 11:50:51 +0100

    format check solutions

--- libs/pages/kabas-tasks/src/lib/components/manage-kabas-tasks-detail/manage-kabas-tasks-detail.component.ts
+++ libs/pages/kabas-tasks/src/lib/components/manage-kabas-tasks-detail/manage-kabas-tasks-detail.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'campus-manage-kabas-tasks-detail',
   templateUrl: './manage-kabas-tasks-detail.component.html',
   styleUrls: ['./manage-kabas-tasks-detail.component.scss']
 })
-export class ManageKabasTasksDetailComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ManageKabasTasksDetailComponent {
+  constructor() {}
 }
