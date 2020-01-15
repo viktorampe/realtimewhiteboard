844eec3679589300543190997d8c667af8c8c223
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2020-01-15 17:23:33 +0100

    add test

--- libs/ui/src/lib/confirmation-pop-up/confirmation-pop-up.component.spec.ts
+++ libs/ui/src/lib/confirmation-pop-up/confirmation-pop-up.component.spec.ts
@@ -1,16 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { ConfirmationPopUpComponent } from './confirmation-pop-up.component';
 
+// file.only
 describe('ConfirmationPopUpComponent', () => {
   let component: ConfirmationPopUpComponent;
   let fixture: ComponentFixture<ConfirmationPopUpComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ConfirmationPopUpComponent ]
-    })
-    .compileComponents();
+      declarations: [ConfirmationPopUpComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +21,23 @@ describe('ConfirmationPopUpComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should execute the action handlers', () => {
+    component.actions = [
+      {
+        handler: jest.fn(),
+        icon: 'foo',
+        label: 'foo'
+      }
+    ];
+
+    fixture.detectChanges();
+
+    const button: HTMLButtonElement = fixture.debugElement.query(
+      By.css('button')
+    ).nativeElement;
+    button.click();
+    expect(component.actions[0].handler).toHaveBeenCalled();
   });
 });
