0d2866d0fb2dae44cd157836cf2130fbecd380e5
Yanndejo <yannis.dejonghe@hotmail.com>
2020-01-24 13:56:44 +0100

    added UI/UX

--- libs/pages/kabas-tasks/src/lib/pages-kabas-tasks.module.ts
+++ libs/pages/kabas-tasks/src/lib/pages-kabas-tasks.module.ts
@@ -19,8 +19,8 @@ import { ManageKabasTasksAssigneeModalComponent } from './components/manage-kaba
 import { ManageKabasTasksDetailComponent } from './components/manage-kabas-tasks-detail/manage-kabas-tasks-detail.component';
 import { ManageKabasTasksOverviewComponent } from './components/manage-kabas-tasks-overview/manage-kabas-tasks-overview.component';
 import { NewTaskComponent } from './components/new-task/new-task.component';
-import { TaskEduContentListItemComponent } from './components/task-edu-content-list-item/task-edu-content-list-item.component';
 import { PrintPaperTaskModalComponent } from './components/print-paper-task-modal/print-paper-task-modal.component';
+import { TaskEduContentListItemComponent } from './components/task-edu-content-list-item/task-edu-content-list-item.component';
 import { TaskListItemComponent } from './components/task-list-item/task-list-item.component';
 import { PagesKabasTasksRoutingModule } from './pages-kabas-tasks-routing.module';
 
@@ -46,6 +46,7 @@ import { PagesKabasTasksRoutingModule } from './pages-kabas-tasks-routing.module
   declarations: [
     ManageKabasTasksOverviewComponent,
     TaskListItemComponent,
+    TaskEduContentListItemComponent,
     ManageKabasTasksDetailComponent,
     ManageKabasTasksAssigneeModalComponent,
     ManageKabasTasksAddAssigneesComponent,
