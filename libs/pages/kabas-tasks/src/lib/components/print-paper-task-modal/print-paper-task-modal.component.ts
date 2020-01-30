0038cfdfe80ed1c997928fdf35c7c21092de1ab3
karlvaniseghem <karl.vaniseghem@icloud.com>
2020-01-30 13:15:54 +0100

    cr frederik

--- libs/pages/kabas-tasks/src/lib/components/print-paper-task-modal/print-paper-task-modal.component.ts
+++ libs/pages/kabas-tasks/src/lib/components/print-paper-task-modal/print-paper-task-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { PrintPaperTaskModalDataInterface } from './print-paper-task-modal-data.interface';
 import { PrintPaperTaskModalResultEnum } from './print-paper-task-modal-result.enum';
@@ -8,14 +8,43 @@ import { PrintPaperTaskModalResultEnum } from './print-paper-task-modal-result.e
   templateUrl: './print-paper-task-modal.component.html',
   styleUrls: ['./print-paper-task-modal.component.scss']
 })
-export class PrintPaperTaskModalComponent {
+export class PrintPaperTaskModalComponent implements OnInit {
   constructor(
     @Inject(MAT_DIALOG_DATA)
-    private data: PrintPaperTaskModalDataInterface,
+    private data: PrintPaperTaskModalDataInterface = { disable: [] },
     private dialogRef: MatDialogRef<PrintPaperTaskModalComponent>
   ) {}
 
   public printEnum = PrintPaperTaskModalResultEnum;
+  public actions = [];
+  public ngOnInit() {
+    this.actions = [
+      {
+        label: 'Afdrukken met namen',
+        handler: () => this.clickPrintTask(true),
+        tooltip: this.isButtonDisabled(PrintPaperTaskModalResultEnum.WITH_NAMES)
+          ? 'Deze taak is aan niemand toegekend.'
+          : '',
+        disabled: this.isButtonDisabled(
+          PrintPaperTaskModalResultEnum.WITH_NAMES
+        )
+      },
+      {
+        label: 'Afdrukken zonder namen',
+        handler: () => this.clickPrintTask(false),
+        tooltip: '',
+        disabled: this.isButtonDisabled(
+          PrintPaperTaskModalResultEnum.WITHOUT_NAMES
+        )
+      },
+      {
+        label: 'Correctiesleutel afdrukken',
+        handler: () => this.clickPrintSolution(),
+        tooltip: '',
+        disabled: this.isButtonDisabled(PrintPaperTaskModalResultEnum.SOLUTION)
+      }
+    ];
+  }
 
   public isButtonDisabled(type: PrintPaperTaskModalResultEnum) {
     return this.data.disable && this.data.disable.includes(type);
