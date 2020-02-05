53a637d2987eb9ef9102bfc6111f14b88a3adc61
lukasnys <lukas.nys2@student.vives.be>
2020-02-05 12:11:09 +0100

    Changed the way of updating progressbar

--- libs/whiteboard/src/lib/progress-bar/progress-bar.component.ts
+++ libs/whiteboard/src/lib/progress-bar/progress-bar.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  SimpleChange,
+  SimpleChanges
+} from '@angular/core';
 
 @Component({
   selector: 'campus-progress-bar',
@@ -9,9 +15,23 @@ export class ProgressBarComponent implements OnInit {
   @Input() amountOfImages: number;
   @Input() amountCompleted: number;
 
+  progressPercentage: string;
+
   constructor() {}
 
   ngOnInit() {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    const amountOfImagesChange: SimpleChange = changes.amountOfImages;
+    const amountCompletedChange: SimpleChange = changes.amountCompleted;
+
+    if (
+      amountOfImagesChange.previousValue !==
+        amountOfImagesChange.currentValue ||
+      amountCompletedChange.previousValue !== amountCompletedChange.currentValue
+    )
+      this.progressPercentage = this.getProgressPercentage();
+  }
 
   getProgressPercentage() {
     return (this.amountCompleted / this.amountOfImages) * 100 + '%';
