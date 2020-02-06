446e768bca9f3c90754ef1dedbf3fd57ff19e418
lukasnys <lukas.nys2@student.vives.be>
2020-02-06 13:17:21 +0100

    Removed duplicate test

--- libs/whiteboard/src/lib/whiteboard/whiteboard.component.spec.ts
+++ libs/whiteboard/src/lib/whiteboard/whiteboard.component.spec.ts
@@ -148,18 +148,6 @@ describe('WhiteboardComponent', () => {
     expect(openDialogSpy).not.toHaveBeenCalled();
   });
 
-  it('should not open a confirmation dialog when the bulk delete button is clicked if there are no selected items', () => {
-    const mockDialogRef = {
-      afterClosed: () => of(false),
-      close: null
-    } as MatDialogRef<ConfirmationModalComponent>;
-    openDialogSpy.mockReturnValue(mockDialogRef);
-
-    component.selectedCards = [];
-
-    expect(openDialogSpy).not.toHaveBeenCalled();
-  });
-
   it('should open a confirmation dialog when the bulk delete button is clicked and there are selected items', () => {
     const mockDialogRef = {
       afterClosed: () => of(false),
