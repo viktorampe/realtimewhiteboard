d27f048b7c3300df01236d6357e161e2cd045425
lukasnys <lukas.nys2@student.vives.be>
2020-02-07 08:18:24 +0100

    Fixed tests from dev

--- libs/whiteboard/src/lib/card/card.component.spec.ts
+++ libs/whiteboard/src/lib/card/card.component.spec.ts
@@ -178,7 +178,9 @@ describe('CardComponent', () => {
 
   it('should emit the right card when a card is selected', () => {
     spyOn(component.select, 'emit');
-    const checkboxes = fixture.debugElement.queryAll(By.css('.card__header__checkbox'));
+    const checkboxes = fixture.debugElement.queryAll(
+      By.css('.card__header__checkbox')
+    );
     const checkbox = checkboxes[0].nativeElement;
     checkbox.click(); // van false naar true
     fixture.detectChanges();
@@ -187,7 +189,9 @@ describe('CardComponent', () => {
 
   it('should emit the right card when a card is deselected', () => {
     spyOn(component.deselect, 'emit');
-    const checkboxes = fixture.debugElement.queryAll(By.css('.card__header__checkbox'));
+    const checkboxes = fixture.debugElement.queryAll(
+      By.css('.card__header__checkbox')
+    );
     const checkbox = checkboxes[0].nativeElement;
     checkbox.click(); // van false naar true
     fixture.detectChanges();
