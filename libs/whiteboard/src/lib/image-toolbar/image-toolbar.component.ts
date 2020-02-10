e0ce2baecdf81a441cf4162edb7afefefacc90b7
viktorampe <ampe.viktor@gmail.com>
2020-02-10 10:21:19 +0100

    update

--- libs/whiteboard/src/lib/image-toolbar/image-toolbar.component.ts
+++ libs/whiteboard/src/lib/image-toolbar/image-toolbar.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'campus-image-toolbar',
   templateUrl: './image-toolbar.component.html',
-  styleUrls: ['./image-toolbar.component.css']
+  styleUrls: ['./image-toolbar.component.scss']
 })
 export class ImageToolbarComponent implements OnInit {
+  @Output() removeClicked = new EventEmitter<void>();
+  @Output() updateClicked = new EventEmitter<void>();
+
   constructor() {}
 
   ngOnInit() {}
