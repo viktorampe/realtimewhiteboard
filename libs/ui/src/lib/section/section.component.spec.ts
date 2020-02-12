38def1def9b7596876fc2405b4e23fb1753e4598
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2020-02-12 13:15:17 +0100

    add tests

--- libs/ui/src/lib/section/section.component.spec.ts
+++ libs/ui/src/lib/section/section.component.spec.ts
@@ -1,10 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { configureTestSuite } from 'ng-bullet';
-import { SectionComponent } from './section.component';
-
+import { SectionComponent, SectionModeEnum } from './section.component';
+// file.only
 describe('SectionComponent', () => {
   let component: SectionComponent;
   let fixture: ComponentFixture<SectionComponent>;
+  let sectionClickSpy: jest.SpyInstance;
 
   configureTestSuite(() => {
     TestBed.configureTestingModule({
@@ -16,9 +17,24 @@ describe('SectionComponent', () => {
     fixture = TestBed.createComponent(SectionComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    sectionClickSpy = jest.spyOn(component.sectionClick, 'emit');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('clickSection()', () => {
+    it('should trigger an emit when in editable mode', () => {
+      component.mode = SectionModeEnum.EDITABLE;
+      component.clickSection();
+      expect(sectionClickSpy).toHaveBeenCalled();
+    });
+
+    it('should not trigger an emit when not in editable mode', () => {
+      component.mode = SectionModeEnum.EDITING;
+      component.clickSection();
+      expect(sectionClickSpy).not.toHaveBeenCalled();
+    });
   });
 });
