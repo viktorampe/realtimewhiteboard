6cf233f24cdb96874402f801bea4bd0abcf46924
lukasnys <lukas.nys2@student.vives.be>
2020-02-14 11:20:28 +0100

    Added tests

--- libs/whiteboard/src/lib/card-image/card-image.component.spec.ts
+++ libs/whiteboard/src/lib/card-image/card-image.component.spec.ts
@@ -1,5 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatIconModule } from '@angular/material';
+import { By } from '@angular/platform-browser';
+import { Mode } from '../../shared/enums/mode.enum';
 import { ImageToolbarComponent } from '../image-toolbar/image-toolbar.component';
 import { CardImageComponent } from './card-image.component';
 
@@ -22,5 +24,25 @@ describe('CardImageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show image toolbar if mode is EditMode', () => {
+    component.mode = Mode.EditMode;
+    fixture.detectChanges();
+
+    const toolbar = fixture.debugElement.queryAll(
+      By.css('campus-image-toolbar')
+    );
+    expect(toolbar.length).not.toBe(0);
+  });
+
+  it('should not show image toolbar if mode is SelectedMode', () => {
+    component.mode = Mode.SelectedMode;
+    fixture.detectChanges();
+
+    const toolbar = fixture.debugElement.queryAll(
+      By.css('campus-image-toolbar')
+    );
+    expect(toolbar.length).toBe(0);
   });
 });
