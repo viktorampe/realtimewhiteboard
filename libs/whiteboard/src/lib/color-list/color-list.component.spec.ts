f62ba1c1a7698f912cd3779238458981a6e1bbf6
lukasnys <lukas.nys2@student.vives.be>
2020-02-14 12:13:32 +0100

    Uppercased L in ColorList

--- libs/whiteboard/src/lib/color-list/color-list.component.spec.ts
+++ libs/whiteboard/src/lib/color-list/color-list.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ColorListComponent } from './color-list.component';
 
-describe('ColorlistComponent', () => {
+describe('ColorListComponent', () => {
   let component: ColorListComponent;
   let fixture: ComponentFixture<ColorListComponent>;
 
