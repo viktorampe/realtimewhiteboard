35e9d78fdc51b70d5e6eabb9ab7298d4357efc88
Yanndejo <yannis.dejonghe@hotmail.com>
2020-02-24 11:01:40 +0100

    add test to selector

---
+++ libs/shared/src/lib/services/collection-manager/edu-content-collection-manager-selectors.spec.ts
@@ -0,0 +1,36 @@
+import { taskCollection } from './edu-content-collection-manager-selectors';
+
+describe('Kabas-tasks viewmodel selectors', () => {
+  describe('taskCollection', () => {
+    it('should return digital and paper seperated', () => {
+      const tasks = [
+        {
+          id: 1,
+          name: 'testje',
+          isPaperTask: true
+        },
+        {
+          id: 2,
+          name: 'testje2',
+          isPaperTask: false
+        }
+      ];
+      const result = [
+        {
+          id: 1,
+          label: 'testje',
+          icon: 'task'
+        },
+        {
+          id: 2,
+          label: 'testje2',
+          icon: 'task'
+        }
+      ];
+      expect(taskCollection.projector(tasks, {})).toEqual({
+        digital: [result[1]],
+        paper: [result[0]]
+      });
+    });
+  });
+});
