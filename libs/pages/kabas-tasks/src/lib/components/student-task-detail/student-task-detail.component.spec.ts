642460218ac508692c659eb06b8ea5c05f82f64c
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2020-03-03 11:28:17 +0100

    Grrrrrrrrrrrrr

--- libs/pages/kabas-tasks/src/lib/components/student-task-detail/student-task-detail.component.spec.ts
+++ libs/pages/kabas-tasks/src/lib/components/student-task-detail/student-task-detail.component.spec.ts
@@ -1,5 +1,3 @@
-//file.only
-
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ENVIRONMENT_UI_TOKEN, UiModule } from '@campus/ui';
