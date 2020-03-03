cc9f5d7693b273c805c084dbdee8e6383d1ef0e8
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2020-03-03 11:51:35 +0100

    Forgot inputs

--- libs/ui/src/lib/collapsible-container/collapsible-container.component.ts
+++ libs/ui/src/lib/collapsible-container/collapsible-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, HostBinding, Input } from '@angular/core';
 
 @Component({
   selector: 'campus-collapsible-container',
@@ -6,6 +6,9 @@ import { Component, HostBinding } from '@angular/core';
   styleUrls: ['./collapsible-container.component.scss']
 })
 export class CollapsibleContainerComponent {
+  @Input() isOpen: boolean;
+  @Input() isDisabled = false;
+
   @HostBinding('class.ui-collapsible-container')
   private uiCollapsibleContainerClass = true;
 
