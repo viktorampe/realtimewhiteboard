3cefc554db99e7aa360e902bc88dcf8786bed98a
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2020-03-04 13:34:46 +0100

    add inputs that tom totally forgot

--- libs/ui/src/lib/rating/rating.component.ts
+++ libs/ui/src/lib/rating/rating.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component, HostBinding, Input } from '@angular/core';
 
 @Component({
   selector: 'campus-rating',
@@ -6,6 +6,10 @@ import { Component, HostBinding } from '@angular/core';
   styleUrls: ['./rating.component.scss']
 })
 export class RatingComponent {
+  @Input() count: number;
+  @Input() total: number;
+  @Input() icon: string;
+
   @HostBinding('class.ui-rating')
   private uiRatingClass = true;
 
