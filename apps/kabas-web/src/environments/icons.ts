4a47c55d36397985a03973d323002991f57cee0a
karlvaniseghem <karl.vaniseghem@icloud.com>
2020-03-06 10:21:14 +0100

    styling assignees modal

--- apps/kabas-web/src/environments/icons.ts
+++ apps/kabas-web/src/environments/icons.ts
@@ -89,5 +89,6 @@ export const icons = {
 
   //empty-states
   'empty-state-all-done': 'assets/empty-states/all-done.svg',
-  'empty-state-no-educontent': 'assets/empty-states/no-educontent.svg'
+  'empty-state-no-educontent': 'assets/empty-states/no-educontent.svg',
+  'empty-state-no-users': 'assets/empty-states/no-users.svg'
 };
