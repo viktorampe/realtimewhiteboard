96d225c1e7eb0b3eb7e18e095cf4a8195021cf5c
Yanndejo <yannis.dejonghe@hotmail.com>
2020-03-09 14:38:05 +0100

    split stream

--- libs/pages/kabas-tasks/src/lib/components/student-tasks.viewmodel.ts
+++ libs/pages/kabas-tasks/src/lib/components/student-tasks.viewmodel.ts
@@ -1,6 +1,18 @@
 import { Observable } from 'rxjs';
+import { filter } from 'rxjs/operators';
 import { StudentTaskInterface } from '../interfaces/StudentTask.interface';
 
 export class StudentTasksViewModel {
   public studentTasks$: Observable<StudentTaskInterface>;
+  public activeTasks$: Observable<StudentTaskInterface>;
+  public finishedTasks$: Observable<StudentTaskInterface>;
+
+  constructor() {
+    this.setupStreams();
+  }
+
+  public setupStreams() {
+    this.finishedTasks$ = this.studentTasks$.pipe(filter(st => st.isFinished));
+    this.activeTasks$ = this.studentTasks$.pipe(filter(st => !st.isFinished));
+  }
 }
