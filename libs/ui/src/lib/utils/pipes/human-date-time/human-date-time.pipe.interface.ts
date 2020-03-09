db74ba252adb7d80c39f1873e36653a9bdadf20b
david.boute <david.boute@diekeure.be>
2020-03-09 16:36:18 +0100

    setup pipe mechanism

---
+++ libs/ui/src/lib/utils/pipes/human-date-time/human-date-time.pipe.interface.ts
@@ -0,0 +1,10 @@
+export interface HumanDateTimeRuleInterface {
+  condition(date: number, referenceDate: number): boolean;
+  value(date: number, referenceDate: number): string;
+}
+
+export interface HumanDateTimeArgsInterface {
+  rules: HumanDateTimeRuleInterface[];
+  referenceDate?: Date; // default -> now
+  locale?: string; // default -> 'nl-BE'
+}
