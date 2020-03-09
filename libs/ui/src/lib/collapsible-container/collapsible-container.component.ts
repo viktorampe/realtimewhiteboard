14565374821c34925536f446495ea823b951764f
karlvaniseghem <karl.vaniseghem@icloud.com>
2020-03-09 16:45:27 +0100

    initial commit

--- libs/ui/src/lib/collapsible-container/collapsible-container.component.ts
+++ libs/ui/src/lib/collapsible-container/collapsible-container.component.ts
@@ -1,16 +1,50 @@
-import { Component, HostBinding, Input } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
+import {
+  Component,
+  EventEmitter,
+  HostBinding,
+  Input,
+  Output
+} from '@angular/core';
 
 @Component({
   selector: 'campus-collapsible-container',
   templateUrl: './collapsible-container.component.html',
-  styleUrls: ['./collapsible-container.component.scss']
+  styleUrls: ['./collapsible-container.component.scss'],
+  animations: [
+    trigger('toggleOpened', [
+      state('closed', style({ height: 0 })),
+      state('opened,', style({ height: '*' })),
+
+      transition(
+        'closed <=> opened',
+        animate('500ms cubic-bezier(.43,0,.31,1)')
+      )
+    ])
+  ]
 })
 export class CollapsibleContainerComponent {
-  @Input() isOpen: boolean;
-  @Input() isDisabled = false;
-
   @HostBinding('class.ui-collapsible-container')
   private uiCollapsibleContainerClass = true;
+
+  @HostBinding('class.ui-collapsible-container--open')
+  @Input()
+  open = false;
+
+  @Input() isDisabled = false;
+
+  @Output() openChange = new EventEmitter<boolean>();
+
+  @HostBinding('@toggleOpened')
+  get toggleContainer(): string {
+    return this.open ? 'opened' : 'closed';
+  }
 
   constructor() {}
 }
