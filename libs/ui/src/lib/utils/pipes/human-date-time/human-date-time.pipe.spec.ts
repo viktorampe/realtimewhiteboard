53841956c120be2258eef9134f4ff970e82611f4
david.boute <david.boute@diekeure.be>
2020-03-10 13:45:56 +0100

    fix for azure agents

--- libs/ui/src/lib/utils/pipes/human-date-time/human-date-time.pipe.spec.ts
+++ libs/ui/src/lib/utils/pipes/human-date-time/human-date-time.pipe.spec.ts
@@ -156,7 +156,7 @@ describe('HumanDateTimePipe', () => {
     it('should show the text for dateformat', () => {
       date.setDate(date.getDate() - 7);
 
-      const expected = '10/17/2018'; // note us-EN -> so devops agents have the formatting loaded
+      const expected = date.toLocaleDateString('nl-BE');
       expect(pipe.transform(date)).toBe(expected);
     });
 
