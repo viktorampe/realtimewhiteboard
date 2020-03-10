fcef0633daa7d9d43dfbf902978bb68b9fdaf5cb
lukasnys <lukas.nys2@student.vives.be>
2020-03-10 09:09:37 +0100

    Added empty state

--- libs/whiteboard/src/lib/services/whiteboard-http.service.ts
+++ libs/whiteboard/src/lib/services/whiteboard-http.service.ts
@@ -10,8 +10,6 @@ import {
   switchMap,
   take
 } from 'rxjs/operators';
-import { v4 as uuidv4 } from 'uuid';
-import { ModeEnum } from '../../lib/enums/mode.enum';
 import WhiteboardInterface from '../../lib/models/whiteboard.interface';
 import ImageInterface from '../models/image.interface';
 
@@ -127,114 +125,117 @@ export class WhiteboardHttpService implements WhiteboardHttpServiceInterface {
 
   private getWhiteboardMock(): WhiteboardInterface {
     const whiteboard: WhiteboardInterface = {
-      title: 'Welcome to the 90s',
+      title: '',
       cards: [],
-      shelfCards: [
-        {
-          id: uuidv4(),
-          mode: ModeEnum.SHELF,
-          viewModeImage: true,
-          description: 'Windows 95',
-          image: {
-            imageUrl:
-              'https://cdn01.pijpermedia.nl/RDLrupoRZtt-R7W4iqxYQTyZ9bY=/1290x726/center/middle/https://cdn.pijper.io/source/upcoming/9/950f46fae4_1412162307_13-Redenen-waarom-Windows-95-best-wel-vet-was.jpg'
-          },
-          color: '#2EA03D',
-          top: 56,
-          left: 82
-        },
-        {
-          id: uuidv4(),
-          mode: ModeEnum.SHELF,
-          viewModeImage: true,
-          description: 'Get Ready',
-          image: {
-            imageUrl:
-              'https://vivavlaanderen.radio2.be/images/2665/1100x0/mvp15qbt7vcmjeko_get_ready_1996.jpg'
-          },
-          color: '#00A7E2',
-          top: 296,
-          left: 334
-        },
-        {
-          id: uuidv4(),
-          mode: ModeEnum.SHELF,
-          viewModeImage: true,
-          description: 'Candy',
-          image: {
-            imageUrl:
-              'https://cdn.shopify.com/s/files/1/0736/7879/files/top-10-retro-candies-from-the-1990s-efrutti-gummy-candies-candy-district-sweetest-online-candy-store-canada.png?v=1545772588'
-          },
-          color: '#00A7E2',
-          top: 57,
-          left: 515
-        },
-        {
-          id: uuidv4(),
-          mode: ModeEnum.SHELF,
-          viewModeImage: true,
-          description: 'VRC',
-          image: {
-            imageUrl:
-              'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/stack-of-video-tapes-royalty-free-image-93422307-1563903351.jpg?crop=0.668xw:1.00xh;0.206xw,0&resize=768:*'
-          },
-          color: '#00A7E2',
-          top: 295,
-          left: 708
-        },
-        {
-          id: uuidv4(),
-          mode: ModeEnum.SHELF,
-          viewModeImage: true,
-          description: 'Gameboy',
-          image: {
-            imageUrl:
-              'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/nintendo-game-boy-handheld-video-game-console-taken-on-july-news-photo-1065385418-1563905496.jpg?crop=0.79806xw:1xh;center,top&resize=768:*'
-          },
-          color: '#00A7E2',
-          top: 63,
-          left: 918
-        },
-        {
-          id: uuidv4(),
-          mode: ModeEnum.SHELF,
-          viewModeImage: true,
-          description: 'Home Alone',
-          image: {
-            imageUrl:
-              'https://vroegert.nl/wp-content/uploads/2016/10/Homealonefeau1.jpg'
-          },
-          color: '#2EA03D',
-          top: 0,
-          left: 0
-        },
-        {
-          id: uuidv4(),
-          mode: ModeEnum.SHELF,
-          viewModeImage: true,
-          description: 'NSYNC',
-          image: {
-            imageUrl:
-              'https://www.muzjig.com/wp-content/uploads/2015/08/14383945783922-forgotten-90s-boy-bands-where-are-they-now-1-16708-1364312072-2_big.jpg'
-          },
-          color: '#2EA03D',
-          top: 0,
-          left: 0
-        },
-        {
-          id: uuidv4(),
-          mode: ModeEnum.SHELF,
-          viewModeImage: true,
-          description: 'Capri Sun',
-          image: {
-            imageUrl:
-              'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/boxes-of-capri-sun-juice-sit-on-shelves-at-a-grocery-store-news-photo-526195522-1563903441.jpg?crop=0.668xw:1.00xh;0.0204xw,0&resize=768:*'
-          },
-          color: '#2EA03D',
-          top: 0,
-          left: 0
-        }
-      ]
+      shelfCards: []
+      // title: 'Welcome to the 90s',
+      // cards: [],
+      // shelfCards: [
+      //   {
+      //     id: uuidv4(),
+      //     mode: ModeEnum.SHELF,
+      //     viewModeImage: true,
+      //     description: 'Windows 95',
+      //     image: {
+      //       imageUrl:
+      //         'https://cdn01.pijpermedia.nl/RDLrupoRZtt-R7W4iqxYQTyZ9bY=/1290x726/center/middle/https://cdn.pijper.io/source/upcoming/9/950f46fae4_1412162307_13-Redenen-waarom-Windows-95-best-wel-vet-was.jpg'
+      //     },
+      //     color: '#2EA03D',
+      //     top: 56,
+      //     left: 82
+      //   },
+      //   {
+      //     id: uuidv4(),
+      //     mode: ModeEnum.SHELF,
+      //     viewModeImage: true,
+      //     description: 'Get Ready',
+      //     image: {
+      //       imageUrl:
+      //         'https://vivavlaanderen.radio2.be/images/2665/1100x0/mvp15qbt7vcmjeko_get_ready_1996.jpg'
+      //     },
+      //     color: '#00A7E2',
+      //     top: 296,
+      //     left: 334
+      //   },
+      //   {
+      //     id: uuidv4(),
+      //     mode: ModeEnum.SHELF,
+      //     viewModeImage: true,
+      //     description: 'Candy',
+      //     image: {
+      //       imageUrl:
+      //         'https://cdn.shopify.com/s/files/1/0736/7879/files/top-10-retro-candies-from-the-1990s-efrutti-gummy-candies-candy-district-sweetest-online-candy-store-canada.png?v=1545772588'
+      //     },
+      //     color: '#00A7E2',
+      //     top: 57,
+      //     left: 515
+      //   },
+      //   {
+      //     id: uuidv4(),
+      //     mode: ModeEnum.SHELF,
+      //     viewModeImage: true,
+      //     description: 'VRC',
+      //     image: {
+      //       imageUrl:
+      //         'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/stack-of-video-tapes-royalty-free-image-93422307-1563903351.jpg?crop=0.668xw:1.00xh;0.206xw,0&resize=768:*'
+      //     },
+      //     color: '#00A7E2',
+      //     top: 295,
+      //     left: 708
+      //   },
+      //   {
+      //     id: uuidv4(),
+      //     mode: ModeEnum.SHELF,
+      //     viewModeImage: true,
+      //     description: 'Gameboy',
+      //     image: {
+      //       imageUrl:
+      //         'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/nintendo-game-boy-handheld-video-game-console-taken-on-july-news-photo-1065385418-1563905496.jpg?crop=0.79806xw:1xh;center,top&resize=768:*'
+      //     },
+      //     color: '#00A7E2',
+      //     top: 63,
+      //     left: 918
+      //   },
+      //   {
+      //     id: uuidv4(),
+      //     mode: ModeEnum.SHELF,
+      //     viewModeImage: true,
+      //     description: 'Home Alone',
+      //     image: {
+      //       imageUrl:
+      //         'https://vroegert.nl/wp-content/uploads/2016/10/Homealonefeau1.jpg'
+      //     },
+      //     color: '#2EA03D',
+      //     top: 0,
+      //     left: 0
+      //   },
+      //   {
+      //     id: uuidv4(),
+      //     mode: ModeEnum.SHELF,
+      //     viewModeImage: true,
+      //     description: 'NSYNC',
+      //     image: {
+      //       imageUrl:
+      //         'https://www.muzjig.com/wp-content/uploads/2015/08/14383945783922-forgotten-90s-boy-bands-where-are-they-now-1-16708-1364312072-2_big.jpg'
+      //     },
+      //     color: '#2EA03D',
+      //     top: 0,
+      //     left: 0
+      //   },
+      //   {
+      //     id: uuidv4(),
+      //     mode: ModeEnum.SHELF,
+      //     viewModeImage: true,
+      //     description: 'Capri Sun',
+      //     image: {
+      //       imageUrl:
+      //         'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/boxes-of-capri-sun-juice-sit-on-shelves-at-a-grocery-store-news-photo-526195522-1563903441.jpg?crop=0.668xw:1.00xh;0.0204xw,0&resize=768:*'
+      //     },
+      //     color: '#2EA03D',
+      //     top: 0,
+      //     left: 0
+      //   }
+      // ]
     };
     return whiteboard;
   }
