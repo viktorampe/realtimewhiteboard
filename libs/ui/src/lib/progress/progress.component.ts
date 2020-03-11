7eb9b804a7b8d89cabf34bbaec57a98e4ce0c568
karlvaniseghem <karl.vaniseghem@icloud.com>
2020-03-11 16:48:13 +0100

    added component and tests

--- libs/ui/src/lib/progress/progress.component.ts
+++ libs/ui/src/lib/progress/progress.component.ts
@@ -1,24 +1,65 @@
-import { Component, HostBinding, Input } from '@angular/core';
+import {
+  Component,
+  ContentChild,
+  Directive,
+  ElementRef,
+  HostBinding,
+  Input,
+  OnChanges,
+  SimpleChanges
+} from '@angular/core';
 
 export enum ProgressFormEnum {
   LINEAR = 'linear',
   CIRCULAR = 'circular'
 }
 
+export enum ProgressModeEnum {
+  DETERMINATE = 'determinate',
+  INDETERMINATE = 'indeterminate'
+}
+
+@Directive({
+  selector:
+    '[campusCompletedProgressIcon], [completedProgressIcon], completed-progress-icon'
+})
+export class CompletedProgressIconDirective {}
+
 @Component({
   selector: 'campus-progress',
   templateUrl: './progress.component.html',
   styleUrls: ['./progress.component.scss']
 })
-export class ProgressComponent {
-  @Input() count?: number;
+export class ProgressComponent implements OnChanges {
+  public forms: typeof ProgressFormEnum = ProgressFormEnum;
+  public modes: typeof ProgressModeEnum = ProgressModeEnum;
+  public percentage = 0;
+
   @Input() total? = 100;
-  @Input() completedIcon?: string;
+  @Input() count: number;
+
   @Input() showPercentage? = false;
-  @Input() form: ProgressFormEnum;
+  @Input() form: ProgressFormEnum = ProgressFormEnum.CIRCULAR;
+  @Input() mode? = ProgressModeEnum.DETERMINATE;
+  @Input() diameter? = 100;
 
   @HostBinding('class.ui-progress')
   private uiProgressClass = true;
 
+  @ContentChild(CompletedProgressIconDirective, { static: true })
+  completedIcon: ElementRef;
+
   constructor() {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    let count = this.count;
+    let total = this.total;
+    if (changes.count) {
+      count = changes.count.currentValue;
+    }
+    if (changes.total) {
+      total = changes.total.currentValue;
+    }
+    this.percentage = Math.min(Math.ceil((count / total) * 100), 100);
+  }
 }
