7e0bc4c563c206186e80bdedd8384f6a7b9f1f27
lukasnys <lukas.nys2@student.vives.be>
2020-03-11 16:25:59 +0100

    Aded image file upload

--- libs/whiteboard/src/lib/services/whiteboard-http.service.ts
+++ libs/whiteboard/src/lib/services/whiteboard-http.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpEventType, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, Subject, throwError, timer } from 'rxjs';
+import { BehaviorSubject, Observable, Subject, throwError } from 'rxjs';
 import {
   catchError,
   filter,
@@ -8,12 +8,11 @@ import {
   mapTo,
   retry,
   switchMap,
-  take
+  take,
+  tap
 } from 'rxjs/operators';
-import { v4 as uuidv4 } from 'uuid';
-import { ModeEnum } from '../../lib/enums/mode.enum';
 import WhiteboardInterface from '../../lib/models/whiteboard.interface';
-import CardInterface from '../models/card.interface';
+import { ModeEnum } from '../enums/mode.enum';
 import ImageInterface from '../models/image.interface';
 
 const RETRY_AMOUNT = 2;
@@ -36,6 +35,7 @@ export class WhiteboardHttpService implements WhiteboardHttpServiceInterface {
   private apiSettings$ = new BehaviorSubject<WhiteboardHttpSettingsInterface>(
     null
   );
+  private eduContentId: number;
   private _errors$ = new Subject<Error>();
 
   constructor(private http: HttpClient) {}
@@ -56,6 +56,7 @@ export class WhiteboardHttpService implements WhiteboardHttpServiceInterface {
       ),
       retry(RETRY_AMOUNT),
       catchError(this.handleError.bind(this)),
+      tap(response => (this.eduContentId = response.eduContentId)),
       map(
         (response: any): WhiteboardInterface =>
           response
@@ -63,21 +64,13 @@ export class WhiteboardHttpService implements WhiteboardHttpServiceInterface {
             : { title: '', cards: [], shelfCards: {} }
       ),
       map(
-        (whiteboard: any): WhiteboardInterface => ({
+        (whiteboard: WhiteboardInterface): WhiteboardInterface => ({
           title: whiteboard.title,
           cards: [],
-          shelfCards: whiteboard.cards.map(
-            (c: any): CardInterface => ({
-              id: uuidv4(),
-              image: c.image ? { imageUrl: c.image } : {},
-              viewModeImage: !!c.image,
-              top: 0,
-              left: 0,
-              description: c.description,
-              mode: ModeEnum.SHELF,
-              color: c.color
-            })
-          )
+          shelfCards: whiteboard.cards.map(c => ({
+            ...c,
+            mode: ModeEnum.SHELF
+          }))
         })
       )
     );
@@ -89,15 +82,19 @@ export class WhiteboardHttpService implements WhiteboardHttpServiceInterface {
     const apiSettings: WhiteboardHttpSettingsInterface = this.getSettings();
 
     const response$ = this.http
-      .post(
-        apiSettings.apiBase + apiSettings.metadataId,
-        JSON.stringify(whiteboard)
+      .patch(
+        apiSettings.apiBase +
+          '/api/eduContentMetadata/' +
+          apiSettings.metadataId,
+        { data: JSON.stringify(whiteboard) },
+        { withCredentials: true }
       )
       .pipe(
         retry(RETRY_AMOUNT),
         catchError(this.handleError.bind(this)),
         mapTo(true)
       );
+
     return response$;
   }
 
@@ -110,36 +107,46 @@ export class WhiteboardHttpService implements WhiteboardHttpServiceInterface {
         return { progress: Math.round((100 * event.loaded) / event.total) };
 
       case HttpEventType.Response:
-        return { imageUrl: event.imageUrl };
+        const apiSettings: WhiteboardHttpSettingsInterface = this.getSettings();
+        return {
+          imageUrl:
+            apiSettings.apiBase +
+            '/api/EduFiles/' +
+            event.body.storageInfo.eduFileId +
+            '/redirectUrl'
+        };
+
+      default:
+        return {};
     }
   }
 
   public uploadFile(file: File): Observable<ImageInterface> {
+    const apiSettings: WhiteboardHttpSettingsInterface = this.getSettings();
+
     const formData: FormData = new FormData();
     formData.append('file', file, file.name);
 
-    const request = new HttpRequest('POST', '/upload/file', file, {
-      reportProgress: true
-    });
-
-    const request$ = this.http.request(request);
-    request$.pipe(
-      retry(RETRY_AMOUNT),
-      catchError(this.handleError.bind(this)),
-      map(event => this.getEventMessage(event))
+    const request = new HttpRequest(
+      'POST',
+      apiSettings.apiBase +
+        '/api/EduContentFiles/' +
+        this.eduContentId +
+        '/store',
+      formData,
+      {
+        reportProgress: true,
+        withCredentials: true
+      }
     );
 
-    return timer(0, 10).pipe(
-      take(101),
-      map(progress => {
-        if (progress === 100) {
-          return {
-            imageUrl:
-              'https://cdn.iconscout.com/icon/free/png-512/css-118-569410.png'
-          };
-        }
-        return { progress: progress };
-      })
+    const request$ = this.http.request(request);
+    return request$.pipe(
+      retry(RETRY_AMOUNT),
+      catchError(this.handleError.bind(this)),
+      filter(event => !!event),
+      map(event => this.getEventMessage(event)),
+      filter(response => !!response.imageUrl || !!response.progress)
     );
   }
 
