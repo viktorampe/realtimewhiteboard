94f79e814bfbdde201df1aa209eedc6d398fd424
karlvaniseghem <karl.vaniseghem@icloud.com>
2020-03-12 16:39:48 +0100

    refactored ufp with campus-rating

--- apps/kabas-web/src/app/components/searchresults/practice-search-result/practice-search-result.component.spec.ts
+++ apps/kabas-web/src/app/components/searchresults/practice-search-result/practice-search-result.component.spec.ts
@@ -240,7 +240,7 @@ describe('PracticeSearchResultComponent', () => {
     it('should show the possible actions for the eduContent', () => {
       const actionDEs = fixture.debugElement
         .query(By.directive(ListItemActionsDirective))
-        .queryAll(By.css('span'));
+        .queryAll(By.css('.ui-button--inline'));
 
       expect(actionDEs.length).toBe(mockActions.length);
       actionDEs.forEach((actionDE, index) => {
