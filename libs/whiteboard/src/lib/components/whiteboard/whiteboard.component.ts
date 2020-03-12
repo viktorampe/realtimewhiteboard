0f9627d12f327cf2b50945c02b98cbcb13ef04ff
Philine-Bottecaer <tinhnghia.bottecaer@student.vives.be>
2020-03-12 13:42:29 +0100

    Aanpassen whiteboard.ts

--- libs/whiteboard/src/lib/components/whiteboard/whiteboard.component.ts
+++ libs/whiteboard/src/lib/components/whiteboard/whiteboard.component.ts
@@ -376,9 +376,13 @@ export class WhiteboardComponent implements OnChanges {
       .filter(c => c.mode !== ModeEnum.UPLOAD)
       .forEach(c => (c.mode = ModeEnum.IDLE));
     const { card, event, cardElement } = $event;
-    const currentMode = this.selectedCards.length
-      ? ModeEnum.MULTISELECT
-      : ModeEnum.IDLE;
+    // const currentMode = this.selectedCards.length
+    //   ? ModeEnum.MULTISELECTSELECTED
+    //   : ModeEnum.IDLE;
+    let currentMode = ModeEnum.IDLE;
+    if (this.selectedCards.length) {
+      currentMode = ModeEnum.MULTISELECTSELECTED;
+    }
 
     const workspaceCard: CardInterface = {
       ...card,
@@ -389,6 +393,10 @@ export class WhiteboardComponent implements OnChanges {
         (167 + cardElement.offsetTop) -
         Math.abs(event.distance.y)
     };
+
+    this.whiteboard$.value.cards
+      .filter(c => c.mode === ModeEnum.MULTISELECTSELECTED)
+      .forEach(c => (c.mode = ModeEnum.MULTISELECTSELECTED));
 
     if (
       !this.whiteboard$.value.cards
