7e7e56b67435165af71ef1820880e45809165acf
viktorampe <ampe.viktor@gmail.com>
2020-03-12 15:41:03 +0100

    fix

--- libs/whiteboard/src/lib/components/settings/settings.component.ts
+++ libs/whiteboard/src/lib/components/settings/settings.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'campus-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.css']
+  styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
