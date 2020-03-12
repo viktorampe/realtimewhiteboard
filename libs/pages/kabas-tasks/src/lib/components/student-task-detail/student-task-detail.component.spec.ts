8b617b89f40cdf17c575c71078ab6091359b8fc8
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2020-03-12 09:30:23 +0100

    Write tests

--- libs/pages/kabas-tasks/src/lib/components/student-task-detail/student-task-detail.component.spec.ts
+++ libs/pages/kabas-tasks/src/lib/components/student-task-detail/student-task-detail.component.spec.ts
@@ -1,12 +1,19 @@
+// file.only
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ENVIRONMENT_UI_TOKEN, UiModule } from '@campus/ui';
+import { hot } from '@nrwl/angular/testing';
 import { configureTestSuite } from 'ng-bullet';
+import { StudentTaskContentFixture } from '../../interfaces/StudentTaskContent.fixture';
+import { StudentTasksViewModel } from '../student-tasks.viewmodel';
+import { MockStudentTasksViewModel } from '../student-tasks.viewmodel.mock';
 import { StudentTaskDetailComponent } from './student-task-detail.component';
 
 describe('StudentTaskDetailComponent', () => {
   let component: StudentTaskDetailComponent;
   let fixture: ComponentFixture<StudentTaskDetailComponent>;
+  let viewModel: MockStudentTasksViewModel;
 
   configureTestSuite(() => {
     TestBed.configureTestingModule({
@@ -15,6 +22,10 @@ describe('StudentTaskDetailComponent', () => {
         {
           provide: ENVIRONMENT_UI_TOKEN,
           useValue: {}
+        },
+        {
+          provide: StudentTasksViewModel,
+          useClass: MockStudentTasksViewModel
         }
       ],
       declarations: [StudentTaskDetailComponent]
@@ -22,6 +33,7 @@ describe('StudentTaskDetailComponent', () => {
   });
 
   beforeEach(() => {
+    viewModel = TestBed.get(StudentTasksViewModel);
     fixture = TestBed.createComponent(StudentTaskDetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -29,5 +41,53 @@ describe('StudentTaskDetailComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('streams', () => {
+    const mockContents = [
+      new StudentTaskContentFixture({
+        name: 'Required 1',
+        required: true
+      }),
+      new StudentTaskContentFixture({
+        name: 'Required 2',
+        required: true
+      }),
+      new StudentTaskContentFixture({
+        name: 'Optional 1',
+        required: false
+      }),
+      new StudentTaskContentFixture({
+        name: 'Optional 2',
+        required: false
+      })
+    ];
+
+    beforeEach(() => {
+      viewModel.currentTask$.next({
+        ...viewModel.currentTask$.value,
+        contents: mockContents
+      });
+    });
+
+    describe('requiredTaskContents', () => {
+      it('should contain only required task contents', () => {
+        expect(component.requiredTaskContents$).toBeObservable(
+          hot('a', {
+            a: [mockContents[0], mockContents[1]]
+          })
+        );
+      });
+    });
+
+    describe('optionalTaskContents', () => {
+      it('should contain only optional task contents', () => {
+        expect(component.optionalTaskContents$).toBeObservable(
+          hot('a', {
+            a: [mockContents[2], mockContents[3]]
+          })
+        );
+      });
+    });
   });
 });
