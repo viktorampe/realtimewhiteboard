55e8ca8a7fc2ad11f619455dffee2f2b2bc85750
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2020-03-12 13:18:38 +0100

    Create guard + tests, add to routing

---
+++ libs/pages/kabas-tasks/src/lib/guards/valid-task-instance.guard.spec.ts
@@ -0,0 +1,150 @@
+// file.only
+
+import { TestBed } from '@angular/core/testing';
+import {
+  ActivatedRouteSnapshot,
+  Router,
+  RouterStateSnapshot,
+  UrlTree
+} from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import {
+  DalState,
+  EffectFeedback,
+  EffectFeedbackActions,
+  Priority,
+  TaskInstance,
+  TaskInstanceFixture,
+  TaskInstanceQueries
+} from '@campus/dal';
+import { Dictionary } from '@ngrx/entity';
+import { Store } from '@ngrx/store';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { hot } from '@nrwl/angular/testing';
+import { configureTestSuite } from 'ng-bullet';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { StudentTaskOverviewResolver } from '../components/student-task-overview/student-task-overview.resolver';
+import { ValidTaskInstanceGuard } from './valid-task-instance.guard';
+
+describe('ValidTaskInstanceGuard', () => {
+  const uuid = '123';
+  const taskId = 5;
+  const activatedRouteSnapshot = <ActivatedRouteSnapshot>{
+    params: {
+      id: taskId
+    } as unknown
+  };
+  const urlTree: UrlTree = new UrlTree();
+
+  let shouldResolve$: BehaviorSubject<boolean>;
+
+  let validInstanceGuard: ValidTaskInstanceGuard;
+  let store: MockStore<DalState>;
+
+  configureTestSuite(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      providers: [
+        provideMockStore(),
+        ValidTaskInstanceGuard,
+        {
+          provide: StudentTaskOverviewResolver,
+          useValue: {
+            resolve: () => shouldResolve$
+          }
+        },
+        {
+          provide: 'uuid',
+          useValue: () => uuid
+        },
+        {
+          provide: Router,
+          useValue: {
+            createUrlTree: () => urlTree
+          }
+        }
+      ]
+    });
+  });
+
+  beforeEach(() => {
+    validInstanceGuard = TestBed.get(ValidTaskInstanceGuard);
+    store = TestBed.get(Store);
+
+    shouldResolve$ = new BehaviorSubject(true);
+  });
+
+  function setGroupedTaskInstances(
+    instancesGrouped: Dictionary<TaskInstance[]>
+  ) {
+    return store.overrideSelector(
+      TaskInstanceQueries.getAllGroupedByTaskId,
+      instancesGrouped
+    );
+  }
+
+  function canActivate() {
+    return validInstanceGuard.canActivate(
+      activatedRouteSnapshot,
+      <RouterStateSnapshot>{}
+    ) as Observable<boolean>;
+  }
+
+  it('should return the UrlTree to redirect to when the task does not exist', () => {
+    setGroupedTaskInstances({
+      1: []
+    });
+
+    expect(canActivate()).toBeObservable(
+      hot('(a|)', {
+        a: urlTree
+      })
+    );
+  });
+
+  it('should return the UrlTree to redirect to when the task has no TaskInstances', () => {
+    setGroupedTaskInstances({
+      5: []
+    });
+
+    expect(canActivate()).toBeObservable(
+      hot('(a|)', {
+        a: urlTree
+      })
+    );
+  });
+
+  it('should return true when the task exists and has instances', () => {
+    setGroupedTaskInstances({
+      5: [new TaskInstanceFixture()]
+    });
+
+    expect(canActivate()).toBeObservable(
+      hot('(a|)', {
+        a: true
+      })
+    );
+  });
+
+  it('should dispatch EffectFeedback when the guard returns false', done => {
+    setGroupedTaskInstances({});
+    jest.spyOn(store, 'dispatch');
+
+    canActivate().subscribe(() => {
+      expect(store.dispatch).toHaveBeenCalledWith(
+        new EffectFeedbackActions.AddEffectFeedback({
+          effectFeedback: new EffectFeedback({
+            id: uuid,
+            triggerAction: null,
+            message: 'De taak bestaat niet of is niet aan jou toegekend.',
+            type: 'error',
+            userActions: [],
+            priority: Priority.HIGH
+          })
+        })
+      );
+
+      done();
+    });
+  });
+});
