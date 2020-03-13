268355e13c92f0c1ee8ea76161a45ad5b781965b
david.boute <david.boute@diekeure.be>
2020-03-13 12:13:21 +0100

    starsFromScore

--- libs/utils/src/lib/functions/result.functions.ts
+++ libs/utils/src/lib/functions/result.functions.ts
@@ -1,16 +1,12 @@
 export class ResultFunctions {
   public static starsFromScore(score: number, total = 100) {
-    if (score) {
-      if (score === 100) {
-        return 3;
-      }
-      if (score >= 75) {
-        return 2;
-      }
-      if (score >= 50) {
-        return 1;
-      }
-    }
+    if (!score || !total) return 0;
+
+    const percentage = (score * 100) / total;
+    if (percentage >= 100) return 3;
+    if (percentage >= 75) return 2;
+    if (percentage >= 50) return 1;
+
     return 0;
   }
 }
