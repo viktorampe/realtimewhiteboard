9e591f4f9d8f23dab9e89913d2a589bd06b587a8
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2020-03-13 13:56:39 +0100

    Test WIP

--- libs/pages/kabas-tasks/src/lib/components/student-task-detail/student-task-detail.component.spec.ts
+++ libs/pages/kabas-tasks/src/lib/components/student-task-detail/student-task-detail.component.spec.ts
@@ -1,5 +1,9 @@
+// file.only
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MockDate } from '@campus/testing';
 import { ENVIRONMENT_UI_TOKEN, UiModule } from '@campus/ui';
 import { hot } from '@nrwl/angular/testing';
 import { configureTestSuite } from 'ng-bullet';
@@ -12,6 +16,18 @@ describe('StudentTaskDetailComponent', () => {
   let component: StudentTaskDetailComponent;
   let fixture: ComponentFixture<StudentTaskDetailComponent>;
   let viewModel: MockStudentTasksViewModel;
+
+  const today = new Date(2020, 2, 9);
+  const yesterday = new Date(2020, 2, 8);
+  const tomorrow = new Date(2020, 2, 10);
+  const dayAfterTomorrow = new Date(2020, 2, 11);
+  const thursday = new Date(2020, 2, 12);
+  const sunday = new Date(2020, 2, 15);
+  const nextWeek = new Date(2020, 2, 16);
+  const endNextWeek = new Date(2020, 2, 22);
+  const farFuture = new Date(2020, 3, 20);
+
+  const dateMock = new MockDate(today);
 
   configureTestSuite(() => {
     TestBed.configureTestingModule({
@@ -28,6 +44,10 @@ describe('StudentTaskDetailComponent', () => {
       ],
       declarations: [StudentTaskDetailComponent]
     });
+  });
+
+  afterAll(() => {
+    dateMock.returnRealDate();
   });
 
   beforeEach(() => {
@@ -85,6 +105,75 @@ describe('StudentTaskDetailComponent', () => {
             a: [mockContents[2], mockContents[3]]
           })
         );
+      });
+    });
+  });
+
+  describe('dateTimeArguments', () => {
+    const testCases = [
+      {
+        it: 'yesterday',
+        input: yesterday,
+        expected: yesterday.toLocaleDateString('nl-BE')
+      },
+      {
+        it: 'today',
+        input: today,
+        expected: 'vandaag'
+      },
+      {
+        it: 'tomorrow',
+        input: tomorrow,
+        expected: 'morgen'
+      },
+      {
+        it: 'day after tomorrow',
+        input: dayAfterTomorrow,
+        expected: 'overmorgen'
+      },
+      {
+        it: 'thursday',
+        input: thursday,
+        expected: 'donderdag'
+      },
+      {
+        it: 'sunday',
+        input: sunday,
+        expected: 'zondag'
+      },
+      {
+        it: 'next week',
+        input: nextWeek,
+        expected: 'volgende week'
+      },
+      {
+        it: 'next week - end',
+        input: endNextWeek,
+        expected: 'volgende week'
+      },
+      {
+        it: 'future',
+        input: farFuture,
+        expected: farFuture.toLocaleDateString('nl-BE')
+      }
+    ];
+
+    testCases.forEach(testCase => {
+      it(`should return the right date label - ${testCase.it}`, () => {
+        viewModel.currentTask$.next({
+          ...viewModel.currentTask$.value,
+          endDate: testCase.input
+        });
+        fixture.detectChanges();
+
+        console.log(new Date());
+
+        const finishByCE = fixture.debugElement.query(
+          By.css('[data-cy=task-finish-by]')
+        );
+        const finishByText = finishByCE.nativeElement.textContent;
+
+        expect(finishByText).toContain(testCase.expected);
       });
     });
   });
