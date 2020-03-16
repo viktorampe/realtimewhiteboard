da95730cc43828139059957c7abf4adb54bcbf77
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2020-03-16 15:03:23 +0100

    implement viewmodel in mock

--- libs/pages/kabas-tasks/src/lib/components/student-tasks.viewmodel.mock.ts
+++ libs/pages/kabas-tasks/src/lib/components/student-tasks.viewmodel.mock.ts
@@ -1,11 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Params } from '@angular/router';
+import { EduContent, ResultInterface, TaskInterface } from '@campus/dal';
 import { ViewModelInterface } from '@campus/testing';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { StudentTaskFixture } from '../interfaces/StudentTask.fixture';
 import { StudentTaskInterface } from '../interfaces/StudentTask.interface';
 import { StudentTaskWithContentFixture } from '../interfaces/StudentTaskWithContent.fixture';
 import { StudentTaskWithContentInterface } from '../interfaces/StudentTaskWithContent.interface';
 import { StudentTasksViewModel } from './student-tasks.viewmodel';
-
+@Injectable({ providedIn: 'root' })
 export class MockStudentTasksViewModel
   implements ViewModelInterface<StudentTasksViewModel> {
   public studentTasks$: BehaviorSubject<StudentTaskInterface[]>;
@@ -27,6 +30,15 @@ export class MockStudentTasksViewModel
       this.studentTasks
     );
   }
+  public routeParams$: Observable<Params>;
+  openTask(task: TaskInterface): void {}
+  openEduContentAsExercise(eduContent: EduContent): void {}
+  openEduContentAsSolution(eduContent: EduContent): void {}
+  openEduContentFromResult(result: ResultInterface): void {}
+  openEduContentAsStream(eduContent: EduContent): void {}
+  openEduContentAsDownload(eduContent: EduContent): void {}
+  openBoeke(eduContent: EduContent): void {}
+  previewEduContentAsImage(eduContent: EduContent): void {}
 
   public setupStreams() {}
 }
