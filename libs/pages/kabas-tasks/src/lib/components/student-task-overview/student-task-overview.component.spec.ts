03b61b39c7241ced25e2743eba943dede51b9d9c
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2020-03-17 13:55:03 +0100

    Add TODO

--- libs/pages/kabas-tasks/src/lib/components/student-task-overview/student-task-overview.component.spec.ts
+++ libs/pages/kabas-tasks/src/lib/components/student-task-overview/student-task-overview.component.spec.ts
@@ -1,5 +1,3 @@
-// file.only
-
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatIconRegistry } from '@angular/material';
 import { By } from '@angular/platform-browser';
