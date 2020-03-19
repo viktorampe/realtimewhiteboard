75375c10e03a4ec6671816c4705a64f149098959
karlvaniseghem <karl.vaniseghem@icloud.com>
2020-03-19 16:17:43 +0100

    initial tests

---
+++ libs/ui/src/lib/backdrop/backdrop.component.spec.ts
@@ -0,0 +1,182 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick
+} from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { configureTestSuite } from 'ng-bullet';
+import { ENVIRONMENT_UI_TOKEN } from '../tokens';
+import {
+  BackdropCollapseActionDirective,
+  BackdropComponent,
+  BackdropRevealActionDirective,
+  BackLayerContentDirective,
+  FrontLayerContentDirective
+} from './backdrop.component';
+
+@Component({
+  template: `
+    <campus-backdrop [dropped]="true">
+      <back-layer-content></back-layer-content>
+      <backdrop-reveal-action>REVEAL</backdrop-reveal-action>
+      <backdrop-collapse-action>COLLAPSE</backdrop-collapse-action>
+      <front-layer-content></front-layer-content>
+    </campus-backdrop>
+  `
+})
+class BackdropDroppedComponent {}
+@Component({
+  template: `
+    <campus-backdrop [dropped]="false">
+      <back-layer-content></back-layer-content>
+      <backdrop-reveal-action>REVEAL</backdrop-reveal-action>
+      <backdrop-collapse-action>COLLAPSE</backdrop-collapse-action>
+      <front-layer-content></front-layer-content>
+    </campus-backdrop>
+  `
+})
+class BackdropCollapsedComponent {
+  public onDroppedChanged(value) {}
+}
+
+@Component({
+  template: `
+    <campus-backdrop [static]="true" (droppedChange)="onDroppedChanged($event)">
+      <back-layer-content></back-layer-content>
+      <backdrop-reveal-action>STATIC</backdrop-reveal-action>
+      <front-layer-content></front-layer-content>
+    </campus-backdrop>
+  `
+})
+class BackdropStaticComponent {
+  public show = false;
+  public onDroppedChanged(value) {
+    this.show = value;
+  }
+}
+
+describe('BasicBackdropComponent', () => {
+  let component: BackdropComponent;
+  let fixture: ComponentFixture<BackdropComponent>;
+
+  //file.only
+  configureTestSuite(() => {
+    TestBed.configureTestingModule({
+      imports: [NoopAnimationsModule, CommonModule],
+      providers: [{ provide: ENVIRONMENT_UI_TOKEN, useValue: {} }],
+      declarations: [
+        BackdropComponent,
+        BackLayerContentDirective,
+        FrontLayerContentDirective,
+        BackdropRevealActionDirective,
+        BackdropCollapseActionDirective,
+        BackdropDroppedComponent,
+        BackdropCollapsedComponent,
+        BackdropStaticComponent
+      ]
+    });
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BackdropComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should add --dropped to classList when dropped', () => {
+    const bacdropDE = fixture.debugElement.query(By.css('.ui-backdrop'));
+    component.dropped = true;
+    fixture.detectChanges();
+
+    expect(bacdropDE.nativeElement.classList).toContain('ui-backdrop--dropped');
+  });
+  it('should remove --dropped from classList when dropped', () => {
+    const bacdropDE = fixture.debugElement.query(By.css('.ui-backdrop'));
+    component.dropped = false;
+    fixture.detectChanges();
+
+    expect(bacdropDE.nativeElement.classList).not.toContain(
+      'ui-backdrop--dropped'
+    );
+  });
+
+  describe('Collapsed', () => {
+    let hostComponent: BackdropCollapsedComponent;
+    let hostFixture: ComponentFixture<BackdropCollapsedComponent>;
+
+    beforeEach(() => {
+      hostFixture = TestBed.createComponent(BackdropCollapsedComponent);
+      hostComponent = hostFixture.componentInstance;
+      hostFixture.detectChanges();
+    });
+    it('should show reveal action when collapsed', () => {
+      const revealDE = hostFixture.debugElement.query(
+        By.directive(BackdropRevealActionDirective)
+      );
+      const collapseDE = hostFixture.debugElement.query(
+        By.directive(BackdropCollapseActionDirective)
+      );
+
+      expect(revealDE).not.toBeNull();
+      expect(collapseDE).toBeNull();
+    });
+    it('should update dropped when not in static mode', () => {
+      const emitSpy = jest.spyOn(component.droppedChange, 'emit');
+      component.dropped = false;
+
+      component.updateDropped(true);
+      expect(emitSpy).toHaveBeenCalledWith(true);
+    });
+  });
+
+  describe('Dropped', () => {
+    let hostComponent: BackdropDroppedComponent;
+    let hostFixture: ComponentFixture<BackdropDroppedComponent>;
+
+    beforeEach(() => {
+      hostFixture = TestBed.createComponent(BackdropDroppedComponent);
+      hostComponent = hostFixture.componentInstance;
+      hostFixture.detectChanges();
+    });
+    it('should show collpase action when dropped', () => {
+      const revealDE = hostFixture.debugElement.query(
+        By.directive(BackdropRevealActionDirective)
+      );
+      const collapseDE = hostFixture.debugElement.query(
+        By.directive(BackdropCollapseActionDirective)
+      );
+
+      expect(revealDE).toBeNull();
+      expect(collapseDE).not.toBeNull();
+    });
+  });
+
+  describe('Static', () => {
+    let hostComponent: BackdropStaticComponent;
+    let hostFixture: ComponentFixture<BackdropStaticComponent>;
+
+    beforeEach(() => {
+      hostFixture = TestBed.createComponent(BackdropStaticComponent);
+      hostComponent = hostFixture.componentInstance;
+      hostFixture.detectChanges();
+    });
+    it('should not update dropped when static', fakeAsync(() => {
+      const emitSpy = jest.spyOn(hostComponent, 'onDroppedChanged');
+      const revealDE = hostFixture.debugElement.query(
+        By.css('.ui-backdrop__header__reveal-action')
+      );
+      hostFixture.detectChanges();
+      revealDE.nativeElement.click();
+      tick();
+      expect(hostComponent.show).toBeFalsy();
+    }));
+  });
+});
