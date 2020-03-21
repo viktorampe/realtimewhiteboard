5f74a6d8cf9053a6c7a7cbd16d6eaa6e154bdadd
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2020-03-21 18:36:28 +0100

    add icons

--- apps/whiteboard-standalone/src/environments/icons.ts
+++ apps/whiteboard-standalone/src/environments/icons.ts
@@ -1,12 +1,17 @@
 export const icons: { [icon: string]: string } = {
   check: 'assets/icons/check.svg',
-  'delete-forever': 'assets/icons/material/delete_forever.svg',
-  'add-photo-alternate': 'assets/icons/material/add_photo_alternate.svg',
-  multiselect: 'assets/icons/multiselect.svg',
-  'multiselect-selected': 'assets/icons/multiselect-selected.svg',
+  'delete-forever': 'assets/icons/whiteboard/image-delete.svg',
+  'add-photo-alternate': 'assets/icons/whiteboard/image-edit.svg',
+  multiselect: 'assets/icons/whiteboard/multiselect.svg',
+  'multiselect-selected': 'assets/icons/whiteboard/multiselect-selected.svg',
   'open-in-new': 'assets/icons/material/open_in_new.svg',
   plus: 'assets/icons/add.svg',
   delete: 'assets/icons/material/delete.svg',
   flip: 'assets/icons/material/flip_camera.svg',
-  edit: 'assets/icons/material/edit.svg'
+  edit: 'assets/icons/material/edit.svg',
+  settings: 'assets/icons/settings.svg',
+  remove: 'assets/icons/material/arrow_down.svg',
+  'arrow-down': 'assets/icons/material/arrow_down.svg',
+  'empty-state-whiteboard-empty': 'assets/empty-states/whiteboard-empty.svg',
+  'empty-state-whiteboard-drop': 'assets/empty-states/whiteboard-drop.svg'
 };
