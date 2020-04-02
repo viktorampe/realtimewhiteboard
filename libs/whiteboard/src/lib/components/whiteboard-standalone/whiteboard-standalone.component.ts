c0dd5ee1c22e1bbc36abee5db747c3a374347efc
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2020-04-02 17:27:47 +0200

    add default values for cards

--- libs/whiteboard/src/lib/components/whiteboard-standalone/whiteboard-standalone.component.ts
+++ libs/whiteboard/src/lib/components/whiteboard-standalone/whiteboard-standalone.component.ts
@@ -153,7 +153,9 @@ export class WhiteboardStandaloneComponent implements OnChanges, OnInit {
     this.shelfCards$ = this.whiteboard$.pipe(
       map(whiteboard => {
         // all cards coming from the API should be added to the shelf
-        return [...whiteboard.cards, ...whiteboard.shelfCards] || [];
+        const cards = whiteboard.cards || [];
+        const shelfCards = whiteboard.shelfCards || [];
+        return [...cards, ...shelfCards] || [];
       }),
       map(shelfCards => {
         return shelfCards.map(c => ({
