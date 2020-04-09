c36886a832cbd1d850d93cb2e41f3436a4a1cb21
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2020-04-09 10:53:52 +0200

    use angular component

--- apps/whiteboard-standalone/src/app/app.component.ts
+++ apps/whiteboard-standalone/src/app/app.component.ts
@@ -7,4 +7,77 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'whiteboard-standalone';
+  canManage = false;
+  apiBase = 'http://api.kabas.localhost:3000/api';
+  whiteboard = {
+    title: 'Welcome to the 90s',
+    shelfCards: [
+      {
+        id: 1,
+        description: 'Windows 95',
+        image: {
+          imageUrl: '/EduFiles/8/redirectUrl'
+        },
+        color: '#2EA03D'
+      },
+      {
+        id: 2,
+        description: 'Get Ready',
+        image: {
+          imageUrl: '/EduFiles/8/redirectUrl'
+        },
+        color: '#00A7E2'
+      },
+      {
+        id: 3,
+        description: 'Candy',
+        image: {
+          imageUrl: '/EduFiles/8/redirectUrl'
+        },
+        color: '#00A7E2'
+      },
+      {
+        id: 4,
+        description: 'VRC',
+        image: {
+          imageUrl: '/EduFiles/8/redirectUrl'
+        },
+        color: '#00A7E2'
+      },
+      {
+        id: 5,
+        description: 'Gameboy',
+        image: {
+          imageUrl: '/EduFiles/8/redirectUrl'
+        },
+        color: '#00A7E2'
+      },
+      {
+        id: 6,
+        description: 'Home Alone',
+        image: {
+          imageUrl: '/EduFiles/8/redirectUrl'
+        },
+        color: '#2EA03D'
+      },
+      {
+        id: 7,
+        description: 'NSYNC',
+        image: {
+          imageUrl: '/EduFiles/8/redirectUrl'
+        },
+        color: '#2EA03D'
+      },
+      {
+        id: 8,
+        description: 'Capri Sun',
+        image: {
+          imageUrl: '/EduFiles/8/redirectUrl'
+        },
+        color: '#2EA03D'
+      }
+    ],
+    cards: [],
+    defaultColor: ''
+  };
 }
--- apps/whiteboard-standalone/src/app/app.module.ts
+++ apps/whiteboard-standalone/src/app/app.module.ts
@@ -1,60 +1,22 @@
-import { Inject, NgModule } from '@angular/core';
-import { MatIconRegistry } from '@angular/material';
-import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { DalModule } from '@campus/dal';
-import { ENVIRONMENT_ICON_MAPPING_TOKEN } from '@campus/shared';
 import { WhiteboardModule } from '@campus/whiteboard';
-import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { PlayGroundComponent } from './play-ground/play-ground.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, PlayGroundComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    DalModule.forRoot({ apiBaseUrl: environment.api.APIBase }),
-    RouterModule.forRoot(
-      [
-        { path: '', pathMatch: 'full', redirectTo: 'login' },
-        { path: 'login', component: LoginComponent },
-        { path: 'whiteboard', component: PlayGroundComponent }
-      ],
-      {
-        initialNavigation: 'enabled'
-      }
-    ),
+    RouterModule.forRoot([], {
+      initialNavigation: 'enabled'
+    }),
     WhiteboardModule
   ],
-  providers: [
-    {
-      provide: ENVIRONMENT_ICON_MAPPING_TOKEN,
-      useValue: environment.iconMapping
-    }
-  ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [],
+  entryComponents: []
 })
 export class AppModule {
-  constructor(
-    private iconRegistry: MatIconRegistry,
-    private sanitizer: DomSanitizer,
-    @Inject(ENVIRONMENT_ICON_MAPPING_TOKEN)
-    private iconMapping: { [icon: string]: string }
-  ) {
-    for (const key in this.iconMapping) {
-      if (key.indexOf(':') > 0) {
-        this.iconRegistry.addSvgIconInNamespace(
-          key.split(':')[0],
-          key.split(':')[1],
-          this.sanitizer.bypassSecurityTrustResourceUrl(this.iconMapping[key])
-        );
-      } else {
-        this.iconRegistry.addSvgIcon(
-          key,
-          this.sanitizer.bypassSecurityTrustResourceUrl(this.iconMapping[key])
-        );
-      }
-    }
-  }
+  constructor() {}
 }
