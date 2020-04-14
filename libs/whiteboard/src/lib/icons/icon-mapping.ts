e86a0801cdb6cb789e4487580119f0373278e96a
viktorampe <ampe.viktor@gmail.com>
2020-04-14 16:06:26 +0200

    styling update

--- libs/whiteboard/src/lib/icons/icon-mapping.ts
+++ libs/whiteboard/src/lib/icons/icon-mapping.ts
@@ -5,6 +5,7 @@ import {
 import {
   addSvg,
   arrowDownSvg,
+  arrowUpSvg,
   deleteSvg,
   editSvg,
   flipCameraSvg,
@@ -39,6 +40,7 @@ export const iconMap: { [icon: string]: string } = {
   settings: settingsSvg,
   remove: removeSvg,
   'arrow-down': arrowDownSvg,
+  'arrow-up': arrowUpSvg,
   'empty-state-whiteboard-empty': whiteboardEmptyStateSvg,
   'empty-state-whiteboard-drop': whiteboardDropSvg,
   'return-to-shelf': returnToShelfSvg
--- libs/whiteboard/src/lib/icons/material/whiteboard-material-icons.ts
+++ libs/whiteboard/src/lib/icons/material/whiteboard-material-icons.ts
@@ -16,3 +16,4 @@ export const deleteSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="24" hei
 export const arrowBackSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>`;
 export const addSvg = `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/><path d="M0 0h24v24H0z" fill="none"/></svg>`;
 export const returnToShelfSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="24px" height="24px"><path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z"/><path d="M0 0h24v24H0z" fill="none"/></svg>`;
+export const arrowUpSvg = `<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z"/><path d="M0 0h24v24H0z" fill="none"/></svg>`;
