577e5191fd02c1af9a7ed3ee5a9121cf2918ace9
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2020-04-14 11:30:57 +0200

    remove icons

--- apps/whiteboard-standalone/src/environments/environment.staging.ts
+++ apps/whiteboard-standalone/src/environments/environment.staging.ts
@@ -1,9 +1,6 @@
-import { icons } from './icons';
-
 export const environment = {
   production: true,
   api: {
     APIBase: 'https://api.staging.lk2020.be'
-  },
-  iconMapping: icons
+  }
 };
--- apps/whiteboard-standalone/src/environments/environment.ts
+++ apps/whiteboard-standalone/src/environments/environment.ts
@@ -1,5 +1,3 @@
-import { icons } from './icons';
-
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
@@ -8,8 +6,7 @@ export const environment = {
   production: false,
   api: {
     APIBase: 'http://api.kabas.localhost:3000'
-  },
-  iconMapping: icons
+  }
 };
 
 /*
