ff222779be7d7dd93d11dac13fa13a703522fc60
DKDOMAIN\fgryspeerdt <frederic.gryspeerdt@gmail.com>
2020-04-21 15:15:12 +0200

    refactor settings to pick a color palette

--- libs/whiteboard/src/lib/components/settings/settings.component.ts
+++ libs/whiteboard/src/lib/components/settings/settings.component.ts
@@ -1,26 +1,87 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
 import { SettingsInterface } from '../../models/settings.interface';
+import { ColorInterface } from '../color-list/color-list.component';
+
+const defaultColorPaletteMap = {
+  wouw: [
+    {
+      label: 'L1',
+      hexCode: '#d9328a'
+    },
+    {
+      label: 'L2',
+      hexCode: '#00b3c4'
+    },
+    {
+      label: 'L3',
+      hexCode: '#afcb27'
+    },
+    {
+      label: 'L4',
+      hexCode: '#ea9d04'
+    },
+    {
+      label: 'L5',
+      hexCode: '#963a8e'
+    },
+    {
+      label: 'L6',
+      hexCode: '#e40521'
+    }
+  ],
+  passepartout: [
+    { label: 'L5', hexCode: '#6ec3c1' },
+    {
+      label: 'L6',
+      hexCode: '#e94b2b'
+    }
+  ]
+};
 
 @Component({
   selector: 'campus-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
-export class SettingsComponent {
-  @Input() activeColor: string;
+export class SettingsComponent implements OnInit {
+  @Input() themeColor: string;
   @Input() title: string;
+  @Input() colorPalettes: {
+    [paletteName: string]: ColorInterface[];
+  } = defaultColorPaletteMap;
+
   @Output() update = new EventEmitter<SettingsInterface>();
 
-  constructor() {}
+  settingsForm: FormGroup;
+  colorPaletteOptions = Object.keys(this.colorPalettes);
+  pickedColorPalette$: Observable<string>;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.settingsForm = this.buildForm();
+    this.pickedColorPalette$ = this.settingsForm.get(
+      'colorPalette'
+    ).valueChanges;
+  }
+
+  private buildForm(): FormGroup {
+    return this.fb.group({
+      title: new FormControl(this.title || ''),
+      colorPalette: new FormControl('')
+    });
+  }
 
   setDefaultColor(color: string) {
-    this.activeColor = color;
+    this.themeColor = color;
   }
 
   onSubmit() {
     this.update.emit({
-      title: this.title,
-      defaultColor: this.activeColor
+      title: this.settingsForm.value.title,
+      defaultColor: this.themeColor
     });
   }
 }
