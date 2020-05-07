faba333a0c577727c608a20843c091ef7c39fd1e
Viktor Ampe <ampe.viktor@gmail.com>
2020-05-07 11:54:53 +0200

    graphQL update

--- apps/realtime-whiteboard/src/app/services/activeplayer/activeplayer.service.ts
+++ apps/realtime-whiteboard/src/app/services/activeplayer/activeplayer.service.ts
@@ -11,12 +11,14 @@ export class ActiveplayerService {
   constructor(private cookieService: CookieService) {}
 
   setActivePlayer(player: Player) {
+    console.log('set cookie: ', player);
     this.cookieService.set(this.key, JSON.stringify(player));
   }
 
   getActivePlayer(): Player {
     const player = this.cookieService.get(this.key);
     if (player) {
+      console.log('get cookie: ', JSON.parse(player));
       return JSON.parse(player);
     } else {
       return null;
