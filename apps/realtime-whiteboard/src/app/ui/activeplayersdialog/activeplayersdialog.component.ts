60e66f47e449b15885c6984dfceedf2ba050a68c
Viktor Ampe <ampe.viktor@gmail.com>
2020-06-06 16:56:44 +0200

    remove player functionality

--- apps/realtime-whiteboard/src/app/ui/activeplayersdialog/activeplayersdialog.component.ts
+++ apps/realtime-whiteboard/src/app/ui/activeplayersdialog/activeplayersdialog.component.ts
@@ -1,5 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { BehaviorSubject } from 'rxjs';
+import Player from '../../models/player';
+import { RealtimeSessionService } from '../../services/realtimesession/realtime-session.service';
 import { SessiondetailsdialogComponent } from '../sessiondetailsdialog/sessiondetailsdialog.component';
 
 @Component({
@@ -8,10 +11,21 @@ import { SessiondetailsdialogComponent } from '../sessiondetailsdialog/sessionde
   styleUrls: ['./activeplayersdialog.component.scss']
 })
 export class ActiveplayersdialogComponent implements OnInit {
+  players$ = new BehaviorSubject<Player[]>(null);
+
   constructor(
     public dialogRef: MatDialogRef<SessiondetailsdialogComponent>,
+    private sessionService: RealtimeSessionService,
+
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.players$.next(this.data.players);
+    console.log(this.players$.value);
+  }
+
+  removePlayer(player: Player) {
+    this.sessionService.deletePlayer(player).subscribe(() => {});
+  }
 }
--- apps/realtime-whiteboard/src/app/ui/playertable/playertable.component.ts
+++ apps/realtime-whiteboard/src/app/ui/playertable/playertable.component.ts
@@ -1,5 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material';
+import {
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild
+} from '@angular/core';
+import { MatTable, MatTableDataSource } from '@angular/material';
 import Player from '../../models/player';
 
 @Component({
@@ -9,14 +17,23 @@ import Player from '../../models/player';
 })
 export class PlayertableComponent implements OnInit {
   @Input() players: Player[];
-  displayedColumns: string[] = ['name'];
+  @Output() onRemovePlayer = new EventEmitter<Player>();
+  @ViewChild(MatTable, { static: false }) table: MatTable<any>;
+
+  displayedColumns: string[] = ['name', 'action'];
   dataSource: any;
 
-  constructor() {
+  constructor(private changeDetectorRefs: ChangeDetectorRef) {
     this.dataSource = new MatTableDataSource<Player>();
   }
 
   ngOnInit() {
+    this.dataSource = new MatTableDataSource(this.players);
+  }
+
+  removePlayer(player: Player) {
+    this.onRemovePlayer.emit(player);
+    this.players = this.players.filter(p => p.id !== player.id);
     this.dataSource = new MatTableDataSource(this.players);
   }
 }
